e2accd985c511112978f605878e01b19230ae9b3
Michael Schlottke-Lakemper <michael@sloede.com>
2020-01-29 22:16:44 +0100

    Add calculation for D hat

--- src/Dg.jl
+++ src/Dg.jl
@@ -20,19 +20,23 @@ struct Dg{SysEqn <: SysEqnMod.AbstractSysEqn{nvars_} where nvars_, N, Np1}
   nodecoordinate::Array{Float64, 2}
   nodes::SVector{Np1}
   weights::SVector{Np1}
+  dhat::SMatrix{Np1, Np1}
 end
+
 
 polydeg(dg::Dg{SysEqn, N}) where {SysEqn, N} = N
 syseqn(dg::Dg{SysEqn, N}) where {SysEqn, N} = dg.syseqn
 SysEqnMod.nvars(dg::Dg{SysEqn, N}) where {SysEqn, N} = SysEqnMod.nvars(syseqn(dg))
+
 
 function Dg(s::SysEqnMod.AbstractSysEqn{nvars_}, mesh, N) where nvars_
   ncells = mesh.ncells
   u = zeros(Float64, nvars_, N + 1, ncells)
   ut = zeros(Float64, nvars_, N + 1, ncells)
   nodes, weights = legendre(N + 1, both)
+  dhat = calcdhat(nodes, weights)
   dg = Dg{typeof(s), N, N + 1}(s, u, ut, ncells, Array{Float64,1}(undef, ncells),
-                        Array{Float64,2}(undef, N + 1, ncells), nodes, weights)
+                        Array{Float64,2}(undef, N + 1, ncells), nodes, weights, dhat)
 
   for c in 1:ncells
     dx = mesh.length[c]
@@ -42,6 +46,53 @@ function Dg(s::SysEqnMod.AbstractSysEqn{nvars_}, mesh, N) where nvars_
 
   return dg
 end
+
+
+function calcdhat(nodes, weights)
+  nnodes = length(nodes)
+  dhat = polynomialderivativematrix(nodes)
+  dhat = transpose(dhat)
+
+  for n = 1:nnodes, j = 1:nnodes
+    dhat[j, n] *= -weights[n] / weights[j]
+  end
+
+  return dhat
+end
+
+
+function polynomialderivativematrix(nodes)
+  nnodes = length(nodes)
+  d = zeros(nnodes, nnodes)
+  wbary = barycentricweights(nodes)
+
+  for i = 1:nnodes, j = 1:nnodes
+    if j != i
+      d[i, j] = wbary[j] / wbary[i] * 1 / (nodes[i] - nodes[j])
+      d[i, i] -= d[i, j]
+    end
+  end
+
+  return d
+end
+
+
+function barycentricweights(nodes)
+  nnodes = length(nodes)
+  weights = ones(nnodes)
+
+  for j = 2:nnodes, k = 1:(j-1)
+    weights[k] *= nodes[k] - nodes[j]
+    weights[j] *= nodes[j] - nodes[k]
+  end
+
+  for j = 1:nnodes
+    weights[j] = 1 / weights[j]
+  end
+
+  return weights
+end
+
 
 function setinitialconditions(dg, t)
   for c = 1:dg.ncells
