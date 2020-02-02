e97bceea0466f71bef97d3cd37e39f12a846263a
Michael Schlottke-Lakemper <michael@sloede.com>
2020-02-02 23:22:33 +0100

    Allow specifying a range of valid values for parameters

--- src/auxiliary/auxiliary.jl
+++ src/auxiliary/auxiliary.jl
@@ -16,16 +16,24 @@ function parse_parameters_file(filename::AbstractString)
   parameters["default"] = Pkg.TOML.parsefile(filename)
 end
 
-function parameter(name::String, default=nothing)
+function parameter(name::String, default=nothing; valid=nothing)
   if haskey(parameters["default"], name)
-    return parameters["default"][name]
+    value = parameters["default"][name]
   else
     if default == nothing
       error("requested paramter '$name' does not exist and no default value was provided")
     else
-      return default
+      value = default
     end
   end
+
+  if valid != nothing
+    if !(value in valid)
+      error("'$value' is not a valid value for parameter '$name'")
+    end
+  end
+
+  return value
 end
 
 end
