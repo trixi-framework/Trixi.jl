23e13e086e478e904b517d31f846f05c2845598c
Michael Schlottke-Lakemper <michael@sloede.com>
2020-02-02 23:22:53 +0100

    Allow output to text

--- src/io/io.jl
+++ src/io/io.jl
@@ -3,6 +3,7 @@ module Io
 using ..Jul1dge
 using ..DgMod
 import ..Equation
+using ..Auxiliary
 
 using HDF5
 using Printf
@@ -13,34 +14,87 @@ export save_solution_file
 """
     save_solution_file(dg, timestep::Integer)
 
-Save current DG solution with some context information as a HDF5 file for postprocessing.
+Save current DG solution by forming a timestep-based filename and then
+dispatching on the 'output_format' parameter.
 """
 function save_solution_file(dg, timestep::Integer)
-  # Filename based on current time step
-  filename = @sprintf("solution_%06d.h5", timestep) 
+  # Filename without extension based on current time step
+  filename = @sprintf("solution_%06d", timestep) 
 
+  # Dispatch on property
+  output_format = parameter("output_format", "hdf5", valid=["hdf5", "text"])
+  save_solution_file(Val(Symbol(output_format)), dg, filename::String)
+end
+
+
+"""
+    save_solution_file(::Val{:hdf5}, dg, filename::String)
+
+Save current DG solution with some context information as a HDF5 file for
+postprocessing.
+"""
+function save_solution_file(::Val{:hdf5}, dg, filename::String)
   # Open file (clobber existing content)
-  h5open(filename, "w") do file
+  h5open(filename * ".h5", "w") do file
     s = syseqn(dg)
     N = polydeg(dg)
     nvars_ = Equation.nvars(dg)
 
-    # Get root group
-    root = file["/"]
+    # Add context information as attributes
+    attrs(file)["ndim"] = ndim
+    attrs(file)["syseqn"] = s.name
+    attrs(file)["polydeg"] = N
+    attrs(file)["nvars"] = nvars_
+    attrs(file)["ncells"] = dg.ncells
 
     # Add coordinates as 1D arrays
-    for d = 1:ndim
-      root["coordinates_$d"] = dg.nodecoordinate[:]
-    end
+    file["coordinates_0"] = dg.nodecoordinate[:]
 
     # Store each variable of the solution
     for v = 1:nvars_
       # Convert to 1D array
-      root["variables_$v"] = dg.u[v, :, :][:]
+      file["variables_$v"] = dg.u[v, :, :][:]
 
       # Add variable name as attribute
-      var = root["variables_$v"]
+      var = file["variables_$v"]
       attrs(var)["name"] = s.varnames[v]
+    end
+  end
+end
+
+
+"""
+    save_solution_file(::Val{:text}, dg, filename::String)
+
+Save current DG solution as a plain text file with fixed-width space-separated
+values, with the first line containing the column names.
+"""
+function save_solution_file(::Val{:text}, dg, filename::String)
+  # Open file (clobber existing content)
+  open(filename * ".dat", "w") do file
+    s = syseqn(dg)
+    N = polydeg(dg)
+    nnodes = N + 1
+    nvars_ = Equation.nvars(dg)
+
+    # Write column names, put in quotation marks to account for whitespace in names
+    columns = Vector{String}()
+    for d = 1:ndim
+      push!(columns, "\"coordinates_$d\"")
+    end
+    for v = 1:nvars_
+      push!(columns, "\"$(s.varnames[v])\"")
+    end
+    println(file, join(columns, " "))
+
+    # Write data
+    for cell_id = 1:dg.ncells, i = 1:nnodes
+      data = Vector{String}()
+      push!(data, @sprintf("% 10.8e", dg.nodecoordinate[i, cell_id]))
+      for v = 1:nvars_
+        push!(data, @sprintf("% 10.8e", dg.u[v, i, cell_id]))
+      end
+      println(file, join(data, " "))
     end
   end
 end
