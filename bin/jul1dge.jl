35ce5d1119c3dd405c1907d4fee79f2c1512413f
Michael Schlottke-Lakemper <michael@sloede.com>
2020-02-03 09:40:43 +0100

    Move executable to own folder

--- bin/jul1dge.jl
+++ bin/jul1dge.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 
 # Include file with definition of main function
-include("main.jl")
+include("../src/main.jl")
 
 # Run jul1dge but handle user interrupts gracefully (Ctrl-c)
 @Auxiliary.interruptable run()
