8b845daa0084fce7b6e2a457e2b41000054fa43a
Michael Schlottke-Lakemper <michael@sloede.com>
2020-02-03 10:40:14 +0100

    Remove obsolete code

--- src/io/io.jl
+++ src/io/io.jl
@@ -99,35 +99,4 @@ function save_solution_file(::Val{:text}, dg, filename::String)
   end
 end
 
-
-
-
-####################################################################################################
-# Original version with direct solution-to-image output
-#
-# using Plots
-# import GR
-# 
-# export plot2file
-# 
-# function plot2file(dg, filename)
-#   gr()
-#   GR.inline("png")
-#   x = dg.nodecoordinate[:]
-#   y = zeros(length(x), nvars(dg))
-#   s = syseqn(dg)
-#   nnodes = polydeg(dg) + 1
-#   for v = 1:nvars(dg)
-#     for c in 1:dg.ncells
-#       for i = 1:nnodes
-#         y[(c - 1) * nnodes + i, v] = dg.u[v, i, c]
-#       end
-#     end
-#     plot(x, y, label=s.varnames[:], xlims=(-10.5, 10.5), ylims=(-1, 2),
-#          size=(1600,1200), thickness_scaling=3)
-#   end
-#   savefig(filename)
-# end
-####################################################################################################
-
 end
