b01488a564e6857d22abdea034c6e9a79ae6a820
Michael Schlottke-Lakemper <michael@sloede.com>
2020-03-02 10:47:26 +0100

    Change ndim to 2

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -2,7 +2,7 @@ module Trixi
 
 # Set ndim as a short, module-wide constant.
 # Rationale: This makes code easier to understand than using hardcoded dimension values.
-const ndim = 1
+const ndim = 2
 export ndim
 
 # Include all top-level submodule files
