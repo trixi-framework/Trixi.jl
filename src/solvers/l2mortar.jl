18a53885491384498fc7d04a731ecf224ae253ed
Michael Schlottke-Lakemper <michael@sloede.com>
2020-03-03 21:50:43 +0100

    Add Gauss nodes-based reverse mortar projection

--- src/solvers/l2mortar.jl
+++ src/solvers/l2mortar.jl
@@ -1,7 +1,8 @@
 module L2Mortar
 
 using ..Interpolation: gauss_lobatto_nodes_weights, barycentric_weights,
-                       lagrange_interpolating_polynomials
+                       lagrange_interpolating_polynomials, gauss_nodes_weights,
+                       polynomial_interpolation_matrix
 
 
 function calc_forward_upper(n_nodes)
@@ -41,38 +42,48 @@ end
 
 
 function calc_reverse_upper(n_nodes)
-  # Calculate nodes, weights, and barycentric weights
-  nodes, weights = gauss_lobatto_nodes_weights(n_nodes)
-  wbary = barycentric_weights(nodes)
+  # Calculate nodes, weights, and barycentric weights for Legendre-Gauss
+  gauss_nodes, gauss_weights = gauss_nodes_weights(n_nodes)
+  gauss_wbary = barycentric_weights(gauss_nodes)
 
   # Calculate projection matrix (actually: discrete L2 projection with errors)
   operator = zeros(n_nodes, n_nodes)
   for j in 1:n_nodes
-    poly = lagrange_interpolating_polynomials(1/2 * (nodes[j] + 1), nodes, wbary)
+    poly = lagrange_interpolating_polynomials(1/2 * (gauss_nodes[j] + 1), gauss_nodes, gauss_wbary)
     for i in 1:n_nodes
-      operator[i, j] = 1/2 * poly[i] * weights[j]/weights[i]
+      operator[i, j] = 1/2 * poly[i] * gauss_weights[j]/gauss_weights[i]
     end
   end
 
-  return operator
+  # Calculate Vandermondes
+  lobatto_nodes, lobatto_weights = gauss_lobatto_nodes_weights(n_nodes)
+  gauss2lobatto = polynomial_interpolation_matrix(gauss_nodes, lobatto_nodes)
+  lobatto2gauss = polynomial_interpolation_matrix(lobatto_nodes, gauss_nodes)
+
+  return gauss2lobatto * operator * lobatto2gauss
 end
 
 
 function calc_reverse_lower(n_nodes)
-  # Calculate nodes, weights, and barycentric weights
-  nodes, weights = gauss_lobatto_nodes_weights(n_nodes)
-  wbary = barycentric_weights(nodes)
+  # Calculate nodes, weights, and barycentric weights for Legendre-Gauss
+  gauss_nodes, gauss_weights = gauss_nodes_weights(n_nodes)
+  gauss_wbary = barycentric_weights(gauss_nodes)
 
   # Calculate projection matrix (actually: discrete L2 projection with errors)
   operator = zeros(n_nodes, n_nodes)
   for j in 1:n_nodes
-    poly = lagrange_interpolating_polynomials(1/2 * (nodes[j] - 1), nodes, wbary)
+    poly = lagrange_interpolating_polynomials(1/2 * (gauss_nodes[j] - 1), gauss_nodes, gauss_wbary)
     for i in 1:n_nodes
-      operator[i, j] = 1/2 * poly[i] * weights[j]/weights[i]
+      operator[i, j] = 1/2 * poly[i] * gauss_weights[j]/gauss_weights[i]
     end
   end
 
-  return operator
+  # Calculate Vandermondes
+  lobatto_nodes, lobatto_weights = gauss_lobatto_nodes_weights(n_nodes)
+  gauss2lobatto = polynomial_interpolation_matrix(gauss_nodes, lobatto_nodes)
+  lobatto2gauss = polynomial_interpolation_matrix(lobatto_nodes, gauss_nodes)
+
+  return gauss2lobatto * operator * lobatto2gauss
 end
 
 
