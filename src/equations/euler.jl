df205e66a9fb19193c412628356234880591f359
Michael Schlottke-Lakemper <michael@sloede.com>
2020-03-03 12:14:42 +0100

    Re-add HLLC flux (not yet working)

--- src/equations/euler.jl
+++ src/equations/euler.jl
@@ -245,6 +245,47 @@ function Equations.riemann!(flux_surfaces::AbstractArray{Float64, 1},
     flux_surfaces[2] = 1/2 * (f_ll[2] + f_rr[2]) - 1/2 * λ_max * (rho_v1_rr - rho_v1_ll)
     flux_surfaces[3] = 1/2 * (f_ll[3] + f_rr[3]) - 1/2 * λ_max * (rho_v2_rr - rho_v2_ll)
     flux_surfaces[4] = 1/2 * (f_ll[4] + f_rr[4]) - 1/2 * λ_max * (rho_e_rr  - rho_e_ll)
+  elseif equation.riemann_solver == "hllc"
+    error("not yet implemented or tested")
+    v_tilde = (sqrt(rho_ll) * v_ll + sqrt(rho_rr) * v_rr) / (sqrt(rho_ll) + sqrt(rho_rr))
+    h_ll = (rho_e_ll + p_ll) / rho_ll
+    h_rr = (rho_e_rr + p_rr) / rho_rr
+    h_tilde = (sqrt(rho_ll) * h_ll + sqrt(rho_rr) * h_rr) / (sqrt(rho_ll) + sqrt(rho_rr))
+    c_tilde = sqrt((equation.gamma - 1) * (h_tilde - 1/2 * v_tilde^2))
+    s_ll = v_tilde - c_tilde
+    s_rr = v_tilde + c_tilde
+
+    if s_ll > 0
+      flux_surfaces[1, surface_id] = f_ll[1]
+      flux_surfaces[2, surface_id] = f_ll[2]
+      flux_surfaces[3, surface_id] = f_ll[3]
+    elseif s_rr < 0
+      flux_surfaces[1, surface_id] = f_rr[1]
+      flux_surfaces[2, surface_id] = f_rr[2]
+      flux_surfaces[3, surface_id] = f_rr[3]
+    else
+      s_star = ((p_rr - p_ll + rho_ll * v_ll * (s_ll - v_ll) - rho_rr * v_rr * (s_rr - v_rr))
+                / (rho_ll * (s_ll - v_ll) - rho_rr * (s_rr - v_rr)))
+      if s_ll <= 0 && 0 <= s_star
+        flux_surfaces[1, surface_id] = (f_ll[1] + s_ll *
+            (rho_ll * (s_ll - v_ll)/(s_ll - s_star) - rho_ll))
+        flux_surfaces[2, surface_id] = (f_ll[2] + s_ll *
+            (rho_ll * (s_ll - v_ll)/(s_ll - s_star) * s_star - rho_v_ll))
+        flux_surfaces[3, surface_id] = (f_ll[3] + s_ll *
+            (rho_ll * (s_ll - v_ll)/(s_ll - s_star) *
+            (rho_e_ll/rho_ll + (s_star - v_ll) * (s_star + rho_ll/(rho_ll * (s_ll - v_ll))))
+            - rho_e_ll))
+      else
+        flux_surfaces[1, surface_id] = (f_rr[1] + s_rr *
+            (rho_rr * (s_rr - v_rr)/(s_rr - s_star) - rho_rr))
+        flux_surfaces[2, surface_id] = (f_rr[2] + s_rr *
+            (rho_rr * (s_rr - v_rr)/(s_rr - s_star) * s_star - rho_v_rr))
+        flux_surfaces[3, surface_id] = (f_rr[3] + s_rr *
+            (rho_rr * (s_rr - v_rr)/(s_rr - s_star) *
+            (rho_e_rr/rho_rr + (s_star - v_rr) * (s_star + rho_rr/(rho_rr * (s_rr - v_rr))))
+            - rho_e_rr))
+      end
+    end
   else
     error("unknown Riemann solver '$(equation.riemann_solver)'")
   end
