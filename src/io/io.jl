0ddafe72c1d20f055ffb941c1b143853e7eccb97
Michael Schlottke-Lakemper <michael@sloede.com>
2020-03-05 11:32:24 +0100

    Remove text as an output format option -> HDF5 is now the only game in town (rationale: reduce complexity, avoid inconsistencies)

--- src/io/io.jl
+++ src/io/io.jl
@@ -66,9 +66,9 @@ function load_restart_file!(dg::Dg, restart_filename::String)
 end
 
 
-# Save file capable for restarting the solver.
-function save_restart_file(solver::AbstractSolver, mesh::TreeMesh,
-                           time::Real, dt::Real, timestep::Integer)
+# Save current DG solution with some context information as a HDF5 file for
+# restarting.
+function save_restart_file(dg::Dg, mesh::TreeMesh, time::Real, dt::Real, timestep::Integer)
   # Create output directory (if it does not exist)
   output_directory = parameter("output_directory", "out")
   mkpath(output_directory)
@@ -76,20 +76,10 @@ function save_restart_file(solver::AbstractSolver, mesh::TreeMesh,
   # Filename without extension based on current time step
   filename = joinpath(output_directory, @sprintf("restart_%06d", timestep))
 
-  # Check output format - restart files are always written in HDF5, as loading
-  # from a text file is not supported
-  if parameter("output_format", "hdf5") != "hdf5"
-    error("parameter 'output_format' must be set to 'hdf5' to write restart files")
-  end
+  # Convert time and time step size to floats
+  time = convert(Float64, time)
+  dt = convert(Float64, dt)
 
-  save_restart_file(filename, solver, mesh, convert(Float64, time), convert(Float64, dt), timestep)
-end
-
-
-# Save current DG solution with some context information as a HDF5 file for
-# restarting.
-function save_restart_file(filename::String, dg::Dg, mesh::TreeMesh,
-                           time::Float64, dt::Float64, timestep::Integer)
   # Open file (clobber existing content)
   h5open(filename * ".h5", "w") do file
     equation = equations(dg)
@@ -123,10 +113,9 @@ function save_restart_file(filename::String, dg::Dg, mesh::TreeMesh,
 end
 
 
-# Save current solution by forming a timestep-based filename and then
-# dispatching on the 'output_format' parameter.
-function save_solution_file(solver::AbstractSolver, mesh::TreeMesh,
-                            time::Real, dt::Real, timestep::Integer)
+# Save current DG solution with some context information as a HDF5 file for
+# postprocessing.
+function save_solution_file(dg::Dg, mesh::TreeMesh, time::Real, dt::Real, timestep::Integer)
   # Create output directory (if it does not exist)
   output_directory = parameter("output_directory", "out")
   mkpath(output_directory)
@@ -134,18 +123,10 @@ function save_solution_file(solver::AbstractSolver, mesh::TreeMesh,
   # Filename without extension based on current time step
   filename = joinpath(output_directory, @sprintf("solution_%06d", timestep))
 
-  # Dispatch on format property
-  output_format = parameter("output_format", "hdf5", valid=["hdf5", "text"])
-  save_solution_file(Val(Symbol(output_format)), filename, solver, mesh,
-                     convert(Float64, time), convert(Float64, dt), timestep)
-end
+  # Convert time and time step size to floats
+  time = convert(Float64, time)
+  dt = convert(Float64, dt)
 
-
-# Save current DG solution with some context information as a HDF5 file for
-# postprocessing.
-function save_solution_file(::Val{:hdf5}, filename::String, dg::Dg,
-                            mesh::TreeMesh, time::Float64, dt::Float64,
-                            timestep::Integer)
   # Open file (clobber existing content)
   h5open(filename * ".h5", "w") do file
     equation = equations(dg)
@@ -190,63 +171,7 @@ function save_solution_file(::Val{:hdf5}, filename::String, dg::Dg,
 end
 
 
-# Save current DG solution as a plain text file with fixed-width space-separated
-# values, with the first line containing the column names.
-function save_solution_file(::Val{:text}, filename::String, dg::Dg, mesh::TreeMesh,
-                            time::Float64, dt::Float64, timestep::Integer)
-  # Open file (clobber existing content)
-  open(filename * ".dat", "w") do file
-    equation = equations(dg)
-    N = polydeg(dg)
-    n_nodes = N + 1
-
-    # Convert to primitive variables if requested
-    output_variables = parameter("output_variables",
-                                "conservative",
-                                valid=["conservative", "primitive"])
-    if output_variables == "conservative"
-      data = dg.elements.u
-      varnames = equation.varnames_cons
-    else
-      data = cons2prim(equation, dg.elements.u)
-      varnames = equation.varnames_prim
-    end
-
-    # Add context information as comments in the first lines of the file
-    println(file, "# ndim = $ndim")
-    println(file, "# equations = \"$(equation.name)\"")
-    println(file, "# N = $N")
-    println(file, "# n_vars = $(nvariables(dg))")
-    println(file, "# n_elements = $(dg.n_elements)")
-    println(file, "# mesh_file = \"$(mesh.current_filename)\"")
-    println(file, "# time = $(time)")
-    println(file, "# dt = $(dt)")
-    println(file, "# timestep = $(timestep)")
-
-    # Write column names, put in quotation marks to account for whitespace in names
-    columns = Vector{String}(undef, ndim + nvariables(dg))
-    columns[1] = @sprintf("%-15s", "\"x\"")
-    columns[2] = @sprintf("%-15s", "\"y\"")
-    for v = 1:nvariables(dg)
-      columns[v+2] = @sprintf("%-15s", "\"$(varnames[v])\"")
-    end
-    println(file, strip(join(columns, " ")))
-
-    # Write data
-    for element_id = 1:dg.n_elements, j = 1:n_nodes, i = 1:n_nodes
-      data_out = Vector{String}(undef, ndim + nvariables(dg))
-      data_out[1] = @sprintf("%+10.8e", dg.elements.node_coordinates[1, i, j, element_id])
-      data_out[2] = @sprintf("%+10.8e", dg.elements.node_coordinates[2, i, j, element_id])
-      for v = 1:nvariables(dg)
-        data_out[v+2] = @sprintf("%+10.8e", data[v, i, j, element_id])
-      end
-      println(file, join(data_out, " "))
-    end
-  end
-end
-
-
-# Save mesh file
+# Save current mesh with some context information as an HDF5 file.
 function save_mesh_file(mesh::TreeMesh, timestep::Integer=-1)
   # Create output directory (if it does not exist)
   output_directory = parameter("output_directory", "out")
@@ -259,16 +184,7 @@ function save_mesh_file(mesh::TreeMesh, timestep::Integer=-1)
     filename = joinpath(output_directory, "mesh")
   end
 
-  # Dispatch on format property
-  output_format = parameter("output_format", "hdf5", valid=["hdf5", "text"])
-  filename = save_mesh_file(Val(Symbol(output_format)), filename, mesh)
-
-  return filename
-end
-
-
-# Save current mesh with some context information as an HDF5 file.
-function save_mesh_file(::Val{:hdf5}, filename::String, mesh::TreeMesh)
+  # Create output directory (if it does not exist)
   # Open file (clobber existing content)
   h5open(filename * ".h5", "w") do file
     # Add context information as attributes
@@ -290,98 +206,6 @@ function save_mesh_file(::Val{:hdf5}, filename::String, mesh::TreeMesh)
   end
 
   return filename * ".h5"
-end
-
-
-# Save current mesh with some context information as a text file.
-function save_mesh_file(::Val{:text}, filename::String, mesh::TreeMesh)
-  # Open file (clobber existing content)
-  open(filename * ".dat", "w") do file
-    # Add context information as comments in the first lines of the file
-    n_cells = length(mesh.tree)
-    println(file, "# ndim = $(ndim)")
-    println(file, "# n_cells = $(n_cells)")
-    println(file, "# n_leaf_cells = $(count_leaf_cells(mesh.tree))")
-    println(file, "# minimum_level = $(minimum_level(mesh.tree))")
-    println(file, "# maximum_level = $(maximum_level(mesh.tree))")
-    println(file, "# center_level_0 = $(mesh.tree.center_level_0)")
-    println(file, "# length_level_0 = $(mesh.tree.length_level_0)")
-
-    # Write column names, put in quotation marks to account for whitespace in names
-    n_columns = (1                                # parent ids
-                 + n_children_per_cell(mesh.tree) # child ids
-                 + n_directions(mesh.tree)        # neighbor ids
-                 + 1                              # levels
-                 + ndim)                          # coordinates
-    columns = Vector{String}(undef, n_columns)
-
-    # Parent ids
-    offset = 0
-    columns[offset + 1] = @sprintf("%-10s", "parent_ids")
-    offset += 1
-
-    # Child ids
-    for i in 1:n_children_per_cell(mesh.tree)
-      columns[offset + i] = @sprintf("%-11s", "child_ids_$i")
-    end
-    offset += n_children_per_cell(mesh.tree)
-
-    # Neighbor ids
-    for i in 1:n_directions(mesh.tree)
-      columns[offset + i] = @sprintf("%-14s", "neighbor_ids_$i")
-    end
-    offset += n_directions(mesh.tree)
-
-    # Levels
-    columns[offset + 1] = @sprintf("%-6s", "levels")
-    offset += 1
-
-    # Coordinates
-    for i = 1:ndim
-      columns[offset + i] = @sprintf("%-15s", "coordinates_$i")
-    end
-    offset += ndim
-
-    # Print to file
-    println(file, strip(join(columns, "  ")))
-
-    # Write data
-    for cell_id = 1:n_cells
-      data_out = Vector{String}(undef, n_columns)
-
-      # Parent ids
-      offset = 0
-      data_out[offset + 1] = @sprintf("%10d", mesh.tree.parent_ids[cell_id])
-      offset += 1
-
-      # Child ids
-      for i = 1:n_children_per_cell(mesh.tree)
-        data_out[offset + i] = @sprintf("%11d", mesh.tree.child_ids[i, cell_id])
-      end
-      offset += n_children_per_cell(mesh.tree)
-
-      # Neighbor ids
-      for i = 1:n_directions(mesh.tree)
-        data_out[offset + i] = @sprintf("%14d", mesh.tree.neighbor_ids[i, cell_id])
-      end
-      offset += n_directions(mesh.tree)
-
-      # Levels
-      data_out[offset + 1] = @sprintf("%6d", mesh.tree.levels[cell_id])
-      offset += 1
-
-      # Coordinates
-      for i = 1:ndim
-        data_out[offset + i] = @sprintf("%+10.8e", mesh.tree.coordinates[i, cell_id])
-      end
-      offset += ndim
-
-      # Print to file
-      println(file, join(data_out, "  "))
-    end
-  end
-
-  return filename * ".dat"
 end
 
 
