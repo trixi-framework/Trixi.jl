632a1dc3b27f1c94f8287bb6b86c9a83644e9823
Michael Schlottke-Lakemper <michael@sloede.com>
2020-03-08 12:42:37 +0100

    have_mpi -> is_mpi_enabled

--- src/parallel/parallel.jl
+++ src/parallel/parallel.jl
@@ -1,7 +1,7 @@
 module Parallel
 
 export finalize
-export have_mpi
+export is_mpi_enabled
 export comm
 export domain_id
 export ndomains
@@ -22,12 +22,12 @@ if Base.find_package("MPI") !== nothing && _use_mpi
     MPI.Init()
   end
 
-  const _have_mpi = true
+  const _is_mpi_enabled = true
   const _comm = MPI.COMM_WORLD
   const _domain_id = MPI.Comm_rank(_comm)
   const _n_domains = MPI.Comm_size(_comm)
 else
-  const _have_mpi = false
+  const _is_mpi_enabled = false
   const _comm = nothing
   const _domain_id = 0
   const _n_domains = 1
@@ -35,7 +35,7 @@ end
 
 
 # Method to check if MPI is available in general
-have_mpi() = _have_mpi
+is_mpi_enabled() = _is_mpi_enabled
 
 # Default MPI communicator
 comm() = _comm
@@ -46,26 +46,26 @@ n_domains() = _n_domains
 is_mpi_root() = domain_id() == 0
 
 # Check if this is a parallel run
-if_parallel() = have_mpi() && n_domains() > 1
+if_parallel() = is_mpi_enabled() && n_domains() > 1
 
 
 # Macro to enable expressions only if MPI is used
 macro mpi(expr)
-  if have_mpi()
+  if is_mpi_enabled()
     return :($(esc(expr)))
   end
 end
 
 # Macro to enable expressions only if MPI is used and more than one rank is used
 macro mpi_parallel(expr)
-  if have_mpi() && is_parallel()
+  if is_mpi_enabled() && is_parallel()
     return :($(esc(expr)))
   end
 end
 
 # Macro to enable expressions only if MPI is used and this is the MPI root
 macro mpi_root(expr)
-  if have_mpi() && is_mpi_root()
+  if is_mpi_enabled() && is_mpi_root()
     return :($(esc(expr)))
   end
 end
@@ -73,7 +73,7 @@ end
 
 # Finalize MPI
 function finalize()
-  if have_mpi() && _use_mpi && !isinteractive() && MPI.Initialized() && !MPI.Finalized()
+  if is_mpi_enabled() && _use_mpi && !isinteractive() && MPI.Initialized() && !MPI.Finalized()
     MPI.Finalize()
   end
 end
