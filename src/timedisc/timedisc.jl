69111f10abd4524fbc6926799b9b988be3fd53a0
Michael Schlottke-Lakemper <michael@sloede.com>
2020-03-27 14:21:56 +0100

    Allow switching time integration scheme in parameters file

--- src/timedisc/timedisc.jl
+++ src/timedisc/timedisc.jl
@@ -2,7 +2,7 @@ module TimeDisc
 
 using ..Trixi
 using ..Solvers: AbstractSolver, rhs!
-using ..Auxiliary: timer
+using ..Auxiliary: timer, parameter
 using TimerOutputs: @timeit
 
 export timestep!
@@ -10,7 +10,36 @@ export timestep!
 
 # Integrate solution by repeatedly calling the rhs! method on the solver solution.
 function timestep!(solver::AbstractSolver, t::Float64, dt::Float64)
-  # Coefficients for Carpenter's 5-stage 4th-order low-storage Runge-Kutta method
+  time_integration_scheme = Symbol(parameter("time_integration_scheme", "carpenter_4_5",
+                                             valid=("carpenter_4_5", "paired_rk_2_16")))
+  timestep!(solver, Val(time_integration_scheme), t, dt)
+end
+
+
+# Carpenter's 4th-order 5-stage low-storage Runge-Kutta method
+function timestep!(solver::AbstractSolver, ::Val{:paired_rk_2_16}, t::Float64, dt::Float64)
+  a = [0.0, 567301805773.0 / 1357537059087.0,2404267990393.0 / 2016746695238.0,
+       3550918686646.0 / 2091501179385.0, 1275806237668.0 / 842570457699.0]
+  b = [1432997174477.0 / 9575080441755.0, 5161836677717.0 / 13612068292357.0,
+       1720146321549.0 / 2090206949498.0, 3134564353537.0 / 4481467310338.0,
+       2277821191437.0 / 14882151754819.0]
+  c = [0.0, 1432997174477.0 / 9575080441755.0, 2526269341429.0 / 6820363962896.0,
+       2006345519317.0 / 3224310063776.0, 2802321613138.0 / 2924317926251.0]
+
+  for stage = 1:5
+    t_stage = t + dt * c[stage]
+    @timeit timer() "rhs" rhs!(solver, t_stage)
+    @timeit timer() "Runge-Kutta step" begin
+      @. solver.elements.u_rungekutta = (solver.elements.u_t
+                                         - solver.elements.u_rungekutta * a[stage])
+      @. solver.elements.u += solver.elements.u_rungekutta * b[stage] * dt
+    end
+  end
+end
+
+
+# Carpenter's 4th-order 5-stage low-storage Runge-Kutta method
+function timestep!(solver::AbstractSolver, ::Val{:carpenter_4_5}, t::Float64, dt::Float64)
   a = [0.0, 567301805773.0 / 1357537059087.0,2404267990393.0 / 2016746695238.0,
        3550918686646.0 / 2091501179385.0, 1275806237668.0 / 842570457699.0]
   b = [1432997174477.0 / 9575080441755.0, 5161836677717.0 / 13612068292357.0,
