d9284735b43d4def6dd593da576b08a35c691b99
Hendrik Ranocha <mail@ranocha.de>
2020-05-26 13:57:42 +0200

    Apply suggestion to src/equations/euler.jl

--- src/equations/euler.jl
+++ src/equations/euler.jl
@@ -501,7 +501,7 @@ end
 
 Entropy conserving two-point flux by Chandrashekar (2013)
   Kinetic Energy Preserving and Entropy Stable Finite Volume Schemes
-  for Compressible CompressibleEulerEquations and Navier-Stokes Equations
+  for Compressible Euler and Navier-Stokes Equations
 [DOI: 10.4208/cicp.170712.010313a](https://doi.org/10.4208/cicp.170712.010313a)
 """
 @inline function chandrashekar_flux(equation::CompressibleEulerEquations, orientation,
