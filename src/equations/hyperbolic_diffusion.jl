fa2b5c6c6ab9b6f64f76bf179540c7243c767cfe
Michael Schlottke-Lakemper <michael@sloede.com>
2020-06-06 06:10:56 +0200

    Make residual output the default for HypDiff calculations

--- src/equations/hyperbolic_diffusion.jl
+++ src/equations/hyperbolic_diffusion.jl
@@ -27,7 +27,7 @@ end
 get_name(::HyperbolicDiffusionEquations) = "HyperbolicDiffusionEquations"
 varnames_cons(::HyperbolicDiffusionEquations) = @SVector ["phi", "p", "q"]
 varnames_prim(::HyperbolicDiffusionEquations) = @SVector ["phi", "p", "q"]
-default_analysis_quantities(::HyperbolicDiffusionEquations) = (:l2_error, :linf_error)
+default_analysis_quantities(::HyperbolicDiffusionEquations) = (:l2_error, :linf_error, :residual)
 
 
 # Set initial conditions at physical location `x` for pseudo-time `t`
