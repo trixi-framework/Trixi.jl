8c3a09a6df281472db7b53b101d83af87c930172
Hendrik Ranocha <mail@ranocha.de>
2020-08-03 11:51:49 +0200

    another flux for the Euler equations

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -47,7 +47,7 @@ include("run.jl")
 # export types/functions that define the public API of Trixi
 export CompressibleEulerEquations, IdealGlmMhdEquations, HyperbolicDiffusionEquations, LinearScalarAdvectionEquation
 export flux_central, flux_lax_friedrichs,
-       flux_chandrashekar, flux_derigs_etal, flux_kennedy_gruber, flux_kuya_etal
+       flux_chandrashekar, flux_ranocha, flux_derigs_etal, flux_kennedy_gruber, flux_kuya_etal
 
 
 end
