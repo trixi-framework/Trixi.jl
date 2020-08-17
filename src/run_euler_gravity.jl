8adf81f91fe67c1bf5f6af44a7fa063fa0fa9a9b
Michael Schlottke-Lakemper <michael@sloede.com>
2020-08-17 21:34:34 +0200

    Split run methods into two, once for coupled Euler-gravity and one for all other simulations

--- src/run_euler_gravity.jl
+++ src/run_euler_gravity.jl
@@ -1,99 +1,12 @@
 
-using LinearMaps: LinearMap
-
-
-"""
-    run(parameters_file=nothing; verbose=false, args=nothing, refinement_level_increment=0, parameters...)
-
-Run a Trixi simulation with the parameters in `parameters_file`.
-Parameters can be overriden by specifying them as keyword arguments (see examples).
-
-If `verbose` is `true`, additional output will be generated on the terminal
-that may help with debugging.  If `args` is given, it should be an
-`ARGS`-like array of strings that holds command line arguments, and will be
-interpreted by the `parse_commandline_arguments` function. In this case, the values of
-`parameters_file` and `verbose` are ignored. If a value for
-`refinement_level_increment` is given, `initial_refinement_level` will be
- increased by this value before running the simulation (mostly used by EOC analysis).
-
-# Examples
-```julia
-julia> Trixi.run("examples/parameters.toml", verbose=true)
-[...]
-```
-
-Without changing the parameters file we can start a simulation with `N = 1` and
-`t_end = 0.5` as follows:
-```julia
-julia> Trixi.run("examples/parameters.toml", N=1, t_end=0.5)
-[...]
-```
-"""
-function run(parameters_file=nothing; verbose=false, args=nothing, refinement_level_increment=0, parameters...)
-  # Reset timer
-  reset_timer!(timer())
-
-  # Read command line or keyword arguments and parse parameters file
-  init_parameters(parameters_file; verbose=verbose, args=args,
-      refinement_level_increment=refinement_level_increment, parameters...)
-
-  # Separate initialization and execution into two functions such that Julia can specialize
-  # the code in `run_simulation` for the actual type of `solver` and `mesh`
-  if parameter("equations") == "euler_gravity"
-    globals[:euler_gravity] = true
-    mesh, solver, time_parameters, time_integration_function = init_simulation_euler_gravity()
-    run_simulation_euler_gravity(mesh, solver, time_parameters, time_integration_function)
-  else
-    globals[:euler_gravity] = false
-    mesh, solver, time_parameters, time_integration_function = init_simulation()
-    run_simulation(mesh, solver, time_parameters, time_integration_function)
-  end
-end
-
-
-function init_parameters(parameters_file=nothing; verbose=false, args=nothing, refinement_level_increment=0, parameters...)
-  # Read command line or keyword arguments
-  @timeit timer() "parse command line" if !isnothing(args)
-    # If args are given explicitly, parse command line arguments
-    args = parse_commandline_arguments(args)
-  else
-    # Otherwise interpret keyword arguments as command line arguments
-    args = Dict{String, Any}()
-    if isnothing(parameters_file)
-      error("missing 'parameters_file' argument")
-    end
-    args["parameters_file"] = parameters_file
-    args["verbose"] = verbose
-  end
-
-  # Set global verbosity
-  globals[:verbose] = args["verbose"]
-
-  # Parse parameters file
-  @timeit timer() "read parameter file" parse_parameters_file(args["parameters_file"])
-
-  # Override specified parameters
-  for (parameter, value) in parameters
-    setparameter(string(parameter), value)
-  end
-
-  # Start simulation with an increased initial refinement level if specified
-  # for convergence analysis
-  if refinement_level_increment != 0
-    setparameter("initial_refinement_level",
-      parameter("initial_refinement_level") + refinement_level_increment)
-  end
-end
-
-
-function init_simulation()
+function init_simulation_euler_gravity()
   # Print starup message
   print_startup_message()
 
   # Check if this is a restart from a previous result or a new simulation
   restart = parameter("restart", false)
   if restart
-    restart_filename = parameter("restart_filename")
+    error("restarting not yet supported for coupled Euler-gravity simulations")
   end
 
   # Initialize mesh
@@ -112,13 +25,18 @@ function init_simulation()
   # Initialize system of equations
   print("Initializing system of equations... ")
   equations_name = parameter("equations")
-  equations = make_equations(equations_name)
+  @assert equations_name == "euler_gravity" "This only works with 'euler_gravity' as equations type"
+  equations_euler = make_equations("CompressibleEulerEquations")
+  equations_gravity = make_equations("HyperbolicDiffusionEquations")
   println("done")
 
   # Initialize solver
   print("Initializing solver... ")
   solver_name = parameter("solver", valid=["dg"])
-  solver = make_solver(solver_name, equations, mesh)
+  solver = make_solver(solver_name, equations_euler, mesh)
+  solver_gravity = make_solver(solver_name, equations_gravity, mesh,
+                               surface_flux=flux_lax_friedrichs, volume_flux=flux_central)
+  # `solver` = Euler solver -> this is to keep differences to original method to a minimum
   println("done")
 
   # Sanity checks
@@ -142,18 +60,20 @@ function init_simulation()
     time = t_start
     step = 0
     set_initial_conditions!(solver, time)
+    set_initial_conditions!(solver_gravity, time)
     println("done")
 
     # If AMR is enabled, adapt mesh and re-apply ICs
     if amr_interval > 0 && adapt_initial_conditions
       @timeit timer() "initial condition AMR" has_changed = adapt!(mesh, solver, time,
-          only_refine=adapt_initial_conditions_only_refine)
+          only_refine=adapt_initial_conditions_only_refine, passive_solvers=(solver_gravity,))
 
       # Iterate until mesh does not change anymore
       while has_changed
         set_initial_conditions!(solver, time)
+        set_initial_conditions!(solver_gravity, time)
         @timeit timer() "initial condition AMR" has_changed = adapt!(mesh, solver, time,
-            only_refine=adapt_initial_conditions_only_refine)
+            only_refine=adapt_initial_conditions_only_refine, passive_solvers=(solver_gravity,))
       end
 
       # Save mesh file
@@ -182,14 +102,19 @@ function init_simulation()
   min_dx = domain_length / 2^max_level
   max_dx = domain_length / 2^min_level
   s = ""
-  s *= """| Simulation setup
+  s *= """| Simulation setup (Euler + Gravity)
           | ----------------
           | working directory:  $(pwd())
           | parameters file:    $(parameter("parameters_file"))
-          | equations:          $(get_name(equations))
-          | | #variables:       $(nvariables(equations))
-          | | variable names:   $(join(varnames_cons(equations), ", "))
-          | sources:            $sources
+          | equations:          $(equations_name)
+          | | Euler:
+          | | | #variables:     $(nvariables(equations_euler))
+          | | | variable names: $(join(varnames_cons(equations_euler), ", "))
+          | | | sources:        $sources
+          | | Gravity:
+          | | | #variables:     $(nvariables(equations_gravity))
+          | | | variable names: $(join(varnames_cons(equations_gravity), ", "))
+          | | | sources:        $sources
           | restart:            $(restart ? "yes" : "no")
           """
   if restart
@@ -216,14 +141,19 @@ function init_simulation()
           | | solver:           $solver_name
           | | N:                $N
           | | CFL:              $cfl
-          | | volume integral:  $(get_name(solver.volume_integral_type))
-          | | volume flux:      $(get_name(solver.volume_flux))
-          | | surface flux:     $(get_name(solver.surface_flux))
+          | | Euler solver:
+          | | | volume integral:  $(get_name(solver.volume_integral_type))
+          | | | volume flux:      $(get_name(solver.volume_flux))
+          | | | surface flux:     $(get_name(solver.surface_flux))
+          | | Gravity solver:
+          | | | volume integral:  $(get_name(solver_gravity.volume_integral_type))
+          | | | volume flux:      $(get_name(solver_gravity.volume_flux))
+          | | | surface flux:     $(get_name(solver_gravity.surface_flux))
           | | #elements:        $(solver.n_elements)
           | | #surfaces:        $(solver.n_surfaces)
           | | #boundaries:      $(solver.n_boundaries)
           | | #l2mortars:       $(solver.n_l2mortars)
-          | | #DOFs:            $(ndofs(solver))
+          | | #DOFs:            $(ndofs(solver)) + $(ndofs(solver_gravity))
           |
           | Mesh
           | | #cells:           $(length(mesh.tree))
@@ -253,12 +183,21 @@ function init_simulation()
     # we need to make sure, that derived quantities, such as e.g. blending
     # factor is already computed for the initial condition
     @notimeit timer() rhs!(solver, time)
-    save_solution_file(solver, mesh, time, 0, step)
-  end
+    save_solution_file(solver, mesh, time, 0, step, "euler")
 
+    @notimeit timer() rhs!(solver_gravity, time)
+    save_solution_file(solver_gravity, mesh, time, 0, step, "gravity")
+  end
   # Print initial solution analysis and initialize solution analysis
   if analysis_interval > 0
-    analyze_solution(solver, mesh, time, 0, step, 0, 0)
+    if get_name(solver.initial_conditions) == "initial_conditions_eoc_test_coupled_euler_gravity"
+      analyze_solution(solver, mesh, time, 0, step, 0, 0, solver_gravity=solver_gravity)
+      # comment out for anything other than coupling convergence test
+      println()
+      analyze_solution(solver_gravity, mesh, time, 0, step, 0, 0)
+    else
+      analyze_solution(solver, mesh, time, 0, step, 0, 0, solver_gravity=solver_gravity)
+    end
   end
 
   time_parameters = (time=time, step=step, t_end=t_end, cfl=cfl,
@@ -270,16 +209,19 @@ function init_simulation()
                     solution_interval=solution_interval,
                     amr_interval=amr_interval,
                     restart_interval=restart_interval)
-  return mesh, solver, time_parameters, time_integration_function
+  return mesh, (solver, solver_gravity), time_parameters, time_integration_function
 end
 
 
-function run_simulation(mesh, solver, time_parameters, time_integration_function)
+function run_simulation_euler_gravity(mesh, solvers, time_parameters, time_integration_function)
   @unpack time, step, t_end, cfl, n_steps_max,
           save_final_solution, save_final_restart,
           analysis_interval, alive_interval,
           solution_interval, amr_interval,
           restart_interval = time_parameters
+
+  solver_euler, solver_gravity = solvers
+  solver = solver_euler # To keep differences to `run_simulation` to a minimum
 
   loop_start_time = time_ns()
   analysis_start_time = time_ns()
@@ -291,6 +233,7 @@ function run_simulation(mesh, solver, time_parameters, time_integration_function
   local l2_error, linf_error
 
   # Start main loop (loop until final time step is reached)
+  globals[:gravity_subcycles] = 0
   finalstep = false
   first_loop_iteration = true
   @timeit timer() "main loop" while !finalstep
@@ -309,7 +252,9 @@ function run_simulation(mesh, solver, time_parameters, time_integration_function
     end
 
     # Evolve solution by one time step
-    time_integration_function(solver, time, dt)
+    @timeit timer() "timestep_euler_gravity!" begin
+      timestep_euler_gravity!(solver_euler, solver_gravity, time, dt, time_parameters)
+    end
     step += 1
     time += dt
     n_analysis_timesteps += 1
@@ -339,8 +284,24 @@ function run_simulation(mesh, solver, time_parameters, time_integration_function
                           (n_analysis_timesteps * ndofs(solver)))
 
       # Analyze solution
-      l2_error, linf_error = @timeit timer() "analyze solution" analyze_solution(
-          solver, mesh, time, dt, step, runtime_absolute, runtime_relative)
+      if get_name(solver_euler.initial_conditions) == "initial_conditions_eoc_test_coupled_euler_gravity"
+        l2_euler, linf_euler = @timeit timer() "analyze solution" analyze_solution(
+            solver, mesh, time, dt, step, runtime_absolute, runtime_relative, solver_gravity=solver_gravity)
+        # Pull gravity solver information from file
+        timestep_gravity = eval(Symbol(parameter("time_integration_scheme_gravity")))
+        cfl_gravity = parameter("cfl_gravity")::Float64
+        rho0 = parameter("rho0")::Float64
+        G = parameter("G")::Float64
+        gravity_parameters = (; timestep_gravity, cfl_gravity, rho0, G)
+        update_gravity!(solver_gravity, solver_euler.elements.u, gravity_parameters)
+        l2_hypdiff, linf_hypdiff = @timeit timer() "analyze solution" analyze_solution(
+            solver_gravity, mesh, time, dt, step, runtime_absolute, runtime_relative)
+        l2_error   = vcat(l2_euler  , l2_hypdiff)
+        linf_error = vcat(linf_euler, linf_hypdiff)
+      else
+        l2_error, linf_error = @timeit timer() "analyze solution" analyze_solution(
+            solver, mesh, time, dt, step, runtime_absolute, runtime_relative, solver_gravity=solver_gravity)
+      end
 
       # Reset time and counters
       analysis_start_time = time_ns()
@@ -376,7 +337,8 @@ function run_simulation(mesh, solver, time_parameters, time_integration_function
         end
 
         # Then write solution file
-        save_solution_file(solver, mesh, time, dt, step)
+        save_solution_file(solver, mesh, time, dt, step, "euler")
+        save_solution_file(solver_gravity, mesh, time, dt, step, "gravity")
       end
       output_time += time_ns() - output_start_time
     end
@@ -400,7 +362,8 @@ function run_simulation(mesh, solver, time_parameters, time_integration_function
 
     # Perform adaptive mesh refinement
     if amr_interval > 0 && (step % amr_interval == 0) && !finalstep
-      @timeit timer() "AMR" has_changed = adapt!(mesh, solver, time)
+      @timeit timer() "AMR" has_changed = adapt!(mesh, solver, time,
+                                                 passive_solvers=(solver_gravity,))
 
       # Store if mesh has changed to write changed mesh file before next restart/solution output
       if has_changed
@@ -422,119 +385,12 @@ function run_simulation(mesh, solver, time_parameters, time_integration_function
   println()
 
   # Return error norms for EOC calculation
-  return l2_error, linf_error, varnames_cons(solver.equations)
-end
-
-
-"""
-    convtest(parameters_file, iterations; parameters...)
-
-Run multiple Trixi simulations with the parameters in `parameters_file` and compute
-the experimental order of convergence (EOC) in the ``L^2`` and ``L^\\infty`` norm.
-The number of runs is specified by `iterations` and in each run the initial
-refinement level will be increased by 1. Parameters can be overriden by specifying them as
-additional keyword arguments, which are passed to the respective call to `run`..
-"""
-function convtest(parameters_file, iterations; parameters...)
-  @assert(iterations > 1, "Number of iterations must be bigger than 1 for a convergence analysis")
-
-  # Types of errors to be calcuated
-  errors = Dict(:L2 => Float64[], :Linf => Float64[])
-
-  # Declare variable to access variable names after for loop
-  local variablenames
-
-  # Run trixi and extract errors
-  for i = 1:iterations
-    println(string("Running convtest iteration ", i, "/", iterations))
-    l2_error, linf_error, variablenames = run(parameters_file; refinement_level_increment = i - 1,
-                                              parameters...)
-
-    # Collect errors as one vector to reshape later
-    append!(errors[:L2], l2_error)
-    append!(errors[:Linf], linf_error)
-  end
-
-  # Number of variables
-  nvariables = length(variablenames)
-
-  # Reshape errors to get a matrix where the i-th row represents the i-th iteration
-  # and the j-th column represents the j-th variable
-  errorsmatrix = Dict(kind => transpose(reshape(error, (nvariables, iterations))) for (kind, error) in errors)
-
-  # Calculate EOCs where the columns represent the variables
-  # As dx halves in every iteration the denominator needs to be log(1/2)
-  eocs = Dict(kind => log.(error[2:end, :] ./ error[1:end-1, :]) ./ log(1 / 2) for (kind, error) in errorsmatrix)
-
-
-  for (kind, error) in errorsmatrix
-    println(kind)
-
-    for v in variablenames
-      @printf("%-20s", v)
-    end
-    println("")
-
-    for k = 1:nvariables
-      @printf("%-10s", "error")
-      @printf("%-10s", "EOC")
-    end
-    println("")
-
-    # Print errors for the first iteration
-    for k = 1:nvariables
-      @printf("%-10.2e", error[1, k])
-      @printf("%-10s", "-")
-    end
-    println("")
-
-    # For the following iterations print errors and EOCs
-    for j = 2:iterations
-      for k = 1:nvariables
-        @printf("%-10.2e", error[j, k])
-        @printf("%-10.2f", eocs[kind][j-1, k])
-      end
-      println("")
-    end
-    println("")
-
-    # Print mean EOCs
-    for k = 1:nvariables
-      @printf("%-10s", "mean")
-      @printf("%-10.2f", sum(eocs[kind][:, k]) ./ length(eocs[kind][:, k]))
-    end
-    println("")
-    println("-"^80)
+  println("Number of gravity subcycles: ", globals[:gravity_subcycles])
+  if get_name(solver_euler.initial_conditions) == "initial_conditions_eoc_test_coupled_euler_gravity"
+    return l2_error, linf_error, vcat(varnames_cons(solver.equations),
+                                      varnames_cons(solver_gravity.equations))
+  else
+    return l2_error, linf_error, varnames_cons(solver.equations)
   end
 end
 
-
-function compute_linear_structure(parameters_file=nothing, source_terms=nothing; verbose=false, args=nothing, refinement_level_increment=0, parameters...)
-  # Read command line or keyword arguments and parse parameters file
-  init_parameters(parameters_file; verbose=verbose, args=args,
-      refinement_level_increment=refinement_level_increment, parameters...)
-  mesh, solver, time_parameters = init_simulation()
-
-  equations(solver) isa Union{LinearScalarAdvectionEquation, HyperbolicDiffusionEquations} ||
-    throw(ArgumentError("Only linear problems are supported."))
-
-  # get the right hand side from the source terms
-  solver.elements.u .= 0
-  rhs!(solver, 0)
-  b = vec(-solver.elements.u_t) |> copy
-
-  # set the source terms to zero to extract the linear operator
-  solver = Dg(solver.equations, solver.surface_flux, solver.volume_flux, solver.initial_conditions,
-              source_terms, mesh, polydeg(solver))
-  A = LinearMap(length(solver.elements.u), ismutating=true) do dest,src
-    vec(solver.elements.u) .= src
-    rhs!(solver, 0)
-    dest .= vec(solver.elements.u_t)
-  end
-
-  A, b
-end
-
-
-# Include source file with init and run methods for coupled Euler-gravity simulations
-include("run_euler_gravity.jl")
