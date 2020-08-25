7fc47a6406dd75a4a57c10cb491f2d93e9420adc
Michael Schlottke-Lakemper <michael@sloede.com>
2020-08-25 11:48:28 +0200

    Add 3D mortar parameter file + tests

--- test/test_examples_3d.jl
+++ test/test_examples_3d.jl
@@ -24,6 +24,16 @@ const EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "
             l2   = [0.010323099666828388, 0.00972876713766357, 0.00972876713766343, 0.009728767137663324, 0.015080409341036285],
             linf = [0.034894880154510144, 0.03383545920056008, 0.033835459200560525, 0.03383545920054587, 0.06785780622711979])
   end
+  @testset "parameters_mortar.toml" begin
+    test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_mortar.toml"),
+            l2   = [0.0018461483161353273],
+            linf = [0.017728496545256434])
+  end
+  @testset "parameters_mortar_euler.toml" begin
+    test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_mortar_euler.toml"),
+            l2   = [0.0019011097431965655, 0.0018289464087588392, 0.0018289464087585998, 0.0018289464087588862, 0.003354766311541738],
+            linf = [0.011918594206950184, 0.011808582644224241, 0.011808582644249999, 0.011808582644239785, 0.02464803617735356])
+  end
 end
 
 # Clean up afterwards: delete Trixi output directory
