4890314da5bdd187b3e7342c9eda87ced381752a
Michael Schlottke-Lakemper <michael@sloede.com>
2020-08-28 17:44:57 +0200

    Add enclosing `@testset` around all tests to see final tally

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,22 @@
+using Test
+
 # run tests on Travis CI in parallel
 const TRIXI_TEST = get(ENV, "TRIXI_TEST", "all")
+@time @testset "Trixi.jl tests" begin
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "2D"
+    include("test_examples_2d.jl")
+  end
 
-@time if TRIXI_TEST == "all" || TRIXI_TEST == "2D"
-  include("test_examples_2d.jl")
-end
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "3D"
+    include("test_examples_3d.jl")
+  end
 
-@time if TRIXI_TEST == "all" || TRIXI_TEST == "3D"
-  include("test_examples_3d.jl")
-end
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "misc"
+    include("test_manual.jl")
+    include("test_elixirs.jl")
+  end
 
-@time if TRIXI_TEST == "all" || TRIXI_TEST == "misc"
-  include("test_manual.jl")
-  include("test_elixirs.jl")
-end
-
-@time if TRIXI_TEST == "all" || TRIXI_TEST == "paper-self-gravitating-gas-dynamics"
-  include("test_paper-self-gravitating-gas-dynamics.jl")
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "paper-self-gravitating-gas-dynamics"
+    include("test_paper-self-gravitating-gas-dynamics.jl")
+  end
 end
