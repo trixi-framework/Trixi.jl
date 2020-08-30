90ed7403e91d82f33700370b14ca6748f32e20f9
Hendrik Ranocha <mail@ranocha.de>
2020-08-30 15:45:03 +0200

    add ICOSAHOM reference for flux_ranocha [ci skip]

--- src/equations/2d/compressible_euler.jl
+++ src/equations/2d/compressible_euler.jl
@@ -588,6 +588,10 @@ Entropy conserving and kinetic energy preserving two-point flux by Ranocha (2018
   Generalised Summation-by-Parts Operators and Entropy Stability of Numerical Methods
   for Hyperbolic Balance Laws
 [PhD thesis, TU Braunschweig](https://cuvillier.de/en/shop/publications/7743)
+See also Ranocha (2020)
+  Entropy Conserving and Kinetic Energy Preserving Numerical Methods for
+  the Euler Equations Using Summation-by-Parts Operators
+[Proceedings of ICOSAHOM 2018](https://doi.org/10.1007/978-3-030-39647-3_42)
 """
 @inline function flux_ranocha(u_ll, u_rr, orientation, equation::CompressibleEulerEquations2D)
   # Unpack left and right state
