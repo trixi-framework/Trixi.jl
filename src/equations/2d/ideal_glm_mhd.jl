ddce420b8222445bb29419b9ee23d3a21ce1f6cb
Andrew Winters <andrew.ross.winters@liu.se>
2020-09-03 14:30:08 +0200

    bug fix in the new test

--- src/equations/2d/ideal_glm_mhd.jl
+++ src/equations/2d/ideal_glm_mhd.jl
@@ -21,7 +21,7 @@ have_nonconservative_terms(::IdealGlmMhdEquations2D) = Val(true)
 varnames_cons(::IdealGlmMhdEquations2D) = @SVector ["rho", "rho_v1", "rho_v2", "rho_v3", "rho_e", "B1", "B2", "B3", "psi"]
 varnames_prim(::IdealGlmMhdEquations2D) = @SVector ["rho", "v1", "v2", "v3", "p", "B1", "B2", "B3", "psi"]
 default_analysis_quantities(::IdealGlmMhdEquations2D) = (:l2_error, :linf_error, :dsdu_ut,
-                                                       :l2_divb, :linf_divb)
+                                                         :l2_divb, :linf_divb)
 
 
 # Set initial conditions at physical location `x` for time `t`
