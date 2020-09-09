f7d24632b2387438418e41d7fc742af8b41521fb
Michael Schlottke-Lakemper <michael@sloede.com>
2020-09-09 16:49:05 +0200

    Remove mesh from Dg2D struct again

--- src/solvers/dg/2d/dg.jl
+++ src/solvers/dg/2d/dg.jl
@@ -6,7 +6,6 @@ mutable struct Dg2D{Eqn<:AbstractEquation, MeshType, NVARS, POLYDEG,
                   InverseVandermondeLegendre, MortarMatrix,
                   VectorAnalysisNnodes, AnalysisVandermonde} <: AbstractDg{2, POLYDEG, MeshType}
   equations::Eqn
-  mesh_that_should_not_be_used::MeshType
 
   surface_flux_function::SurfaceFlux
   volume_flux_function::VolumeFlux
@@ -125,11 +124,13 @@ function Dg2D(equation::AbstractEquation{NDIMS, NVARS}, surface_flux_function, v
   # Initialize interpolation data structures
   n_nodes = POLYDEG + 1
   nodes, weights = gauss_lobatto_nodes_weights(n_nodes)
+  nodes = SVector{POLYDEG+1}(nodes)
   inverse_weights = 1 ./ weights
   _, inverse_vandermonde_legendre = vandermonde_legendre(nodes)
   lhat = zeros(n_nodes, 2)
   lhat[:, 1] = calc_lhat(-1.0, nodes, weights)
   lhat[:, 2] = calc_lhat( 1.0, nodes, weights)
+  lhat = SMatrix{POLYDEG+1,2}(lhat)
 
   # Initialize differentiation operator
   volume_integral_type = Val(Symbol(parameter("volume_integral_type", "weak_form",
@@ -139,6 +140,7 @@ function Dg2D(equation::AbstractEquation{NDIMS, NVARS}, surface_flux_function, v
     volume_integral_type = Val(:weak_form)
   end
   dhat = calc_dhat(nodes, weights)
+  dhat = SMatrix{POLYDEG+1,POLYDEG+1}(dhat)
   dsplit = calc_dsplit(nodes, weights)
   dsplit_transposed = transpose(calc_dsplit(nodes, weights))
 
@@ -149,11 +151,18 @@ function Dg2D(equation::AbstractEquation{NDIMS, NVARS}, surface_flux_function, v
   l2mortar_reverse_lower = calc_reverse_lower(n_nodes, Val(:gauss))
   ecmortar_reverse_upper = calc_reverse_upper(n_nodes, Val(:gauss_lobatto))
   ecmortar_reverse_lower = calc_reverse_lower(n_nodes, Val(:gauss_lobatto))
+  mortar_forward_upper   = SMatrix{POLYDEG+1,POLYDEG+1}(mortar_forward_upper)
+  mortar_forward_lower   = SMatrix{POLYDEG+1,POLYDEG+1}(mortar_forward_lower)
+  l2mortar_reverse_upper = SMatrix{POLYDEG+1,POLYDEG+1}(l2mortar_reverse_upper)
+  l2mortar_reverse_lower = SMatrix{POLYDEG+1,POLYDEG+1}(l2mortar_reverse_lower)
+  ecmortar_reverse_upper = SMatrix{POLYDEG+1,POLYDEG+1}(ecmortar_reverse_upper)
+  ecmortar_reverse_lower = SMatrix{POLYDEG+1,POLYDEG+1}(ecmortar_reverse_lower)
 
   # Initialize data structures for error analysis (by default, we use twice the
   # number of analysis nodes as the normal solution)
   analysis_polydeg = 2 * (n_nodes) - 1
   analysis_nodes, analysis_weights = gauss_lobatto_nodes_weights(analysis_polydeg + 1)
+  analysis_nodes = SVector{analysis_polydeg+1}(analysis_nodes)
   analysis_weights_volume = analysis_weights
   analysis_vandermonde = polynomial_interpolation_matrix(nodes, analysis_nodes)
   analysis_total_volume = mesh.tree.length_level_0^ndims(mesh)
@@ -250,8 +259,11 @@ function Dg2D(equation::AbstractEquation{NDIMS, NVARS}, surface_flux_function, v
   initial_state_integrals = Vector{Float64}()
 
   # Create actual DG solver instance
-  dg = Dg2D(
-      equation, mesh,
+  dg = Dg2D{typeof(equation), typeof(mesh), NVARS, POLYDEG, typeof(surface_flux_function), typeof(volume_flux_function),
+            typeof(initial_conditions), typeof(source_terms), typeof(mortar_type), typeof(volume_integral_type), typeof(shock_indicator_variable),
+            typeof(nodes), typeof(dhat), typeof(lhat), typeof(inverse_vandermonde_legendre), typeof(mortar_forward_upper),
+            typeof(analysis_nodes), typeof(analysis_vandermonde)}(
+      equation,
       surface_flux_function, volume_flux_function,
       initial_conditions, source_terms,
       elements, n_elements,
