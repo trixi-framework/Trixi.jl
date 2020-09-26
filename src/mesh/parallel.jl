5d3dbce979135b94c50bab1d16ccf423a368252f
Michael Schlottke-Lakemper <michael@sloede.com>
2020-09-26 06:30:44 +0200

    Move partition! to parallel.jl

--- src/mesh/parallel.jl
+++ src/mesh/parallel.jl
@@ -1,3 +1,40 @@
+# Partition mesh using a static domain decomposition algorithm based on leaf cell count alone
+function partition!(mesh)
+  # Determine number of leaf cells per rank
+  leaves = leaf_cells(mesh.tree)
+  @assert length(leaves) > n_mpi_ranks()
+  n_leaves_per_rank = OffsetArray(fill(div(length(leaves), n_mpi_ranks()), n_mpi_ranks()),
+                                  0:(n_mpi_ranks() - 1))
+  for d in 0:(rem(length(leaves), n_mpi_ranks()) - 1)
+    n_leaves_per_rank[d] += 1
+  end
+  @assert sum(n_leaves_per_rank) == length(leaves)
+
+  # Assign MPI ranks to all cells such that all ancestors of each cell - if not yet assigned to a
+  # rank - belong to the same rank
+  mesh.first_cell_by_rank = similar(n_leaves_per_rank)
+  mesh.n_cells_by_rank = similar(n_leaves_per_rank)
+
+  leaf_count = 0
+  last_id = leaves[n_leaves_per_rank[0]]
+  mesh.first_cell_by_rank[0] = 1
+  mesh.n_cells_by_rank[0] = last_id
+  mesh.tree.mpi_ranks[1:last_id] .= 0
+  for d in 1:(length(n_leaves_per_rank)-1)
+    leaf_count += n_leaves_per_rank[d-1]
+    last_id = leaves[leaf_count + n_leaves_per_rank[d]]
+    mesh.first_cell_by_rank[d] = mesh.first_cell_by_rank[d-1] + mesh.n_cells_by_rank[d-1]
+    mesh.n_cells_by_rank[d] = last_id - mesh.first_cell_by_rank[d] + 1
+    mesh.tree.mpi_ranks[mesh.first_cell_by_rank[d]:last_id] .= d
+  end
+
+  @assert all(x->x >= 0, mesh.tree.mpi_ranks[1:length(mesh.tree)])
+  @assert sum(mesh.n_cells_by_rank) == length(mesh.tree)
+
+  return nothing
+end
+
+
 function load_mesh(restart_filename, mpi_parallel::Val{true})
   # Get number of spatial dimensions
   ndims_ = parameter("ndims")
