11e3d5b517d7335cfb5205f8173ce551220eedb6
Michael Schlottke-Lakemper <michael@sloede.com>
2020-10-03 17:11:47 +0200

    Update manual tests

--- test/test_manual.jl
+++ test/test_manual.jl
@@ -9,16 +9,16 @@ isdir(outdir) && rm(outdir, recursive=true)
 
 # Run various manual (= non-parameter-file-triggered tests)
 @testset "Manual tests" begin
-  @testset "Tree" begin
+  @testset "SerialTree" begin
     @testset "constructors" begin
-      @test_nowarn Trixi.Tree(Val(1), 10, 0.0, 1.0)
+      @test_nowarn Trixi.SerialTree(Val(1), 10, 0.0, 1.0)
     end
 
     @testset "helper functions" begin
-      t = Trixi.Tree(Val(1), 10, 0.0, 1.0)
+      t = Trixi.SerialTree(Val(1), 10, 0.0, 1.0)
       @test_nowarn show(t)
       @test Trixi.ndims(t) == 1
-      @test Trixi.ndims(Trixi.Tree{1}) == 1
+      @test Trixi.ndims(Trixi.SerialTree{1}) == 1
       @test Trixi.has_any_neighbor(t, 1, 1) == true
       @test Trixi.isperiodic(t, 1) == true
       @test Trixi.n_children_per_cell(t) == 2
@@ -27,7 +27,7 @@ isdir(outdir) && rm(outdir, recursive=true)
     end
 
     @testset "refine!/coarsen!" begin
-      t = Trixi.Tree(Val(1), 10, 0.0, 1.0)
+      t = Trixi.SerialTree(Val(1), 10, 0.0, 1.0)
       @test Trixi.refine!(t) == [1]
       @test Trixi.coarsen!(t) == [1]
       @test Trixi.refine!(t) == [1]
@@ -38,6 +38,12 @@ isdir(outdir) && rm(outdir, recursive=true)
       @test Trixi.coarsen_box!(t, [-0.5], [0.0]) == [2]
       @test Trixi.coarsen_box!(t, 0.0, 0.5) == [3]
       @test isnothing(Trixi.reset_data_structures!(t))
+    end
+  end
+
+  @testset "ParallelTree" begin
+    @testset "constructors" begin
+      @test_nowarn Trixi.ParallelTree(Val(1), 10, 0.0, 1.0)
     end
   end
 
