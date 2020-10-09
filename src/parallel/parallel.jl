bd3874f4e60f3ab0bc063155ba3b436d7182edff
Michael Schlottke-Lakemper <michael@sloede.com>
2020-10-09 22:35:32 +0200

    Remove unused methods

--- src/parallel/parallel.jl
+++ src/parallel/parallel.jl
@@ -47,19 +47,14 @@ const MPI_IS_ROOT = Ref(true)
 
 @inline mpi_comm() = MPI.COMM_WORLD
 
-@inline mpi_rank(comm) = MPI.Comm_rank(comm)
 @inline mpi_rank() = MPI_RANK[]
 
-@inline n_mpi_ranks(comm) = MPI.Comm_size(comm)
 @inline n_mpi_ranks() = MPI_SIZE[]
 
-@inline is_parallel(comm) = n_mpi_ranks(comm) > 1
 @inline is_parallel() = MPI_IS_PARALLEL[]
 
-@inline is_serial(comm) = !is_parallel(comm)
 @inline is_serial() = MPI_IS_SERIAL[]
 
-@inline is_mpi_root(comm) = is_serial() || mpi_rank(comm) == 0
 @inline is_mpi_root() = MPI_IS_ROOT[]
 
 @inline mpi_root() = 0
