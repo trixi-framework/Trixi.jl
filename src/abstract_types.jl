35f50977ed7812b958cf25cbd23b9bad15ba58bb
Hendrik Ranocha <mail@ranocha.de>
2020-10-12 06:23:09 +0200

    move more definitions to abstract_types.jl

--- src/abstract_types.jl
+++ src/abstract_types.jl
@@ -9,4 +9,30 @@ abstract type AbstractSemidiscretization end
 abstract type AbstractEquations{NDIMS, NVARS} end
 
 
+# abstract supertype of specific SBP bases such as a Lobatto-Legendre nodal basis
+abstract type AbstractBasisSBP{RealT<:Real} end
+
+
+# abstract supertype of mortar methods, e.g. using L² projections
+abstract type AbstractMortar{RealT<:Real} end
+
+# abstract supertype of mortar methods using L² projection
+# which will be specialized for different SBP bases
+abstract type MortarL2{RealT<:Real} <: AbstractMortar{RealT} end
+
+
+# abstract supertype of functionality related to the analysis of
+# numerical solutions, e.g. the calculation of errors
+abstract type SolutionAnalyzer{RealT<:Real} end
+
+
+# abstract supertype of grid-transfer methods used for AMR,
+# e.g. refinement and coarsening based on L² projections
+abstract type AdaptorAMR{RealT<:Real} end
+
+# abstract supertype of AMR grid-transfer operations using L² projections
+# which will be specialized for different SBP bases
+abstract type AdaptorL2{RealT<:Real} <: AdaptorAMR{RealT} end
+
+
 # TODO: Taal decide, which abstract types shall be defined here?
