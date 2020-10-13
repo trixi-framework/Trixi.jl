6237abc7a139b03c6708fdd48efe659bf4c241b3
Andrew Winters <andrew.ross.winters@liu.se>
2020-10-13 09:44:26 +0200

    added a test for the new Euler vortex elixir

--- test/test_examples_2d.jl
+++ test/test_examples_2d.jl
@@ -66,6 +66,11 @@ const EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "
     tspan = (0.0, 1.0))
   end
 
+  @testset "elixir_euler_vortex.jl" begin
+  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_vortex.jl"),
+    l2   = [3.6343269600298813e-6, 0.003211138535470753, 0.0032111483996635176, 0.004545716490663252],
+    linf = [7.900701569263369e-5, 0.03056161620998943, 0.030502702817406968, 0.04287693324734221])
+
   @testset "elixir_mhd_ec.jl" begin
   test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_mhd_ec.jl"),
     l2   = [0.03628315925311581, 0.04301306535453907, 0.042998910996002976, 0.025746791646914315, 0.1620587870592711, 0.01745580631201365, 0.01745656644392971, 0.02688212902288343, 0.00014263322984147517],
