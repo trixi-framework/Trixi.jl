f893f772f6c825eac4a32d954e368d3c3a401823
Andrew Winters <andrew.ross.winters@liu.se>
2020-10-13 21:19:18 +0200

    elixir added for the mhd blast wave. it runs if nonconservative terms are deactivated but crashes with them even for a single time step.

---
+++ examples/2d/elixir_mhd_blast_wave_amr.jl
@@ -0,0 +1,80 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+##
+# OBS! This crashes due to an issue with the combination of shockcapturing + non-conservative terms
+#      It can be run if "have_nonconservative_terms(::IdealGlmMhdEquations2D) = Val(true)" is set
+#      to false in the ideal_glm_mhd.jl
+
+###############################################################################
+# semidiscretization of the compressible ideal GLM-MHD equations
+
+equations = IdealGlmMhdEquations2D(1.4)
+
+initial_conditions = initial_conditions_mhd_blast_wave
+
+surface_flux = flux_lax_friedrichs
+volume_flux  = flux_central
+basis = LobattoLegendreBasis(3)
+indicator_sc = IndicatorHennemannGassner(equations, basis,
+                                         alpha_max=0.5,
+                                         alpha_min=0.001,
+                                         alpha_smooth=true,
+                                         variable=density_pressure)
+volume_integral = VolumeIntegralShockCapturingHG(indicator_sc;
+                                                 volume_flux_dg=volume_flux,
+                                                 volume_flux_fv=surface_flux)
+solver = DGSEM(basis, surface_flux, volume_integral)
+
+coordinates_min = (-0.5, -0.5)
+coordinates_max = ( 0.5,  0.5)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=4,
+                n_cells_max=10_000)
+
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_conditions, solver)
+
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 0.01)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+indicator_amr = IndicatorHennemannGassner(semi,
+                                          alpha_max=0.5,
+                                          alpha_min=0.001,
+                                          alpha_smooth=true,
+                                          variable=density_pressure)
+amr_indicator = IndicatorThreeLevel(semi, indicator_amr,
+                                    base_level=4,
+                                    max_level =6, max_threshold=0.01)
+amr_callback = AMRCallback(semi, amr_indicator,
+                           interval=5,
+                           adapt_initial_conditions=true,
+                           adapt_initial_conditions_only_refine=true)
+
+stepsize_callback = StepsizeCallback(cfl=0.5)
+
+save_solution = SaveSolutionCallback(interval=100,
+                                     save_initial_solution=true,
+                                     save_final_solution=true,
+                                     solution_variables=:primitive)
+
+analysis_interval = 100
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+
+callbacks = CallbackSet(summary_callback, amr_callback, stepsize_callback, save_solution, analysis_callback, alive_callback)
+
+
+###############################################################################
+# run the simulation
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false), dt=stepsize_callback(ode),
+            save_everystep=false, callback=callbacks);
+summary_callback() # print the timer summary
--- src/Trixi.jl
+++ src/Trixi.jl
@@ -78,7 +78,8 @@ export flux_central, flux_lax_friedrichs, flux_hll,
 export initial_conditions_convergence_test,
        initial_conditions_gauss,
        initial_conditions_weak_blast_wave, initial_conditions_blast_wave,
-       initial_conditions_isentropic_vortex
+       initial_conditions_isentropic_vortex,
+       initial_conditions_mhd_blast_wave
 
 export source_terms_convergence_test, source_terms_harmonic
 
