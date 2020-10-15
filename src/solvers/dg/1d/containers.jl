1ba7e073bed5e117b39c80c6b2d129a74149b7e3
Hendrik Ranocha <mail@ranocha.de>
2020-10-15 09:22:01 +0200

    resize! for 1D containers

--- src/solvers/dg/1d/containers.jl
+++ src/solvers/dg/1d/containers.jl
@@ -2,7 +2,6 @@
 # Container data structure (structure-of-arrays style) for DG elements
 # TODO: Taal refactor, remove u, u_t, u_tmp2, u_tmp3
 # TODO: Taal refactor, remove NVARS, POLYDEG?
-# TODO: Taal refactor, mutable struct or resize! for AMR?
 mutable struct ElementContainer1D{RealT<:Real, NVARS, POLYDEG} <: AbstractContainer
   u::Array{RealT, 3}                   # [variables, i, elements]
   u_t::Array{RealT, 3}                 # [variables, i, elements]
@@ -12,17 +11,51 @@ mutable struct ElementContainer1D{RealT<:Real, NVARS, POLYDEG} <: AbstractContai
   node_coordinates::Array{RealT, 3}    # [orientation, i, elements]
   surface_flux_values::Array{RealT, 3} # [variables, direction, elements]
   cell_ids::Vector{Int}                # [elements]
+  # internal `resize!`able storage
+  _u::Vector{RealT}
+  _u_t::Vector{RealT}
+  _u_tmp2::Vector{RealT}
+  _u_tmp3::Vector{RealT}
+  _node_coordinates::Vector{RealT}
+  _surface_flux_values::Vector{RealT}
 end
 
-function Base.copy!(dst::ElementContainer1D, src::ElementContainer1D)
-  dst.u                   = src.u
-  dst.u_t                 = src.u_t
-  dst.u_tmp2              = src.u_tmp2
-  dst.u_tmp3              = src.u_tmp3
-  dst.inverse_jacobian    = src.inverse_jacobian
-  dst.node_coordinates    = src.node_coordinates
-  dst.surface_flux_values = src.surface_flux_values
-  dst.cell_ids            = src.cell_ids
+function Base.resize!(elements::ElementContainer1D{RealT, NVARS, POLYDEG},
+                      capacity) where {RealT, NVARS, POLYDEG}
+  n_nodes = POLYDEG + 1
+  @unpack _u, _u_t, _u_tmp2, _u_tmp3, _node_coordinates, _surface_flux_values,
+          inverse_jacobian, cell_ids = elements
+
+  resize!(_u, NVARS * n_nodes * capacity)
+  elements.u = unsafe_wrap(Array, pointer(_u),
+                           (NVARS, n_nodes, capacity))
+
+  resize!(_u_t, NVARS * n_nodes * capacity)
+  elements.u_t = unsafe_wrap(Array, pointer(_u_t),
+                             (NVARS, n_nodes, capacity))
+
+  resize!(_u_tmp2, NVARS * n_nodes * capacity)
+  _u_tmp2 .= zero(eltype(_u_tmp2))
+  elements.u_tmp2 = unsafe_wrap(Array, pointer(_u_tmp2),
+                                (NVARS, n_nodes, capacity))
+
+  resize!(_u_tmp3, NVARS * n_nodes * capacity)
+  _u_tmp3 .= zero(eltype(_u_tmp3))
+  elements.u_tmp3 = unsafe_wrap(Array, pointer(_u_tmp3),
+                                (NVARS, n_nodes, capacity))
+
+  resize!(inverse_jacobian, capacity)
+
+  resize!(_node_coordinates, 1 * n_nodes * capacity)
+  elements.node_coordinates = unsafe_wrap(Array, pointer(_node_coordinates),
+                                          (1, n_nodes, capacity))
+
+  resize!(_surface_flux_values, NVARS * 2 * 1 * capacity)
+  elements.surface_flux_values = unsafe_wrap(Array, pointer(_surface_flux_values),
+                                             (NVARS, 2 * 1, capacity))
+
+  resize!(cell_ids, capacity)
+
   return nothing
 end
 
@@ -32,41 +65,72 @@ function ElementContainer1D{RealT, NVARS, POLYDEG}(capacity::Integer) where {Rea
   nan = convert(RealT, NaN)
 
   # Initialize fields with defaults
-  u = fill(nan, NVARS, n_nodes, capacity)
-  u_t = fill(nan, NVARS, n_nodes, capacity)
+  _u = fill(nan, NVARS * n_nodes * capacity)
+  u = unsafe_wrap(Array, pointer(_u),
+                  (NVARS, n_nodes, capacity))
+
+  _u_t = fill(nan, NVARS *  n_nodes * capacity)
+  u_t = unsafe_wrap(Array, pointer(_u_t),
+                    (NVARS, n_nodes, capacity))
+
   # u_rungakutta is initialized to non-NaN since it is used directly
-  u_tmp2 = fill(zero(RealT), NVARS, n_nodes, capacity)
-  u_tmp3 = fill(zero(RealT), NVARS, n_nodes, capacity)
+  _u_tmp2 = fill(zero(RealT), NVARS * n_nodes * capacity)
+  u_tmp2 = unsafe_wrap(Array, pointer(_u_tmp2),
+                       (NVARS, n_nodes, capacity))
+
+  _u_tmp3 = fill(zero(RealT), NVARS * n_nodes * capacity)
+  u_tmp3 = unsafe_wrap(Array, pointer(_u_tmp3),
+                       (NVARS, n_nodes, capacity))
+
   inverse_jacobian = fill(nan, capacity)
-  node_coordinates = fill(nan, 1, n_nodes, capacity)
-  surface_flux_values = fill(nan, NVARS, 2 * 1, capacity)
+
+  _node_coordinates = fill(nan, 1 * n_nodes * capacity)
+  node_coordinates = unsafe_wrap(Array, pointer(_node_coordinates),
+                                 (1, n_nodes, capacity))
+
+  _surface_flux_values = fill(nan, NVARS * 2 * 1 * capacity)
+  surface_flux_values = unsafe_wrap(Array, pointer(_surface_flux_values),
+                                    (NVARS, 2 * 1, capacity))
+
   cell_ids = fill(typemin(Int), capacity)
 
-  elements = ElementContainer1D{RealT, NVARS, POLYDEG}(
+  return ElementContainer1D{RealT, NVARS, POLYDEG}(
     u, u_t, u_tmp2, u_tmp3,
-    inverse_jacobian, node_coordinates, surface_flux_values, cell_ids)
-
-  return elements
+    inverse_jacobian, node_coordinates, surface_flux_values, cell_ids,
+    _u, _u_t, _u_tmp2, _u_tmp3, _node_coordinates, _surface_flux_values)
 end
 
 
 # Return number of elements
-nelements(elements::ElementContainer1D) = length(elements.cell_ids)
+@inline nelements(elements::ElementContainer1D) = length(elements.cell_ids)
 
 
 # Container data structure (structure-of-arrays style) for DG interfaces
 # TODO: Taal refactor, remove NVARS, POLYDEG?
-# TODO: Taal refactor, mutable struct or resize! for AMR?
 mutable struct InterfaceContainer1D{RealT<:Real, NVARS, POLYDEG} <: AbstractContainer
   u::Array{RealT, 3}        # [leftright, variables, interfaces]
   neighbor_ids::Matrix{Int} # [leftright, interfaces]
   orientations::Vector{Int} # [interfaces]
+  # internal `resize!`able storage
+  _u::Vector{RealT}
+  _neighbor_ids::Vector{Int}
 end
 
-function Base.copy!(dst::InterfaceContainer1D, src::InterfaceContainer1D)
-  dst.u            = src.u
-  dst.neighbor_ids = src.neighbor_ids
-  dst.orientations = src.orientations
+function Base.resize!(interfaces::InterfaceContainer1D{RealT, NVARS, POLYDEG},
+                      capacity) where {RealT, NVARS, POLYDEG}
+  n_nodes = POLYDEG + 1
+  @unpack _u, _neighbor_ids, orientations = interfaces
+
+  resize!(_u, 2 * NVARS * capacity)
+  interfaces.u = unsafe_wrap(Array, pointer(_u),
+                             (2, NVARS, capacity))
+
+  resize!(_neighbor_ids, 2 * capacity)
+  interfaces.neighbor_ids = unsafe_wrap(Array, pointer(_neighbor_ids ),
+                                        (2, capacity))
+
+  resize!(orientations, capacity)
+
   return nothing
 end
 
@@ -76,24 +140,28 @@ function InterfaceContainer1D{RealT, NVARS, POLYDEG}(capacity::Integer) where {R
   nan = convert(RealT, NaN)
 
   # Initialize fields with defaults
-  u = fill(nan, 2, NVARS, capacity)
-  neighbor_ids = fill(typemin(Int), 2, capacity)
+  _u = fill(nan, 2 * NVARS * capacity)
+  u = unsafe_wrap(Array, pointer(_u),
+                  (2, NVARS, capacity))
+
+  _neighbor_ids = fill(typemin(Int), 2 * capacity)
+  neighbor_ids = unsafe_wrap(Array, pointer(_neighbor_ids ),
+                             (2, capacity))
+
   orientations = fill(typemin(Int), capacity)
 
-  interfaces = InterfaceContainer1D{RealT, NVARS, POLYDEG}(
-    u, neighbor_ids, orientations)
-
-  return interfaces
+  return InterfaceContainer1D{RealT, NVARS, POLYDEG}(
+    u, neighbor_ids, orientations,
+    _u, _neighbor_ids)
 end
 
 
 # Return number of interfaces
-ninterfaces(interfaces::InterfaceContainer1D) = length(interfaces.orientations)
+@inline ninterfaces(interfaces::InterfaceContainer1D) = length(interfaces.orientations)
 
 
 # Container data structure (structure-of-arrays style) for DG boundaries
 # TODO: Taal refactor, remove NVARS, POLYDEG?
-# TODO: Taal refactor, mutable struct or resize! for AMR?
 mutable struct BoundaryContainer1D{RealT<:Real, NVARS, POLYDEG} <: AbstractContainer
   u::Array{RealT, 3}                # [leftright, variables, boundaries]
   neighbor_ids::Vector{Int}         # [boundaries]
@@ -101,15 +169,31 @@ mutable struct BoundaryContainer1D{RealT<:Real, NVARS, POLYDEG} <: AbstractConta
   neighbor_sides::Vector{Int}       # [boundaries]
   node_coordinates::Array{RealT, 2} # [orientation, elements]
   n_boundaries_per_direction::SVector{2, Int} # [direction]
+  # internal `resize!`able storage
+  _u::Vector{RealT}
+  _node_coordinates::Vector{RealT}
 end
 
-function Base.copy!(dst::BoundaryContainer1D, src::BoundaryContainer1D)
-  dst.u                          = src.u
-  dst.neighbor_ids               = src.neighbor_ids
-  dst.orientations               = src.orientations
-  dst.neighbor_sides             = src.neighbor_sides
-  dst.node_coordinates           = src.node_coordinates
-  dst.n_boundaries_per_direction = src.n_boundaries_per_direction
+function Base.resize!(boundaries::BoundaryContainer1D{RealT, NVARS, POLYDEG},
+                      capacity) where {RealT, NVARS, POLYDEG}
+  n_nodes = POLYDEG + 1
+  @unpack _u, _node_coordinates,
+          neighbor_ids, orientations, neighbor_sides = boundaries
+
+  resize!(_u, 2 * NVARS * capacity)
+  boundaries.u = unsafe_wrap(Array, pointer(_u),
+                             (2, NVARS, capacity))
+
+  resize!(_node_coordinates, 1 * capacity)
+  boundaries.node_coordinates = unsafe_wrap(Array, pointer(_node_coordinates ),
+                                            (1, capacity))
+
+  resize!(neighbor_ids, capacity)
+
+  resize!(orientations, capacity)
+
+  resize!(neighbor_sides, capacity)
+
   return nothing
 end
 
@@ -119,18 +203,26 @@ function BoundaryContainer1D{RealT, NVARS, POLYDEG}(capacity::Integer) where {Re
   nan = convert(RealT, NaN)
 
   # Initialize fields with defaults
-  u = fill(nan, 2, NVARS, capacity)
+  _u = fill(nan, 2 * NVARS * capacity)
+  u = unsafe_wrap(Array, pointer(_u),
+                  (2, NVARS, capacity))
+
   neighbor_ids = fill(typemin(Int), capacity)
+
   orientations = fill(typemin(Int), capacity)
+
   neighbor_sides = fill(typemin(Int), capacity)
-  node_coordinates = fill(nan, 1, capacity)
+
+  _node_coordinates = fill(nan, 1 * capacity)
+  node_coordinates = unsafe_wrap(Array, pointer(_node_coordinates),
+                                 (1, capacity))
+
   n_boundaries_per_direction = SVector(0, 0)
 
-  boundaries = BoundaryContainer1D{RealT, NVARS, POLYDEG}(
+  return BoundaryContainer1D{RealT, NVARS, POLYDEG}(
     u, neighbor_ids, orientations, neighbor_sides,
-    node_coordinates, n_boundaries_per_direction)
-
-  return boundaries
+    node_coordinates, n_boundaries_per_direction,
+    _u, _node_coordinates)
 end
 
 
