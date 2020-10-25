cbb7673cd2732e5c9a518b2bb798f5f586737f56
Hendrik Ranocha <mail@ranocha.de>
2020-10-25 15:10:07 +0100

    more comments on compute_coefficients

--- src/semidiscretization.jl
+++ src/semidiscretization.jl
@@ -77,6 +77,9 @@ spectral element method ([`DGSEM`](@ref)) are the values of `func` at the
 Lobatto-Legendre nodes. Similarly, a classical finite difference method will use
 the values of `func` at the nodes of the grid assoociated with the semidiscretization
 `semi`.
+
+For semidiscretizations `semi` associated with an initial condition, `func` can be omitted
+to use the given initial condition at time `t`.
 """
 function compute_coefficients(func, t, semi::AbstractSemidiscretization)
   u_ode = allocate_coefficients(mesh_equations_solver_cache(semi)...)
