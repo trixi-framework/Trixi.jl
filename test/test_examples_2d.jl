a69376f5a462e996b66d11106a2bba10374054f3
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2020-10-27 11:34:06 +0100

    Updated docstring of calc_volume_integral! and changed volume flux of MHD rotor test

--- test/test_examples_2d.jl
+++ test/test_examples_2d.jl
@@ -326,7 +326,7 @@ end
     test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_rotor.toml"),
             l2   = [1.251062755110083, 1.8146501210703296, 1.6947702119211985, 0.0, 2.286126233832582, 0.2142656906510094, 0.23453619782492716, 0.0, 0.003028222728287231],
             linf = [10.472575582440642, 14.089807317509075, 15.561008992418031, 0.0, 16.731740428408308, 1.3219222046920664, 1.4167075971532137, 0.0, 0.08046487935486654],
-            t_end = 0.05, volume_flux = "flux_derigs_etal")
+            t_end = 0.05)
   end
   @testset "parameters_mhd_blast_wave.toml" begin
     test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_mhd_blast_wave.toml"),
