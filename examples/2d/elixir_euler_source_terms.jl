1bb8665c9a46c4815682a3bdeea720a520974d30
Michael Schlottke-Lakemper <michael@sloede.com>
2020-10-29 23:00:11 +0100

    Restore Taam CFLs

--- examples/2d/elixir_euler_source_terms.jl
+++ examples/2d/elixir_euler_source_terms.jl
@@ -31,7 +31,9 @@ ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
 
-stepsize_callback = StepsizeCallback(cfl=1.0)
+# FIXME Taal restore after Taam sync
+# stepsize_callback = StepsizeCallback(cfl=1.0)
+stepsize_callback = StepsizeCallback(cfl=0.6)
 
 save_solution = SaveSolutionCallback(interval=100,
                                      save_initial_solution=true,
