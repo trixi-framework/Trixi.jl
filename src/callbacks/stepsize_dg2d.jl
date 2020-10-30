6936040d7cad3a7c0b9e844418fd8ee88e02c651
Michael Schlottke-Lakemper <michael@sloede.com>
2020-10-30 05:59:50 +0100

    Properly break 2D stepsize calculaton only for MHD

--- src/callbacks/stepsize_dg2d.jl
+++ src/callbacks/stepsize_dg2d.jl
@@ -7,18 +7,29 @@ function max_dt(u::AbstractArray{<:Any,4}, t, mesh::TreeMesh{2},
 
   for element in eachelement(dg, cache)
     max_λ1 = max_λ2 = zero(max_scaled_speed)
-    # FIXME Taal restore after Taam sync: remove following lines once c_h is updated properly:
+    # FIXME Taal restore after Taam sync: remove following if-else once c_h is updated properly:
     if equations isa AbstractIdealGlmMhdEquations
       equations.c_h = 0.0
+      for j in nnodes(dg), i in eachnode(dg)
+        u_node = get_node_vars(u, equations, dg, i, j, element)
+        λ1, λ2 = max_abs_speeds(u_node, equations)
+        max_λ1 = max(max_λ1, λ1)
+        max_λ2 = max(max_λ2, λ2)
+      end
+    else
+      for j in eachnode(dg), i in eachnode(dg)
+        u_node = get_node_vars(u, equations, dg, i, j, element)
+        λ1, λ2 = max_abs_speeds(u_node, equations)
+        max_λ1 = max(max_λ1, λ1)
+        max_λ2 = max(max_λ2, λ2)
+      end
     end
-    # FIXME Taal restore after Taam sync
     # for j in eachnode(dg), i in eachnode(dg)
-    for j in 4, i in eachnode(dg)
-      u_node = get_node_vars(u, equations, dg, i, j, element)
-      λ1, λ2 = max_abs_speeds(u_node, equations)
-      max_λ1 = max(max_λ1, λ1)
-      max_λ2 = max(max_λ2, λ2)
-    end
+    #   u_node = get_node_vars(u, equations, dg, i, j, element)
+    #   λ1, λ2 = max_abs_speeds(u_node, equations)
+    #   max_λ1 = max(max_λ1, λ1)
+    #   max_λ2 = max(max_λ2, λ2)
+    # end
     inv_jacobian = cache.elements.inverse_jacobian[element]
     max_scaled_speed = max(max_scaled_speed, inv_jacobian * (max_λ1 + max_λ2))
   end
