a5fbff22f5118b3a2f585a7a2a30bd2eafdfa16b
Michael Schlottke-Lakemper <mschlott@math.uni-koeln.de>
2020-10-30 15:48:34 +0100

    Update failing testcase

--- test/test_examples_2d.jl
+++ test/test_examples_2d.jl
@@ -51,13 +51,13 @@ const EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "
   end
   @testset "parameters_amr.toml" begin
     test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_amr.toml"),
-            l2   = [0.011021735725622825],
+            l2   = [0.010844189678803203],
             linf = [0.0491178481591637])
   end
   @testset "parameters_amr_nonperiodic.toml" begin
     test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_amr_nonperiodic.toml"),
-            l2   = [0.008203315648075351],
-            linf = [0.04172459513670268])
+            l2   = [0.008016815805080098],
+            linf = [0.04229543866599861])
   end
   @testset "parameters_amr_vortex.toml" begin
     test_trixi_run(joinpath(EXAMPLES_DIR, "parameters_amr_vortex.toml"),
