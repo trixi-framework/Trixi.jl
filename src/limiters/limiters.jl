d86bd245cc486da1716bae2411b3339f5274f69e
Hendrik Ranocha <mail@ranocha.de>
2020-11-02 12:26:30 +0100

    add comment suggested by @gregorgassner [ci skip]

--- src/limiters/limiters.jl
+++ src/limiters/limiters.jl
@@ -9,6 +9,8 @@ The fully-discrete positivity-preserving limiter of
   [doi: 10.1098/rspa.2011.0153](https://doi.org/10.1098/rspa.2011.0153)
 The limiter is applied to all scalar `variables` in their given order
 using the associated `thresholds` to determine the minimal acceptable values.
+The order of the `variables` is important and might have a strong influence
+on the robustness.
 """
 struct PositivityPreservingLimiterZhangShu{N, Thresholds<:NTuple{N,<:Real}, Variables<:NTuple{N,Any}}
   thresholds::Thresholds
