b7b76ff3a4f34d22b8fb49bd8da48d4de8f78e91
Hendrik Ranocha <mail@ranocha.de>
2020-11-12 11:32:44 +0100

    fix merge conflicts

--- test/test_examples_2d_parallel.jl
+++ test/test_examples_2d_parallel.jl
@@ -17,76 +17,76 @@ const EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "
 # Run basic tests
 @testset "Examples 2D" begin
   @testset "elixir_advection_basic.jl" begin
-    test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
       l2   = [9.144681765639205e-6],
       linf = [6.437440532547356e-5])
   end
 
   @testset "elixir_advection_restart.jl" begin
-    test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_restart.jl"),
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_restart.jl"),
       l2   = [1.2148032444677485e-5],
       linf = [6.495644794757283e-5])
   end
 
 
   @testset "elixir_hyp_diff_llf.jl" begin
-    test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_llf.jl"),
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_llf.jl"),
       l2   = [0.00015687751816056159, 0.001025986772217084, 0.0010259867722169909],
       linf = [0.0011986956416591976, 0.006423873516411049, 0.006423873516411049])
   end
 
   @testset "elixir_hyp_diff_harmonic_nonperiodic.jl" begin
-    test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_harmonic_nonperiodic.jl"),
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_harmonic_nonperiodic.jl"),
       l2   = [8.61813235543625e-8, 5.619399844542781e-7, 5.6193998447443e-7],
       linf = [1.124861862180196e-6, 8.622436471039663e-6, 8.622436470151484e-6])
   end
 
   @testset "elixir_hyp_diff_nonperiodic.jl" begin
-    test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_nonperiodic.jl"),
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_nonperiodic.jl"),
       l2   = [8.523077653955306e-6, 2.8779323653065056e-5, 5.4549427691297846e-5],
       linf = [5.5227409524905013e-5, 0.0001454489597927185, 0.00032396328684569653])
   end
 
   @testset "elixir_hyp_diff_upwind.jl" begin
-    test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_upwind.jl"),
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_hyp_diff_upwind.jl"),
       l2   = [5.868147556385677e-6, 3.805179273239753e-5, 3.805179273248075e-5],
       linf = [3.7019654930525725e-5, 0.00021224229433514097, 0.00021224229433514097])
   end
 
 
   @testset "elixir_euler_source_terms.jl" begin
-  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_source_terms.jl"),
+  @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_source_terms.jl"),
     l2   = [8.517783186497567e-7, 1.2350199409361865e-6, 1.2350199409828616e-6, 4.277884398786315e-6],
     linf = [8.357934254688004e-6, 1.0326389653148027e-5, 1.0326389654924384e-5, 4.4961900057316484e-5])
   end
 
   @testset "elixir_euler_density_wave.jl" begin
-  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_density_wave.jl"),
+  @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_density_wave.jl"),
     l2   = [0.0010600778457965205, 0.00010600778457646603, 0.0002120155691588112, 2.6501946142012653e-5],
     linf = [0.006614198043407127, 0.0006614198043931596, 0.001322839608845383, 0.00016535495117153687],
     tspan = (0.0, 0.5))
   end
 
   @testset "elixir_euler_nonperiodic.jl" begin
-  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_nonperiodic.jl"),
+  @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_nonperiodic.jl"),
     l2   = [2.3652137675654753e-6, 2.1386731303685556e-6, 2.138673130413185e-6, 6.009920290578574e-6],
     linf = [1.4080448659026246e-5, 1.7581818010814487e-5, 1.758181801525538e-5, 5.9568540361709665e-5])
   end
 
   @testset "elixir_euler_ec.jl" begin
-  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_ec.jl"),
+  @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_ec.jl"),
     l2   = [0.061733846713578594, 0.05020086119442834, 0.05020836856347214, 0.2259064869636338],
     linf = [0.29894122391731826, 0.30853631977725215, 0.3084722538869674, 1.0652455597305965])
   end
 
   @testset "elixir_euler_vortex.jl" begin
-  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_vortex.jl"),
+  @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_vortex.jl"),
     l2   = [3.6342636871275523e-6, 0.0032111366825032443, 0.0032111479254594345, 0.004545714785045611],
     linf = [7.903587114788113e-5, 0.030561314311228993, 0.030502600162385596, 0.042876297246817074])
   end
 
   @testset "elixir_euler_vortex_shockcapturing.jl" begin
-  test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_vortex_shockcapturing.jl"),
+  @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_vortex_shockcapturing.jl"),
     l2   = [3.80342739421474e-6, 5.561118953968859e-5, 5.564042529709319e-5, 0.0001570628548096201],
     linf = [8.491382365727329e-5, 0.0009602965158113097, 0.0009669978616948516, 0.0030750353269972663])
   end
@@ -95,6 +95,6 @@ end
 # Clean up afterwards: delete Trixi output directory
 Trixi.mpi_isroot() && @test_nowarn rm(outdir, recursive=true)
 
-end # 2D
+end # Parallel 2D
 
-end #module
+end # module
