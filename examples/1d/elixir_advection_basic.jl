d2c2fa099d291f99d00a06ebea2bc27e2191d539
Michael Schlottke-Lakemper <michael@sloede.com>
2020-11-16 16:03:57 +0100

    Add more comments to basic and extended elixirs

--- examples/1d/elixir_advection_basic.jl
+++ examples/1d/elixir_advection_basic.jl
@@ -13,11 +13,13 @@ solver = DGSEM(3, flux_lax_friedrichs)
 
 coordinates_min = -1 # minimum coordinate 
 coordinates_max =  1 # maximum coordinate
+
+# Create a uniformely refined mesh with periodic boundaries
 mesh = TreeMesh(coordinates_min, coordinates_max,
                 initial_refinement_level=4,
-                n_cells_max=30_000)
+                n_cells_max=30_000) # set maximum capacity of tree data structure
 
-
+# A semidiscretization collects data structures and functions for the spatial discretization
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergence_test, solver)
 
 
@@ -27,22 +29,31 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergen
 # Create ODE problem with time span from 0.0 to 1.0
 ode = semidiscretize(semi, (0.0, 1.0));
 
+# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
+# and resets the timers
 summary_callback = SummaryCallback()
 
+# The StepsizeCallback handles the re-calculcation of the maximum Δt after each time step
 stepsize_callback = StepsizeCallback(cfl=1.6)
 
+# The SaveSolutionCallback allows to save the solution to a file in regular intervals
 save_solution = SaveSolutionCallback(interval=100,
                                      solution_variables=:primitive)
 
+# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
 analysis_callback = AnalysisCallback(semi, interval=100)
 
+# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
 callbacks = CallbackSet(summary_callback, stepsize_callback, save_solution, analysis_callback)
 
 
 ###############################################################################
 # run the simulation
 
+# OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
 sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
             dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep=false, callback=callbacks);
-summary_callback() # print the timer summary
+
+# Print the timer summary
+summary_callback()
