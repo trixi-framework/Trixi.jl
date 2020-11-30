1d5bf4b03fd01d883c68bca838045b6fbc52a1fd
Michael Schlottke-Lakemper <michael@sloede.com>
2020-11-30 16:52:12 +0100

    Fix collision callback

--- src/callbacks_step/lbm_collision_dg.jl
+++ src/callbacks_step/lbm_collision_dg.jl
@@ -4,7 +4,7 @@ function apply_collision!(collision_op, u, dt, mesh, equations, dg::DG, cache)
     for j in eachnode(dg), i in eachnode(dg)
       u_node = get_node_vars(u, equations, dg, i, j, element)
       update = collision_op(u_node, dt, equations)
-      add_to_node_vars!(u, u_node, equations, dg, i, j, element)
+      add_to_node_vars!(u, update, equations, dg, i, j, element)
     end
   end
 
