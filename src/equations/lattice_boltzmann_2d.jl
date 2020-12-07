4c312ef7900c8bf0c127e21c771f20fbfb40f645
Michael Schlottke-Lakemper <michael@sloede.com>
2020-12-07 10:20:44 +0100

    Improve documenation for LBM

--- src/equations/lattice_boltzmann_2d.jl
+++ src/equations/lattice_boltzmann_2d.jl
@@ -18,17 +18,19 @@ number, one can set the macroscopic reference velocity `u0` directly (`Ma` needs
 viscosity `nu` directly (in this case, `Re` needs to be set to `nothing`).
 
 
-The nine discrete velocity directions of the D2Q9 scheme are sorted as follows:
-
+The nine discrete velocity directions of the D2Q9 scheme are sorted as follows [4]:
 ```
-  6  2  5
-   ╲ │ ╱
-    ╲│╱
-  3──9──1
-    ╱│╲
-   ╱ │ ╲
-  7  4  8
+  6     2     5       y
+    ┌───┼───┐         │
+    │       │         │
+  3 ┼   9   ┼ 1        ──── x
+    │       │        ╱
+    └───┼───┘       ╱ 
+  7     4     8    z
 ```
+Note that usually the velocities are numbered from `0` to `8`, where `0` corresponds to the zero
+velocity. Due to Julia using 1-based indexing, here we use indices from `1` to `9`, where `1`
+through `8` correspond to the velocity directions in [4] and `9` is the zero velocity.
 
 The corresponding opposite directions are:
 * 1 ←→ 3
@@ -50,6 +52,8 @@ The main sources for the base implementation were
    Raum**, Master Thesis, University of Cologne, 2018.
 3. Dieter Hänel, **Molekulare Gasdynamik**, Springer-Verlag Berlin Heidelberg, 2004
    [doi:10.1007/3-540-35047-0](https://doi.org/10.1007/3-540-35047-0)
+4. Dieter Krüger et al., **The Lattice Boltzmann Method**, Springer International Publishing, 2017
+   [doi:10.1007/978-3-319-44649-3](https://doi.org/10.1007/978-3-319-44649-3)
 """
 struct LatticeBoltzmannEquations2D{RealT<:Real, CollisionOp} <: AbstractLatticeBoltzmannEquation{2, 9}
   c::RealT    # mean thermal molecular velocity
@@ -107,7 +111,7 @@ function LatticeBoltzmannEquations2D(; Ma, Re, collision_op=collision_bgk,
   # Promote to common data type
   Ma, Re, c, L, rho0, u0, nu = promote(Ma, Re, c, L, rho0, u0, nu)
 
-  # Source for weights and speeds: https://cims.nyu.edu/~billbao/report930.pdf
+  # Source for weights and speeds: [4] in the docstring above
   weights  = @SVector [1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36, 4/9]
   v_alpha1 = @SVector [ c,   0,  -c,   0,   c,   -c,   -c,    c,    0 ]
   v_alpha2 = @SVector [ 0,   c,   0,  -c,   c,    c,   -c,   -c,    0 ]
--- src/equations/lattice_boltzmann_3d.jl
+++ src/equations/lattice_boltzmann_3d.jl
@@ -18,28 +18,69 @@ number, one can set the macroscopic reference velocity `u0` directly (`Ma` needs
 viscosity `nu` directly (in this case, `Re` needs to be set to `nothing`).
 
 
-The twenty-seven discrete velocity directions of the D3Q27 scheme are sorted as follows:
-
-```
-  6  2  5
-   ╲ │ ╱
-    ╲│╱
-  3──9──1
-    ╱│╲
-   ╱ │ ╲
-  7  4  8
-```
+The twenty-seven discrete velocity directions of the D3Q27 scheme are sorted as follows [4]:
+* plane at `z = -1`:
+  ```
+    24    17     21       y
+       ┌───┼───┐          │
+       │       │          │
+    10 ┼   6   ┼ 15        ──── x
+       │       │         ╱
+       └───┼───┘        ╱ 
+    20    12     26    z           
+  ```
+* plane at `z = 0`:
+  ```
+    14     3     7        y
+       ┌───┼───┐          │
+       │       │          │
+     2 ┼  27   ┼ 1         ──── x
+       │       │         ╱
+       └───┼───┘        ╱ 
+     8     4     13    z
+  ```
+* plane at `z = +1`:
+  ```
+    25    11     19       y
+       ┌───┼───┐          │
+       │       │          │
+    16 ┼   5   ┼ 9         ──── x
+       │       │         ╱
+       └───┼───┘        ╱ 
+    22    18     23    z
+  ```
+Note that usually the velocities are numbered from `0` to `26`, where `0` corresponds to the zero
+velocity. Due to Julia using 1-based indexing, here we use indices from `1` to `27`, where `1`
+through `26` correspond to the velocity directions in [4] and `27` is the zero velocity.
 
 The corresponding opposite directions are:
-* 1 ←→ 3
-* 2 ←→ 4
-* 3 ←→ 1
-* 4 ←→ 2
-* 5 ←→ 7
-* 6 ←→ 8
-* 7 ←→ 5
-* 8 ←→ 6
-* 9 ←→ 9
+*  1 ←→   2
+*  2 ←→   1
+*  3 ←→   4
+*  4 ←→   3
+*  5 ←→   6
+*  6 ←→   5
+*  7 ←→   8
+*  8 ←→   7
+*  9 ←→  10
+* 10 ←→   9
+* 11 ←→  12
+* 12 ←→  11
+* 13 ←→  14
+* 14 ←→  13
+* 15 ←→  16
+* 16 ←→  15
+* 17 ←→  18
+* 18 ←→  17
+* 19 ←→  20
+* 20 ←→  19
+* 21 ←→  22
+* 22 ←→  21
+* 23 ←→  24
+* 24 ←→  23
+* 25 ←→  26
+* 26 ←→  25
+* 27 ←→  27
 
 The main sources for the base implementation were
 1. Misun Min, Taehun Lee, **A spectral-element discontinuous Galerkin lattice Boltzmann method for
@@ -50,6 +91,8 @@ The main sources for the base implementation were
    Raum**, Master Thesis, University of Cologne, 2018.
 3. Dieter Hänel, **Molekulare Gasdynamik**, Springer-Verlag Berlin Heidelberg, 2004
    [doi:10.1007/3-540-35047-0](https://doi.org/10.1007/3-540-35047-0)
+4. Dieter Krüger et al., **The Lattice Boltzmann Method**, Springer International Publishing, 2017
+   [doi:10.1007/978-3-319-44649-3](https://doi.org/10.1007/978-3-319-44649-3)
 """
 struct LatticeBoltzmannEquations3D{RealT<:Real, CollisionOp} <: AbstractLatticeBoltzmannEquation{3, 27}
   c::RealT    # mean thermal molecular velocity
@@ -108,7 +151,7 @@ function LatticeBoltzmannEquations3D(; Ma, Re, collision_op=collision_bgk,
   # Promote to common data type
   Ma, Re, c, L, rho0, u0, nu = promote(Ma, Re, c, L, rho0, u0, nu)
 
-  # Source for weights and speeds: https://cims.nyu.edu/~billbao/report930.pdf
+  # Source for weights and speeds: [4] in docstring above
   weights  = @SVector [1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36, 4/9]
   v_alpha1 = @SVector [ c,   0,  -c,   0,   c,   -c,   -c,    c,    0 ]
   v_alpha2 = @SVector [ 0,   c,   0,  -c,   c,    c,   -c,   -c,    0 ]
