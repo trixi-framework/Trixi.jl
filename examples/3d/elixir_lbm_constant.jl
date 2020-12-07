e110b8a2ce31f970c1bdddf6ed6f1062d803ea69
Michael Schlottke-Lakemper <michael@sloede.com>
2020-12-07 14:40:59 +0100

    Add free-stream preservation elixir + test for LBM 3D

---
+++ examples/3d/elixir_lbm_constant.jl
@@ -0,0 +1,63 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+###############################################################################
+# semidiscretization of the Lattice-Boltzmann equations for the D2Q9 scheme
+
+equations = LatticeBoltzmannEquations3D(Ma=0.1, Re=Inf)
+
+initial_condition = initial_condition_constant
+
+surface_flux = flux_lax_friedrichs
+solver = DGSEM(3, surface_flux)
+
+coordinates_min = (-1, -1, -1)
+coordinates_max = ( 1,  1,  1)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=1,
+                n_cells_max=10_000,)
+
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver)
+
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 1.0)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 10
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+
+save_restart = SaveRestartCallback(interval=100,
+                                   save_final_restart=true)
+
+save_solution = SaveSolutionCallback(interval=100,
+                                     save_initial_solution=true,
+                                     save_final_solution=true,
+                                     solution_variables=cons2macroscopic)
+
+stepsize_callback = StepsizeCallback(cfl=1.0)
+
+collision_callback = LBMCollisionCallback()
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback, alive_callback, 
+                        save_restart, save_solution,
+                        stepsize_callback,
+                        collision_callback)
+
+
+###############################################################################
+# run the simulation
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep=false, callback=callbacks, maxiters=1e5);
+summary_callback() # print the timer summary
