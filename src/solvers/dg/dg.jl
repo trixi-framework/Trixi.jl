34435b793f7ee008155453818d44def13cb696fd
Hendrik Ranocha <mail@ranocha.de>
2020-12-08 06:29:05 +0100

    fix type instability of get_node_vars for more than ten variables

--- src/solvers/dg/dg.jl
+++ src/solvers/dg/dg.jl
@@ -172,7 +172,12 @@ end
 end
 
 @inline function get_node_vars(u, equations, solver::DG, indices...)
-  SVector(ntuple(v -> u[v, indices...], nvariables(equations)))
+  # There is a cut-off at `n == 10` inside of the method
+  # `ntuple(f::F, n::Integer) where F` in Base at ntuple.jl:17
+  # in Julia `v1.5`, making leading to type instabilities if
+  # more than ten variables are used. That's why we use
+  # `Val(...)` below.
+  SVector(ntuple(v -> u[v, indices...], Val(nvariables(equations))))
 end
 
 @inline function get_surface_node_vars(u, equations, solver::DG, indices...)
