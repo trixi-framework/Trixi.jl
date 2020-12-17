108b7901eef4b0ba741ada50c2ffd153bd250e31
Michael Schlottke-Lakemper <michael@sloede.com>
2020-12-17 07:16:07 +0100

    Clean up files

---
+++ src/visualization/convert.jl
@@ -0,0 +1,89 @@
+function get_data_2d(center_level_0, length_level_0, leaf_cells, coordinates, levels, ndims,
+                     unstructured_data, n_nodes,
+                     grid_lines=false, max_supported_level=11, nvisnodes=nothing,
+                     slice_axis=:z, slice_axis_intercept=0)
+  # Determine resolution for data interpolation
+  max_level = maximum(levels)
+  if max_level > max_supported_level
+    error("Maximum refinement level in data file $max_level is higher than " *
+          "maximum supported level $max_supported_level")
+  end
+  max_available_nodes_per_finest_element = 2^(max_supported_level - max_level)
+  if nvisnodes === nothing
+    max_nvisnodes = 2 * n_nodes
+  elseif nvisnodes == 0
+    max_nvisnodes = n_nodes
+  else
+    max_nvisnodes = nvisnodes
+  end
+  nvisnodes_at_max_level = min(max_available_nodes_per_finest_element, max_nvisnodes)
+  resolution = nvisnodes_at_max_level * 2^max_level
+  nvisnodes_per_level = [2^(max_level - level)*nvisnodes_at_max_level for level in 0:max_level]
+  # nvisnodes_per_level is an array (accessed by "level + 1" to accommodate
+  # level-0-cell) that contains the number of visualization nodes for any
+  # refinement level to visualize on an equidistant grid
+
+  if ndims == 3
+    (unstructured_data, coordinates, levels,
+        center_level_0) = unstructured_2d_to_3d(unstructured_data,
+        coordinates, levels, length_level_0, center_level_0, slice_axis,
+        slice_axis_intercept)
+  end
+
+  # Normalize element coordinates: move center to (0, 0) and domain size to [-1, 1]²
+  n_elements = length(levels)
+  normalized_coordinates = similar(coordinates)
+  for element_id in 1:n_elements
+    @views normalized_coordinates[:, element_id] .= (
+          (coordinates[:, element_id] .- center_level_0) ./ (length_level_0 / 2 ))
+  end
+
+  # Interpolate unstructured DG data to structured data
+  (structured_data =
+      unstructured2structured(unstructured_data, normalized_coordinates,
+                              levels, resolution, nvisnodes_per_level))
+
+  # Interpolate cell-centered values to node-centered values
+  node_centered_data = cell2node(structured_data)
+
+  # Determine axis coordinates for contour plot
+  xs = collect(range(-1, 1, length=resolution+1)) .* length_level_0/2 .+ center_level_0[1]
+  ys = collect(range(-1, 1, length=resolution+1)) .* length_level_0/2 .+ center_level_0[2]
+
+  # Determine element vertices to plot grid lines
+  if grid_lines
+    mesh_vertices_x, mesh_vertices_y = calc_vertices(coordinates, levels, length_level_0)
+  else
+    mesh_vertices_x = mesh_vertices_y = nothing
+  end
+
+  return xs, ys, node_centered_data, mesh_vertices_x, mesh_vertices_y
+end
+
+function get_unstructured_data(u, semi, solution_variables)
+  mesh, equations, solver, cache = mesh_equations_solver_cache(semi)
+
+  if solution_variables === cons2cons
+    raw_data = u
+    n_vars = size(raw_data, 1)
+  else
+    # Reinterpret the solution array as an array of conservative variables,
+    # compute the solution variables via broadcasting, and reinterpret the
+    # result as a plain array of floating point numbers
+    raw_data = Array(reinterpret(eltype(u),
+           solution_variables.(reinterpret(SVector{nvariables(equations),eltype(u)}, u),
+                      Ref(equations))))
+    n_vars = size(raw_data, 1)
+  end
+
+  unstructured_data = Array{Float64}(undef,
+                                     ntuple((d) -> nnodes(solver), ndims(equations))...,
+                                     nelements(solver, cache), n_vars)
+  for variable in 1:n_vars
+    @views unstructured_data[.., :, variable] .= raw_data[variable, .., :]
+  end
+
+  return unstructured_data
+end
+
+
