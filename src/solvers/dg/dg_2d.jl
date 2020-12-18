2bf83b6f4f534d82ac2bfd85c8d70367b66eebf0
Hendrik Ranocha <mail@ranocha.de>
2020-12-18 19:39:04 +0100

    add additional comments on mul! with true as argument [skip ci]

--- src/solvers/dg/dg_2d.jl
+++ src/solvers/dg/dg_2d.jl
@@ -1008,6 +1008,9 @@ end
     # surface_flux_values[v, :, direction, large_element] .=
     #   (mortar_l2.reverse_upper * fstar_upper[v, :] + mortar_l2.reverse_lower * fstar_lower[v, :])
     # but faster and does not allocate.
+    # Note that `true * some_float == some_float` in Julia, i.e. `true` acts as
+    # a universal `one`. Hence, the second `mul!` means "add the matrix-vector
+    # product to the current value of the destination".
     @views mul!(surface_flux_values[v, :, direction, large_element],
                 mortar_l2.reverse_upper, fstar_upper[v, :])
     @views mul!(surface_flux_values[v, :, direction, large_element],
