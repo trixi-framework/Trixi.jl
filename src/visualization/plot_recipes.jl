78c768aa9095271779d2c950ce7777edcf8d8b9d
Michael Schlottke-Lakemper <michael@sloede.com>
2020-12-19 07:40:05 +0100

    Add more documentation

--- src/visualization/plot_recipes.jl
+++ src/visualization/plot_recipes.jl
@@ -1,3 +1,17 @@
+# Convenience type to allow dispatch on ODESolution objects that were created by Trixi
+#
+# Note: This is an experimental feature and may be changed in future releases without notice.
+const TrixiODESolution = ODESolution{T, N, uType, uType2, DType, tType, rateType, P} where
+    {T, N, uType, uType2, DType, tType, rateType, P<:ODEProblem{uType_, tType_, isinplace, P_} where
+     {uType_, tType_, isinplace, P_<:AbstractSemidiscretization}}
+
+
+"""
+    PlotData2D
+
+Holds all relevant data for creating 2D plots of multiple solution variables and to visualize the
+mesh.
+"""
 struct PlotData2D{Coordinates, Data, VariableNames, Vertices}
   x::Coordinates
   y::Coordinates
@@ -7,14 +21,50 @@ struct PlotData2D{Coordinates, Data, VariableNames, Vertices}
   mesh_vertices_y::Vertices
 end
 
+
 """
     PlotData2D(u, semi;
+               solution_variables=cons2cons,
                grid_lines=true, max_supported_level=11, nvisnodes=nothing,
-               slice_axis=:z, slice_axis_intercept=0, solution_variables=cons2cons)
+               slice_axis=:z, slice_axis_intercept=0)
+
+Create a new `PlotData2D` object that can be used for visualizing 2D/3D DGSEM solution data array
+`u` with `Plots.jl`. All relevant geometrical information is extracted from the semidiscretization
+`semi`. By default, the conservative variables from the solution are used for plotting. This can be
+changed by passing an appropriate conversion function to `solution_variables`.
+
+If `grid_lines` is `true`, also extract grid vertices for visualizing the mesh. The output
+resolution is indirectly set via `max_supported_level`: all data is interpolated to
+`2^max_supported_level` uniformly distributed points in each spatial direction, also setting the
+maximum allowed refinement level in the solution. `nvisnodes` specifies the number of visualization
+nodes to be used. If it is `nothing`, twice the number of solution DG nodes are used for
+visualization, and if set to `0`, exactly the number of nodes in the DG elements are used.
+
+When visualizing data from a three-dimensional simulation, a 2D slice is extracted for plotting.
+`slice_axis` specifies the axis orthogonal to the slice plane and may be `:x`, `:y`, or `:z`. The
+point on the slice axis where it intersects with the slice plane is given in `slice_axis_intercept`.
+Both of these values are ignored when visualizing 2D data.
+
+# Examples
+```julia
+julia> using Trixi, Plots
+
+julia> trixi_include(default_example())
+[...]
+
+julia> pd = PlotData2D(sol)
+PlotData2D(...)
+
+julia> plot(pd) # To plot all available variables
+
+julia> plot(pd["scalar"]) # To plot only a single variable
+
+julia> plot!(pd["mesh"]) # To add mesh lines to the plot
 """
 function PlotData2D(u, semi;
+                    solution_variables=cons2cons,
                     grid_lines=true, max_supported_level=11, nvisnodes=nothing,
-                    slice_axis=:z, slice_axis_intercept=0, solution_variables=cons2cons)
+                    slice_axis=:z, slice_axis_intercept=0)
   mesh, equations, solver, _ = mesh_equations_solver_cache(semi)
   @assert ndims(mesh) in (2, 3) "unsupported number of dimensions $ndims (must be 2 or 3)"
 
@@ -37,30 +87,57 @@ function PlotData2D(u, semi;
   return PlotData2D(x, y, data, variable_names, mesh_vertices_x, mesh_vertices_y)
 end
 
+"""
+    PlotData2D(u_ode::AbstractVector, semi; kwargs...)
+
+Create a `PlotData2D` object from a one-dimensional ODE solution `u_ode` and the semidiscretization
+`semi`.
+"""
 PlotData2D(u_ode::AbstractVector, semi; kwargs...) = PlotData2D(wrap_array(u_ode, semi), semi; kwargs...)
+
+"""
+    PlotData2D(sol::TrixiODESolution; kwargs...)
+
+Create a `PlotData2D` object from an `ODESolution` created by Trixi.
+"""
 PlotData2D(sol::TrixiODESolution; kwargs...) = PlotData2D(sol.u[end], sol.prob.p; kwargs...)
 
-Base.getindex(pd::PlotData2D, variable_name) = PlotDataSeries2D(pd, variable_name)
+function Base.getindex(pd::PlotData2D, variable_name)
+  if variable_name == "mesh"
+    variable_id = 0
+  else
+    variable_id = findfirst(isequal(variable_name), pd.variable_names)
+    if isnothing(variable_id)
+      throw(KeyError(variable_name))
+    end
+  end
 
+  PlotDataSeries2D(pd, variable_name, variable_id)
+end
+
+function Base.show(io::IO, pd::PlotData2D)
+  print(io, "PlotData2D(...)")
+end
+ 
 
 # Auxiliary data structure for visualizing a single variable
+#
+# Note: This is an experimental feature and may be changed in future releases without notice.
 struct PlotDataSeries2D{PD<:PlotData2D}
   plot_data::PD
-  variable::String
+  variable_name::String
+  variable_id::Int
 end
 
 
-# Visualize a single variable in a 2D plot
+# Visualize a single variable in a 2D plot (default: heatmap)
+#
+# Note: This is an experimental feature and may be changed in future releases without notice.
 @recipe function f(pds::PlotDataSeries2D)
   pd = pds.plot_data
-  variable = pds.variable
+  variable_name = pds.variable_name
+  variable_id = pds.variable_id
 
-  if variable != "mesh"
-    variable_id = findfirst(isequal(variable), pd.variable_names)
-    if isnothing(variable_id)
-      error("variable '$variable' was not found in data set (existing: $(join(pd.variable_names, ", ")))")
-    end
-  end
 
   xlims --> (pd.x[begin], pd.x[end])
   ylims --> (pd.y[begin], pd.y[end])
@@ -68,7 +145,7 @@ end
   legend -->  :none
 
   # Add data series
-  if pds.variable == "mesh"
+  if pds.variable_name == "mesh"
     @series begin
       seriestype := :path
       linecolor := :black
@@ -76,7 +153,7 @@ end
       pd.mesh_vertices_x, pd.mesh_vertices_y
     end
   else
-    title --> pds.variable
+    title --> pds.variable_name
     colorbar --> :true
 
     @series begin
@@ -92,6 +169,8 @@ end
 
 
 # Plot all available variables at once for convenience
+#
+# Note: This is an experimental feature and may be changed in future releases without notice.
 @recipe function f(pd::PlotData2D)
   # Create layout that is as square as possible, with a preference for more columns than rows if not
   cols = ceil(Int, sqrt(length(pd.variable_names)))
@@ -99,10 +178,10 @@ end
   layout := (rows, cols)
 
   # Plot all existing variables
-  for (i, variable) in enumerate(pd.variable_names)
+  for (i, variable_name) in enumerate(pd.variable_names)
     @series begin
       subplot := i
-      pd[variable]
+      pd[variable_name]
     end
   end
 
@@ -120,13 +199,16 @@ end
 
 
 # Create a PlotData2D plot directly from an ODESolution for convenience
+#
+# Note: This is an experimental feature and may be changed in future releases without notice.
 @recipe function f(sol::TrixiODESolution;
+                   solution_variables=cons2cons,
                    grid_lines=true, max_supported_level=11, nvisnodes=nothing, slice_axis=:z,
-                   slice_axis_intercept=0, solution_variables=cons2cons)
+                   slice_axis_intercept=0)
   return PlotData2D(sol;
+                    solution_variables=solution_variables,
                     grid_lines=grid_lines, max_supported_level=max_supported_level,
                     nvisnodes=nvisnodes, slice_axis=slice_axis,
-                    slice_axis_intercept=slice_axis_intercept,
-                    solution_variables=solution_variables)
+                    slice_axis_intercept=slice_axis_intercept)
 end
 
