feac1dcb6d1542ff108b0ad9a1b234bd6b2e3f9c
Michael Schlottke-Lakemper <michael@sloede.com>
2020-12-22 12:41:28 +0100

    Allow customization of in-situ plotting

--- examples/2d/elixir_euler_blast_wave_amr.jl
+++ examples/2d/elixir_euler_blast_wave_amr.jl
@@ -50,6 +50,8 @@ save_solution = SaveSolutionCallback(interval=100,
                                      save_final_solution=true,
                                      solution_variables=cons2prim)
 
+visualization = VisualizationCallback(interval=100,  variable_names="rho", show_mesh=true)
+
 amr_indicator = IndicatorHennemannGassner(semi,
                                           alpha_max=0.5,
                                           alpha_min=0.001,
@@ -67,7 +69,7 @@ stepsize_callback = StepsizeCallback(cfl=0.9)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback, alive_callback, 
-                        save_solution,
+                        save_solution, visualization,
                         amr_callback, stepsize_callback)
 
 
--- src/callbacks_step/visualization.jl
+++ src/callbacks_step/visualization.jl
@@ -1,18 +1,24 @@
 
-mutable struct VisualizationCallback{SolutionVariables, PlotType}
+mutable struct VisualizationCallback{SolutionVariables, VariableNames, PlotDataCreator}
   interval::Int
+  plot_arguments::Dict{Symbol,Any}
   solution_variables::SolutionVariables
-  plot_type::PlotType
+  variable_names::VariableNames
+  plot_data_creator::PlotDataCreator
+  show_mesh::Bool
 end
 
 
 function Base.show(io::IO, cb::DiscreteCallback{Condition,Affect!}) where {Condition, Affect!<:VisualizationCallback}
   visualization_callback = cb.affect!
-  @unpack interval, solution_variables, plot_type = visualization_callback
+  @unpack interval, plot_arguments, solution_variables, variable_names, plot_data_creator, show_mesh = visualization_callback
   print(io, "VisualizationCallback(",
             "interval=", interval, ", ",
+            "plot_arguments=", plot_arguments, ", ",
             "solution_variables=", solution_variables, ", ",
-            "plot_type=", plot_type, ")")
+            "variable_names=", variable_names, ", ",
+            "plot_data_creator=", plot_data_creator, ", ",
+            "show_mesh=", show_mesh, ")")
 end
 
 function Base.show(io::IO, ::MIME"text/plain", cb::DiscreteCallback{Condition,Affect!}) where {Condition, Affect!<:VisualizationCallback}
@@ -23,7 +29,11 @@ function Base.show(io::IO, ::MIME"text/plain", cb::DiscreteCallback{Condition,Af
 
     setup = [
              "interval" => visualization_callback.interval,
+             "plot arguments" => visualization_callback.plot_arguments,
              "solution variables" => visualization_callback.solution_variables,
+             "variable names" => visualization_callback.variable_names,
+             "plot data creator" => visualization_callback.plot_data_creator,
+             "show mesh" => visualization_callback.show_mesh,
             ]
     summary_box(io, "VisualizationCallback", setup)
   end
@@ -32,10 +42,15 @@ end
 
 function VisualizationCallback(; interval=0,
                                  solution_variables=cons2prim,
-                                 plot_type=PlotData2D)
+                                 variable_names=[],
+                                 plot_data_creator=PlotData2D,
+                                 show_mesh=false,
+                                 plot_arguments...)
   mpi_isparallel() && error("this callback not work in parallel yet")
 
-  visualization_callback = VisualizationCallback(interval, solution_variables, plot_type)
+  visualization_callback = VisualizationCallback(interval, Dict{Symbol,Any}(plot_arguments),
+                                                 solution_variables, variable_names,
+                                                 plot_data_creator, show_mesh)
 
   DiscreteCallback(visualization_callback, visualization_callback, # the first one is the condition, the second the affect!
                    save_positions=(false,false),
@@ -64,14 +79,41 @@ end
 function (visualization_callback::VisualizationCallback)(integrator)
   u_ode = integrator.u
   semi = integrator.p
+  @unpack plot_arguments, solution_variables, variable_names, plot_data_creator, show_mesh = visualization_callback
 
   # Extract plot data
-  plot_data = visualization_callback.plot_type(u_ode, semi)
+  plot_data = plot_data_creator(u_ode, semi, solution_variables=solution_variables)
+
+  # If variable names were not specified, plot everything
+  if isempty(variable_names)
+    variable_names = String[keys(plot_data)...]
+  end
 
   # Create plot
-  display(plot(plot_data))
+  create_plot(plot_data, variable_names, show_mesh, plot_arguments)
 
   # avoid re-evaluating possible FSAL stages
   u_modified!(integrator, false)
   return nothing
 end
+
+
+function create_plot(plot_data, variable_names::Vector{String}, show_mesh, plot_arguments)
+  # Gather subplots
+  plots = []
+  for v in variable_names
+    push!(plots, plot(plot_data[v], plot_arguments...))
+  end
+  if show_mesh
+    push!(plots, plot(getmesh(plot_data), plot_arguments...))
+  end
+
+  # Determine layout
+  cols = ceil(Int, sqrt(length(plots)))
+  rows = ceil(Int, length(plots)/cols)
+  layout = (rows, cols)
+
+  # Show plot
+  display(plot(plots..., layout=layout))
+end
+create_plot(plot_data, variable_name::String, args...) = create_plot(plot_data, [variable_name], args...)
