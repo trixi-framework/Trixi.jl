fc6d930bc60cbafb45840899b20a17e2bda92c89
Felipe <loko1@DESKTOP-MP2S0TM>
2021-01-18 11:59:07 +0100

    ,

--- test/test_examples_2d_parallel.jl
+++ test/test_examples_2d_parallel.jl
@@ -37,7 +37,7 @@ const EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "
       linf = [0.005591345460903119])
   end
 
-  @testset "elixir_advection_amr_coarsen_once.jl" begin
+  @testset "elixir_advection_amr_coarsen_twice.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_amr_coarsen_once.jl"),
       l2   = [0.01705757912209016],
       linf = [0.17326485913649792])
