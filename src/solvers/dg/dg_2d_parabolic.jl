9e996770ef0da66dfb6bbd76d8a97b668e1501d4
Michael Schlottke-Lakemper <michael@sloede.com>
2021-01-18 18:23:50 +0100

    Implement mortar fluxes

--- src/solvers/dg/dg_2d_parabolic.jl
+++ src/solvers/dg/dg_2d_parabolic.jl
@@ -26,12 +26,13 @@ function rhs!(du::AbstractArray{<:Any,4}, u, gradients, t,
   @timeit_debug timer() "boundary flux" calc_boundary_flux!(cache, t, boundary_conditions, equations, dg)
 
   # Prolong solution to mortars
-  @timeit_debug timer() "prolong2mortars" prolong2mortars!(cache, u, equations, dg.mortar, dg)
+  @timeit_debug timer() "prolong2mortars" prolong2mortars!(cache, cache_gradients, u, gradients,
+                                                           equations, dg.mortar, dg)
 
   # Calculate mortar fluxes
   @timeit_debug timer() "mortar flux" calc_mortar_flux!(cache.elements.surface_flux_values,
                                                         have_nonconservative_terms(equations), equations,
-                                                        dg.mortar, dg, cache)
+                                                        dg.mortar, dg, cache, cache_gradients)
 
   # Calculate surface integrals
   @timeit_debug timer() "surface integral" calc_surface_integral!(du, equations, dg, cache)
@@ -77,7 +78,8 @@ function calc_volume_integral!(du::AbstractArray{<:Any,4}, u, gradients,
 end
 
 
-function prolong2interfaces!(cache, cache_gradients, u::AbstractArray{<:Any,4}, gradients, equations, dg::DG)
+function prolong2interfaces!(cache, cache_gradients, u::AbstractArray{<:Any,4}, gradients,
+                             equations, dg::DG)
   prolong2interfaces!(cache, u, equations, dg)
   prolong2interfaces!(cache_gradients[1], gradients[1], equations, dg)
   prolong2interfaces!(cache_gradients[2], gradients[2], equations, dg)
@@ -120,6 +122,67 @@ function calc_interface_flux!(surface_flux_values::AbstractArray{<:Any,4},
         surface_flux_values[v, i, right_direction, right_id] = flux[v]
       end
     end
+  end
+
+  return nothing
+end
+
+
+function prolong2mortars!(cache, cache_gradients, u::AbstractArray{<:Any,4}, gradients,
+                          equations, mortar_l2::LobattoLegendreMortarL2, dg::DGSEM)
+  prolong2mortars!(cache, u, equations, mortar_l2, dg)
+  prolong2mortars!(cache_gradients[1], gradients[1], equations, mortar_l2, dg)
+  prolong2mortars!(cache_gradients[2], gradients[2], equations, mortar_l2, dg)
+
+  return nothing
+end
+
+
+function calc_mortar_flux!(surface_flux_values::AbstractArray{<:Any,4},
+                           nonconservative_terms::Val{false}, equations,
+                           mortar_l2::LobattoLegendreMortarL2, dg::DG, cache, cache_gradients)
+  @unpack neighbor_ids, u_lower, u_upper, orientations = cache.mortars
+  @unpack fstar_upper_threaded, fstar_lower_threaded = cache
+  gradients_x_upper = cache_gradients[1].mortars.u_upper
+  gradients_x_lower = cache_gradients[1].mortars.u_lower
+  gradients_y_upper = cache_gradients[2].mortars.u_upper
+  gradients_y_lower = cache_gradients[2].mortars.u_lower
+  gradients_upper = (gradients_x_upper, gradients_y_upper)
+  gradients_lower = (gradients_x_lower, gradients_y_lower)
+
+  Threads.@threads for mortar in eachmortar(dg, cache)
+    # Choose thread-specific pre-allocated container
+    fstar_upper = fstar_upper_threaded[Threads.threadid()]
+    fstar_lower = fstar_lower_threaded[Threads.threadid()]
+
+    # Calculate fluxes
+    orientation = orientations[mortar]
+    calc_fstar!(fstar_upper, equations, dg, u_upper, gradients_upper, mortar, orientation)
+    calc_fstar!(fstar_lower, equations, dg, u_lower, gradients_lower, mortar, orientation)
+
+    mortar_fluxes_to_elements!(surface_flux_values, equations, mortar_l2, dg, cache,
+                               mortar, fstar_upper, fstar_lower)
+  end
+
+  return nothing
+end
+
+
+@inline function calc_fstar!(destination::AbstractArray{<:Any,2}, equations, dg::DGSEM,
+                             u_interfaces, gradients_interfaces, interface, orientation)
+  @unpack surface_flux = dg
+
+  for i in eachnode(dg)
+    # Call pointwise two-point numerical flux function
+    u_ll, u_rr = get_surface_node_vars(u_interfaces, equations, dg, i, interface)
+    gradients_x_ll, gradients_x_rr = get_surface_node_vars(gradients_interfaces[1], equations, dg, i, interface)
+    gradients_y_ll, gradients_y_rr = get_surface_node_vars(gradients_interfaces[2], equations, dg, i, interface)
+    gradients_ll = (gradients_x_ll, gradients_y_ll)
+    gradients_rr = (gradients_x_rr, gradients_y_rr)
+    flux = surface_flux(u_ll, u_rr, gradients_x_ll, gradients_y_ll, orientation, equations)
+
+    # Copy flux to left and right element storage
+    set_node_vars!(destination, flux, equations, dg, i)
   end
 
   return nothing
