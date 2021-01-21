8de50a2f4ff7930d9758c6c89722ef4b8a18ba4a
Felipe <loko1@DESKTOP-MP2S0TM>
2021-01-21 10:25:34 +0100

    initial condition cos

--- src/equations/linear_scalar_advection_2d.jl
+++ src/equations/linear_scalar_advection_2d.jl
@@ -66,6 +66,20 @@ function initial_condition_convergence_test(x, t, equation::LinearScalarAdvectio
 end
 
 
+#A smooth cosine initial condition similarly shaped like the Gaussian one.
+function initial_condition_cos(x, t, equation::LinearScalarAdvectionEquation2D)
+  # Store translated coordinate for easy use of exact solution
+  x_trans = x_trans_periodic_2d(x - equation.advectionvelocity * t)
+  radius = 1.85
+  x_norm = sqrt((x_trans[1])^2+(x_trans[2])^2)
+  if x_norm < radius
+    scalar = 0.5+cos(pi/radius*x_norm)/2
+  else
+    scalar = 0
+  end
+  return @SVector [scalar]
+end
+
 """
     initial_condition_gauss(x, t, equation::LinearScalarAdvectionEquation2D)
 
