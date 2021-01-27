bbf6dc89d57e688dad3a7f5aebeb9685386c89e3
Gregor Gassner <ggassner@uni-koeln.de>
2021-01-27 21:33:26 +0100

    add hllc flux, first assessment of EOC

--- examples/2d/elixir_euler_source_terms.jl
+++ examples/2d/elixir_euler_source_terms.jl
@@ -10,7 +10,12 @@ equations = CompressibleEulerEquations2D(1.4)
 initial_condition = initial_condition_convergence_test
 
 surface_flux = flux_lax_friedrichs
-solver = DGSEM(3, surface_flux)
+#surface_flux = flux_central
+                                                 
+#solver = DGSEM(3, surface_flux)
+basis = LobattoLegendreBasis(3)
+volume_integral = VolumeIntegralPureLGLFiniteVolume(flux_hllc)
+solver = DGSEM(basis, surface_flux, volume_integral)
 
 coordinates_min = (0, 0)
 coordinates_max = (2, 2)
@@ -41,7 +46,7 @@ save_solution = SaveSolutionCallback(interval=100,
                                      save_final_solution=true,
                                      solution_variables=cons2prim)
 
-stepsize_callback = StepsizeCallback(cfl=1.0)
+stepsize_callback = StepsizeCallback(cfl=0.5)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback, alive_callback, 
