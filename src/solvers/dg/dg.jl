6a676d92a7d2dc925aef281908ad6d0e73f52df1
gregorgassner <ggassner@uni-koeln.de>
2021-02-02 17:13:11 +0100

    Update src/solvers/dg/dg.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/solvers/dg/dg.jl
+++ src/solvers/dg/dg.jl
@@ -97,6 +97,8 @@ A volume integral, that only uses the subcell FV scheme from the paper
   "A provably entropy stable subcell shock capturing approach for high order split form DG"
   [arXiv: 2008.12044](https://arxiv.org/abs/2008.12044)
 This gives a formally O1 accurate FV scheme on LGL type subcell mesh
+!!! warning "Experimental implementation"
+    This is an experimental feature and may change in future releases.
 """
 struct VolumeIntegralPureLGLFiniteVolume{VolumeFluxFV} <: AbstractVolumeIntegral
   volume_flux_fv::VolumeFluxFV # non-symmetric in general, e.g. entropy-dissipative
