4214457528a6d151f43a5e6b982fbf8330831d8b
Hendrik Ranocha <mail@ranocha.de>
2021-02-03 15:28:22 +0100

    re-export StaticArrays.SVector and EllipsisNotation's ..; closes #431

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -20,18 +20,22 @@ module Trixi
 using LinearAlgebra: dot, mul!
 using Printf: @printf, @sprintf, println
 
+# import @reexport now to make it available for further imports/exports
+using Reexport: @reexport
+
 import DiffEqBase: ODEProblem, ODESolution, ODEFunction,
                    get_du, get_tmp_cache, u_modified!,
                    get_proposed_dt, set_proposed_dt!, terminate!
 using DiffEqCallbacks: CallbackSet, DiscreteCallback
-using EllipsisNotation # ..
+@reexport using EllipsisNotation # ..
 using HDF5: h5open, attributes
 using LinearMaps: LinearMap
 import MPI
 using OffsetArrays: OffsetArray, OffsetVector
 using RecipesBase
 using Requires
-using StaticArrays: @SVector, MVector, MArray, SVector, SMatrix
+@reexport using StaticArrays: SVector
+using StaticArrays: @SVector, MVector, MArray, SMatrix
 using TimerOutputs: @notimeit, @timeit_debug, TimerOutput, print_timer, reset_timer!
 using UnPack: @unpack, @pack!
 
@@ -110,7 +114,7 @@ export initial_condition_eoc_test_coupled_euler_gravity, source_terms_eoc_test_c
 export initial_condition_lid_driven_cavity, boundary_condition_lid_driven_cavity
 export initial_condition_couette_steady, initial_condition_couette_unsteady, boundary_condition_couette
 
-export cons2cons, cons2prim, cons2macroscopic
+export cons2cons, cons2prim, cons2macroscopic, prim2cons
 export density, pressure, density_pressure, velocity
 export entropy, energy_total, energy_kinetic, energy_internal, energy_magnetic, cross_helicity
 
