66597456352da442dba33b945781a3ff29f6f551
Hendrik Ranocha <mail@ranocha.de>
2021-02-10 16:19:18 +0100

    WIP: test further ERK optimizations

--- examples/paper-self-gravitating-gas-dynamics/elixir_eulergravity_sedov_blast_wave.jl
+++ examples/paper-self-gravitating-gas-dynamics/elixir_eulergravity_sedov_blast_wave.jl
@@ -52,7 +52,7 @@ semi_gravity = SemidiscretizationHyperbolic(mesh, equations_gravity, initial_con
 # combining both semidiscretizations for Euler + self-gravity
 parameters = ParametersEulerGravity(background_density=0.0, # aka rho0
                                     gravitational_constant=6.674e-8, # aka G
-                                    cfl=2.4,                 # 2.4
+                                    cfl=1.0,                 # 2.4
                                     resid_tol=3.0e-10,       # 1.0e-4,    3.0e-10, 3e-11 ≈ grid-converged
                                     resid_tol_type=:l2_full, # :linf_phi, :l2_full
                                     n_iterations_max=10_000,
