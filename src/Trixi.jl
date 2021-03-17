589a6a797e4f5795cc53e1b8e03b3b93c319ad89
Lars Christmann <lars@l12n.eu>
2021-03-17 09:43:21 +0100

    Do not export global_mean_vars

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -125,7 +125,6 @@ export initial_condition_monopole, boundary_condition_monopole
 export cons2cons, cons2prim, cons2macroscopic, prim2cons
 export density, pressure, density_pressure, velocity
 export entropy, energy_total, energy_kinetic, energy_internal, energy_magnetic, cross_helicity
-export global_mean_vars
 
 export TreeMesh
 
