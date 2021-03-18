831c7c228c110889399b6726ec1500888039e04c
Erik Faulhaber <erik.faulhaber@web.de>
2021-03-18 14:20:32 +0100

    Enable all tests again

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,35 +14,35 @@ const TRIXI_MPI_NPROCS = clamp(Sys.CPU_THREADS, 2, 3)
     include("test_examples_2d_part1.jl")
   end
 
-  # @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d_part2"
-  #   include("test_examples_2d_part2.jl")
-  # end
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d_part2"
+    include("test_examples_2d_part2.jl")
+  end
 
-  # @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d_parallel"
-  #   # Do a dummy `@test true`:
-  #   # If the process errors out the testset would error out as well,
-  #   # cf. https://github.com/JuliaParallel/MPI.jl/pull/391
-  #   @test true
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d_parallel"
+    # Do a dummy `@test true`:
+    # If the process errors out the testset would error out as well,
+    # cf. https://github.com/JuliaParallel/MPI.jl/pull/391
+    @test true
 
-  #   # Based on `runtests.jl` from `MPI.jl` and `PencilArrays.jl`
-  #   # Precompilation disabled to prevent race conditions when loading packages
-  #   # TODO: We can remove the flag `--compiled-modules=no` on Julia v1.6.
-  #   mpiexec() do cmd
-  #     run(`$cmd -n $TRIXI_MPI_NPROCS $(Base.julia_cmd()) --compiled-modules=no --threads=1 --check-bounds=yes test_examples_2d_parallel.jl`)
-  #   end
-  # end
+    # Based on `runtests.jl` from `MPI.jl` and `PencilArrays.jl`
+    # Precompilation disabled to prevent race conditions when loading packages
+    # TODO: We can remove the flag `--compiled-modules=no` on Julia v1.6.
+    mpiexec() do cmd
+      run(`$cmd -n $TRIXI_MPI_NPROCS $(Base.julia_cmd()) --compiled-modules=no --threads=1 --check-bounds=yes test_examples_2d_parallel.jl`)
+    end
+  end
 
   @time if TRIXI_TEST == "all" || TRIXI_TEST == "3d"
     include("test_examples_3d.jl")
   end
 
-  # @time if TRIXI_TEST == "all" || TRIXI_TEST == "misc"
-  #   include("test_unit.jl")
-  #   include("test_special_elixirs.jl")
-  #   include("test_visualization.jl")
-  # end
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "misc"
+    include("test_unit.jl")
+    include("test_special_elixirs.jl")
+    include("test_visualization.jl")
+  end
 
-  # @time if TRIXI_TEST == "all" || TRIXI_TEST == "paper-self-gravitating-gas-dynamics"
-  #   include("test_paper-self-gravitating-gas-dynamics.jl")
-  # end
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "paper-self-gravitating-gas-dynamics"
+    include("test_paper-self-gravitating-gas-dynamics.jl")
+  end
 end
