5dcb20a45ea334c99ed2f9512a167dd27694dfb1
Andrew Winters <andrew.ross.winters@liu.se>
2021-04-09 20:37:58 +0200

    removed another TODO

--- src/solvers/dg_unstructured_quad/dg.jl
+++ src/solvers/dg_unstructured_quad/dg.jl
@@ -4,5 +4,4 @@ include("dg_element_geometry_2d.jl")
 include("containers_2d.jl")
 include("dg_2d.jl")
 
-# TODO: each element, each interface, each boundary wrappers
 @inline ndofs(mesh::UnstructuredQuadMesh, dg::DG, cache) = nelements(cache.elements) * nnodes(dg)^ndims(mesh)
