9392d90e6d5d0caf09cfc6e4bdb1abfc1f10d73c
Hendrik Ranocha <ranocha@users.noreply.github.com>
2021-04-10 12:48:54 +0200

    Benchmarks (#525)
    * initiate benchmarking using PkgBenchmark
    
    * add some docs
    
    * WIP
    
    * add .gitignore
    
    * Revert "WIP"
    
    This reverts commit 611ee827405c3f2e68e96e37efc5886c40bfe5cb.
    
    * ignore benchmark results
    
    * more benchmark setups
    
    * add draft of GitHub action for benchmarks
    
    * no elixir_advection_amr_nonperiodic.jl in 3D
    
    * segfault???
    
    * fix gitignore and remove tune.json
    
    * Apply suggestions from code review
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>
    
    * mention run_benchmarks.jl in the docs
    
    * add amr_nonperiodic benchmark
    
    * link to example result
    
    * move content of benchmarks to benchmark

---
+++ benchmark/run_benchmarks.jl
@@ -0,0 +1,20 @@
+
+using PkgBenchmark
+using Trixi
+
+let results = judge(Trixi,
+            BenchmarkConfig(juliacmd=`$(Base.julia_cmd()) --check-bounds=no --threads=1`), # target
+            BenchmarkConfig(juliacmd=`$(Base.julia_cmd()) --check-bounds=no --threads=1`, id="main") # baseline
+        )
+
+    export_markdown(joinpath(pathof(Trixi) |> dirname |> dirname, "benchmark", "results_threads1.md"), results)
+end
+
+
+let results = judge(Trixi,
+            BenchmarkConfig(juliacmd=`$(Base.julia_cmd()) --check-bounds=no --threads=2`), # target
+            BenchmarkConfig(juliacmd=`$(Base.julia_cmd()) --check-bounds=no --threads=2`, id="main") # baseline
+        )
+
+    export_markdown(joinpath(pathof(Trixi) |> dirname |> dirname, "benchmark", "results_threads2.md"), results)
+end
