bcbfb120ac4eb92c2a7cadb7706bbd399598f8f9
Hendrik Ranocha <ranocha@users.noreply.github.com>
2021-04-18 08:51:42 +0200

    Apply suggestions from code review
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -57,9 +57,9 @@ end
 """
     flux(u, orientation_or_normal, equations)
 
-Given the conservative variables `u`, calculate the (physical) flux in spatial
-direction `orientation::Integer` or `normal::AbstractVector` for the
-corresponding set of governing `equations`.
+Given the conservative variables `u`, calculate the (physical) flux in Cartesian
+direction `orientation::Integer` or in arbitrary direction `normal::AbstractVector`
+for the corresponding set of governing `equations`.
 `orientation` is `1`, `2`, and `3` for the x-, y-, and z-directions, respectively.
 """
 function flux end
