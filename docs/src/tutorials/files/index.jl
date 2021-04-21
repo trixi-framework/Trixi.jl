916ff3565ef9a96f4cf64bc07396ec99283d08e6
Benjamin Bolm <benjamin.bolm@gmx.de>
2021-04-21 11:53:03 +0200

    Add Documenter and cleanup folders

---
+++ docs/src/tutorials/files/index.jl
@@ -0,0 +1,25 @@
+# Welcome to the tutorials of Trixi
+# Dann teste ich mal LateX-Code:
+#
+# ```math
+# x_\epsilon = \sum_{x=1}^3 x
+# ```
+# 
+# Und Julia Code:
+x = 3
+y = 4
+x + y
+
+# The end.
+# Test for latex:
+# ```math
+# \left\lbrace
+# \begin{aligned}
+# -∇\cdot\sigma(u) = 0 \ &\text{in} \ \Omega,\\
+# u = 0 \ &\text{on}\ \Gamma_{\rm G},\\
+# u_1 = \delta \ &\text{on}\ \Gamma_{\rm B},\\
+# \sigma(u)\cdot n = 0 \ &\text{on}\  \Gamma_{\rm N}.\\
+# \end{aligned}
+# \right.
+# ```
+
--- docs/src/tutorials/make.jl
+++ docs/src/tutorials/make.jl
@@ -16,9 +16,13 @@ end
 function preprocess_docs(content)
   return string(tutorial_title, "\n\n", content)#"\n", binder_badge, "\n", nbviwer_badge, "\n\n", content)
 end
+Literate.notebook(joinpath(@__DIR__,"files/index.jl"), joinpath(@__DIR__,"notebooks"); preprocess=preprocess_notebook)
+Literate.markdown(joinpath(@__DIR__,"files/index.jl"), joinpath(@__DIR__,"src"); preprocess=preprocess_docs, codefence="```julia" => "```")
 
-Literate.notebook(joinpath(@__DIR__,"first_example.jl"), joinpath(@__DIR__,"notebooks"); preprocess=preprocess_notebook)
-Literate.markdown(joinpath(@__DIR__,"first_example.jl"), joinpath(@__DIR__,"markdowns"); preprocess=preprocess_docs, codefence="```julia" => "```")
+Literate.notebook(joinpath(@__DIR__,"files/first_example.jl"), joinpath(@__DIR__,"notebooks"); preprocess=preprocess_notebook)
+Literate.markdown(joinpath(@__DIR__,"files/first_example.jl"), joinpath(@__DIR__,"src"); preprocess=preprocess_docs, codefence="```julia" => "```")
 
 # Literate.notebook(joinpath(@__DIR__,"adding_a_new_equation.jl"), joinpath(@__DIR__,"notebooks"); preprocess=preprocess_notebook)
-Literate.markdown(joinpath(@__DIR__,"adding_a_new_equation.jl"), joinpath(@__DIR__,"markdowns"); preprocess=preprocess_docs, codefence="```julia" => "```")
+# Literate.markdown(joinpath(@__DIR__,"adding_a_new_equation.jl"), joinpath(@__DIR__,"markdowns"); preprocess=preprocess_docs, codefence="```julia" => "```")
+
+makedocs(sitename="My Documentation")# , root = joinpath(@__DIR__, "out", "markdowns"))
