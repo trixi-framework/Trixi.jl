e2c79cb6bfea023518c596e20b1e85037f7915f4
Hendrik Ranocha <mail@ranocha.de>
2021-04-21 17:28:07 +0200

    remove annotations from analysis stuff

--- src/semidiscretization/semidiscretization_hyperbolic.jl
+++ src/semidiscretization/semidiscretization_hyperbolic.jl
@@ -167,7 +167,6 @@ end
 end
 
 
-#TODO
 function calc_error_norms(func, u_ode::AbstractVector, t, analyzer, semi::SemidiscretizationHyperbolic, cache_analysis)
   @unpack mesh, equations, initial_condition, solver, cache = semi
   u = wrap_array(u_ode, mesh, equations, solver, cache)
