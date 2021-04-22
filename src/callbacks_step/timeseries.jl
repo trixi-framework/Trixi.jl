87256c032728e5e33d73ace93907132fbdd54659
Michael Schlottke-Lakemper <michael@sloede.com>
2021-04-22 15:13:37 +0200

    Refactor storage to a DG-specific file

--- src/callbacks_step/timeseries.jl
+++ src/callbacks_step/timeseries.jl
@@ -170,31 +170,9 @@ end
 
   # Store timeseries if this is the last time step
   if isfinished(integrator)
-    @unpack (solution_variables, variable_names,
-             output_directory, filename,
-             point_coordinates, point_data,
-             time, step, timeseries_cache) = timeseries_callback
-    n_points = length(point_data)
-
-    h5open(joinpath(output_directory, filename), "w") do file
-      # Add context information as attributes
-      n_variables = length(variable_names)
-      attributes(file)["ndims"] = ndims(mesh)
-      attributes(file)["equations"] = get_name(equations)
-      attributes(file)["polydeg"] = polydeg(solver)
-      attributes(file)["n_vars"] = n_variables
-      attributes(file)["n_points"] = n_points
-      attributes(file)["interval"] = interval
-      attributes(file)["variable_names"] = collect(variable_names)
-
-      file["time"] = time
-      file["timestep"] = step
-      file["point_coordinates"] = point_coordinates
-      for p in 1:n_points
-        # Store data as 2D array for convenience
-        file["point_data_$p"] = reshape(point_data[p], n_variables, length(time))
-      end
-    end
+    semi = integrator.p
+    mesh, equations, solver, _ = mesh_equations_solver_cache(semi)
+    save_timeseries_file(timeseries_callback, mesh, equations, solver)
   end
 
   # avoid re-evaluating possible FSAL stages
@@ -204,4 +182,5 @@ end
 end
 
 
+include("timeseries_dg.jl")
 include("timeseries_dg2d.jl")
