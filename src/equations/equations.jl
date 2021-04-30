ea539bd3f090d9a9e97482db5224ee6901304fbe
erik-f <44124897+erik-f@users.noreply.github.com>
2021-04-30 12:24:08 +0200

    Update docstring
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -95,6 +95,11 @@ Create a Dirichlet boundary condition that uses the function `boundary_value_fun
 to specify the values at the boundary.
 This can be used to create a boundary condition that specifies exact boundary values
 by passing the exact solution of the equation.
+The passed boundary value function will be called with the same arguments as an initial condition function is called, i.e., as
+```julia
+boundary_value_function(x, t, equation)
+```
+where `x` specifies the coordinates, `t` is the current time, and `equation` is the corresponding system of equations.
 
 # Examples
 ```julia
