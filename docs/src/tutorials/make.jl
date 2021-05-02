cde8f2562fd75ead3f6ca4bb864bbc4f0102a39b
bennibolm <benjamin.bolm@gmx.de>
2021-05-02 18:56:54 +0200

    Clean up folder structure and update make.jl

--- docs/src/tutorials/make.jl
+++ docs/src/tutorials/make.jl
@@ -2,23 +2,62 @@ using Documenter
 using Literate
 using Trixi
 
-# For title, preprocessing, ... look into `make.jl` of `gridap/Tutorials`
-tutorial_file = "Start"
-Literate.notebook(joinpath(@__DIR__,"files/index.jl"), joinpath(@__DIR__,"notebooks"); name=tutorial_file, execute=false)
-Literate.markdown(joinpath(@__DIR__,"files/index.jl"), joinpath(@__DIR__,"src"); name=tutorial_file, codefence="```julia" => "```")
+files = [
+    "First Example"=>"first_example.jl",
+    "Adding a new equation"=>"adding_a_new_equation.jl",
+    "Differentiable programming"=>"differentiable_programming.jl"]
 
-tutorial_file = "First example"
-Literate.notebook(joinpath(@__DIR__,"files/first_example.jl"), joinpath(@__DIR__,"notebooks"); name=tutorial_file, execute=false)
-Literate.markdown(joinpath(@__DIR__,"files/first_example.jl"), joinpath(@__DIR__,"src"); name=tutorial_file, codefence="```julia" => "```")
 
-tutorial_file = "Adding a new equation"
-Literate.notebook(joinpath(@__DIR__,"files/adding_a_new_equation.jl"), joinpath(@__DIR__,"notebooks"); name=tutorial_file, execute=false)
-Literate.markdown(joinpath(@__DIR__,"files/adding_a_new_equation.jl"), joinpath(@__DIR__,"src"); name=tutorial_file, codefence="```julia" => "```")
+pages_dir = joinpath(@__DIR__,"src","pages")
+notebooks_dir = joinpath(@__DIR__,"src","notebooks")
 
-tutorial_file = "Differentiable programming"
-Literate.notebook(joinpath(@__DIR__,"files/differentiable_programming.jl"), joinpath(@__DIR__,"notebooks"); name=tutorial_file, execute=false)
-Literate.markdown(joinpath(@__DIR__,"files/differentiable_programming.jl"), joinpath(@__DIR__,"src"); name=tutorial_file, codefence="```julia" => "```")
+repo_src = joinpath(@__DIR__,"src","files")
 
-makedocs(sitename="My Documentation")# , root = joinpath(@__DIR__, "out", "markdowns"))
+Sys.rm(pages_dir;recursive=true,force=true)
+Sys.rm(notebooks_dir;recursive=true,force=true)
 
-# Run notebook with `notebook(joinpath(@__DIR__, "docs/src/tutorials/notebooks))`
+# Add index.md file as introduction to navigation menu
+pages = ["Introduction"=> "index.md"]
+
+Literate.markdown(joinpath(repo_src,"index.jl"), joinpath(@__DIR__,"src"); name="index", codefence="```julia" => "```")# preprocess=preprocess_docs)
+
+for (i, (title, filename)) in enumerate(files)
+    tutorial_prefix = string("t$(i)_")
+    tutorial_title = string("# # Tutorial ", i, ": ", title)
+    tutorial_file = string(tutorial_prefix,splitext(filename)[1])
+    notebook_filename = string(tutorial_file, ".ipynb")
+
+    # For title, preprocessing, ... look into `make.jl` of `gridap/Tutorials`
+
+    ## Generate notebooks
+    #function preprocess_notebook(content)
+    #    return string(tutorial_title, "\n\n", content)
+    #end
+    Literate.notebook(joinpath(repo_src,filename), notebooks_dir; name=tutorial_file, documenter=false, execute=false)#, preprocess=preprocess_notebook)
+
+    ## Generate markdown
+    # function preprocess_docs(content)
+    #     return string(tutorial_title, "\n", binder_badge, "\n", nbviwer_badge, "\n\n", content)
+    # end
+    Literate.markdown(joinpath(repo_src,filename), pages_dir; name=tutorial_file, codefence="```julia" => "```")# preprocess=preprocess_docs)
+
+    # Generate navigation menu entries
+    ordered_title = string(i, " ", title)
+    path_to_markdown_file = joinpath("pages",string(tutorial_file,".md"))
+    push!(pages, (ordered_title=>path_to_markdown_file))
+end
+
+# makedocs(
+#     # Specify modules for which docstrings should be shown
+#     # modules = [Trixi],#, Trixi2Vtk, Trixi2Img],
+#     # Set sitename to Trixi
+#     sitename = "Trixi.jl",
+#     format = Documenter.HTML(
+#         # Disable pretty URLs during manual testing
+#         prettyurls = get(ENV, "CI", nothing) == "true",
+#         # Set canonical URL to GitHub pages URL
+#         # canonical = "https://trixi-framework.github.io/Trixi.jl/stable"
+#     ),
+#     pages = pages
+# )
+# Run notebook with `notebook(;dir=joinpath(@__DIR__, "docs/src/tutorials/src/notebooks"))`
---
+++ docs/src/tutorials/src/index.md
@@ -0,0 +1,36 @@
+```@meta
+EditURL = "<unknown>/docs/src/tutorials/src/files/index.jl"
+```
+
+Welcome to the tutorials of Trixi
+Dann teste ich mal LateX-Code:
+
+```math
+x_\epsilon = \sum_{x=1}^3 x
+```
+
+Und Julia Code:
+
+```julia
+x = 3
+y = 4
+x + y
+```
+
+The end.
+Test for latex:
+```math
+\left\lbrace
+\begin{aligned}
+-∇\cdot\sigma(u) = 0 \ &\text{in} \ \Omega,\\
+u = 0 \ &\text{on}\ \Gamma_{\rm G},\\
+u_1 = \delta \ &\text{on}\ \Gamma_{\rm B},\\
+\sigma(u)\cdot n = 0 \ &\text{on}\  \Gamma_{\rm N}.\\
+\end{aligned}
+\right.
+```
+
+---
+
+*This page was generated using [Literate.jl](https://github.com/fredrikekre/Literate.jl).*
+
