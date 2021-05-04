9987cd0bc53be19a7d108ef5aa4b6ea00c724d51
Andrew Winters <andrew.ross.winters@liu.se>
2021-05-04 15:42:19 +0200

    shorten end time of updated test

--- test/test_examples_2d_unstructured_quad.jl
+++ test/test_examples_2d_unstructured_quad.jl
@@ -11,9 +11,9 @@ EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "2d")
 @testset "Unstructured Curve Mesh for Euler" begin
   @testset "elixir_euler_unstructured_quad.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_unstructured_quad.jl"),
-      l2   = [2.2350052041647245e-5, 3.391208446790789e-5, 3.3911984177238527e-5, 7.652531972096782e-5],
-      linf = [0.005276091776615255, 0.007635725818113892, 0.007635614651618772, 0.016749304737659898],
-      tspan = (0.0, 0.75))
+      l2   = [5.439149859993573e-5, 8.438158890695889e-5, 8.438164278782026e-5, 0.00019405355683092538],
+      linf = [0.010981453415713549, 0.017129631853193494, 0.017129625736353127, 0.03927168409120885],
+      tspan = (0.0, 0.5))
   end
 
   @testset "elixir_euler_unstructured_quad_periodic.jl" begin
