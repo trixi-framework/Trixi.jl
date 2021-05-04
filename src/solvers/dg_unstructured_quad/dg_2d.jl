3fa54ddd8ad4416aa4d404a3484e4598616ee74e
Andrew Winters <andrew.ross.winters@liu.se>
2021-05-04 14:25:41 +0200

    updated all tests and meshes to now be free-stream preserving

--- src/solvers/dg_unstructured_quad/dg_2d.jl
+++ src/solvers/dg_unstructured_quad/dg_2d.jl
@@ -22,12 +22,9 @@ function create_cache(mesh::UnstructuredQuadMesh, equations,
 
   # perform a check on the sufficient metric identities condition for free-stream preservation
   # and halt computation if it fails
-
-# TODO: comment me back in once all the tests are updated accordingly
-
-  # if !isapprox(calc_discrete_metric_identities(dg, cache), 0, atol=1e-12)
-  #   throw("metric terms fail free-stream preservation check with maximum error $(calc_discrete_metric_identities(dg, cache))")
-  # end
+  if !isapprox(calc_discrete_metric_identities(dg, cache), 0, atol=1e-12)
+    throw("metric terms fail free-stream preservation check with maximum error $(calc_discrete_metric_identities(dg, cache))")
+  end
 
   return cache
 end
--- test/test_examples_2d_unstructured_quad.jl
+++ test/test_examples_2d_unstructured_quad.jl
@@ -11,21 +11,22 @@ EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "2d")
 @testset "Unstructured Curve Mesh for Euler" begin
   @testset "elixir_euler_unstructured_quad.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_unstructured_quad.jl"),
-      l2   = [7.844127595829963e-5, 0.00011715233141491677, 0.00011715235371677694, 0.0002587793057642201],
-      linf = [0.007935754054894772, 0.011476103071539345, 0.01147611262431103, 0.025080478753547464])
+      l2   = [2.2350052041647245e-5, 3.391208446790789e-5, 3.3911984177238527e-5, 7.652531972096782e-5],
+      linf = [0.005276091776615255, 0.007635725818113892, 0.007635614651618772, 0.016749304737659898],
+      tspan = (0.0, 0.75))
   end
 
   @testset "elixir_euler_unstructured_quad_periodic.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_unstructured_quad_periodic.jl"),
-      l2   = [0.00010720127811104554, 0.00012453133183340216, 0.00012453133183106435, 0.0002862523679720807],
-      linf = [0.005838648336111252, 0.008981476526964016, 0.008981476526997323, 0.019987154066631874])
+      l2   = [0.00010978828464875207, 0.00013010359527356914, 0.00013010359527326057, 0.0002987656724828824],
+      linf = [0.00638626102818618, 0.009804042508242183, 0.009804042508253286, 0.02183139311614468])
   end
 
   @testset "elixir_euler_unstructured_quad_free_stream.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_unstructured_quad_free_stream.jl"),
       l2   = [3.357132645873228e-14, 2.3002541067700127e-13, 1.3730189315973952e-13, 4.673831509252854e-13],
       linf = [4.066857961504411e-12, 4.9736201268579805e-11, 2.5309226936442997e-11, 5.4358295642487064e-11],
-      tspan=(0.0, 0.1))
+      tspan = (0.0, 0.1))
   end
 end
 
