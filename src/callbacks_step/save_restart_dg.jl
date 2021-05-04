7c1f787dbe25e789c413501f2e6f20e6837c9d19
Hendrik Ranocha <mail@ranocha.de>
2021-05-04 15:06:27 +0200

    use wrap_array_plain for MPI restart stuff

--- src/callbacks_step/save_restart_dg.jl
+++ src/callbacks_step/save_restart_dg.jl
@@ -43,7 +43,7 @@ function load_restart_file(mesh::Union{SerialTreeMesh,CurvedMesh}, equations, dg
 
   # allocate memory
   u_ode = allocate_coefficients(mesh, equations, dg, cache)
-  u = wrap_array(u_ode, mesh, equations, dg, cache)
+  u = wrap_array_plain(u_ode, mesh, equations, dg, cache)
 
   h5open(restart_file, "r") do file
     # Read attributes to perform some sanity checks
@@ -144,7 +144,7 @@ function load_restart_file(mesh::ParallelTreeMesh, equations, dg::DG, cache, res
 
   # allocate memory
   u_ode = allocate_coefficients(mesh, equations, dg, cache)
-  u = wrap_array(u_ode, mesh, equations, dg, cache)
+  u = wrap_array_plain(u_ode, mesh, equations, dg, cache)
 
   # non-root ranks only receive data
   if !mpi_isroot()
