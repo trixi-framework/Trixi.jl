c7f098be2bcb7843daea723c6cfb5fa9bc0966d5
Andrew Winters <andrew.ross.winters@liu.se>
2021-05-06 10:42:28 +0200

    added comments for the save_mesh routine for CurvedMesh and UnstructuredQuadMesh to clarify how these routines are linked to Trixi2Vtk

--- src/mesh/mesh_io.jl
+++ src/mesh/mesh_io.jl
@@ -85,6 +85,10 @@ function save_mesh_file(mesh::TreeMesh, output_directory, timestep,
 end
 
 
+# Does does not save the mesh itself to an HDF5 file. Instead saves important attributes
+# of the mesh, like its size and the type of boundary mapping function.
+# Then, within Trixi2Vtk, the CurvedMesh and its node coordinates are reconstructured from
+# these attributes for plotting purposes
 function save_mesh_file(mesh::CurvedMesh, output_directory)
   # Create output directory (if it does not exist)
   mkpath(output_directory)
@@ -104,6 +108,10 @@ function save_mesh_file(mesh::CurvedMesh, output_directory)
 end
 
 
+# Does does not save the mesh itself to an HDF5 file. Instead saves important attributes
+# of the mesh, like its size and the corresponding mesh file used to construct the mesh.
+# Then, within Trixi2Vtk, the UnstructuredQuadMesh and its node coordinates are reconstructured
+# from these attributes for plotting purposes
 function save_mesh_file(mesh::UnstructuredQuadMesh, output_directory)
   # Create output directory (if it does not exist)
   mkpath(output_directory)
