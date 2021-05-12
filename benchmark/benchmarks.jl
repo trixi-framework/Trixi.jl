e67faa4d4f3fceb058a484585099e5e807f1c5b9
Hendrik Ranocha <mail@ranocha.de>
2021-05-12 09:46:49 +0200

    new unstructured benchmark elixir names

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,19 +7,21 @@ const SUITE = BenchmarkGroup()
 let dimension = "2d"
   SUITE[dimension] = BenchmarkGroup()
   EXAMPLES_DIR = joinpath(examples_dir(), dimension)
-  for elixir in ["elixir_advection_extended.jl",
-                 "elixir_advection_extended_curved.jl",
-                 "elixir_advection_amr_nonperiodic.jl",
-                 "elixir_advection_nonperiodic_curved.jl",
-                 "elixir_euler_ec.jl",
-                 "elixir_euler_nonperiodic_curved.jl",
-                 "elixir_euler_unstructured_quad_free_stream.jl",
-                 "elixir_euler_unstructured_quad_periodic.jl",
-                 "elixir_euler_vortex_mortar.jl",
-                 "elixir_euler_vortex_mortar_shockcapturing.jl"]
+  for elixir in [joinpath(EXAMPLES_DIR, "elixir_advection_extended.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_advection_extended_curved.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_advection_amr_nonperiodic.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_advection_nonperiodic_curved.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_euler_ec.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_euler_nonperiodic_curved.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_euler_unstructured_quad_wall_bc.jl"), # this is the only elixir working for polydeg=3
+                 joinpath(EXAMPLES_DIR, "elixir_euler_vortex_mortar.jl"),
+                 joinpath(EXAMPLES_DIR, "elixir_euler_vortex_mortar_shockcapturing.jl"),
+                 joinpath(@__DIR__, "elixir_2d_euler_vortex_tree.jl"),
+                 joinpath(@__DIR__, "elixir_2d_euler_vortex_structured.jl"),
+                 joinpath(@__DIR__, "elixir_2d_euler_vortex_unstructured.jl")]
     SUITE[dimension][elixir] = BenchmarkGroup()
     for polydeg in [3, 7]
-      trixi_include(joinpath(EXAMPLES_DIR, elixir), tspan=(0.0, 1.0e-10); polydeg)
+      trixi_include(elixir, tspan=(0.0, 1.0e-10); polydeg)
       SUITE[dimension][elixir]["p$(polydeg)_rhs!"] = @benchmarkable Trixi.rhs!(
         $(similar(sol.u[end])), $(copy(sol.u[end])), $(semi), $(first(tspan)))
       SUITE[dimension][elixir]["p$(polydeg)_analysis"] = @benchmarkable ($analysis_callback)($sol)
