33dc061374ba974c9d78f5d2c60949cc99b3c39b
Andrew Winters <andrew.ross.winters@liu.se>
2021-05-12 07:59:38 +0200

    Unstructured mesh improvements (#570)
    * adjust container to match CurvedMesh2D, now reuse calc_inverse_jacobian and calc_volume_integral from that solver
    
    * rearrange code to match formatting of other solvers
    
    * unified surface coupling computation for the unstructured solver. Now uses FluxRotated structure. FluxHLL also available in normal direction with a (very) naive estimate of the eigenvalues
    
    * improved comment about the jacobian matrix storage in the mapping files
    
    * can now use the SaveSolutionCallback with UnstructuredQuadMesh but Trixi2Vtk still broken for the unstructured solver
    
    * can now reuse the stepsize callback a la CurveMesh. Added a better check for the free-stream preservation via the discrete metric identities
    
    * added free stream test for the unstrucutred solver on the gingerbread man
    
    * updated all tests and meshes to now be free-stream preserving
    
    * shorten end time of updated test
    
    * updated test again to use the time step callback for additional control
    
    * added mesh file header and updated mesh files such that files generated from HOHQMesh are directly lodaable into Trixi
    
    * updated gingerbread man mesh to match the exact mesh from the latest HOHQ mesh generator. also required an update to the free stream test reference values
    
    * first version of using a dictionary to set boundary conditions in the unstructured solver
    
    * now use keyword arguments is unstructured elixirs for code readibility. UnstructuredQuadMesh now defaults to periodicity false
    
    * added comments for the save_mesh routine for CurvedMesh and UnstructuredQuadMesh to clarify how these routines are linked to Trixi2Vtk
    
    * streamline the metric identities controller function
    
    * mesh files no longer tracked within the repository, instead point to a gist to download the mesh file when needed
    
    * now files with the extension .mesh are ignored to avoid accidental inclusion
    
    * use Download for safety
    
    * update mesh file used for benchmarks
    
    * update benchmark file
    
    * further improvement to make the metric terms check function more compact
    
    * remove the Downloads package to try and fix the broken tests
    
    * cleanup unstructured elixirs and rename index variables in calc_boundary_flux for clarity
    
    * add Downloads to test dependencies
    
    * added ability to set boundary conditions in a more general way for the UnstructuredQuadMesh.
    
    * abstracted the boundary flux computation into an inlined function
    
    * removed return nothing from new function
    
    * some small changes requested in review
    
    * added basic test for the example unstructured mesh in the Trixi docs
    
    * new documentation of the unstructured solver with a video link
    
    * added new test for the default example from the unstructured quad mesh Trixi docs
    
    * adjust notation in boundary condition routines to be explicit with normal_direction
    
    * fix type bug introduced in previous commit
    
    * address most small comments from Michael
    
    * updated naming convention of calc_boundary_flux and added comments
    
    * removed redundant test and elixir file for the unstructured solver
    
    * forgot to change visualization testing now that elixir changed name
    
    Co-authored-by: Hendrik Ranocha <mail@ranocha.de>

--- examples/2d/elixir_euler_unstructured_quad_basic.jl
+++ examples/2d/elixir_euler_unstructured_quad_basic.jl
@@ -10,22 +10,28 @@ equations = CompressibleEulerEquations2D(1.4)
 
 initial_condition = initial_condition_convergence_test
 source_terms = source_terms_convergence_test
-boundary_conditions = boundary_condition_periodic
+
+boundary_condition_convergence_test = BoundaryConditionDirichlet(initial_condition)
+boundary_conditions = Dict( "Slant" => boundary_condition_convergence_test,
+                            "Bezier" => boundary_condition_convergence_test,
+                            "Right"  => boundary_condition_convergence_test,
+                            "Bottom" => boundary_condition_convergence_test,
+                            "Top"    => boundary_condition_convergence_test )
 
 ###############################################################################
 # Get the DG approximation space
 
-solver = DGSEM(polydeg=6, surface_flux=FluxRotated(flux_hll))
+solver = DGSEM(polydeg=8, surface_flux=flux_lax_friedrichs)
 
 ###############################################################################
 # Get the curved quad mesh from a file (downloads the file if not available locally)
 
-default_mesh_file = joinpath(@__DIR__, "mesh_periodic_square_with_twist.mesh")
-isfile(default_mesh_file) || download("https://gist.githubusercontent.com/andrewwinters5000/12ce661d7c354c3d94c74b964b0f1c96/raw/8275b9a60c6e7ebbdea5fc4b4f091c47af3d5273/mesh_periodic_square_with_twist.mesh",
+default_mesh_file = joinpath(@__DIR__, "mesh_trixi_unstructured_mesh_docs.mesh")
+isfile(default_mesh_file) || download("https://gist.githubusercontent.com/andrewwinters5000/52056f1487853fab63b7f4ed7f171c80/raw/9d573387dfdbb8bce2a55db7246f4207663ac07f/mesh_trixi_unstructured_mesh_docs.mesh",
                                        default_mesh_file)
 mesh_file = default_mesh_file
 
-mesh = UnstructuredQuadMesh(mesh_file, periodicity=true)
+mesh = UnstructuredQuadMesh(mesh_file)
 
 ###############################################################################
 # create the semi discretization object
@@ -37,7 +43,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-tspan = (0.0, 1.0)
+tspan = (0.0, 3.0)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
@@ -47,10 +53,22 @@ analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
 
 alive_callback = AliveCallback(analysis_interval=analysis_interval)
 
-callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback)
+save_solution = SaveSolutionCallback(interval=10,
+                                     save_initial_solution=true,
+                                     save_final_solution=true)
+
+stepsize_callback = StepsizeCallback(cfl=0.9)
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback,
+                        alive_callback,
+                        save_solution,
+                        stepsize_callback)
 
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, SSPRK43(), save_everystep=false, callback=callbacks);
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep=false, callback=callbacks);
 summary_callback() # print the timer summary
