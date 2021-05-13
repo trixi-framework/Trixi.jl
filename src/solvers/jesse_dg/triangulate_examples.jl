e3f054377f8f51ba002c0c20841e449d2c4b9eac
Jesse Chan <jesse.chan@rice.edu>
2021-05-13 14:42:50 -0500

    organizing Triangulate.jl functions, adding convenience plot routines

--- src/solvers/jesse_dg/triangulate_examples.jl
+++ src/solvers/jesse_dg/triangulate_examples.jl
@@ -1,65 +1,101 @@
-# domain with a corner
-h = .05
-pointlist = [0.0 0.0 ; 1.0 0.0 ; 1.0  1.0 ; 0.6 0.6; 0.0 1.0]'
-segmentlist = [1 2 ; 2 3 ; 3 4 ; 4 5 ; 5 1 ]'
-segmentmarkerlist = [1, 2, 3, 4, 5]
-triin = Triangulate.TriangulateIO()
-triin.pointlist = Matrix{Cdouble}(pointlist)
-triin.segmentlist = Matrix{Cint}(segmentlist)
-triin.segmentmarkerlist = Vector{Int32}(segmentmarkerlist)
-triout = triangulate(triin,h^2)
+function demo_domain(h = .05)
+    # domain with a corner
+    pointlist = [0.0 0.0 ; 1.0 0.0 ; 1.0  1.0 ; 0.6 0.6; 0.0 1.0]'
+    segmentlist = [1 2 ; 2 3 ; 3 4 ; 4 5 ; 5 1 ]'
+    segmentmarkerlist = [1, 2, 3, 4, 5]
+    triin = Triangulate.TriangulateIO()
+    triin.pointlist = Matrix{Cdouble}(pointlist)
+    triin.segmentlist = Matrix{Cint}(segmentlist)
+    triin.segmentmarkerlist = Vector{Int32}(segmentmarkerlist)
+    triout = triangulate(triin,h^2)
+    return triout
+end
 
-# refinement of a previous mesh
-href = h/2
-angle = @sprintf("%.15f",20)
-area  = @sprintf("%.15f",href^2)
-triout2,_ = triangulate("rpa$(area)q$(angle)Q", triout)
+# refinement of a previous mesh given the current mesh size h
+function refine(triout, h, href = h/2)
+    angle = @sprintf("%.15f",20)
+    area  = @sprintf("%.15f",href^2)
+    triout2,_ = triangulate("rpa$(area)q$(angle)Q", triout)
+    returnt triout2
+end
 
-# hole domains
-h = .05
-triin=Triangulate.TriangulateIO()
-triin.pointlist=Matrix{Cdouble}([0.0 0.0;
-                                 1.0 0.0;
-                                 1.0 1.0;
-                                 0.0 1.0;
-                                 0.4 0.4;
-                                 0.6 0.4;
-                                 0.6 0.6;
-                                 0.4 0.6;
-                                 ]')
-triin.segmentlist=Matrix{Cint}([1 2; 2 3; 3 4; 4 1; 5 6; 6 7; 7 8; 8 5; ]')
-triin.segmentmarkerlist=Vector{Int32}([1, 1,1,1, 2,2,2,2])
-triin.holelist=[0.5 0.5]'
-triout = triangulate(triin,h^2)
+# domain with a square hole
+function square_hole_domain(h = .05)
+    # hole domains
+    triin=Triangulate.TriangulateIO()
+    triin.pointlist=Matrix{Cdouble}([0.0 0.0;
+                                    1.0 0.0;
+                                    1.0 1.0;
+                                    0.0 1.0;
+                                    0.4 0.4;
+                                    0.6 0.4;
+                                    0.6 0.6;
+                                    0.4 0.6;
+                                    ]')
+    triin.segmentlist=Matrix{Cint}([1 2; 2 3; 3 4; 4 1; 5 6; 6 7; 7 8; 8 5; ]')
+    triin.segmentmarkerlist=Vector{Int32}([1, 1,1,1, 2,2,2,2])
+    triin.holelist=[0.5 0.5]'
+    triout = triangulate(triin,h^2)
+    return triout
+end
+
+# scramjet domain 
+function scramjet(h = .1)
+    # hole domains
+    triin=Triangulate.TriangulateIO()
+    triin.pointlist=Matrix{Cdouble}([0.0 0.0;
+                                     8.0 0.0;
+                                     8.0 0.8;    
+                                     0.0 2.0;
+                                     2.0 0.7;
+                                     4.0 0.2; 
+                                     7.0 0.6;
+                                     6.0 0.7;
+                                    ]')
+    triin.segmentlist=Matrix{Cint}([1 2; 2 3; 3 4; 4 1; 5 6; 6 7; 7 8; 8 5; ]')
+    # 1 = wall, 2 = inflow, 3 = outflow 
+    triin.segmentmarkerlist=Vector{Int32}([1, 3, 1, 2, 1, 1, 1, 1])
+    hole_x = sum(triin.pointlist[1,5:8])/length(triin.pointlist[1,5:8])
+    hole_y = sum(triin.pointlist[2,5:8])/length(triin.pointlist[2,5:8])
+    triin.holelist=[hole_x hole_y]'
+    triout = triangulate(triin,h^2)
+    return triout
+end
+
+
 VX,VY,EToV = triangulateIO_to_VXYZEToV(triout)
 
+# plot boundary segments as different colors
+function plot_segment_tags(triout::TriangulateIO)    
+    tags = unique(triout.segmentmarkerlist)
+    num_colors = length(tags)
+    colors = range(HSV(0,1,1), stop=HSV(360-360÷num_colors,1,1), length=num_colors)
+    xseg = zeros(2,size(triout.segmentlist,2))
+    yseg = zeros(2,size(triout.segmentlist,2))
+    segcolor = HSV{Float32}[]
+    for (col,segment) in enumerate(eachcol(triout.segmentlist))
+        xseg[:,col] .= triout.pointlist[1,segment]
+        yseg[:,col] .= triout.pointlist[2,segment]
+        push!(segcolor,colors[triout.segmentmarkerlist[col]])
+    end
+    Plots.plot()
+    for i = 1:num_colors
+        color_ids = findall(triout.segmentmarkerlist .== tags[i])
 
-# plot boundary segments
-colors = [:red,:black,:blue,:orange,:green]
-xseg = zeros(2,size(triout.segmentlist,2))
-yseg = zeros(2,size(triout.segmentlist,2))
-segcolor = Symbol[]
-for (col,segment) in enumerate(eachcol(triout.segmentlist))
-    xseg[:,col] .= VX[segment]
-    yseg[:,col] .= VY[segment]
-    push!(segcolor,colors[triout.segmentmarkerlist[col]])
+        # hack to get around issues with multiple legend labels appearing when plotting multiple series
+        x_i = vec([xseg[:,color_ids]; fill(NaN,length(color_ids))']) 
+        y_i = vec([yseg[:,color_ids]; fill(NaN,length(color_ids))']) 
+
+        Plots.plot!(x_i,y_i,mark=:circle,color=permutedims(segcolor[color_ids]),
+                    ratio = 1,label=string(tags[i])) 
+    end
+    display(plot!())
 end
-Plots.plot(xseg,yseg,mark=:circle,color=permutedims(segcolor),leg=false)
 
 rd = RefElemData(Tri(),3)
 md = MeshData(VX,VY,EToV,rd)
 
-# find Triangle segment labels of boundary faces
-segmentlist = sort(triout.segmentlist,dims=1)
-boundary_faces = findall(vec(md.FToF) .== 1:length(md.FToF))
-boundary_tag = zeros(Int,length(boundary_faces))
-for (f,boundary_face) in enumerate(boundary_faces)
-    element = (boundary_face - 1) ÷ rd.Nfaces + 1
-    face    = (boundary_face - 1) % rd.Nfaces + 1
-    vertex_ids = sort(EToV[element,rd.fv[face]])
-    tag_id = findfirst(c->view(segmentlist,:,c)==vertex_ids,axes(segmentlist,2))
-    boundary_tag[f] = triout.segmentmarkerlist[tag_id]
-end
+
 
 # bc_tag_dict = Dict(1=>"Dirichlet", 2=>"Neumann", 3=>"Robin", 4=>"Inflow", 5=>"Other")
 # Dirichlet() = "Dirichlet bc"
--- src/solvers/jesse_dg/triangulate_support.jl
+++ src/solvers/jesse_dg/triangulate_support.jl
@@ -20,6 +20,8 @@ function triangulateIO_to_VXYZEToV(triout::TriangulateIO)
     return VX,VY,EToV
 end
 
+plotMesh(triout::TriangulateIO) = plotMesh(triangulateIO_to_VXYZEToV(triout)...)
+
 function plotMesh(VX,VY,EToV)
     xmesh = Float64[]
     ymesh = Float64[]
@@ -28,7 +30,8 @@ function plotMesh(VX,VY,EToV)
         append!(xmesh,[VX[ids];NaN])
         append!(ymesh,[VY[ids];NaN])
     end
-    display(Plots.plot(xmesh,ymesh,linecolor=:black,legend=false,ratio=1))
+    display(Plots.plot(xmesh,ymesh,linecolor=:black,legend=false,
+                       ratio=1,title="$(size(EToV,1)) elements"))
 end
 
 function plotMesh!(VX,VY,EToV)
@@ -42,3 +45,26 @@ function plotMesh!(VX,VY,EToV)
     display(Plots.plot!(xmesh,ymesh,linecolor=:black,legend=false,ratio=1))
 end
 
+# find Triangle segment labels of boundary faces
+function segment_labels(triout::TriangulateIO,md::MeshData{2})
+    segmentlist = sort(triout.segmentlist,dims=1)
+    boundary_faces = findall(vec(md.FToF) .== 1:length(md.FToF))
+    boundary_face_tags = zeros(Int,length(boundary_faces))
+    for (f,boundary_face) in enumerate(boundary_faces)
+        element = (boundary_face - 1) ÷ rd.Nfaces + 1
+        face    = (boundary_face - 1) % rd.Nfaces + 1
+        vertex_ids = sort(EToV[element,rd.fv[face]])
+        tag_id = findfirst(c->view(segmentlist,:,c)==vertex_ids,axes(segmentlist,2))
+        boundary_face_tags[f] = triout.segmentmarkerlist[tag_id]
+    end
+    return boundary_face_tags, boundary_faces
+end
+
+function node_boundary_tags(triout::TriangulateIO,md::MeshData{2},rd::RefElemData{2,Tri})
+    boundary_face_tags,boundary_faces = segment_labels(triout,md)
+    node_tags = zeros(Int,size(md.xf,1)÷rd.Nfaces,md.K*rd.Nfaces) # make Nfp x Nfaces*num_elements
+    for (i,boundary_face) in enumerate(boundary_faces)
+        node_tags[:,boundary_face] .= boundary_face_tags[i]
+    end
+    node_tags = reshape(node_tags,size(md.xf)...)
+end
