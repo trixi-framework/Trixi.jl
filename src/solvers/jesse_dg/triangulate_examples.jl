5c4d3f358976eeb6912529c8dd2967f035f32384
Jesse Chan <jesse.chan@rice.edu>
2021-05-13 14:52:21 -0500

    adding plotting routines for boundary segments + renaming

--- src/solvers/jesse_dg/triangulate_examples.jl
+++ src/solvers/jesse_dg/triangulate_examples.jl
@@ -63,39 +63,8 @@ function scramjet(h = .1)
 end
 
 
-VX,VY,EToV = triangulateIO_to_VXYZEToV(triout)
-
-# plot boundary segments as different colors
-function plot_segment_tags(triout::TriangulateIO)    
-    tags = unique(triout.segmentmarkerlist)
-    num_colors = length(tags)
-    colors = range(HSV(0,1,1), stop=HSV(360-360÷num_colors,1,1), length=num_colors)
-    xseg = zeros(2,size(triout.segmentlist,2))
-    yseg = zeros(2,size(triout.segmentlist,2))
-    segcolor = HSV{Float32}[]
-    for (col,segment) in enumerate(eachcol(triout.segmentlist))
-        xseg[:,col] .= triout.pointlist[1,segment]
-        yseg[:,col] .= triout.pointlist[2,segment]
-        push!(segcolor,colors[triout.segmentmarkerlist[col]])
-    end
-    Plots.plot()
-    for i = 1:num_colors
-        color_ids = findall(triout.segmentmarkerlist .== tags[i])
-
-        # hack to get around issues with multiple legend labels appearing when plotting multiple series
-        x_i = vec([xseg[:,color_ids]; fill(NaN,length(color_ids))']) 
-        y_i = vec([yseg[:,color_ids]; fill(NaN,length(color_ids))']) 
-
-        Plots.plot!(x_i,y_i,mark=:circle,color=permutedims(segcolor[color_ids]),
-                    ratio = 1,label=string(tags[i])) 
-    end
-    display(plot!())
-end
-
-rd = RefElemData(Tri(),3)
-md = MeshData(VX,VY,EToV,rd)
-
-
+# rd = RefElemData(Tri(),3)
+# md = MeshData(VX,VY,EToV,rd)
 
 # bc_tag_dict = Dict(1=>"Dirichlet", 2=>"Neumann", 3=>"Robin", 4=>"Inflow", 5=>"Other")
 # Dirichlet() = "Dirichlet bc"
--- src/solvers/jesse_dg/triangulate_support.jl
+++ src/solvers/jesse_dg/triangulate_support.jl
@@ -5,7 +5,7 @@ using Triangulate
 import Triangulate:triangulate
 import StartUpDG:MeshData
 
-# convenience routine 
+# convenience routine (to avoid writing out @sprintf each time!)
 function Triangulate.triangulate(triin::TriangulateIO,maxarea,minangle=20)
     angle = @sprintf("%.15f",minangle)
     area  = @sprintf("%.15f",maxarea)
@@ -13,28 +13,24 @@ function Triangulate.triangulate(triin::TriangulateIO,maxarea,minangle=20)
     return triout
 end
 
-function triangulateIO_to_VXYZEToV(triout::TriangulateIO)
+# convert TriangulateIO object to VX,VY,EToV
+function triangulateIO_to_VXYEToV(triout::TriangulateIO)
     VX,VY = (triout.pointlist[i,:] for i = 1:size(triout.pointlist,1))
     EToV = permutedims(triout.trianglelist)
     Base.swapcols!(EToV,2,3) # to match MeshData ordering
     return VX,VY,EToV
 end
 
-plotMesh(triout::TriangulateIO) = plotMesh(triangulateIO_to_VXYZEToV(triout)...)
+plot_mesh(triout::TriangulateIO) = plot_mesh(triangulateIO_to_VXYEToV(triout)...)
+plot_mesh!(triout::TriangulateIO) = plot_mesh!(triangulateIO_to_VXYEToV(triout)...)
 
-function plotMesh(VX,VY,EToV)
-    xmesh = Float64[]
-    ymesh = Float64[]
-    for vertex_ids in eachrow(EToV)
-        ids = vcat(vertex_ids, vertex_ids[1])
-        append!(xmesh,[VX[ids];NaN])
-        append!(ymesh,[VY[ids];NaN])
-    end
-    display(Plots.plot(xmesh,ymesh,linecolor=:black,legend=false,
-                       ratio=1,title="$(size(EToV,1)) elements"))
+function plot_mesh(VX,VY,EToV)
+    Plots.plot()
+    plot_mesh!(VX,VY,EToV)
 end
 
-function plotMesh!(VX,VY,EToV)
+# plot mesh but hold
+function plot_mesh!(VX,VY,EToV)
     xmesh = Float64[]
     ymesh = Float64[]
     for vertex_ids in eachrow(EToV)
@@ -46,7 +42,7 @@ function plotMesh!(VX,VY,EToV)
 end
 
 # find Triangle segment labels of boundary faces
-function segment_labels(triout::TriangulateIO,md::MeshData{2})
+function get_boundary_labels(triout::TriangulateIO,md::MeshData{2})
     segmentlist = sort(triout.segmentlist,dims=1)
     boundary_faces = findall(vec(md.FToF) .== 1:length(md.FToF))
     boundary_face_tags = zeros(Int,length(boundary_faces))
@@ -61,10 +57,37 @@ function segment_labels(triout::TriangulateIO,md::MeshData{2})
 end
 
 function node_boundary_tags(triout::TriangulateIO,md::MeshData{2},rd::RefElemData{2,Tri})
-    boundary_face_tags,boundary_faces = segment_labels(triout,md)
+    boundary_face_tags,boundary_faces = get_boundary_labels(triout,md)
     node_tags = zeros(Int,size(md.xf,1)÷rd.Nfaces,md.K*rd.Nfaces) # make Nfp x Nfaces*num_elements
     for (i,boundary_face) in enumerate(boundary_faces)
         node_tags[:,boundary_face] .= boundary_face_tags[i]
     end
     node_tags = reshape(node_tags,size(md.xf)...)
+end
+
+# plot boundary segments as different colors
+function plot_segment_tags(triout::TriangulateIO)    
+    tags = unique(triout.segmentmarkerlist)
+    num_colors = length(tags)
+    colors = range(HSV(0,1,1), stop=HSV(360-360÷num_colors,1,1), length=num_colors)
+    xseg = zeros(2,size(triout.segmentlist,2))
+    yseg = zeros(2,size(triout.segmentlist,2))
+    segcolor = HSV{Float32}[]
+    for (col,segment) in enumerate(eachcol(triout.segmentlist))
+        xseg[:,col] .= triout.pointlist[1,segment]
+        yseg[:,col] .= triout.pointlist[2,segment]
+        push!(segcolor,colors[triout.segmentmarkerlist[col]])
+    end
+    Plots.plot()
+    for i = 1:num_colors
+        color_ids = findall(triout.segmentmarkerlist .== tags[i])
+
+        # hack to get around issues with multiple legend labels appearing when plotting multiple series
+        x_i = vec([xseg[:,color_ids]; fill(NaN,length(color_ids))']) 
+        y_i = vec([yseg[:,color_ids]; fill(NaN,length(color_ids))']) 
+
+        Plots.plot!(x_i,y_i,mark=:circle,color=permutedims(segcolor[color_ids]),
+                    ratio = 1,label=string(tags[i])) 
+    end
+    display(plot!())
 end
