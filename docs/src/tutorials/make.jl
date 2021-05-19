67fe49cfe141d11f0968ac5c0fa1c40a3dca3a68
Benjamin Bolm <benjamin.bolm@gmx.de>
2021-05-19 20:04:55 +0200

    Remove binder folder

--- docs/src/tutorials/make.jl
+++ docs/src/tutorials/make.jl
@@ -8,7 +8,7 @@ files = [
     "Testing" => "testing_repository.jl",
     ]
 pages_dir       = joinpath(@__DIR__,"pages")
-notebooks_dir   = joinpath(@__DIR__,"../../../binder")
+notebooks_dir   = joinpath(@__DIR__,"notebooks")
 
 repo_src        = joinpath(@__DIR__,"files")
 
@@ -20,9 +20,10 @@ pages = ["Introduction" => "index.md"]
 
 binder_logo = "https://mybinder.org/badge_logo.svg"
 nbviewer_logo = "https://img.shields.io/badge/show-nbviewer-579ACA.svg"
-binder_url = "https://mybinder.org/v2/gh/bennibolm/Trixi.jl/tutorials?filepath=binder/" # @__BINDER_ROOT_URL__
-nbviewer_url = "https://nbviewer.jupyter.org/github/bennibolm/Trixi.jl/tree/tutorials/binder/" # @__NBVIEWER_ROOT_URL__
 
+# `@__BINDER_ROOT_URL__` and `@__NBVIEWER_ROOT_URL__` should work on GITHUB-Actions automatically 
+binder_url = joinpath("https://mybinder.org/v2/gh/bennibolm/Trixi.jl/tutorials?filepath=docs/src/tutorials/notebooks/")
+nbviewer_url = joinpath("https://nbviewer.jupyter.org/github/bennibolm/Trixi.jl/tree/tutorials/docs/src/tutorials/notebooks/")
 
 # Generate markdown
 binder_badge = string("# [![](", binder_logo, ")](", binder_url, ")")
@@ -30,7 +31,8 @@ nbviewer_badge = string("# [![](", nbviewer_logo, ")](", nbviewer_url, ")")
 function preprocess_docs(content)
     return string("# ## Welcome to the tutorials of Trixi.jl", "\n", binder_badge, "\n", nbviewer_badge, "\n\n", content)
 end
-Literate.markdown(joinpath(repo_src,"index.jl"), pages_dir; name="index", documenter=false, execute=true, codefence="```julia" => "```", preprocess=preprocess_docs)
+Literate.markdown(joinpath(repo_src,"index.jl"), pages_dir; name="index", documenter=false,
+                  execute=true, codefence="```julia" => "```", preprocess=preprocess_docs)
 # TODO: With `documenter=false` there is no `link to source` in html file. With `true` the link is not defined because of some `<unkown>`.
 
 for (i, (title, filename)) in enumerate(files)
@@ -45,14 +47,17 @@ for (i, (title, filename)) in enumerate(files)
     function preprocess_notebook(content)
         return string(tutorial_title, "\n\n", content)
     end
-    # TODO: With `documenter=true` no references are written to notebook file. With `documenter=false` there are existing but not working right now.
-    Literate.notebook(joinpath(repo_src,filename), notebooks_dir; name=tutorial_file, documenter=false, execute=true, preprocess=preprocess_notebook)
+    # TODO: With `documenter=true` no references are written to notebook file.
+    #       With `documenter=false` they are existing but not working right now.
+    Literate.notebook(joinpath(repo_src,filename), notebooks_dir; name=tutorial_file, documenter=false,
+                      execute=true, preprocess=preprocess_notebook)
 
     # Generate markdown
     function preprocess_docs(content)
         return string(tutorial_title, "\n", binder_badge_, "\n", nbviewer_badge_, "\n\n", content)
     end
-    Literate.markdown(joinpath(repo_src,filename), pages_dir; name=tutorial_file, documenter=false, execute=true, codefence="```julia" => "```", preprocess=preprocess_docs)
+    Literate.markdown(joinpath(repo_src,filename), pages_dir; name=tutorial_file, documenter=false,
+                      execute=true, codefence="```julia" => "```", preprocess=preprocess_docs)
 
     # Generate navigation menu entries
     ordered_title = string(i, " ", title)
