d2016848ceedd1248a416d2d83c752ba148c91f2
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2021-05-24 14:17:01 +0200

    Add curved p4est Euler FSP example

--- test/test_examples_2d_p4est.jl
+++ test/test_examples_2d_p4est.jl
@@ -26,6 +26,12 @@ EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "2d")
       l2   = [2.3653424742684444e-6, 2.1388875095440695e-6, 2.1388875095548492e-6, 6.010896863397195e-6],
       linf = [1.4080465931654018e-5, 1.7579850587257084e-5, 1.7579850592586155e-5, 5.956893531156027e-5])
   end
+
+  @testset "elixir_euler_free_stream_curved.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_free_stream_curved.jl"),
+      l2   = [2.063350241405049e-15, 1.8571016296925367e-14, 3.1769447886391905e-14, 1.4104095258528071e-14],
+      linf = [1.9539925233402755e-14, 2.9791447087035294e-13, 6.502853810985698e-13, 2.7000623958883807e-13])
+  end
 end
 
 end # module
