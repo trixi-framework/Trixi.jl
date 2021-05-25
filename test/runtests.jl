ad9bb981cbaa95cba3162d85854c7c2856da149e
Hendrik Ranocha <ranocha@users.noreply.github.com>
2021-05-25 17:27:25 +0200

    more parallel tests to reduce CI time (#608)
    * more parallel tests to reduce CI time
    
    * fix test module names

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,8 +46,16 @@ const TRIXI_NTHREADS   = clamp(Sys.CPU_THREADS, 2, 3)
     include("test_examples_2d_part2.jl")
   end
 
-  @time if TRIXI_TEST == "all" || TRIXI_TEST == "3d"
-    include("test_examples_3d.jl")
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d_part3"
+    include("test_examples_2d_part3.jl")
+  end
+
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "3d_part1"
+    include("test_examples_3d_part1.jl")
+  end
+
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "3d_part2"
+    include("test_examples_3d_part2.jl")
   end
 
   @time if TRIXI_TEST == "all" || TRIXI_TEST == "misc"
