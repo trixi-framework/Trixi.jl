442dd4b870e8f4bffaedf8571925620eb5f6c192
Hendrik Ranocha <mail@ranocha.de>
2021-06-11 15:25:42 +0200

    use at-evalpoly in ln_mean

--- src/auxiliary/math.jl
+++ src/auxiliary/math.jl
@@ -41,7 +41,7 @@
   epsilon_f2 = 1.0e-4
   f2 = (x * (x - 2 * y) + y * y) / (x * (x + 2 * y) + y * y) # f2 = f^2
   if f2 < epsilon_f2
-    return (x + y) * 52.5 / (105 + f2 * (35 + f2 * (21 + f2 * 15)))
+    return (x + y) / @evalpoly(f2, 2, 2/3, 2/5, 2/7)
   else
     return (y - x) / log(y / x)
   end
