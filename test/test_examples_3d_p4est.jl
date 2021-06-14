97b955a6603fad99945745026285b280f9ebd847
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2021-06-14 15:31:05 +0200

    Add AMR example with P4estMesh in 3D

--- test/test_examples_3d_p4est.jl
+++ test/test_examples_3d_p4est.jl
@@ -15,6 +15,17 @@ EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "3d")
       linf = [0.0012577781391462928])
   end
 
+  @testset "elixir_advection_p4est_non_conforming.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_p4est_non_conforming.jl"),
+      l2   = [0.0024774648310858928],
+      linf = [0.021727876954353964])
+  end
+
+  @testset "elixir_advection_amr_p4est.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_amr_p4est.jl"),
+      l2   = [9.773852895157622e-6],
+      linf = [0.0005853874124926162])
+  end
 end
 
 end # module
