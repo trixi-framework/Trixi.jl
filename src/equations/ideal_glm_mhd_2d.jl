f277fcd1a6ce73a5e096dbc6f58ad4d19cc3b42f
Hendrik Ranocha <mail@ranocha.de>
2021-06-14 12:55:19 +0200

    add Gregor to reference for flux_hindenlang as discussed with him [skip ci]

--- src/equations/ideal_glm_mhd_2d.jl
+++ src/equations/ideal_glm_mhd_2d.jl
@@ -365,16 +365,16 @@ Entropy conserving and kinetic energy preserving two-point flux of
 Hindenlang (2019), extending [`flux_ranocha`](@ref) to the MHD equations.
 
 ## References
-- Hindenlang (2019)
+- Florian Hindenlang, Gregor Gassner (2019)
   A new entropy conservative two-point flux for ideal MHD equations derived from
   first principles.
   Presented at HONOM 2019: European workshop on high order numerical methods
   for evolutionary PDEs, theory and applications
-- Ranocha (2018)
+- Hendrik Ranocha (2018)
   Generalised Summation-by-Parts Operators and Entropy Stability of Numerical Methods
   for Hyperbolic Balance Laws
   [PhD thesis, TU Braunschweig](https://cuvillier.de/en/shop/publications/7743)
-- Ranocha (2020)
+- HEndrik Ranocha (2020)
   Entropy Conserving and Kinetic Energy Preserving Numerical Methods for
   the Euler Equations Using Summation-by-Parts Operators
   [Proceedings of ICOSAHOM 2018](https://doi.org/10.1007/978-3-030-39647-3_42)
