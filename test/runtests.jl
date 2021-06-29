27e4b3ece1a819e7801308c191ce2ddaa3c2c96a
Jesse Chan <jesse.chan@rice.edu>
2021-06-29 15:29:17 -0500

    fixing test naming: 2d_triangles -> simplices

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,8 +50,8 @@ const TRIXI_NTHREADS   = clamp(Sys.CPU_THREADS, 2, 3)
     include("test_examples_2d_part3.jl")
   end
 
-  @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d_triangles"
-    include("test_examples_2d_triangular_mesh.jl")
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "simplices"
+    include("test_examples_simplicial_mesh.jl")
   end  
 
   @time if TRIXI_TEST == "all" || TRIXI_TEST == "3d_part1"
