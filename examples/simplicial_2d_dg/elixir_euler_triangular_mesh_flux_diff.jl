2ecbda2b4e854b6b892ca7c75c78316f81664fb6
Jesse Chan <jesse.chan@rice.edu>
2021-07-02 00:57:17 -0500

    implemented entropy stable flux differencing on triangular meshes
    tet meshes still have some bugs

---
+++ examples/simplicial_2d_dg/elixir_euler_triangular_mesh_flux_diff.jl
@@ -0,0 +1,49 @@
+# !!! warning "Experimental features"
+
+using StartUpDG
+using Trixi, OrdinaryDiffEq
+
+polydeg = 3
+rd = RefElemData(Tri(), polydeg)
+
+volume_flux = flux_ranocha
+dg = DG(rd, nothing #= mortar =#, 
+        SurfaceIntegralWeakForm(FluxLaxFriedrichs()), VolumeIntegralFluxDifferencing(volume_flux))
+
+equations = CompressibleEulerEquations2D(1.4)
+initial_condition = initial_condition_convergence_test
+source_terms = source_terms_convergence_test
+
+# example where we tag two separate boundary segments of the mesh
+top_boundary(x,y,tol=50*eps()) = abs(y-1)<tol 
+rest_of_boundary(x,y,tol=50*eps()) = !top_boundary(x,y,tol)
+is_on_boundary = Dict(:top => top_boundary, :rest => rest_of_boundary)
+VX, VY, EToV = StartUpDG.uniform_mesh(Tri(), 8)
+mesh = VertexMappedMesh(VX, VY, EToV, rd, is_on_boundary = is_on_boundary)
+
+boundary_condition_convergence_test = BoundaryConditionDirichlet(initial_condition)
+boundary_conditions = (; :top => boundary_condition_convergence_test,
+                        :rest => boundary_condition_convergence_test)
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg,
+                                    source_terms = source_terms, 
+                                    boundary_conditions = boundary_conditions) 
+
+tspan = (0.0, .1)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+alive_callback = AliveCallback(alive_interval=10)
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
+callbacks = CallbackSet(summary_callback, alive_callback, analysis_callback)
+
+###############################################################################
+# run the simulation
+
+dt0 = StartUpDG.estimate_h(rd,mesh.md) / StartUpDG.inverse_trace_constant(rd)
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt = .5*dt0, save_everystep=false, callback=callbacks);
+summary_callback() # print the timer summary
+
+l2,linf = analysis_callback(sol)
--- src/solvers/dg_simplices/fluxdiff.jl
+++ src/solvers/dg_simplices/fluxdiff.jl
@@ -19,6 +19,30 @@ function hadamard_sum_ATr!(du, ATr, volume_flux::VF, u, skip_index=(i,j)->false)
   end
 end
 
+function build_lazy_physical_derivative(elem::Int, orientation::Int, mesh::AbstractMeshData{2}, dg, cache) 
+  @unpack Qrst_skew_Tr = cache
+  @unpack rxJ, sxJ, ryJ, syJ = mesh.md
+  QrskewTr, QsskewTr = Qrst_skew_Tr
+  if orientation == 1
+    return LazyArray(@~ @. 2 * (rxJ[1,elem] * QrskewTr + sxJ[1,elem] * QsskewTr))
+  else
+    return LazyArray(@~ @. 2 * (ryJ[1,elem] * QrskewTr + syJ[1,elem] * QsskewTr))
+  end
+end
+
+function build_lazy_physical_derivative(elem::Int, orientation::Int, mesh::AbstractMeshData{3}, dg, cache) 
+  @unpack Qrst_skew_Tr = cache
+  @unpack rxJ, sxJ, txJ, ryJ, syJ, tyJ, rzJ, szJ, tzJ = mesh.md
+  QrskewTr, QsskewTr, QtskewTr = Qrst_skew_Tr
+  if orientation == 1
+    return LazyArray(@~ @. 2 * (rxJ[1,elem]*QrskewTr + sxJ[1,elem]*QsskewTr) + txJ[1,elem]*QtskewTr)
+  elseif orientation == 2
+    return LazyArray(@~ @. 2 * (ryJ[1,elem]*QrskewTr + syJ[1,elem]*QsskewTr) + tyJ[1,elem]*QtskewTr)
+  elseif orientation == 3
+    return LazyArray(@~ @. 2 * (rzJ[1,elem]*QrskewTr + szJ[1,elem]*QsskewTr) + tzJ[1,elem]*QtskewTr)
+  end
+end
+
 function calc_volume_integral!(du, u::StructArray, volume_integral::VolumeIntegralFluxDifferencing,
                  mesh::VertexMappedMesh, equations, dg::DG, cache) where {DG <: SBPDGFluxDiff}
 
@@ -42,26 +66,114 @@ function calc_volume_integral!(du, u::StructArray, volume_integral::VolumeIntegr
   end
 end
 
-function build_lazy_physical_derivative(elem::Int, orientation::Int, mesh::AbstractMeshData{2}, dg, cache) 
-  @unpack Qrst_skew_Tr = cache
-  @unpack rxJ, sxJ, ryJ, syJ = mesh.md
-  QrskewTr, QsskewTr = Qrst_skew_Tr
-  if orientation == 1
-    return LazyArray(@~ @. 2 * (rxJ[1,elem]*QrskewTr + sxJ[1,elem]*QsskewTr))
-  else
-    return LazyArray(@~ @. 2 * (ryJ[1,elem]*QrskewTr + syJ[1,elem]*QsskewTr))
+
+function create_cache(mesh::VertexMappedMesh{Dim}, equations, dg::DG, 
+                      RealT, uEltype) where {DG <: PolyDGFluxDiff} where {Dim}
+
+  rd = dg.basis
+  @unpack md = mesh
+  
+  # Todo: simplices. Fix this when StartUpDG v0.11.0 releases
+  if Dim==2
+    Qr_hybridized, Qs_hybridized, VhP, Ph = StartUpDG.hybridized_SBP_operators(rd)
+    Qrst_hybridized = (Qr_hybridized, Qs_hybridized) 
+  elseif Dim==3
+    Qr_hybridized, Qs_hybridized, Qt_hybridized, VhP, Ph = StartUpDG.hybridized_SBP_operators(rd)
+    Qrst_hybridized = (Qr_hybridized, Qs_hybridized, Qt_hybridized) 
   end
+  Qrst_skew_Tr = map(A->-.5*(A-A'), Qrst_hybridized)
+  # Qrst_hybridized, VhP, Ph = StartUpDG.hybridized_SBP_operators(rd)
+
+  nvars = nvariables(equations)
+
+  # storage for all quadrature points (concatenated volume / face quadrature points)
+  num_quad_pts_total = rd.Nq + rd.Nfq
+  entropy_projected_u_values = StructArray{SVector{nvars, uEltype}}(ntuple(_->zeros(num_quad_pts_total, md.num_elements), nvars))
+  projected_entropy_var_values = similar(entropy_projected_u_values)
+
+  # initialize views into entropy_projected_u_values
+  u_values = view(entropy_projected_u_values, 1:rd.Nq, :)
+  u_face_values = view(entropy_projected_u_values, rd.Nq+1:num_quad_pts_total, :)
+
+  # temp storage for entropy variables at volume quad points
+  entropy_var_values = StructArray{SVector{nvars, uEltype}}(ntuple(_->zeros(rd.Nq, md.num_elements), nvars))
+
+  # local storage for interface fluxes, rhs, and source
+  flux_face_values = similar(u_face_values)
+  rhs_local_threaded = [StructArray{SVector{nvars, uEltype}}(ntuple(_->zeros(num_quad_pts_total), nvars)) for _ in 1:Threads.nthreads()]
+  local_values_threaded = [StructArray{SVector{nvars, uEltype}}(ntuple(_->zeros(rd.Nq), nvars)) for _ in 1:Threads.nthreads()]
+  
+  return (; md, Qrst_skew_Tr, VhP, Ph, invJ = inv.(md.J), 
+      entropy_var_values, projected_entropy_var_values, entropy_projected_u_values,
+      u_values, u_face_values, rhs_local_threaded, flux_face_values, local_values_threaded)
 end
 
-function build_lazy_physical_derivative(elem::Int, orientation::Int, mesh::AbstractMeshData{3}, dg, cache) 
-  @unpack Qrst_skew_Tr = cache
-  @unpack rxJ, sxJ, txJ, ryJ, syJ, tyJ, rzJ, szJ, tzJ = mesh.md
-  QrskewTr, QsskewTr, QtskewTr = Qrst_skew_Tr
-  if orientation == 1
-    return LazyArray(@~ @. 2 * (rxJ[1,elem]*QrskewTr + sxJ[1,elem]*QsskewTr) + txJ[1,elem]*QtskewTr)
-  elseif orientation == 2
-    return LazyArray(@~ @. 2 * (ryJ[1,elem]*QrskewTr + syJ[1,elem]*QsskewTr) + tyJ[1,elem]*QtskewTr)
-  else 
-    return LazyArray(@~ @. 2 * (rzJ[1,elem]*QrskewTr + szJ[1,elem]*QsskewTr) + tzJ[1,elem]*QtskewTr)
-  end
+function entropy_projection!(cache, u::StructArray, mesh::VertexMappedMesh, 
+                             equations, dg::DG) where {DG <: PolyDGFluxDiff}
+
+  rd = dg.basis
+  @unpack Vq = rd
+  @unpack VhP, entropy_var_values, u_values, entropy_var_values = cache
+  @unpack projected_entropy_var_values, entropy_projected_u_values = cache
+
+  # TODO: simplices. Address hard-coding of `entropy2cons!`
+  StructArrays.foreachfield(mul_by!(Vq), u_values, u)
+  entropy_var_values .= cons2entropy.(u_values, equations)
+
+  # "VhP" fuses the projection "P" with interpolation to volume and face quadrature "Vh" 
+  StructArrays.foreachfield(mul_by!(VhP), projected_entropy_var_values, entropy_var_values)
+  entropy_projected_u_values .= entropy2cons.(projected_entropy_var_values, equations)
 end
+
+function calc_volume_integral!(du, u::StructArray, volume_integral::VolumeIntegralFluxDifferencing,
+                 mesh::VertexMappedMesh, equations, dg::PolyDGFluxDiff, cache) 
+
+  rd = dg.basis 
+  md = mesh.md
+  @unpack entropy_projected_u_values, rhs_local_threaded, Ph = cache
+
+  volume_flux_oriented(i) = let i=i, equations=equations 
+    (u_ll, u_rr)->volume_integral.volume_flux(u_ll, u_rr, i, equations)
+  end
+
+  # Todo: simplices. Dispatch on curved/non-curved mesh types, this code only works for affine meshes (accessing rxJ[1,e],...)
+  @threaded for e in eachelement(mesh, dg, cache)
+    u_local = view(entropy_projected_u_values, :, e)
+    rhs_local = rhs_local_threaded[Threads.threadid()]
+    fill!(rhs_local, zero(eltype(rhs_local)))
+    for i in eachdim(mesh) 
+      Qi_skew_Tr = build_lazy_physical_derivative(e, i, mesh, dg, cache)
+      hadamard_sum_ATr!(rhs_local, Qi_skew_Tr, volume_flux_oriented(i), u_local, 
+                        (i,j)->i > rd.Nq && j > rd.Nq)
+    end
+    StructArrays.foreachfield(mul_by_accum!(Ph), view(du, :, e), rhs_local)
+  end 
+end
+
+function rhs!(du, u::StructArray, t, mesh, equations, 
+              initial_condition, boundary_conditions::BC, source_terms::Source,
+              dg::PolyDGFluxDiff, cache) where {BC, Source}
+
+  @trixi_timeit timer() "Reset du/dt" fill!(du,zero(eltype(du)))
+
+  @trixi_timeit timer() "entropy_projection!" entropy_projection!(cache, u, mesh, equations, dg)
+
+  @trixi_timeit timer() "calc_volume_integral!" calc_volume_integral!(du, u, dg.volume_integral, 
+                                    mesh, equations, dg, cache)
+
+  # the following functions are the same as in VolumeIntegralWeakForm, and can be reused from dg.jl 
+  @trixi_timeit timer() "calc_interface_flux!" calc_interface_flux!(cache, dg.surface_integral, mesh, equations, dg)
+
+  @trixi_timeit timer() "calc_boundary_flux!" calc_boundary_flux!(cache, t, boundary_conditions, mesh, equations, dg)
+
+  @trixi_timeit timer() "calc_surface_integral!" calc_surface_integral!(du, u, dg.surface_integral, mesh, equations, dg, cache)
+  
+  @trixi_timeit timer() "invert_jacobian" invert_jacobian!(du, mesh, equations, dg, cache)
+
+  @trixi_timeit timer() "calc_sources!" calc_sources!(du, u, t, source_terms, mesh, equations, dg, cache)
+
+  return nothing
+end
+
+
+
