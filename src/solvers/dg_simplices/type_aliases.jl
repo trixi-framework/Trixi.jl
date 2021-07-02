32231953978d47d121b90f70757cc168f55a105c
Jesse Chan <jesse.chan@rice.edu>
2021-07-02 00:56:40 -0500

    update type aliases
    more restrictive subtyping

--- src/solvers/dg_simplices/type_aliases.jl
+++ src/solvers/dg_simplices/type_aliases.jl
@@ -2,9 +2,9 @@ const DGWeakForm{Dims, ElemType} = DG{<:RefElemData{Dims, ElemType}, Mortar,
                     <:SurfaceIntegralWeakForm,
                     <:VolumeIntegralWeakForm} where {Mortar}
 const DGFluxDiff{Dim, Elem} = DG{<:RefElemData{Dim, Elem}, 
-                            M, S, <: VolumeIntegralFluxDifferencing} where {Dim, Elem, M, S}
+                            M, <:SurfaceIntegralWeakForm, <:VolumeIntegralFluxDifferencing} where {Dim, Elem, M}
 const PolyDGFluxDiff{Dim, Elem} = DG{<:RefElemData{Dim, Elem, Polynomial}, 
-                            M, S, <: VolumeIntegralFluxDifferencing} where {Dim, Elem, M, S}
+                            M, <:SurfaceIntegralWeakForm, <:VolumeIntegralFluxDifferencing} where {Dim, Elem, M}
 const SBPDGFluxDiff{Dim, Elem} = DG{<:RefElemData{Dim, Elem, <:SBP}, 
-                            M, S, <: VolumeIntegralFluxDifferencing} where {Dim, Elem, M, S}
+                            M, <:SurfaceIntegralWeakForm, <:VolumeIntegralFluxDifferencing} where {Dim, Elem, M}
                     
