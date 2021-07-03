bddfdb5c6df95daa17b9d60cb26da9eae6a7532c
Jesse Chan <jesse.chan@rice.edu>
2021-07-03 14:11:25 -0500

    updating VertexMappedMesh
    - swapping Dim -> NDIMS 
    - removing Tv, Ti from VertexMappedMesh type parameters

--- src/solvers/dg_simplices/mesh.jl
+++ src/solvers/dg_simplices/mesh.jl
@@ -1,33 +1,33 @@
 # !!! warning "Experimental features"
 
-abstract type AbstractMeshData{Dim, ElemType} end
+abstract type AbstractMeshData{NDIMS, ElemType} end
 
 """
-    VertexMappedMesh{Dim, ElemType, Nboundaries, Tv, Ti} <: AbstractMeshData{Dim, ElemType}
+    VertexMappedMesh{NDIMS, ElemType, Nboundaries, Tv, Ti} <: AbstractMeshData{NDIMS, ElemType}
 
 `VertexMappedMesh` describes a mesh which is constructed by an reference-to-physical 
 mapping which can be constructed using only the vertex positions. 
 
 Wraps `MeshData` and `boundary_faces` in a dispatchable mesh type.
 """
-struct VertexMappedMesh{Dim, ElemType, Nboundaries, Tv, Ti} <: AbstractMeshData{Dim, ElemType}
-  md::MeshData{Dim, Tv, Ti}
+struct VertexMappedMesh{NDIMS, ElemType, MeshDataT <: MeshData{NDIMS}, Nboundaries} <: AbstractMeshData{NDIMS, ElemType}
+  md::MeshDataT
   boundary_faces::Dict{Symbol, Vector{Int}}
 end
 
-Base.ndims(::VertexMappedMesh{Dim}) where {Dim} = Dim
+Base.ndims(::VertexMappedMesh{NDIMS}) where {NDIMS} = NDIMS
 
-function Base.show(io::IO, mesh::VertexMappedMesh{Dim, ElemType, Nb, RealT}) where {Dim, ElemType, Nb, RealT}
-  @nospecialize mesh
-  print(io, "$ElemType VertexMappedMesh with Dim = $Dim and RealT = $RealT.")
+function Base.show(io::IO, mesh::VertexMappedMesh{NDIMS, ElemType}) where {NDIMS, ElemType}
+  @nospecialize mesh # reduce precompilation time
+  print(io, "$ElemType VertexMappedMesh with NDIMS = $NDIMS.")
 end
 
-function Base.show(io::IO, ::MIME"text/plain", mesh::VertexMappedMesh{Dim, ElemType, Nb, RealT}) where {Dim, ElemType, Nb, RealT}
-  @nospecialize mesh  
+function Base.show(io::IO, ::MIME"text/plain", mesh::VertexMappedMesh{NDIMS, ElemType, MeshDataT, Nb}) where {NDIMS, ElemType, MeshDataT, Nb}
+  @nospecialize mesh # reduce precompilation time
   if get(io, :compact, false)
     show(io, mesh)
   else
-    summary_header(io, "VertexMappedMesh{$Dim, $ElemType, $Nb, $RealT}, ")
+    summary_header(io, "VertexMappedMesh{$NDIMS, $ElemType, $MeshDataT, $Nb}, ")
     summary_line(io, "number of elements", mesh.md.num_elements)
     summary_line(io, "number of boundaries", length(mesh.boundary_faces))
     for (boundary_name, faces) in mesh.boundary_faces
@@ -38,35 +38,33 @@ function Base.show(io::IO, ::MIME"text/plain", mesh::VertexMappedMesh{Dim, ElemT
 end
 
 """
-    VertexMappedMesh(VXYZ::NTuple{Dim, Vector{Tv}}, EToV, rd::RefElemData;
+    VertexMappedMesh(VXYZ::NTuple{NDIMS, Vector{Tv}}, EToV, rd::RefElemData;
                      is_on_boundary = nothing,
-                     is_periodic::NTuple{Dim, Bool} = ntuple(_->false, Dim)) where {Dim, Tv}
+                     is_periodic::NTuple{NDIMS, Bool} = ntuple(_->false, NDIMS)) where {NDIMS, Tv}
 
 - `VXYZ` is a tuple of vectors of vertex coordinates
-- `EToV` is a matrix containing element-to-vertex connectivities for each element
+- `EToV` is a 2D array containing element-to-vertex connectivities for each element
+- `rd` is a `RefElemData` from `StartUpDG.jl`, and contains information associated with to the 
+reference element (e.g., quadrature, basis evaluation, differentiation, etc).
 - `is_on_boundary` specifies boundary using a `Dict{Symbol, <:Function}`
 - `is_periodic` is a tuple of booleans specifying periodicity = `true`/`false` in the (x,y,z) direction.
 """
-VertexMappedMesh(VX, VY, EToV, rd, args...; kwargs...) = VertexMappedMesh((VX, VY), EToV, rd, args...; kwargs...)
-VertexMappedMesh(VX, VY, VZ, EToV, rd, args...; kwargs...) = VertexMappedMesh((VX, VY, VZ), EToV, rd, args...; kwargs...)
-
-function VertexMappedMesh(VXYZ::NTuple{Dim, Vector{Tv}}, EToV::Matrix{Ti}, rd::RefElemData;
+function VertexMappedMesh(VXYZ::NTuple{NDIMS, Vector{Tv}}, EToV::Array{Ti,2}, rd::RefElemData;
                           is_on_boundary = nothing,
-                          is_periodic::NTuple{Dim, Bool} = ntuple(_->false, Dim)) where {Dim, Tv, Ti}
+                          is_periodic::NTuple{NDIMS, Bool} = ntuple(_->false, NDIMS)) where {NDIMS, Tv, Ti}
 
   md = MeshData(VXYZ..., EToV, rd)
   md = StartUpDG.make_periodic(md, is_periodic)
   boundary_faces = StartUpDG.tag_boundary_faces(md, is_on_boundary)  
-  return VertexMappedMesh{Dim, typeof(rd.elementType), length(boundary_faces), Tv, Ti}(md, boundary_faces)
+  return VertexMappedMesh{NDIMS, typeof(rd.elementType), typeof(md), length(boundary_faces)}(md, boundary_faces)
 end
 
 """
-    VertexMappedMesh(VXYZ::NTuple{Dim, Vector{Tv}}, EToV, rd::RefElemData;
-                     triangulateIO, boundary_dict::Dict{Symbol, Int})
+  VertexMappedMesh(triangulateIO, rd::RefElemData{2, Tri}, boundary_dict::Dict{Symbol, Int}) 
 
-- `VXYZ` is a tuple of vectors of vertex coordinates
-- `EToV` is a matrix containing element-to-vertex connectivities for each element
 - `triangulateIO` is a `TriangulateIO` mesh representation
+- `rd` is a `RefElemData` from `StartUpDG.jl`, and contains information associated with to the 
+reference element (e.g., quadrature, basis evaluation, differentiation, etc).
 - `boundary_dict` is a `Dict{Symbol, Int}` which associates each integer `TriangulateIO` boundary tag with a Symbol
 """
 function VertexMappedMesh(triangulateIO, rd::RefElemData{2, Tri}, boundary_dict::Dict{Symbol, Int}) 
@@ -74,5 +72,9 @@ function VertexMappedMesh(triangulateIO, rd::RefElemData{2, Tri}, boundary_dict:
   VX, VY, EToV = StartUpDG.triangulateIO_to_VXYEToV(triangulateIO)
   md = MeshData(VX, VY, EToV, rd)
   boundary_faces = StartUpDG.tag_boundary_faces(triangulateIO, rd, md, boundary_dict)  
-  return VertexMappedMesh{2, typeof(rd.elementType), length(boundary_faces), eltype(VX), eltype(EToV)}(md, boundary_faces)
+  return VertexMappedMesh{2, typeof(rd.elementType), typeof(md), length(boundary_faces)}(md, boundary_faces)
 end
+
+VertexMappedMesh(VX, VY, EToV, rd, args...; kwargs...) = VertexMappedMesh((VX, VY), EToV, rd, args...; kwargs...)
+VertexMappedMesh(VX, VY, VZ, EToV, rd, args...; kwargs...) = VertexMappedMesh((VX, VY, VZ), EToV, rd, args...; kwargs...)
+
