fa1d6abfe2b47907e82d1bb3da19eb5bcff7f4ed
Jesse Chan <jesse.chan@rice.edu>
2021-07-05 23:15:25 -0500

    adding MultiDG convenience constructor

--- src/solvers/dg_simplices/types_and_traits.jl
+++ src/solvers/dg_simplices/types_and_traits.jl
@@ -4,6 +4,33 @@
 const MultiDG{NDIMS, ElemType, ApproxType, SurfaceIntegral, VolumeIntegral} =
   DG{<:RefElemData{NDIMS, ElemType, ApproxType}, Mortar, SurfaceIntegral, VolumeIntegral} where {Mortar}
 
+"""
+    MultiDG(; polydeg::Integer,
+              elem_type::AbstractElemShape,
+              approximation_type=Polynomial(),
+              surface_flux=flux_central,
+              surface_integral=SurfaceIntegralWeakForm(surface_flux),
+              volume_integral=VolumeIntegralWeakForm())
+
+Create a discontinuous Galerkin method which uses
+- approximations of polynomial degree `polydeg`
+- element type `elem_type` (`Tri()`, `Quad()`, `Tet()`, and `Hex()` currently supported)
+
+Optional:
+- approximation type of `approximation_type` (default is `Polynomial()`; `SBP()` also supported for
+`Tri()`, `Quad()`, and `Hex()` element types).
+"""
+function MultiDG(; polydeg::Integer,
+                   elem_type::AbstractElemShape,
+                   approximation_type=Polynomial(),
+                   surface_flux=flux_central,
+                   surface_integral=SurfaceIntegralWeakForm(surface_flux),
+                   volume_integral=VolumeIntegralWeakForm())
+  rd = RefElemData(elem_type, approximation_type, polydeg)
+  return MultiDG(rd, surface_integral, volume_integral)
+end
+
+# type aliases for dispatch purposes
 const MultiDGWeakForm{NDIMS, ElemType, ApproxType} =
   MultiDG{NDIMS, ElemType, ApproxType, <:SurfaceIntegralWeakForm, <:VolumeIntegralWeakForm}
 
