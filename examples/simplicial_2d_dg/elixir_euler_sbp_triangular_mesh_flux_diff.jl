5cc9542967a7a31468abce4d7d6d9d33b77c3190
Jesse Chan <jchan985@gmail.com>
2021-07-05 23:57:56 -0500

    Apply suggestions from code review
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- examples/simplicial_2d_dg/elixir_euler_sbp_triangular_mesh_flux_diff.jl
+++ examples/simplicial_2d_dg/elixir_euler_sbp_triangular_mesh_flux_diff.jl
@@ -29,7 +29,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg,
                                     source_terms = source_terms,
                                     boundary_conditions = boundary_conditions)
 
-tspan = (0.0, .1)
+tspan = (0.0, 0.1)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
@@ -47,7 +47,5 @@ callbacks = CallbackSet(summary_callback, alive_callback, analysis_callback)
 
 dt0 = StartUpDG.estimate_h(rd,mesh.md) / StartUpDG.inverse_trace_constant(rd)
 sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt = .5*dt0, save_everystep=false, callback=callbacks);
+            dt = 0.5*dt0, save_everystep=false, callback=callbacks);
 summary_callback() # print the timer summary
-
-l2,linf = analysis_callback(sol)
