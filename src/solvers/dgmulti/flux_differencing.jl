cafa9042c0fd834ad1950d83b2ca15ff124c7f66
Jesse Chan <jesse.chan@rice.edu>
2021-08-07 15:40:12 -0500

    storing A instead of ATr for flux differencing for simplicity/clarity

--- src/solvers/dgmulti/flux_differencing.jl
+++ src/solvers/dgmulti/flux_differencing.jl
@@ -4,29 +4,28 @@
 # See https://ranocha.de/blog/Optimizing_EC_Trixi for further details.
 @muladd begin
 
-#   hadamard_sum_A_transposed!(du, ATr, volume_flux, orientation, u, equations,
+#   hadamard_sum!(du, A, volume_flux, orientation, u, equations,
 #                              sparsity_pattern, skip_index=(i,j)->false)
 #
 # Computes the flux difference ∑_j A[i, j] * f(u_i, u_j) and accumulates the result into `du`.
 # - `du`, `u` are vectors
-# - `ATr` is the transpose of the flux differencing matrix `A`. The transpose is used for
-#   faster traversal since matrices are column major in Julia.
+# - `A` is the skew-symmetric flux differencing matrix.
 # - `sparsity_pattern`: either `nothing` or an AbstractSparseMatrix which specifies the sparsity
 #   pattern of `A`
-@inline function hadamard_sum_A_transposed!(du, ATr, volume_flux, orientation, u, equations,
-                                            sparsity_pattern::Nothing, skip_index=(i,j)->false)
+@inline function hadamard_sum!(du, A, volume_flux, orientation, u, equations,
+                               sparsity_pattern::Nothing, skip_index=(i,j)->false)
 
-  rows, cols = axes(ATr)
-  for i in cols
+  rows, cols = axes(A)
+  for i in rows
     u_i = u[i]
     du_i = du[i]
-    for j in rows
+    for j in cols
       # This routine computes only the upper-triangular part of the hadamard sum (A .* F).
       # We avoid computing the lower-triangular part, and instead accumulate those contributions
       # while computing the upper-triangular part (using the fact that A is skew-symmetric and F
       # is symmetric).
       if j > i && !skip_index(i, j)
-          AF_ij = ATr[j,i] * volume_flux(u_i, u[j], orientation, equations)
+          AF_ij = A[i,j] * volume_flux(u_i, u[j], orientation, equations)
           du_i = du_i + AF_ij
           du[j] = du[j] - AF_ij
       end
@@ -35,8 +34,8 @@
   end
 end
 
-@inline function hadamard_sum_A_transposed!(du, ATr, volume_flux, orientation, u,
-                                            equations, sparsity_pattern::AbstractSparseMatrix{Bool})
+@inline function hadamard_sum!(du, A, volume_flux, orientation, u,
+                               equations, sparsity_pattern::AbstractSparseMatrix{Bool})
   n = size(sparsity_pattern, 2)
   rows = rowvals(sparsity_pattern)
   for i in 1:n
@@ -45,7 +44,7 @@ end
     for id in nzrange(sparsity_pattern, i)
       j = rows[id]
       if j > i
-        AF_ij = ATr[j,i] * volume_flux(u_i, u[j], orientation, equations)
+        AF_ij = A[i,j] * volume_flux(u_i, u[j], orientation, equations)
         du_i = du_i + AF_ij
         du[j] = du[j] - AF_ij
       end
@@ -60,35 +59,33 @@ end
 # combinations of differentiation operators on-the-fly in an allocation-free manner.
 function build_lazy_physical_derivative(element, orientation,
                                         mesh::VertexMappedMesh{2}, dg, cache)
-  @unpack Qrst_skew_Tr = cache
+  @unpack Qrst_skew = cache
   @unpack rxJ, sxJ, ryJ, syJ = mesh.md
-  QrskewTr, QsskewTr = Qrst_skew_Tr
   if orientation == 1
-    return LazyMatrixLinearCombo(Qrst_skew_Tr, 2 .* (rxJ[1,element], sxJ[1,element]))
+    return LazyMatrixLinearCombo(Qrst_skew, 2 .* (rxJ[1,element], sxJ[1,element]))
   elseif orientation == 2
-    return LazyMatrixLinearCombo(Qrst_skew_Tr, 2 .* (ryJ[1,element], syJ[1,element]))
+    return LazyMatrixLinearCombo(Qrst_skew, 2 .* (ryJ[1,element], syJ[1,element]))
   end
 end
 
 function build_lazy_physical_derivative(element, orientation,
                                         mesh::VertexMappedMesh{3}, dg, cache)
-  @unpack Qrst_skew_Tr = cache
-  QrskewTr, QsskewTr, QtskewTr = Qrst_skew_Tr
+  @unpack Qrst_skew = cache
   @unpack rxJ, sxJ, txJ, ryJ, syJ, tyJ, rzJ, szJ, tzJ = mesh.md
   if orientation == 1
-    return LazyMatrixLinearCombo(Qrst_skew_Tr, 2 .* (rxJ[1,element], sxJ[1,element], txJ[1,element]))
+    return LazyMatrixLinearCombo(Qrst_skew, 2 .* (rxJ[1,element], sxJ[1,element], txJ[1,element]))
   elseif orientation == 2
-    return LazyMatrixLinearCombo(Qrst_skew_Tr, 2 .* (ryJ[1,element], syJ[1,element], tyJ[1,element]))
+    return LazyMatrixLinearCombo(Qrst_skew, 2 .* (ryJ[1,element], syJ[1,element], tyJ[1,element]))
   elseif orientation == 3
-    return LazyMatrixLinearCombo(Qrst_skew_Tr, 2 .* (rzJ[1,element], szJ[1,element], tzJ[1,element]))
+    return LazyMatrixLinearCombo(Qrst_skew, 2 .* (rzJ[1,element], szJ[1,element], tzJ[1,element]))
   end
 end
 
 function compute_flux_differencing_SBP_matrices(dg::DGMulti)
   rd = dg.basis
   Qrst_hybridized, VhP, Ph = StartUpDG.hybridized_SBP_operators(rd)
-  Qrst_skew_Tr = map(A -> -0.5 * (A - A'), Qrst_hybridized)
-  return Qrst_skew_Tr, VhP, Ph
+  Qrst_skew = map(A -> 0.5 * (A - A'), Qrst_hybridized)
+  return Qrst_skew, VhP, Ph
 end
 
 function compute_flux_differencing_SBP_matrices(dg::DGMultiFluxDiff{<:SBP})
@@ -96,7 +93,7 @@ function compute_flux_differencing_SBP_matrices(dg::DGMultiFluxDiff{<:SBP})
   @unpack M, Drst, Pq = rd
   Qrst = map(D -> M * D, Drst)
   Qrst_skew = map(A -> 0.5 * (A - A'), Qrst)
-  return permutedims.(Qrst_skew)
+  return Qrst_skew
 end
 
 # precompute sparsity pattern for optimized flux differencing routines for tensor product elements
@@ -114,8 +111,8 @@ function create_cache(mesh::VertexMappedMesh, equations, dg::DGMultiFluxDiff{<:S
   md = mesh.md
 
   # for use with flux differencing schemes
-  Qrst_skew_Tr = compute_flux_differencing_SBP_matrices(dg)
-  sparsity_pattern = compute_sparsity_pattern(Qrst_skew_Tr, dg)
+  Qrst_skew = compute_flux_differencing_SBP_matrices(dg)
+  sparsity_pattern = compute_sparsity_pattern(Qrst_skew, dg)
 
   nvars = nvariables(equations)
 
@@ -141,8 +138,8 @@ function create_cache(mesh::VertexMappedMesh, equations, dg::DGMultiFluxDiff{<:P
   rd = dg.basis
   @unpack md = mesh
 
-  Qrst_skew_Tr, VhP, Ph = compute_flux_differencing_SBP_matrices(dg)
-  sparsity_pattern = compute_sparsity_pattern(Qrst_skew_Tr, dg)
+  Qrst_skew, VhP, Ph = compute_flux_differencing_SBP_matrices(dg)
+  sparsity_pattern = compute_sparsity_pattern(Qrst_skew, dg)
 
   nvars = nvariables(equations)
 
@@ -168,7 +165,7 @@ function create_cache(mesh::VertexMappedMesh, equations, dg::DGMultiFluxDiff{<:P
   fluxdiff_local_threaded = [zeros(SVector{nvars, uEltype}, num_quad_points_total) for _ in 1:Threads.nthreads()]
   rhs_local_threaded = [allocate_nested_array(uEltype, nvars, (num_quad_points_total,))  for _ in 1:Threads.nthreads()]
 
-  return (; md, Qrst_skew_Tr, sparsity_pattern, VhP, Ph, invJ = inv.(md.J),
+  return (; md, Qrst_skew, sparsity_pattern, VhP, Ph, invJ = inv.(md.J),
             entropy_var_values, projected_entropy_var_values, entropy_projected_u_values,
             u_values, u_face_values,  flux_face_values,
             local_values_threaded, fluxdiff_local_threaded, rhs_local_threaded)
@@ -203,13 +200,13 @@ function calc_volume_integral!(du, u, volume_integral,
     fill!(fluxdiff_local, zero(eltype(fluxdiff_local)))
     u_local = view(u, :, e)
     for i in eachdim(mesh)
-      Qi_skew_Tr = build_lazy_physical_derivative(e, i, mesh, dg, cache)
+      Qi_skew = build_lazy_physical_derivative(e, i, mesh, dg, cache)
 
-      # use `sparsity_pattern` to dispatch hadamard_sum_A_transposed. If using Tri or Tet elements,
-      # the Qi_skew_Tr matrices are dense, and sparsity_pattern = nothing. If using Quad or Hex
+      # use `sparsity_pattern` to dispatch hadamard_sum!. If using Tri or Tet elements, the
+      # Qi_skew matrices are dense, and sparsity_pattern = nothing. If using Quad or Hex
       # elements with an SBP approximationType, then sparsity_pattern::AbstractSparseMatrix{Bool}.
-      hadamard_sum_A_transposed!(fluxdiff_local, Qi_skew_Tr, volume_flux, i,
-                                 u_local, equations, sparsity_pattern)
+      hadamard_sum!(fluxdiff_local, Qi_skew, volume_flux, i,
+                    u_local, equations, sparsity_pattern)
     end
 
     for i in each_quad_node(mesh, dg, cache)
@@ -226,7 +223,7 @@ function calc_volume_integral!(du, u, volume_integral,
   @unpack fluxdiff_local_threaded, rhs_local_threaded = cache
   @unpack volume_flux = volume_integral
 
-  # skips subblock of Qi_skew_Tr which we know is zero by construction
+  # skips subblock of Qi_skew which we know is zero by construction
   skip_index(i,j) = i > rd.Nq && j > rd.Nq
 
   # Todo: simplices. Dispatch on curved/non-curved mesh types, this code only works for affine meshes (accessing rxJ[1,e],...)
@@ -235,9 +232,9 @@ function calc_volume_integral!(du, u, volume_integral,
     fill!(fluxdiff_local, zero(eltype(fluxdiff_local)))
     u_local = view(entropy_projected_u_values, :, e)
     for i in eachdim(mesh)
-      Qi_skew_Tr = build_lazy_physical_derivative(e, i, mesh, dg, cache)
-      hadamard_sum_A_transposed!(fluxdiff_local, Qi_skew_Tr, volume_flux, i,
-                                 u_local, equations, sparsity_pattern, skip_index)
+      Qi_skew = build_lazy_physical_derivative(e, i, mesh, dg, cache)
+      hadamard_sum!(fluxdiff_local, Qi_skew, volume_flux, i,
+                    u_local, equations, sparsity_pattern, skip_index)
     end
 
     # convert fluxdiff_local::Vector{<:SVector} to StructArray{<:SVector}
