5536cbc17f32f144054baa547f59722db327fe82
Jesse Chan <jesse.chan@rice.edu>
2021-08-12 21:44:09 -0500

    reorganizing how dgmulti files are included

---
+++ src/solvers/dgmulti.jl
@@ -0,0 +1,5 @@
+# includes solver files for DGMulti solvers
+include("dgmulti/types.jl")
+include("dgmulti/dg.jl")
+include("dgmulti/flux_differencing.jl")
+# include("dgmulti/gauss_collocation.jl")
