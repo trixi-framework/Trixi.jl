e881bdfd2a09162bb74df206fd4b684381457976
Jesse Chan <jesse.chan@rice.edu>
2021-08-17 13:36:53 -0500

    adding some unit tests

--- test/test_visualization.jl
+++ test/test_visualization.jl
@@ -270,8 +270,14 @@ isdir(outdir) && rm(outdir, recursive=true)
 
   @testset "Makie visualization tests for UnstructuredMesh2D" begin
     @test_nowarn_debug trixi_include(@__MODULE__, joinpath(examples_dir(), "unstructured_2d_dgsem", "elixir_euler_wall_bc.jl"))
-    @test_nowarn_debug Makie.plot(sol) # test heatmap plot
     @test_nowarn_debug Trixi.iplot(sol) # test interactive surface plot
+    @test_nowarn_debug Makie.plot(sol) # test heatmap plot
+
+    fa = Makie.plot(sol) # test heatmap plot
+    fig, axes = fa # test unpacking/iteration for FigureAndAxes
+    @test_nowarn_debug Base.show(fa) === nothing
+    @test_nowarn_debug typeof(fig) <: Makie.Figure
+    @test_nowarn_debug typeof(axes) <: AbstractArray{<:Makie.Axis}
   end
 end
 
