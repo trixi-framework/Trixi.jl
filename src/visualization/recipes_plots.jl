2bc9b9eb44a25d4dcdd1f2471eadf174186b5810
Michael Schlottke-Lakemper <michael@sloede.com>
2021-08-19 07:55:04 +0200

    Update src/visualization/recipes_plots.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/visualization/recipes_plots.jl
+++ src/visualization/recipes_plots.jl
@@ -194,9 +194,9 @@ end
 # Note: If you change the defaults values here, you need to also change them in the PlotData1D or PlotData2D
 #       constructor.
 RecipesBase.@recipe function f(u, semi::AbstractSemidiscretization;
-                   solution_variables=nothing,
-                   grid_lines=true, max_supported_level=11, nvisnodes=nothing, slice=:xy,
-                   point=(0.0, 0.0, 0.0), curve=nothing)
+                               solution_variables=nothing,
+                               grid_lines=true, max_supported_level=11, nvisnodes=nothing, slice=:xy,
+                               point=(0.0, 0.0, 0.0), curve=nothing)
   # Create a PlotData1D or PlotData2D object depending on the dimension.
   if ndims(semi) == 1
     return PlotData1D(u, semi; solution_variables, nvisnodes, slice, point, curve)
