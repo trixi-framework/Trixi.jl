4a3997211a5aabcb16577a9ea7a744dd5e39f4ec
Lucas Mager <59776189+MagLuc@users.noreply.github.com>
2021-08-20 18:59:44 +0200

    Changing adding some examples for the 1d Navier-Stokes eq.

--- src/equations/hyperbolic_navier_stokes_1d.jl
+++ src/equations/hyperbolic_navier_stokes_1d.jl
@@ -42,32 +42,85 @@ default_analysis_errors(::HyperbolicNavierStokesEquations1D) = (:l2_error, :linf
   abs(du[1])
 end
 
+@inline function initial_condition_constructed_periodic(x, t, equations::HyperbolicNavierStokesEquations1D)
+  @unpack gamma, Pr, L, Tinf, C, Minf, Reinf = equations
+
+  sinx, cosx = sincos(x[1])
+
+  rho = (sinx + 2.0)^2
+  v1 = sinx + 2.0
+  E = rho
+  p = (gamma - 1) * rho^2 / 2
+  T = gamma * p / rho
+  mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
+  tau = 4/3 * mu * cosx
+  q = -mu*gamma/Pr * v1*cosx
+
+  return SVector(rho, rho*v1, rho*E, tau, q)
+end
+
+@inline function source_terms_constructed_periodic(u, x, t, equations::HyperbolicNavierStokesEquations1D)
+  u1, u2, u3, u4, u5 = u
+  @unpack gamma, Pr, L, Tinf, C, Minf, Reinf = equations
+
+  sinx, cosx = sincos(x[1])
+
+  rho = (sinx + 2.0)^2
+  v1 = sinx + 2.0
+  E = rho
+  p = (gamma - 1) * rho^2/2
+  pd = 2 * (gamma - 1) * v1^3 *cosx
+  T = gamma * p / rho
+  Td = gamma * (gamma - 1) * v1 * cosx
+  mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
+  mud = mu * Td * (1.5/T - 1/(T+C/Tinf))
+  tau = 4/3 * mu * cosx
+  taud = 4/3 * (mud*cosx - mu*sinx)
+  q = -mu*gamma/Pr * v1*cosx
+  qd = -gamma/Pr * (mud * v1*cosx + mu * (cosx^2-v1*sinx))
+
+  mu_v = 4/3 * mu     # viscosity of stress
+  mu_h = gamma*mu/Pr  # viscosity of heat flux
+
+  dq1 = 3 * rho * cosx
+  dq2 = 4 * v1^3 * cosx + pd - taud
+  dq3 = 2.5 * (gamma + 1) * v1^4 * cosx - taud*v1 - tau*cosx + qd
+  dq4 = -tau / mu_v
+  dq5 = -q / mu_h
+
+  return SVector(dq1, dq2, dq3, dq4, dq5)
+end
+
 """
     initial_condition_constructed_exp(x, t, equations::HyperbolicNavierStokesEquations1D)
 
-A non-priodic smooth initial condition.
-Can be used in combination with [`source_terms_constructed_exp`](@ref) and BoundaryConditionDirichlet.
+A non-priodic smooth initial condition. In the initial guess the primal variables
+density ρ, velocity v and specific total energy E follow linear functions.
+The function can be used in combination with [`source_terms_constructed_exp`](@ref) and BoundaryConditionDirichlet.
 The primal variables follow exponential functions.
 """
 @inline function initial_condition_constructed_exp(x, t, equations::HyperbolicNavierStokesEquations1D)
   @unpack gamma, Pr, L, Tinf, C, Minf, Reinf = equations
 
-  rho = exp(x[1])
-  v1 = exp(0.5*x[1])
-  E = rho
-  p = (gamma - 1) * 0.5 * exp(2*x[1])
-  T = gamma * (gamma-1) * rho/2
-  mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
-  tau = 2/3 * mu * v1
-  q = -mu*gamma/Pr * T
+  if iszero(t)
+    rho = (exp(1.0) - 1.0)*x[1] + 1.0
+    v1 = (exp(0.5) - 1.0)*x[1] + 1.0
+    E = rho
+    T = gamma * (gamma-1) * 0.5 * exp(x[1])
+    mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
+    tau = 2/3 * mu * v1
+    q = -mu/((gamma-1)*Pr) * T
+  else
 
-  if t == 0
-    delta = 0.01
-    rho *= (1+delta)
-    v1 *=(1-delta)
-    E *= (1+delta)
-    tau *= (1+delta/2)
-    q *= (1-delta/2)
+    rho = exp(x[1])
+    v1 = exp(0.5*x[1])
+    E = rho
+    p = (gamma - 1) * 0.5 * exp(2*x[1])
+    T = gamma * (gamma-1) * rho/2
+    mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
+    tau = 2/3 * mu * v1
+    q = -mu/((gamma-1)*Pr) * T
+
   end
 
   return SVector(rho, rho*v1, rho*E, tau, q)
@@ -90,7 +143,7 @@ BoundaryConditionDirichlet.
   p = (gamma - 1) * 0.5 * exp(2*x[1])
   T = gamma * (gamma-1) * rho/2
   mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
-  mud = mu * (3/2 - T/(T+C/Tinf))
+  mud = mu * (1.5 - T/(T+C/Tinf))
   tau = 2/3 * mu * v1
   taud = 1/3 * v1 * (mu + 2 * mud)
   q = -mu/((gamma-1)*Pr) * T
@@ -102,10 +155,123 @@ BoundaryConditionDirichlet.
   dq1 = 3/2 * exp(3/2 * x[1])
   dq2 = exp(2*x[1])*(gamma+1) - taud
   dq3 = 5/4*(gamma+1)*exp(5/2*x[1]) - v1 * (taud + 0.5*tau) + qd
+  dq4 = -tau / mu_v
+  dq5 = -q / mu_h
+
+  return SVector(dq1, dq2, dq3, dq4, dq5)
+end
+
+"""
+    initial_condition_constructed_exp2(x, t, equations::HyperbolicNavierStokesEquations1D)
+
+A non-priodic smooth initial condition.
+Can be used in combination with [`source_terms_constructed_exp`](@ref) and BoundaryConditionDirichlet.
+The primal variables follow exponential functions.
+"""
+@inline function initial_condition_constructed_exp2(x, t, equations::HyperbolicNavierStokesEquations1D)
+  @unpack gamma, Pr, L, Tinf, C, Minf, Reinf = equations
+
+  rho = exp(x[1])
+  v1 = exp(-0.5*x[1])
+  E = 1/rho
+  p = (gamma - 1) * 0.5
+  T = gamma * (gamma-1) * E/2
+  mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
+  tau = -2/3 * mu * v1
+  q = mu*gamma/Pr * 0.5 * E
+
+  if iszero(t)
+    delta = 0.01
+    rho *= (1+delta)
+    v1 *=(1-delta)
+    E *= (1+delta)
+    tau *= (1+delta/2)
+    q *= (1-delta/2)
+  end
+
+  return SVector(rho, rho*v1, rho*E, tau, q)
+end
+
+"""
+    source_terms_constructed_exp2(u, x, t, equations::HyperbolicNavierStokesEquations1D)
+
+Source terms that include the forcing function `f(x)` and right hand side for the hyperbolic
+Navier-Stokes system that is used with [`initial_condition_constructed_exp`](@ref) and
+BoundaryConditionDirichlet.
+"""
+@inline function source_terms_constructed_exp2(u, x, t, equations::HyperbolicNavierStokesEquations1D)
+  u1, u2, u3, u4, u5 = u
+  @unpack gamma, Pr, L, Tinf, C, Minf, Reinf = equations
+
+  rho = exp(x[1])
+  v1 = exp(-0.5*x[1])
+  E = 1/rho
+  p = (gamma - 1) * 0.5
+  T = gamma * (gamma-1) * E/2
+  Td = -T
+  mu = Minf/Reinf * (1+C/Tinf)/(T+C/Tinf) * T^(1.5)
+  mud = mu * Td * (1.5/T - 1/(T+C/Tinf))
+  tau = -2/3 * mu * v1
+  taud = 1/3 * v1 * (mu - 2 * mud)
+  q = mu*gamma/Pr * 0.5 * E
+  qd = gamma/Pr * 0.5 * E * (mud - mu)
+
+  mu_v = 4/3 * mu     # viscosity of stress
+  mu_h = gamma*mu/Pr  # viscosity of heat flux
+
+  dq1 = exp(1/2 * x[1])/2
+  dq2 = -taud
+  dq3 = -0.25*(gamma+1)*v1 - v1 * (taud - 0.5*tau) + qd
   dq4 = -u4 / mu_v
   dq5 = -u5 / mu_h
 
   return SVector(dq1, dq2, dq3, dq4, dq5)
+end
+
+"""
+  boundary_condition_viscous_shock(u_inner, orientation, direction, x, t,
+                                                surface_flux_function,
+                                                equations::HyperbolicNavierStokesEquations1D)
+
+Boundary conditions used for the Navier-Stokes viscous shock example.
+The exact solution at the boundarys is calculated as explained in
+  Masatsuka (2013)
+  I Do Like CFD, Too: Vol 1.
+  Freely available at [http://www.cfdbooks.com/](http://www.cfdbooks.com/)
+"""
+function boundary_condition_viscous_shock(u_inner, orientation, direction, x, t,
+                                                surface_flux_function,
+                                                equations::HyperbolicNavierStokesEquations1D)
+  @unpack gamma = equations
+  M_left = 3.5 # Mach number for the left boundary
+
+  if direction == 2 # u_inner is "left" of boundary, u_boundary is "right" of boundary
+
+    p = 1/gamma + 2/(gamma+1)*(M_left^2-1) # pressure in the right boundary
+
+    u1 = (gamma+1)*M_left^2 / ((gamma-1)*M_left^2+2)
+    u2 = M_left
+    u3 = p/(gamma-1) + 0.5*u2^2/u1
+    u4 = 0.0
+    u5 = 0.0
+    u_boundary = SVector(u1, u2, u3, u4, u5)
+
+    # Calculate boundary flux
+    flux = surface_flux_function(u_inner, u_boundary, orientation, equations)
+  else # u_boundary is "left" of boundary, u_inner is "right" of boundary
+
+    u1 = 1.0
+    u2 = M_left
+    u3 = 1.0/(gamma*(gamma-1)) + 0.5*u2^2/u1
+    u4 = 0.0
+    u5 = 0.0
+    u_boundary = SVector(u1, u2, u3, u4, u5)
+
+    # Calculate boundary flux
+    flux = surface_flux_function(u_boundary, u_inner, orientation, equations)
+  end
+
+  return flux
 end
 
 @inline function source_terms_harmonic(u, x, t, equations::HyperbolicNavierStokesEquations1D)
@@ -120,40 +286,6 @@ end
   dq4 = -u4 / mu_v
   dq5 = -u5 / mu_h
   return SVector(zero(dq4), zero(dq4), zero(dq4), dq4, dq5)
-end
-
-"""
-    initial_condition_weak_blast_wave(x, t, equations::HyperbolicNavierStokesEquations1D)
-
-A weak blast wave taken from
-- Sebastian Hennemann, Gregor J. Gassner (2020)
-  A provably entropy stable subcell shock capturing approach for high order split form DG
-  [arXiv: 2008.12044](https://arxiv.org/abs/2008.12044)
-"""
-function initial_condition_weak_blast_wave(x, t, equations::HyperbolicNavierStokesEquations1D)
-  # From Hennemann & Gassner JCP paper 2020 (Sec. 6.3)
-  # Set up polar coordinates
-  inicenter = SVector(0.0)
-  x_norm = x[1] - inicenter[1]
-  r = abs(x_norm)
-  # The following code is equivalent to
-  # phi = atan(0.0, x_norm)
-  # cos_phi = cos(phi)
-  # in 1D but faster
-  cos_phi = x_norm > 0 ? one(x_norm) : -one(x_norm)
-
-  # Calculate primitive variables
-  rho = r > 0.5 ? 1.0 : 1.1691
-  v1  = r > 0.5 ? 0.0 : 0.1882 * cos_phi
-  p   = r > 0.5 ? 1.0 : 1.245
-
-  u1 = rho
-  u2 = rho*v1
-  u3 = p/(equations.gamma-1) + 0.5 * (u2 * v1)
-  u4 = zero(u1)
-  u5 = zero(u1)
-
-  return SVector(u1, u2, u3, u4, u5)
 end
 
 # Calculate 1D flux in for a single point
@@ -249,6 +381,39 @@ end
 # Calculate total energy for a conservative state `u`
 @inline function energy_total(u, equations::HyperbolicNavierStokesEquations1D)
   return u[3]/u[1]
+end
+
+"""
+    calc_viscous_shock_solution
+
+    The fuction calls a fortran routine which calculates the solution of the
+    viscous shock example. The grid on which the solution is calculated is the same
+    as the grid used for Trixis DGSEM solver. The grid is defined by the input
+    variables.
+"""
+
+const lib_path = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "NavierStokes1d_fortran", "ns_shock_lobatto.so")
+
+@inline function calc_viscous_shock_solution(polydeg, refinement_level)
+
+    n_nodes = (polydeg+1)*2^refinement_level
+    epsilon = 0.0000000001
+
+    solution = Vector{Float64}(zeros(6*n_nodes))
+
+    ccall((:calc_solution, lib_path),
+        Cvoid,
+        (Ref{Int64}, Ref{Int64}, Ref{Float64}, Ptr{Float64}),
+        polydeg, refinement_level, epsilon, solution)
+
+    x = solution[1:n_nodes]
+    rho = solution[n_nodes+1:2*n_nodes]
+    rho_v1 = solution[2*n_nodes+1:3*n_nodes]
+    rho_e = solution[3*n_nodes+1:4*n_nodes]
+    tau = solution[4*n_nodes+1:5*n_nodes]
+    q = solution[5*n_nodes+1:6*n_nodes]
+
+    return x, rho, rho_v1, rho_e, tau, q
 end
 
 
