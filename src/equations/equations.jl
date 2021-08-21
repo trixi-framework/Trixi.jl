8b9a98c5e3664d8b4f6bc5386c044d30507a6cca
Jesse Chan <jesse.chan@rice.edu>
2021-08-21 17:25:57 -0500

    adding specialized BoundaryConditionWall for StructuredMesh/TreeMesh

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -176,6 +176,22 @@ struct BoundaryConditionWall{B}
   boundary_value_function::B
 end
 
+@inline function (boundary_condition::BoundaryConditionWall)(u_inner, orientation_or_normal,
+                                                             direction,
+                                                             x, t,
+                                                             surface_flux_function, equations)
+
+  u_boundary = boundary_condition.boundary_value_function(u_inner, orientation_or_normal, equations)
+
+  # Calculate boundary flux
+  if direction in (2, 4, 6) # u_inner is "left" of boundary, u_boundary is "right" of boundary
+    flux = surface_flux_function(u_inner, u_boundary, orientation_or_normal, equations)
+  else # u_boundary is "left" of boundary, u_inner is "right" of boundary
+    flux = surface_flux_function(u_boundary, u_inner, orientation_or_normal, equations)
+  end
+
+  return flux
+end
 @inline function (boundary_condition::BoundaryConditionWall)(u_inner,
                                                              normal_direction::AbstractVector,
                                                              x, t,
