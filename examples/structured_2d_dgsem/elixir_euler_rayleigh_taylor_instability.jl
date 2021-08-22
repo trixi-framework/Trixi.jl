c4d5991e1e459b28b793e825e8f50ca2291b4587
Jesse Chan <jesse.chan@rice.edu>
2021-08-22 10:26:13 -0500

    extending tspan for a "cooler looking" plot

--- examples/structured_2d_dgsem/elixir_euler_rayleigh_taylor_instability.jl
+++ examples/structured_2d_dgsem/elixir_euler_rayleigh_taylor_instability.jl
@@ -96,7 +96,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver;
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-tspan = (0.0, 1.5)
+tspan = (0.0, 1.65)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
