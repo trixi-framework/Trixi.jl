484581c7a8c602489d8b65ac60f3d121c304d8c7
Jesse Chan <jesse.chan@rice.edu>
2021-08-22 13:46:33 -0500

    updating RTI DGMulti elixir

--- examples/dgmulti_2d/elixir_euler_rayleigh_taylor_instability.jl
+++ examples/dgmulti_2d/elixir_euler_rayleigh_taylor_instability.jl
@@ -6,41 +6,87 @@ dg = DGMulti(polydeg = 3, element_type = Quad(), approximation_type = Polynomial
 
 equations = CompressibleEulerEquations2D(1.4)
 
-function create_initial_condition(;slope)
-  u0(x,t,equations) = initial_condition_rti(x, t, equations, slope)
-  return u0
-end
-initial_condition = create_initial_condition(slope = 1000)
-source_terms = source_terms_rti
+"""
+    initial_condition_rayleigh_taylor_instability(coordinates, t, equations::CompressibleEulerEquations2D)
 
-num_elements = 32
+Setup used for the Rayleigh-Taylor instability. Initial condition adapted from
+- Shi, Jing, Yong-Tao Zhang, and Chi-Wang Shu (2003).
+  Resolution of high order WENO schemes for complicated flow structures.
+  [DOI](https://doi.org/10.1016/S0021-9991(03)00094-9).
+
+- Remacle, Jean-François, Joseph E. Flaherty, and Mark S. Shephard (2003).
+  An adaptive discontinuous Galerkin technique with an orthogonal basis applied to compressible
+  flow problems.
+  [DOI](https://doi.org/10.1137/S00361445023830)
+
+The domain is [0, 0.25] x [0, 1]. Boundary conditions can be reflective wall boundary conditions on
+all boundaries or
+- periodic boundary conditions on the left/right boundaries
+- Dirichlet boundary conditions on the top/bottom boundaries
+
+This should be used together with `source_terms_rayleigh_taylor_instability`, which is
+defined below.
+"""
+@inline function initial_condition_rayleigh_taylor_instability(x, t,
+                                                               equations::CompressibleEulerEquations2D,
+                                                               slope=1000)
+  tol = 1e2*eps()
+
+  if x[2] < .5
+    p = 2*x[2] + 1
+  else
+    p = x[2] + 3/2
+  end
+
+  # smooth the discontinuity to avoid ambiguity at element interfaces
+  smoothed_heaviside(x, left, right) = left + 0.5*(1 + tanh(slope * x)) * (right-left)
+  rho = smoothed_heaviside(x[2] - 0.5, 2.0, 1.0)
+
+  c = sqrt(equations.gamma * p / rho)
+  # the velocity is multiplied by sin(pi*y)^6 as in Remacle et al. 2003 to ensure that the
+  # initial condition satisfies reflective boundary conditions at the top/bottom boundaries.
+  v = -0.025 * c * cos(8*pi*x[1]) * sin(pi*x[2])^6
+  u = 0.0
+
+  return prim2cons(SVector(rho, u, v, p), equations)
+end
+
+@inline function source_terms_rayleigh_taylor_instability(u, x, t,
+                                                          equations::CompressibleEulerEquations2D)
+  g = 1.0
+  rho, rho_v1, rho_v2, rho_e = u
+
+  return SVector(0.0, 0.0, g*rho, g*rho_v2)
+end
+
+num_elements = 16
 cells_per_dimension = (num_elements, 4 * num_elements)
 vertex_coordinates, EToV = StartUpDG.uniform_mesh(dg.basis.elementType, cells_per_dimension...)
 
 vx, vy = vertex_coordinates
-vx = map(x-> .25 * .5*(1 + x), vx) # map to [0, .25]
-vy = map(x-> .5*(1+x), vy) # map to [0, 1] for single mode RTI
+vx = map(x-> 0.25 * 0.5*(1 + x), vx) # map to [0, .25]
+vy = map(x-> 0.5 * (1+x), vy) # map to [0, 1] for single mode RTI
 vertex_coordinates = (vx, vy)
 
 # boundary_conditions = (; :entire_boundary => BoundaryConditionDirichlet(initial_condition))
 boundary_conditions = (; :entire_boundary => BoundaryConditionWall(boundary_state_slip_wall))
 
-mesh = VertexMappedMesh(vertex_coordinates, EToV, dg)
+mesh = VertexMappedMesh(vertex_coordinates, EToV, dg; is_periodic=(true,false))
+
+initial_condition = initial_condition_rayleigh_taylor_instability
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg;
-                                    source_terms = source_terms,
+                                    source_terms = source_terms_rayleigh_taylor_instability,
                                     boundary_conditions = boundary_conditions)
 
-tspan = (0.0, 1.95)
+tspan = (0.0, 3.0)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
 alive_callback = AliveCallback(alive_interval=10)
 analysis_interval = 100
 analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
-stepsize_callback = StepsizeCallback(cfl=1.)
 callbacks = CallbackSet(summary_callback,
                         analysis_callback,
-                        stepsize_callback,
                         alive_callback)
 
 ###############################################################################
