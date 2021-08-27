18ce76524c13d155dfbede5228b6232be8f5cbff
Erik Faulhaber <erik.faulhaber@web.de>
2021-08-27 16:51:04 +0200

    Fix tests again

--- test/test_structured_2d.jl
+++ test/test_structured_2d.jl
@@ -182,7 +182,7 @@ isdir(outdir) && rm(outdir, recursive=true)
 
   @trixi_testset "elixir_euler_rayleigh_taylor_instability.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_rayleigh_taylor_instability.jl"),
-      l2   = [0.07623680588263236, 0.007462660867185416, 0.0035846372175190443, 0.01074438372795356],
+      l2   = [0.07623680588263236, 0.007462660867185416, 0.0035846371501533103, 0.01074438328884887],
       linf = [1.1657734093890613, 0.043256773855258314, 0.049225387018938464, 0.03815681528560022],
       tspan = (0.0, 0.3))
   end
