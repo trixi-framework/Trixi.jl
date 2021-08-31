16eb8b1d367115a76a0efcd402f53f47cfb1687d
Lars Christmann <lars@l12n.eu>
2021-08-31 11:42:38 +0200

    Update documentation

--- src/semidiscretization/semidiscretization_euler_acoustics.jl
+++ src/semidiscretization/semidiscretization_euler_acoustics.jl
@@ -14,10 +14,10 @@
 
 Construct a semidiscretization of the acoustic perturbation equations that is coupled with
 the compressible Euler equations via source terms. Both semidiscretizations have to use the same
-mesh and solver. The coupling region is described by a function `source_region` that maps the
-coordinates of a single node to `true` or `false` depending on whether the point lies within
-the coupling region or not. A weighting function `weights` that maps coordinates to weights is
-applied to the acoustic source terms.
+mesh and solvers with a shared basis. The coupling region is described by a function `source_region`
+that maps the coordinates of a single node to `true` or `false` depending on whether the point lies
+within the coupling region or not. A weighting function `weights` that maps coordinates to weights
+is applied to the acoustic source terms.
 Note that this semidiscretization should be used in conjunction with
 [`EulerAcousticsCouplingCallback`](@ref) and only works in two dimensions.
 """
