fdadd713013f90155a410edbc9f2b7f1088ec562
Jesse Chan <jesse.chan@rice.edu>
2021-09-09 14:44:23 -0500

    moving type alias out of @muladd block

--- src/visualization/types.jl
+++ src/visualization/types.jl
@@ -1,3 +1,13 @@
+# Convenience type to allow dispatch on solution objects that were created by Trixi
+#
+# This is a union of a Trixi-specific DiffEqBase.ODESolution and of Trixi's own
+# TimeIntegratorSolution.
+#
+# Note: This is an experimental feature and may be changed in future releases without notice.
+const TrixiODESolution = Union{ODESolution{T, N, uType, uType2, DType, tType, rateType, P} where
+    {T, N, uType, uType2, DType, tType, rateType, P<:ODEProblem{uType_, tType_, isinplace, P_, F_} where
+     {uType_, tType_, isinplace, P_<:AbstractSemidiscretization, F_<:ODEFunction{true, typeof(rhs!)}}}, TimeIntegratorSolution}
+
 # By default, Julia/LLVM does not use fused multiply-add operations (FMAs).
 # Since these FMAs can increase the performance of many numerical algorithms,
 # we need to opt-in explicitly.
@@ -170,17 +180,6 @@ Extract grid lines from `pd` for plotting with `Plots.plot`.
     This is an experimental feature and may change in future releases.
 """
 getmesh(pd::AbstractPlotData) = PlotMesh(pd)
-
-
-# Convenience type to allow dispatch on solution objects that were created by Trixi
-#
-# This is a union of a Trixi-specific DiffEqBase.ODESolution and of Trixi's own
-# TimeIntegratorSolution.
-#
-# Note: This is an experimental feature and may be changed in future releases without notice.
-const TrixiODESolution = Union{ODESolution{T, N, uType, uType2, DType, tType, rateType, P} where
-    {T, N, uType, uType2, DType, tType, rateType, P<:ODEProblem{uType_, tType_, isinplace, P_, F_} where
-     {uType_, tType_, isinplace, P_<:AbstractSemidiscretization, F_<:ODEFunction{true, typeof(rhs!)}}}, TimeIntegratorSolution}
 
 
 """
