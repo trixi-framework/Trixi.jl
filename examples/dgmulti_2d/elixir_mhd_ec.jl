2316afc6c9d7528a0d99bd80012ddd81f0124632
Jesse Chan <jesse.chan@rice.edu>
2021-09-14 19:17:59 -0500

    updating DGMulti MHD elixir

--- examples/dgmulti_2d/elixir_mhd_ec.jl
+++ examples/dgmulti_2d/elixir_mhd_ec.jl
@@ -5,19 +5,17 @@ using Trixi
 ###############################################################################
 # semidiscretization of the compressible ideal GLM-MHD equations
 
-c_h = 1.0
-equations = IdealGlmMhdEquations2D(1.4, c_h)
+equations = IdealGlmMhdEquations2D(5/3)
 
 initial_condition = initial_condition_weak_blast_wave
 
-surface_flux = (flux_hindenlang_gassner, flux_nonconservative_powell)
+surface_flux = (flux_lax_friedrichs, flux_nonconservative_powell)
 volume_flux = (flux_hindenlang_gassner, flux_nonconservative_powell)
-dg = DGMulti(polydeg=1, element_type = Quad(), approximation_type = Polynomial(),
+dg = DGMulti(polydeg=3, element_type = Quad(), approximation_type = Polynomial(),
              surface_integral = SurfaceIntegralWeakForm(surface_flux),
              volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
-
-num_cells_per_dimension = 4
+num_cells_per_dimension = 16
 vertex_coordinates, EToV = StartUpDG.uniform_mesh(dg.basis.elementType, num_cells_per_dimension)
 vertex_coordinates = map(x -> 2.0 .* x, vertex_coordinates) # map domain to [-2, 2]^2
 mesh = VertexMappedMesh(vertex_coordinates, EToV, dg, is_periodic=(true, true))
@@ -27,7 +25,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg)
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-tspan = (0.0, 0.1)
+tspan = (0.0, 0.4)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
@@ -36,22 +34,22 @@ analysis_interval = 100
 analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
 alive_callback = AliveCallback(alive_interval=10, analysis_interval=analysis_interval)
 
-cfl = .50
+cfl = 1.0
 stepsize_callback = StepsizeCallback(cfl=cfl)
 
-glm_speed_callback = GlmSpeedCallback(glm_scale=0.5, cfl=.50)
+glm_speed_callback = GlmSpeedCallback(glm_scale=1.0, cfl=cfl)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback,
                         stepsize_callback,
-                        alive_callback)
-
+                        alive_callback,
+                        glm_speed_callback)
 
 ###############################################################################
 # run the simulation
 
-tol = 1.0e-8
-sol = solve(ode, RDPK3SpFSAL49(), abstol=tol, reltol=tol,
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep=false, callback=callbacks);
 
 summary_callback() # print the timer summary
