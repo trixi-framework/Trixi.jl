da79effa68d1bacd24174a285ba25b0d31d1f25d
Jesse Chan <jesse.chan@rice.edu>
2021-09-23 16:25:06 -0500

    making glm_speed_dg estimate consistent with DGMulti StepsizeCallback

--- src/callbacks_step/glm_speed_dg.jl
+++ src/callbacks_step/glm_speed_dg.jl
@@ -19,7 +19,14 @@ function calc_dt_for_cleaning_speed(cfl::Real, mesh,
                                     equations::Union{AbstractIdealGlmMhdEquations, AbstractIdealGlmMhdMulticomponentEquations},
                                     dg::DGMulti, cache)
   # compute time step for GLM linear advection equation with c_h=1 for a DGMulti discretization.
-  return cfl * estimate_dt(mesh, dg)
+  dt_min = StartUpDG.estimate_h(rd, md)
+
+  # This mimics `max_dt` for `TreeMesh`, except that `nnodes(dg)` is replaced by
+  # `polydeg+1`. This is because `nnodes(dg)` returns the total number of
+  # multi-dimensional nodes for DGMulti solver types, while `nnodes(dg)` returns
+  # the number of 1D nodes for `DGSEM` solvers.
+  polydeg = rd.N
+  return cfl * 2 * dt_min / (polydeg + 1)
 end
 
 
