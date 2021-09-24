57367d6752b2d67d31e3fb55d872d9d27006b5d6
Jesse Chan <jesse.chan@rice.edu>
2021-09-24 11:17:41 -0500

    trying adaptive timestepping for MHD SBP test

--- examples/dgmulti_2d/elixir_mhd_weak_blast_wave_SBP.jl
+++ examples/dgmulti_2d/elixir_mhd_weak_blast_wave_SBP.jl
@@ -52,8 +52,10 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-            save_everystep=false, callback=callbacks);
+# sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+#             dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+#             save_everystep=false, callback=callbacks);
+sol = solve(ode, RDPK3SpFSAL49(), abstol=1.0e-8, reltol=1.0e-8,
+            save_everystep=false, callback=callbacks, maxiters=1e5)
 
 summary_callback() # print the timer summary
