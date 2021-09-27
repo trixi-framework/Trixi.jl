a3d7cbdd59742683b1df90d2522914fbe2c8c62a
Erik Faulhaber <erik.faulhaber@web.de>
2021-09-27 17:53:08 +0200

    Increase tolerance for threaded tests

--- test/test_threaded.jl
+++ test/test_threaded.jl
@@ -50,6 +50,7 @@ Trixi.mpi_isroot() && isdir(outdir) && rm(outdir, recursive=true)
       @test_trixi_include(joinpath(examples_dir(), "structured_2d_dgsem", "elixir_advection_restart.jl"),
         l2   = [0.00016265538283059892],
         linf = [0.0015194298895079283],
+        rtol = 3e-7,
         elixir_file="elixir_advection_waving_flag.jl",
         restart_file="restart_000021.h5")
     end
