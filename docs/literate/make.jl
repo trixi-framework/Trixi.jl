4cbb324c58565464453b7bdf08cac71c26cb2b1c
Benjamin Bolm <benjamin.bolm@gmx.de>
2021-09-30 17:09:09 +0200

    Fix badge links

--- docs/literate/make.jl
+++ docs/literate/make.jl
@@ -13,10 +13,12 @@ function create_files(title, file, repo_src, pages_dir, notebooks_dir; folder=""
     nbviewer_logo = "https://raw.githubusercontent.com/jupyter/design/master/logos/Badges/nbviewer_badge.svg"
     download_logo = "https://camo.githubusercontent.com/aea75103f6d9f690a19cb0e17c06f984ab0f472d9e6fe4eadaa0cc438ba88ada/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f646f776e6c6f61642d6e6f7465626f6f6b2d627269676874677265656e"
 
-    # TODO: tutorials. Fix the links for tutorials in Trixi, not TrixiTutorials
-    binder_url   = "https://mybinder.org/v2/gh/trixi-framework/TrixiTutorials/gh-pages?filepath=dev/notebooks/$notebook_filename"
-    nbviewer_url = "https://nbviewer.jupyter.org/github/trixi-framework/TrixiTutorials/blob/gh-pages/dev/notebooks/$notebook_filename"
-    download_url = "https://raw.githubusercontent.com/trixi-framework/TrixiTutorials/gh-pages/dev/notebooks/$notebook_filename"
+    # TODO: tutorials. Caching notebooks in separate branch in the future.
+    branch = "gh-pages"
+    notebook_path = "previews/PR873/notebooks/$notebook_filename"
+    binder_url   = "https://mybinder.org/v2/gh/trixi-framework/Trixi.jl/$branch?filepath=$notebook_path"
+    nbviewer_url = "https://nbviewer.jupyter.org/github/trixi-framework/Trixi.jl/blob/$branch/$notebook_path"
+    download_url = "https://raw.githubusercontent.com/trixi-framework/Trixi.jl/$branch/$notebook_path"
 
     binder_badge   = "# [![]($binder_logo)]($binder_url)"
     nbviewer_badge = "# [![]($nbviewer_logo)]($nbviewer_url)"
@@ -40,7 +42,7 @@ function create_tutorials(files)
     repo_src        = joinpath(@__DIR__, "src", "files")
 
     pages_dir       = joinpath(@__DIR__, "..", "src", "tutorials")
-    notebooks_dir   = joinpath(@__DIR__, "src", "notebooks")
+    notebooks_dir   = joinpath(@__DIR__, "..", "src", "notebooks")
 
     Sys.rm(pages_dir;       recursive=true, force=true)
     Sys.rm(notebooks_dir;   recursive=true, force=true)
