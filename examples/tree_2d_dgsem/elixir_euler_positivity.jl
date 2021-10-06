a3b7557140f3d3e3e30b557be199a5b0c8376e75
Hendrik Ranocha <mail@ranocha.de>
2021-10-06 15:39:05 +0200

    revert accidental change of elixir_euler_positivity.jl

--- examples/tree_2d_dgsem/elixir_euler_positivity.jl
+++ examples/tree_2d_dgsem/elixir_euler_positivity.jl
@@ -101,12 +101,13 @@ callbacks = CallbackSet(summary_callback,
 limiter! = PositivityPreservingLimiterZhangShu(thresholds=(5.0e-6, 5.0e-6),
                                                variables=(Trixi.density, pressure))
 stage_limiter! = limiter!
+step_limiter!  = limiter!
 
 
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, CarpenterKennedy2N54(stage_limiter!, williamson_condition=false),
+sol = solve(ode, CarpenterKennedy2N54(stage_limiter!, step_limiter!, williamson_condition=false),
             dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep=false, callback=callbacks, maxiters=1e5);
 summary_callback() # print the timer summary
