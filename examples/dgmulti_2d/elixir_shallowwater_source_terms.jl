a9a3e3811d98f094eda1df190fcc70d4b486ff77
Jesse Chan <jchan985@gmail.com>
2021-10-20 15:41:42 -0500

    Supplement DGMulti MHD tests with SWE (#922)
    * adding SWE elixir
    
    * adding tests for SWE (test noncon terms)
    
    * add comment to tests
    
    * adding clarifying comment
    
    * Apply suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * organizing tests
    
    * renaming elixir
    
    * reorganizing tests
    
    * fixing test names
    
    * actually updating tests with adaptive timestepping
    
    Co-authored-by: Jesse Chan <jesse.chan@rice.edu>
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

---
+++ examples/dgmulti_2d/elixir_shallowwater_source_terms.jl
@@ -0,0 +1,47 @@
+using OrdinaryDiffEq
+using Trixi
+
+###############################################################################
+# semidiscretization of the shallow water equations
+
+equations = ShallowWaterEquations2D(gravity_constant=9.81)
+
+initial_condition = initial_condition_convergence_test
+
+volume_flux = (flux_wintermeyer_etal, flux_nonconservative_wintermeyer_etal)
+surface_flux = (flux_lax_friedrichs, flux_nonconservative_fjordholm_etal)
+dg = DGMulti(polydeg=3, element_type = Quad(), approximation_type = SBP(),
+             surface_integral = SurfaceIntegralWeakForm(surface_flux),
+             volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
+
+cells_per_dimension = 8
+vertex_coordinates, EToV = StartUpDG.uniform_mesh(dg.basis.elementType, cells_per_dimension)
+vertex_coordinates = map(x -> sqrt(2) .* 0.5 .* (1 .+ x), vertex_coordinates) # map domain to [0, sqrt(2)]^2
+mesh = VertexMappedMesh(vertex_coordinates, EToV, dg, is_periodic=(true, true))
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg;
+                                    source_terms=source_terms_convergence_test)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 0.4)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback,
+                        alive_callback)
+
+###############################################################################
+# run the simulation
+
+sol = solve(ode, RDPK3SpFSAL49(), abstol=1.0e-7, reltol=1.0e-7,
+            save_everystep=false, callback=callbacks);
+
+summary_callback() # print the timer summary
