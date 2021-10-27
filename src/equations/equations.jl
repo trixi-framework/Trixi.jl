9e59e4247533f35c47b54087b3ad141bbb485655
xzczd <xzczd@users.noreply.github.com>
2021-10-27 20:22:17 +0800

    Update src/equations/equations.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -231,15 +231,11 @@ function entropy end
     cons2entropy(u, equations)
 
 Convert the conserved variables `u` to the entropy variables for a given set of
-`equations` with chosen standard [`entropy`](@ref). `u` is a vector type of the correct length `nvariables(equations)`. 
+`equations` with chosen standard [`entropy`](@ref). 
+`u` is a vector type of the correct length `nvariables(equations)`.
 Notice the function doesn't include any error checks for the purpose of efficiency, 
-so please make sure your input is correct. To apply the function to e.g. `sol.u[end]`, you may use
-```
-data = reinterpret(SVector{nvariables(equations), Float64}, sol.u[end])
-cons2entropy.(data, equations)
-```  
-The inverse conversion is
-performed by [`entropy2cons`](@ref).
+so please make sure your input is correct. 
+The inverse conversion is performed by [`entropy2cons`](@ref).
 """
 function cons2entropy end
 
