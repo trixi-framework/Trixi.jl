fd091f2c3ab7c2d2e58bcd200b66c82f3ff65abf
xzczd <xzczd@users.noreply.github.com>
2021-10-28 19:27:38 +0800

    Update compressible_euler_multicomponent_2d.jl
    Formula preview:
    
    ![CodeCogsEqn (5)](https://user-images.githubusercontent.com/7373095/139247017-8e6714dc-7066-431a-93ba-ccad3b59fbbb.gif)
    ![CodeCogsEqn (6)](https://user-images.githubusercontent.com/7373095/139247018-7671107a-a22a-424a-99ad-eed2b6baf306.gif)
    ![CodeCogsEqn (7)](https://user-images.githubusercontent.com/7373095/139247022-8fb38581-635e-4775-9be3-1a5c973ba55c.gif)
    ![CodeCogsEqn (8)](https://user-images.githubusercontent.com/7373095/139247023-dfb74a4a-b2a9-4d57-803a-a90116755386.gif)
    ![CodeCogsEqn (9)](https://user-images.githubusercontent.com/7373095/139247011-00c909a7-a077-4068-a35d-3205e38cd944.gif)

--- src/equations/compressible_euler_multicomponent_2d.jl
+++ src/equations/compressible_euler_multicomponent_2d.jl
@@ -12,32 +12,38 @@ Multicomponent version of the compressible Euler equations
 ```math
 \partial t
 \begin{pmatrix}
-\rho v_1 \\ \rho v_2 \\ E \\ \rho_1 \\ \rho_2 \\ \vdots \\ \rho_{n}
+\rho v_1 \\ \rho v_2 \\ \rho e \\ \rho_1 \\ \rho_2 \\ \vdots \\ \rho_{n}
 \end{pmatrix}
 +
 \partial x
 \begin{pmatrix}
-\rho v_1^2 + p \\ \rho v_1 v_2 \\ (E+p) v_1 \\ \rho_1 v_1 \\ \rho_2 v_1 \\ \vdots \\ \rho_{n} v_1
+\rho v_1^2 + p \\ \rho v_1 v_2 \\ ( \rho e +p) v_1 \\ \rho_1 v_1 \\ \rho_2 v_1 \\ \vdots \\ \rho_{n} v_1
 \end{pmatrix}
 +
 \partial y
 \begin{pmatrix}
-\rho v_1 v_2 \\ \rho v_2^2 + p \\ (E+p) v_2 \\ \rho_1 v_2 \\ \rho_2 v_2 \\ \vdots \\ \rho_{n} v_2
+\rho v_1 v_2 \\ \rho v_2^2 + p \\ ( \rho e +p) v_2 \\ \rho_1 v_2 \\ \rho_2 v_2 \\ \vdots \\ \rho_{n} v_2
 \end{pmatrix}
 =
 \begin{pmatrix}
 0 \\ 0 \\ 0 \\ 0 \\ 0 \\ \vdots \\ 0
 \end{pmatrix}
 ```
-for calorically perfect gas in two space dimensions. Here 
+for calorically perfect gas in two space dimensions. 
+Here, ``\rho_i`` is the density of component ``i``, ``\rho=\sum_{i=1}^n\rho_i`` the sum of the individual ``\rho_i``, 
+``v_1``, ``v_2`` the velocities, ``e`` the specific total energy **rather than** specific internal energy, and
 ```math
-\begin{aligned}
-&\rho=\sum_{i=1}^n\rho_i \\
-&E=\frac{p}{\gamma-1}+\frac{1}{2}\rho (v_1^2 +v_2^2) \\ 
-&\gamma=\frac{\sum_{i=1}^n\rho_i C_{v,i}\gamma_i}{\sum_{i=1}^n\rho_i C_{v,i}} \\ 
-&C_{v,i}=\frac{R}{\gamma_i-1}
-\end{aligned}
+p = (\gamma - 1) \left( \rho e - \frac{1}{2} \rho (v_1^2 + v_2^2) \right)
 ```
+the pressure,
+```math
+\gamma=\frac{\sum_{i=1}^n\rho_i C_{v,i}\gamma_i}{\sum_{i=1}^n\rho_i C_{v,i}} 
+```
+total heat capacity ratio, ``\gamma_i`` heat capacity ratio of component ``i``,
+```math
+C_{v,i}=\frac{R}{\gamma_i-1}
+```
+specific heat capacity at constant volume of component ``i``.
 
 In case of more than one component, the specific heat ratios `gammas` and the gas constants
 `gas_constants` in [kJ/(kg*K)] should be passed as tuples, e.g., `gammas=(1.4, 1.667)`.
@@ -66,8 +72,18 @@ end
 
 function CompressibleEulerMulticomponentEquations2D(; gammas, gas_constants)
 
-  _gammas        = promote((.0 .+ gammas)...)
-  _gas_constants = promote((.0 .+ gas_constants)...)
+  _gammas                 = promote(gammas...)
+        
+    if isa(_gammas[1], Integer) 
+      _gammas = _gammas .+ .0 
+    end
+        
+  _gas_constants          = promote(gas_constants...)
+        
+    if isa( _gas_constants[1], Integer) 
+       _gas_constants =  _gas_constants .+ .0 
+    end
+        
   RealT          = promote_type(eltype(_gammas), eltype(_gas_constants))
 
   NVARS = length(_gammas) + 3
