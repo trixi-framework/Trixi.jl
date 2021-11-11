f290f3fb758da650dadb9d6ecf7b2455516d2721
Jesse Chan <jesse.chan@rice.edu>
2021-11-11 13:47:27 -0600

    making `get_contravariant_vector` return a SVector instead of Float64

--- src/solvers/dgmulti/flux_differencing.jl
+++ src/solvers/dgmulti/flux_differencing.jl
@@ -182,7 +182,7 @@ end
 # combination of reference derivatives.
 @inline function get_contravariant_vector(element, orientation, mesh::VertexMappedMesh{1})
   @unpack rxJ = mesh.md
-  return 2 * rxJ[1, element] # the 1D contravariant vector reduces to a scaling.
+  return 2 * SVector(rxJ[1, element]) # the 1D contravariant vector reduces to a scaling.
 end
 
 @inline function get_contravariant_vector(element, orientation, mesh::VertexMappedMesh{2})
