8dd7c003dd745b612dfdb0c30d73afea64c9c003
Hendrik Ranocha <mail@ranocha.de>
2021-11-23 12:58:01 +0100

    add comments to test of performance specializatons

--- test/test_performance_specializations.jl
+++ test/test_performance_specializations.jl
@@ -14,32 +14,38 @@ isdir(outdir) && rm(outdir, recursive=true)
   @timed_testset "TreeMesh3D, flux_shima_etal" begin
     trixi_include(@__MODULE__,
       joinpath(examples_dir(), "tree_3d_dgsem", "elixir_euler_ec.jl"),
-      initial_refinement_level=1, tspan=(0.0, 0.0),
+      initial_refinement_level=0, tspan=(0.0, 0.0),
       volume_flux=flux_shima_etal, surface_flux=flux_shima_etal)
     u_ode = copy(sol.u[end])
     du_ode = zero(u_ode)
-    u = Trixi.wrap_array(u_ode, semi)
-    du = Trixi.wrap_array(du_ode, semi)
-    nonconservative_terms = Trixi.have_nonconservative_terms(semi.equations)
 
-    du .= 0
-    Trixi.split_form_kernel!(
-      du, u, 1, semi.mesh,
-      nonconservative_terms, semi.equations,
-      semi.solver.volume_integral.volume_flux, semi.solver, semi.cache)
-    du_specialized = du[:, :, :, :, 1]
+    GC.@preserve u_ode du_ode begin
+      u = Trixi.wrap_array(u_ode, semi)
+      du = Trixi.wrap_array(du_ode, semi)
+      nonconservative_terms = Trixi.have_nonconservative_terms(semi.equations)
 
-    du .= 0
-    invoke(Trixi.split_form_kernel!,
-      Tuple{typeof(du), typeof(u), Integer, typeof(semi.mesh),
-      typeof(nonconservative_terms), typeof(semi.equations),
-      Function, typeof(semi.solver), typeof(semi.cache)},
-      du, u, 1, semi.mesh,
-      nonconservative_terms, semi.equations,
-      semi.solver.volume_integral.volume_flux, semi.solver, semi.cache)
-    du_baseline = du[:, :, :, :, 1]
+      # Call the optimized default version
+      du .= 0
+      Trixi.split_form_kernel!(
+        du, u, 1, semi.mesh,
+        nonconservative_terms, semi.equations,
+        semi.solver.volume_integral.volume_flux, semi.solver, semi.cache, true)
+      du_specialized = du[:, :, :, :, 1]
 
-    @test du_specialized ≈ du_baseline
+      # Call the plain version - note the argument type `Function` of
+      # `semi.solver.volume_integral.volume_flux`
+      du .= 0
+      invoke(Trixi.split_form_kernel!,
+        Tuple{typeof(du), typeof(u), Integer, typeof(semi.mesh),
+        typeof(nonconservative_terms), typeof(semi.equations),
+        Function, typeof(semi.solver), typeof(semi.cache), Bool},
+        du, u, 1, semi.mesh,
+        nonconservative_terms, semi.equations,
+        semi.solver.volume_integral.volume_flux, semi.solver, semi.cache, true)
+      du_baseline = du[:, :, :, :, 1]
+
+      @test du_specialized ≈ du_baseline
+    end
   end
 end
 
