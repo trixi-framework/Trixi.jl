4faf3142ac52d8d1054e5e3ebf8d5854d579db2f
Lucas <lgem@netcologne.de>
2021-11-25 14:17:55 +0100

    Add atmosphere struct

--- src/equations/compressible_moist_euler_2d.jl
+++ src/equations/compressible_moist_euler_2d.jl
@@ -49,6 +49,53 @@ varnames(::typeof(cons2cons), ::CompressibleMoistEulerEquations2D) = ("rho", "rh
 varnames(::typeof(cons2prim), ::CompressibleMoistEulerEquations2D) = ("rho", "v1", "v2", "p", "qv", "ql")
 varnames(::typeof(cons2pot), ::CompressibleMoistEulerEquations2D) = ("rho", "v1", "v2", "pottemp", "qv", "ql")
 
+struct AtmosphereLayers{RealT<:Real}
+  equations::AbstractCompressibleEulerEquations
+  # structure:  1--> i-layer (z = total_hight/precision *(i-1)),  2--> rho, rho_theta, rho_qv, rho_qc
+  AtmosphereLayers::Array{RealT, 2} 
+  total_hight::RealT
+  preciseness::RealT
+  layers::RealT
+  ground_state::Tuple{RealT}
+  equivalentpotential_temperature::RealT
+  mixing_ratios::Tuple{RealT}
+end
+
+function AtmosphereLayers(;equations=CompressibleMoistEulerEquations2D, total_hight=10000, preciseness=10, ground_state=(1.4, 10000), equivalentpotential_temperature=320, mixing_ratios=(0.02, 0.02), RealT=Float64)
+  rho0, p0 = ground_state
+  r_t0, r_v0 = mixing_ratios
+  theta_e0 = equivalentpotential_temperature
+  rho_qv0 = rho0 * r_v0
+  T0 = theta_e0
+  y0 = SVector(p0, rho0, T0, r_t0, r_v0, rho_qv0, theta_e0)
+
+  n = total_hight / preciseness
+  dz = 0.01
+  sol = nlsolve(generate_function_of_y(dz, y0, equations))
+  p, rho, T, r_t, r_v, rho_qv, theta_e = sol.zero
+
+  rho_d = rho / (1 + r_t)
+  rho_ql = rho - rho_d - rho_qv
+  kappa_M=(R_d * rho_d + R_v * rho_qv) / (c_pd * rho_d + c_pv * rho_qv + c_pl * rho_qc)
+  rho_theta = rho * (p0 / p)^kappa_M * T * (1 + (R_v / R_d) *r_v) / (1 + r_t) 
+
+  Val[1, :] = [rho, rho_theta, rho_qv, rho_ql]
+   for i in (1:n)
+    y0 = deepcopy(sol.zero)
+    dz = preciseness
+    sol = nlsolve(generate_function_of_y(dz, y0, equations))
+    p, rho, T, r_t, r_v, rho_qv, theta_e = sol.zero
+
+    rho_d = rho / (1 + r_t)
+    rho_ql = rho - rho_d - rho_qv
+    kappa_M=(R_d * rho_d + R_v * rho_qv) / (c_pd * rho_d + c_pv * rho_qv + c_pl * rho_qc)
+    rho_theta = rho * (p0 / p)^kappa_M * T * (1 + (R_v / R_d) *r_v) / (1 + r_t) 
+
+    Val[i+1, :] = [rho, rho_theta, rho_qv, rho_ql]
+   end
+
+  return InitialStructure{RealT}(equations, Val, total_hight=total_hight, n, dz, (rho0, p0), theta_e0, (r_t0, r_v0))
+end
 
 # Calculate 1D flux for a single point
 @inline function flux(u, orientation::Integer, equations::CompressibleMoistEulerEquations2D)
@@ -134,23 +181,83 @@ end
 # Calculates the absolute temperature of a moist state by solving the 
 # equivalentpotential temperature equation f(T) - theta_e = 0 using nsolve
 function solve_for_absolute_temperature(moist_state, H, equations, T_0)
-  @unpack p_0, c_pd, c_pv = equations
-  @unpack theta_e, p_d, r_v, L_v = moist_state
+  @unpack p_0, c_pd, c_pv, c_pl, L_00 = equations
+  theta_e, rho_d, r_v, r_t = moist_state
+
+  c_p = c_pd + c_pl * r_t
+  p_d = R_d * rho_d * T
+  L_v = L_00 - (c_pl - c_pv) * T
 
 return nlsolve((T) -> (T * (p_0 / p_d)^(R_d / c_p) * 
                H^(-r_v * R_v / c_p) * exp(L_v * r_v / (c_p * T)) - theta_e), T_0)
 end
 
 
-function inital_condition_moist_bubble(x, t, equations::CompressibleMoistEulerEquations2D)
-  @unpack p_0, kappa, g, c_pd, c_vd, R_d, R_v = equations
+function inital_condition_moist_bubble(x, t, equations::CompressibleMoistEulerEquations2D, AtmosphereLayers)
+  @unpack AtmosphereLayers, dz, total_hight = AtmosphereLayers
+  z = x[2] 
+  if (z > total_hight)
+    error("The atmossphere does not match the simulation domain")
+  end
+  n = floor(z/dz)
+  z_l = n * dz
+  rho_l, rho_theta_l, tho_qv_l, rho_ql_l = AtmosphereLayers[n, :]
+  z_r = (n+1) * dz
+  rho_r, rho_theta_r, tho_qv_r, rho_ql_r = AtmosphereLayers[n+1, :]
+  rho = (rho_r * (z - z_l) + rho_l * (z_r - z)) / dz
+  rho_theta = rho * (rho_theta_r / rho_r * (z - z_l) + rho_theta_l / rho_l * (z_r - z)) / dz
+  rho_qv = rho * (rho_qv_r / rho_r * (z - z_l) + rho_qv_l / rho_l * (z_r - z)) / dz
+  rho_ql = rho * (rho_ql_r / rho_r * (z - z_l) + rho_ql_l / rho_l * (z_r - z)) / dz
 
-  θ_e = 320
-  r_t = 0.020
-  Δθ = 0
+  rho, rho_e, rho_qv, rho_ql = PerturbMoistProfile(x, rho, rho_theta, rho_qv, rho_qc, equations::CompressibleMoistEulerEquations2D)
+
+  v1 = 0
+  v2 = 0
+  rho_v1 = rho * v1
+  rho_v2 = rho * v2
+  rho_E = rho_e + 1/2 * rho *(v1^2 + v2^2)
+
+  return SVector(rho, rho_v1, rho_v2, rho_E,  rho_qv, rho_ql)
+end
 
 
-  return SVector(ρ, ρ_v1, ρ_v2, ρ_E,  ρ_qv, ρ_ql)
+function PerturbMoistProfile(x, rho, rho_theta, rho_qv, rho_qc, equations::CompressibleMoistEulerEquations2D)
+  #TODO:
+
+  return SVector(rho, rho_e, rho_qv, rho_ql)
+end
+
+
+function moist_state(y, dz, y0, equations::CompressibleMoistEulerEquations2D)
+  @unpack p_0, g, c_pd, c_vd, R_d, R_v, L_00 = equations
+  p, rho,  T, r_t, r_v, rho_qv, theta_e = y
+  p0, rho0,  T0, r_t0, r_v0, rho_qv0, theta_e0 = y0
+
+  rho_d = rho / (1 + r_t)
+  p_d = R_d * rho_d * T
+  T_C = T - 273.15
+  p_vs = 611.2 * exp(17.62 * T_C / (243.12 + T_C))
+  L = L_00 - (c_pl - c_pv) * T
+
+  F[1] = p - p0 + g * rho * dz 
+  F[2] = p - (R_d * rho_d + R_v * rho_qv) * T
+  F[3] = theta_e - T * (p_d / p0)^(-R_d/
+         (c_pd + c_pl * r_t)) * exp(L * r_v / ((c_pd + c_pl * r_t) * T))
+  F[4] = r_t - r_t0
+  F[5] = rho_qv-rho_d * r_v
+  F[6] = theta_e - theta_e0
+  a = p_vs / (R_v * T) - rho_qv
+  b = rho - rho_qv - rho_d
+  F[7] = a+b-sqrt(a*a+b*b)
+
+  return F
+end
+
+
+function generate_function_of_y(dz, y0, equations::CompressibleMoistEulerEquations2D)
+  function function_of_y(y)
+    return moist_state(y, dz, y0, equations)
+  end
 end
 
 
