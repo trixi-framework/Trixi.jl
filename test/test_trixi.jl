74b76aa01c7ccdd54467c9514a683d5d3a2bdceb
Hendrik Ranocha <mail@ranocha.de>
2021-12-01 10:50:49 +0100

    fix at-test_nowarn_mod when no additional_ignore_content is specified explicitly

--- test/test_trixi.jl
+++ test/test_trixi.jl
@@ -163,7 +163,7 @@ macro test_nowarn_mod(expr, additional_ignore_content=String[])
           "WARNING: importing deprecated binding Colors.RGB4 into PlotUtils.\n",
           r"┌ Warning: Keyword argument letter not supported with Plots.+\n└ @ Plots.+\n",
         ]
-        append!(ignore_content, additional_ignore_content)
+        append!(ignore_content, $additional_ignore_content)
         for pattern in ignore_content
           stderr_content = replace(stderr_content, pattern => "")
         end
