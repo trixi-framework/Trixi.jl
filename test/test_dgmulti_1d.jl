d440fa79e67586c359afbeedbec30adc6aeebdad
Jesse Chan <jesse.chan@rice.edu>
2021-12-08 17:35:00 -0600

    remove linf normalization

--- test/test_dgmulti_1d.jl
+++ test/test_dgmulti_1d.jl
@@ -17,7 +17,7 @@ isdir(outdir) && rm(outdir, recursive=true)
       cells_per_dimension = (16,),
       # division by sqrt(2.0) corresponds to normalization by the square root of the size of the domain
       l2 = [7.853842541289665e-7, 9.609905503440606e-7, 2.832322219966481e-6] ./ sqrt(2.0),
-      linf = [1.5003758788711963e-6, 1.802998748523521e-6, 4.83599270806323e-6] ./ sqrt(2.0)
+      linf = [1.5003758788711963e-6, 1.802998748523521e-6, 4.83599270806323e-6]
     )
   end
 
@@ -34,7 +34,7 @@ isdir(outdir) && rm(outdir, recursive=true)
         derivative_order=1, accuracy_order=4,
         xmin=0.0, xmax=1.0, N=16),
       l2 = [2.6417669774800198e-5, 1.5028052071001864e-5, 7.321424811919818e-5] ./ sqrt(2.0),
-      linf = [6.923356213395238e-5, 3.008425995654207e-5, 0.0002216469993250314] ./ sqrt(2.0)
+      linf = [6.923356213395238e-5, 3.008425995654207e-5, 0.0002216469993250314]
     )
   end
 end
