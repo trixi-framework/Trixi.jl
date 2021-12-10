0b5159b0a39e5758301b14b07f37c0cae390186b
Jesse Chan <jesse.chan@rice.edu>
2021-12-10 09:39:31 -0600

    export new mesh type

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -201,7 +201,8 @@ export trixi_include, examples_dir, get_examples, default_example, default_examp
 
 export convergence_test, jacobian_fd, jacobian_ad_forward, linear_structure
 
-export DGMulti, AbstractMeshData, VertexMappedMesh, estimate_dt
+export DGMulti, AbstractMeshData, DGMultiMesh, estimate_dt
+export VertexMappedMesh # TODO: deprecate
 export GaussSBP
 
 # Visualization-related exports
