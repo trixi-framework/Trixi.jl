9db5a7382a48b1d9410ed4a677fefd41a0e85c0e
Jesse Chan <jesse.chan@rice.edu>
2021-12-10 11:57:55 -0600

    converting more elixirs

--- examples/dgmulti_2d/elixir_euler_rayleigh_taylor_instability.jl
+++ examples/dgmulti_2d/elixir_euler_rayleigh_taylor_instability.jl
@@ -65,15 +65,9 @@ dg = DGMulti(polydeg = 3, element_type = Quad(), approximation_type = Polynomial
              volume_integral = VolumeIntegralFluxDifferencing(flux_ranocha))
 
 num_elements = 16
-cells_per_dimension = (num_elements, 4 * num_elements)
-vertex_coordinates, EToV = StartUpDG.uniform_mesh(dg.basis.elementType, cells_per_dimension...)
-
-# remap the domain by modifying `vertex_coordinates`
-vx, vy = vertex_coordinates
-vx = map(x-> 0.25 * 0.5*(1 + x), vx) # map [-1, 1] to [0, 0.25]
-vy = map(x-> 0.5 * (1+x), vy) # map [-1, 1] to [0, 1] for single mode RTI
-vertex_coordinates = (vx, vy)
-mesh = VertexMappedMesh(vertex_coordinates, EToV, dg; is_periodic=(true,false))
+mesh = CartesianMesh(dg, cells_per_dimension=(num_elements, 4 * num_elements),
+                     coordinates_min=(0.0, 0.0), coordinates_max=(0.25, 1.0),
+                     is_periodic=(true,false))
 
 initial_condition = initial_condition_rayleigh_taylor_instability
 boundary_conditions = (; :entire_boundary => boundary_condition_slip_wall)
