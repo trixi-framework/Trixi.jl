d256320d31388091e2b398b1a7ab5e3d6a944cc7
Jesse Chan <jesse.chan@rice.edu>
2021-12-15 08:31:19 -0600

    remove cruft

--- examples/dgmulti_2d/elixir_euler_fdsbp_periodic.jl
+++ examples/dgmulti_2d/elixir_euler_fdsbp_periodic.jl
@@ -26,7 +26,6 @@ analysis_interval = 100
 analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
 stepsize_callback = StepsizeCallback(cfl=1.0)
 callbacks = CallbackSet(summary_callback, alive_callback, stepsize_callback, analysis_callback)
-# callbacks = CallbackSet(summary_callback, alive_callback, analysis_callback)
 
 ###############################################################################
 # run the simulation
