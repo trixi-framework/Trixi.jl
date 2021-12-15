8aca71eab1a382d6cc159175ee849c488e4e91a8
Hendrik Ranocha <mail@ranocha.de>
2021-12-15 10:09:37 +0100

    be explicit about domain size in FDSBP approximation_type elixirs

--- examples/dgmulti_2d/elixir_euler_fdsbp_periodic.jl
+++ examples/dgmulti_2d/elixir_euler_fdsbp_periodic.jl
@@ -3,7 +3,7 @@ using Trixi, OrdinaryDiffEq
 
 dg = DGMulti(element_type = Quad(),
              approximation_type = periodic_derivative_operator(
-               derivative_order=1, accuracy_order=4, xmin=-1.0, xmax=1.0, N=50),
+               derivative_order=1, accuracy_order=4, xmin=0.0, xmax=1.0, N=50),
              surface_integral = SurfaceIntegralWeakForm(FluxHLL()),
              volume_integral = VolumeIntegralWeakForm())
 
@@ -11,7 +11,8 @@ equations = CompressibleEulerEquations2D(1.4)
 initial_condition = initial_condition_convergence_test
 source_terms = source_terms_convergence_test
 
-mesh = DGMultiMesh(dg)
+mesh = DGMultiMesh(dg, coordinates_min=(-1.0, -1.0),
+                       coordinates_max=( 1.0,  1.0))
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg,
                                     source_terms = source_terms)
--- examples/dgmulti_3d/elixir_euler_fdsbp_periodic.jl
+++ examples/dgmulti_3d/elixir_euler_fdsbp_periodic.jl
@@ -14,11 +14,12 @@ volume_flux  = flux_ranocha
 surface_flux = flux_lax_friedrichs
 solver = DGMulti(element_type = Hex(),
                  approximation_type = periodic_derivative_operator(
-                   derivative_order=1, accuracy_order=4, xmin=-1.0, xmax=1.0, N=20),
+                   derivative_order=1, accuracy_order=4, xmin=0.0, xmax=1.0, N=20),
                  surface_integral= SurfaceIntegralWeakForm(surface_flux),
                  volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
-mesh = DGMultiMesh(solver)
+mesh = DGMultiMesh(solver, coordinates_min=(-1.0, -1.0, -1.0),
+                           coordinates_max=( 1.0,  1.0,  1.0))
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver;
                                     source_terms=source_terms)
