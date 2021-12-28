9db9480b453cd033c8c8fb22d71eb8c46f86544d
Jesse Chan <jesse.chan@rice.edu>
2021-12-28 07:19:55 -0600

    removing misleading comment

--- src/solvers/dgmulti/types.jl
+++ src/solvers/dgmulti/types.jl
@@ -230,8 +230,7 @@ end
                 is_on_boundary=nothing,
                 periodicity=ntuple(_ -> false, NDIMS), kwargs...) where {NDIMS}
 
-Constructs a `Curved()` [`DGMultiMesh`](@ref) with element type `dg.basis.elementType`. The domain is
-the tensor product of the intervals `[coordinates_min[i], coordinates_max[i]]`.
+Constructs a `Curved()` [`DGMultiMesh`](@ref) with element type `dg.basis.elementType`.
 - `mapping` is a function which maps from a reference [-1, 1]^NDIMS domain to a mapped domain,
    e.g., `xy = mapping(x, y)` in 2D.
 - `is_on_boundary` specifies boundary using a `Dict{Symbol, <:Function}`
