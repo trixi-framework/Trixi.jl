c27db3013187605d361024836ff5b3a4e93b1181
Jesse Chan <jesse.chan@rice.edu>
2021-12-29 22:31:19 -0600

    Revert "removing redundant PeriodicFDSBP code"
    This reverts commit ff4bab489180ac42d1d11afc0fab5b774d47aa24.

--- src/solvers/dgmulti/flux_differencing.jl
+++ src/solvers/dgmulti/flux_differencing.jl
@@ -288,7 +288,12 @@ function create_cache(mesh::DGMultiMesh, equations, dg::DGMultiFluxDiffSBP, Real
   u_values = allocate_nested_array(uEltype, nvars, size(md.xq), dg)
   u_face_values = allocate_nested_array(uEltype, nvars, size(md.xf), dg)
   flux_face_values = allocate_nested_array(uEltype, nvars, size(md.xf), dg)
-  lift_scalings = rd.wf ./ rd.wq[rd.Fmask] # lift scalings for diag-norm SBP operators
+  if rd.approximationType isa AbstractPeriodicDerivativeOperator
+    # periodic SBP operators do not need any surface stuff
+    lift_scalings = nothing
+  else
+    lift_scalings = rd.wf ./ rd.wq[rd.Fmask] # lift scalings for diag-norm SBP operators
+  end
 
   local_values_threaded = [allocate_nested_array(uEltype, nvars, (rd.Nq,), dg) for _ in 1:Threads.nthreads()]
 
