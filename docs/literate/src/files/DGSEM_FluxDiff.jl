a7440fd02d449ae11cbd2523b7e59b5bfe3ecd33
Benjamin Bolm <benjamin.bolm@gmx.de>
2022-01-14 15:39:19 +0100

    Update tutorial

--- docs/literate/src/files/DGSEM_FluxDiff.jl
+++ docs/literate/src/files/DGSEM_FluxDiff.jl
@@ -90,12 +90,14 @@
 # \begin{align*}
 # J \underline{\dot{u}}(t) &= - M^{-1} B (\underline{f}^* - \underline{f}) - 2D \underline{f}_{vol}(u^-, u^+)\\[5pt]
 # &= - M^{-1} B (\underline{f}^* - \underline{f}_{vol}(\underline{u}, \underline{u})) - 2D \underline{f}_{vol}(u^-, u^+)\\[5pt]
-# &= - M^{-1} B \underline{f}_{sur}^* - (2D - M^{-1} B) \underline{f}_{vol}
+# &= - M^{-1} B \underline{f}_{sur}^* - (2D - M^{-1} B) \underline{f}_{vol}\\[5pt]
+# &= - M^{-1} B \underline{f}_{sur}^* - D_{split} \underline{f}_{vol}
 # \end{align*}
 # ```
-# This formulation is in a weak form type formulation and can bei implemented by using the matrix $(2D-M^{-1}B)$
-# and two different fluxes. We divide between the surface flux $f=f_{sur}$ used for the numerical flux
-# $f_{sur}^*$ and the already mentioned volume flux $f_{vol}$ especially for this formulation.
+# This formulation is in a weak form type formulation and can be implemented by using the derivative
+# split matrix $D_{split}=(2D-M^{-1}B)$ and two different fluxes. We divide between the surface
+# flux $f=f_{sur}$ used for the numerical flux $f_{sur}^*$ and the already mentioned volume
+# flux $f_{vol}$ especially for this formulation.
 
 
 # This formulation creates a more stable version of DGSEM, because it fulfils entropy stability.
@@ -143,20 +145,15 @@ initial_condition = initial_condition_weak_blast_wave
 
 # In Trixi, flux differencing for the volume integral can be implemented with
 # [`VolumeIntegralFluxDifferencing`](@ref) using symmetric two-point volume fluxes.
-# Here, we are using two different fluxes. First, we set up the entropy conserving and kinetic energy preserving
-# flux `flux_ranocha` by [Hendrik Ranocha (2018)](https://cuvillier.de/en/shop/publications/7743)
-# and second, a modification of the original kinetic energy preserving two-point flux by
-# [Shima et al (2020)](https://doi.org/10.1016/j.jcp.2020.110060).
+# First, we set up a simulation with the entropy conserving and kinetic energy preserving
+# flux [`flux_ranocha`](@ref) by [Hendrik Ranocha (2018)](https://cuvillier.de/en/shop/publications/7743)
+# as surface and volume flux.
 
-# We will numerically confirm the difference in entropy conservation of the two fluxes.
+# We will confirm the entropy conservation property numerically.
 
 volume_flux = flux_ranocha # = f_vol
-solver_ranocha    = DGSEM(polydeg=3, surface_flux=flux_ranocha,
-                          volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
-
-volume_flux = flux_shima_etal # = f_vol
-solver_shima_etal = DGSEM(polydeg=3, surface_flux=flux_shima_etal,
-                          volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
+solver = DGSEM(polydeg=3, surface_flux=volume_flux,
+               volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
 
 # Now, we implement Trixi's `mesh`, `semi` and `ode` in a simple framework. For more information please
 # have a look at the documentation, the basic tutorial [introduction to DG methods](@ref scalar_linear_advection_1d)
@@ -168,131 +165,68 @@ mesh = TreeMesh(coordinates_min, coordinates_max,
                 n_cells_max=10_000,
                 periodicity=true)
 
-semi_ranocha    = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver_ranocha,
-                                               boundary_conditions=boundary_condition_periodic)
-
-semi_shima_etal = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver_shima_etal,
-                                               boundary_conditions=boundary_condition_periodic)
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
+                                    boundary_conditions=boundary_condition_periodic)
 
 ## ODE solvers
 tspan = (0.0, 0.4)
-ode_ranocha    = semidiscretize(semi_ranocha,    tspan);
-ode_shima_etal = semidiscretize(semi_shima_etal, tspan);
+ode = semidiscretize(semi, tspan);
 
-# To compare the entropy conservation of the different fluxes, we will use the analysis calllback
+# To analyse the entropy conservation of the approximation, we will use the analysis calllback
 # implemented in Trixi. It provides some information about the approximation including the entropy change.
-analysis_callback = AnalysisCallback(semi_ranocha, interval=100);
+analysis_callback = AnalysisCallback(semi, interval=100);
 
-# We now run the simulation using `flux_ranocha`.
-sol_ranocha = solve(ode_ranocha, RDPK3SpFSAL49(), abstol=1.0e-6, reltol=1.0e-6,
-                    callback=analysis_callback, save_everystep=false);
+# We now run the simulation using `flux_ranocha` for both surface and volume flux.
+sol = solve(ode, RDPK3SpFSAL49(), abstol=1.0e-6, reltol=1.0e-6,
+            callback=analysis_callback, save_everystep=false);
 # A look at the change in entropy $\sum \partial S/\partial U \cdot U_t$ in the analysis callback
 # confirms that the flux is entropy conserving since the change is about machine precision.
 
-sol_shima_etal = solve(ode_shima_etal, RDPK3SpFSAL49(), abstol=1.0e-6, reltol=1.0e-6,
-                       callback=analysis_callback, save_everystep=false);
-# On the other hand, this flux is not entropy conserving.
-
 # We can plot the approximated solution at the time `t=0.4`.
 using Plots
-plot(sol_ranocha)
+plot(sol)
 
-# Of course, you are not restricted to these two fluxes in Trixi. Here, we will give a short list
+# Now, we can use for instance the dissipative flux [`flux_lax_friedrichs`](@ref) as surface flux
+# to get an entropy stable method.
+using OrdinaryDiffEq, Trixi
+
+gamma = 1.4
+equations = CompressibleEulerEquations2D(gamma)
+
+initial_condition = initial_condition_weak_blast_wave
+
+volume_flux = flux_ranocha # = f_vol
+solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs,
+               volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
+
+coordinates_min = (-2.0, -2.0)
+coordinates_max = ( 2.0,  2.0)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=5,
+                n_cells_max=10_000,
+                periodicity=true)
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
+                                    boundary_conditions=boundary_condition_periodic)
+
+## ODE solvers
+tspan = (0.0, 0.4)
+ode = semidiscretize(semi, tspan);
+
+analysis_callback = AnalysisCallback(semi, interval=100);
+
+# We now run the simulation using the volume flux `flux_ranocha` and surface flux `flux_lax_friedrichs`.
+sol = solve(ode, RDPK3SpFSAL49(), abstol=1.0e-6, reltol=1.0e-6,
+            callback=analysis_callback, save_everystep=false);
+# The change in entropy confirms the expected entropy stability.
+
+using Plots
+plot(sol)
+
+
+# Of course, you can use more than these two fluxes in Trixi. Here, we will give a short list
 # of possible fluxes for the compressible Euler equations.
 # For the volume flux Trixi provides for example [`flux_ranocha`](@ref), [`flux_shima_etal`](@ref),
 # [`flux_chandrashekar`](@ref), [`flux_kennedy_gruber`](@ref).
 # As surface flux you can use all volume fluxes and additionally for instance [`flux_lax_friedrichs`](@ref),
 # [`flux_hll`](@ref), [`flux_hllc`](@ref).
-
-
-# ## Improved stability by flux differencing
-# Now, we want to give an example showing the stability advantage of flux differencing. We are
-# implementing the isentropic vortex by [Shu (1997)](https://ntrs.nasa.gov/citations/19980007543)
-# for the compressible Euler equation in 2D.
-using OrdinaryDiffEq, Trixi
-
-equations = CompressibleEulerEquations2D(1.4)
-
-# The initial condition is the isentropic vortex.
-function initial_condition_isentropic_vortex(x, t, equations::CompressibleEulerEquations2D)
-    ## needs appropriate mesh size, e.g. [-10,-10]x[10,10]
-    ## make sure that the inicenter does not exit the domain, e.g. T=10.0
-    ## initial center of the vortex
-    inicenter = SVector(0.0, 0.0)
-    ## size and strength of the vortex
-    iniamplitude = 0.2
-    ## base flow
-    rho = 1.0
-    v1 = 1.0
-    v2 = 1.0
-    vel = SVector(v1, v2)
-    p = 10.0
-    rt = p / rho                  # ideal gas equation
-    cent = inicenter + vel*t      # advection of center
-    cent = x - cent               # distance to centerpoint
-    ## cent=cross(iniaxis,cent)   # distance to axis, tangent vector, length r
-    ## cross product with iniaxis = [0,0,1]
-    cent = SVector(-cent[2], cent[1])
-    r2 = cent[1]^2 + cent[2]^2
-    du = iniamplitude/(2*π)*exp(0.5*(1-r2))                     # vel. perturbation
-    dtemp = -(equations.gamma-1)/(2*equations.gamma*rt)*du^2    # isentrop
-    rho = rho * (1+dtemp)^(1\(equations.gamma-1))
-    vel = vel + du*cent
-    v1, v2 = vel
-    p = p * (1+dtemp)^(equations.gamma/(equations.gamma-1))
-    prim = SVector(rho, v1, v2, p)
-    return prim2cons(prim, equations)
-end
-initial_condition = initial_condition_isentropic_vortex
-
-# Defining the physical domain, discretized with Trixi's standard mesh [`TreeMesh`](@ref)
-# and $2^4=16$ elements per dimension.
-coordinates_min = (-10.0, -10.0)
-coordinates_max = ( 10.0,  10.0)
-mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=4,
-                n_cells_max=10_000,
-                periodicity=true)
-
-# Now, we define `solver`, `semi` and `ode` and some callbacks for the two different fluxes.
-solver_LLF      = DGSEM(polydeg=5, surface_flux=flux_lax_friedrichs,)
-solver_FluxDiff = DGSEM(polydeg=5, surface_flux=flux_shima_etal,
-                        volume_integral=VolumeIntegralFluxDifferencing(flux_shima_etal))
-
-semi_LLF      = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver_LLF,
-                                             boundary_conditions=boundary_condition_periodic)
-semi_FluxDiff = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver_FluxDiff,
-                                             boundary_conditions=boundary_condition_periodic)
-
-tspan = (0.0, 20.0)
-ode_LLF      = semidiscretize(semi_LLF,      tspan)
-ode_FluxDiff = semidiscretize(semi_FluxDiff, tspan)
-
-analysis_callback = AnalysisCallback(semi_LLF, interval=400)
-
-stepsize_callback = StepsizeCallback(cfl=1.0)
-
-callbacks = CallbackSet(analysis_callback, stepsize_callback);
-
-# We now run the simulation without flux differencing.
-try
-    sol_LLF = solve(ode_LLF, CarpenterKennedy2N54(williamson_condition=false),
-                    dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-                    save_everystep=false, callback=callbacks);
-catch LoadError
-    println("Simulation stops, because of a negative value for the density `rho`.")
-end
-# The simulation crashes shortly after `t=1.0` with a physically meaningless negative value for the density.
-
-# The simulation with flux differencing is not crashing.
-sol_FluxDiff = solve(ode_FluxDiff, CarpenterKennedy2N54(williamson_condition=false),
-                     dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-                     save_everystep=false, callback=callbacks);
-
-using Plots
-plot(sol_FluxDiff)
-
-# Open questions/possibilities:
-# - Use `RDPK3SpFSAL49()`, `CarpenterKennedy2N54(williamson_condition=false)` or another time integration method?
-# - Use analysis, stepsize callback? (More extra code, but more maybe usefull output)
-# - Give another example? (e.g. inviscid Taxlor-Green-Vortex)
