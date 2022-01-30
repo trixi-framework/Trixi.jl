7ea6848145ec61d7ac2179864e4b83752fbb93b3
Will Pazner <will.e.p@gmail.com>
2022-01-30 08:15:02 -0800

    Reduce runtime in KPP test by reducing initial refinement level to 2

--- examples/tree_2d_dgsem/elixir_kpp.jl
+++ examples/tree_2d_dgsem/elixir_kpp.jl
@@ -90,7 +90,7 @@ coordinates_min = (-2.0, -2.0)
 coordinates_max = ( 2.0,  2.0)
 
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=6,
+                initial_refinement_level=2,
                 periodicity=true,
                 n_cells_max=500_000)
 
--- test/test_tree_2d_kpp.jl
+++ test/test_tree_2d_kpp.jl
@@ -10,7 +10,7 @@ EXAMPLES_DIR = joinpath(examples_dir(), "tree_2d_dgsem")
 @testset "KPP" begin
   @trixi_testset "elixir_kpp.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_kpp.jl"),
-      l2   = [0.36563290910786106],
+      l2   = [0.365621500852156],
       linf = [9.116732052340398],
       max_refinement_level = 6,
       tspan = (0.0, 0.01),
