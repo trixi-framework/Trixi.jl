a27fbc40d6f2aebf8c8081a744695919daa930d7
Benjamin Bolm <benjamin.bolm@gmx.de>
2022-03-16 10:36:07 +0100

    Revise tutorial

--- docs/literate/src/files/non_periodic_boundaries.jl
+++ docs/literate/src/files/non_periodic_boundaries.jl
@@ -1,29 +1,29 @@
 #src # Non-periodic boundary conditions
 
-# Besides the typical periodic boundary condition [`boundary_condition_periodic`](@ref) which is the
-# default boundary condition in [Trixi.jl](https://github.com/trixi-framework/Trixi.jl) there are
+# Besides the typical periodic boundary condition [`boundary_condition_periodic`](@ref), which is the
+# default boundary condition in [Trixi.jl](https://github.com/trixi-framework/Trixi.jl), there are
 # some non-periodic BC as well.
 
 
 # # Dirichlet boundary condition
-# We first have a look at the Dirichlet boundary condition [`BoundaryConditionDirichlet`](@ref).
+# First, let's look at the Dirichlet boundary condition [`BoundaryConditionDirichlet`](@ref).
 # ```julia
 # BoundaryConditionDirichlet(boundary_value_function)
 # ```
-# In Trixi, that creates a Dirichlet boundary condition that uses the function `boundary_value_function`
-# to specify the values at the boundary. It can be used to create a boundary condition that specifies
+# In Trixi, this creates a Dirichlet boundary condition where the function `boundary_value_function`
+# is used to set the values at the boundary. It can be used to create a boundary condition that sets
 # exact boundary values by passing the exact solution of the equation.
 
-# The passed boundary value function will be called with the same arguments as an initial condition
-# function is called, i.e., as
+# The passed boundary value function is called with the same arguments as an initial condition
+# function, i.e., as
 # ```julia
 # boundary_value_function(x, t, equations)
 # ```
-# where `x` specifies the coordinates, `t` is the current time, and `equation` is the corresponding
-# system of equations.
+# where `x` specifies the spatial coordinates, `t` is the current time, and `equations` is the
+# corresponding system of equations.
 
 
-# We want to give a short example for a simulation with Dirichlet BC.
+# We want to give a short example for a simulation with a Dirichlet BC.
 
 # Consider the one-dimensional linear advection equation with domain $\Omega=[0, 2]$ and a constant
 # zero initial condition.
@@ -39,7 +39,7 @@ using Plots
 plot(x -> sum(initial_condition(x, 0.0, equations)), label="initial condition", ylim=(-1.5, 1.5))
 
 # Using an advection velocity of `1.0` and the (local) Lax-Friedrichs/Rusanov flux
-# [`FluxLaxFriedrichs`](@ref) we create an inflow boundary on the left and an outflow boundary
+# [`FluxLaxFriedrichs`](@ref), we create an inflow boundary on the left and an outflow boundary
 # on the right. To define the inflow values, we initialize a `boundary_value_function`.
 function boundary_condition_sine_sector(x, t, equation::LinearScalarAdvectionEquation1D)
     if 1.0 <= t <= 3.0
@@ -51,7 +51,7 @@ function boundary_condition_sine_sector(x, t, equation::LinearScalarAdvectionEqu
 end
 boundary_condition = boundary_condition_sine_sector
 
-# We set the BC in negativ and positiv x direction
+# We set the BC in negative and positive x-direction.
 boundary_conditions = (x_neg=BoundaryConditionDirichlet(boundary_condition),
                        x_pos=BoundaryConditionDirichlet(boundary_condition))
 #-
@@ -80,10 +80,10 @@ stepsize_callback = StepsizeCallback(cfl=1.6)
 callbacks = CallbackSet(analysis_callback,
                         stepsize_callback);
 
-# Nodes for the visualization
+# We define some equidistant nodes for the visualization
 visnodes = range(tspan[1], tspan[2], length=300)
 
-# Run the simulation
+# and run the simulation.
 sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
             dt=1, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep=false, saveat=visnodes, callback=callbacks);
@@ -101,16 +101,16 @@ end
 
 
 # # Slip wall boundary condition
-# Moreover, there are other boundary conditions in Trixi. For instance, you can use the slip wall boundary
-# condition. It is defined for various equations and different mesh types. Each combination requires
-# its own formulation.
+# Moreover, there are other boundary conditions in Trixi. For instance, you can use the slip wall
+# boundary condition [`boundary_condition_slip_wall`](@ref). It is defined for various equations
+# and different mesh types. Each combination requires its own formulation.
 
 # For example, explanations for the `CompressibleEulerEquations2D` can be found [here](https://trixi-framework.github.io/Trixi.jl/stable/reference-trixi/#Trixi.boundary_condition_slip_wall-Tuple{Any,%20AbstractVector{T}%20where%20T,%20Any,%20Any,%20Any,%20CompressibleEulerEquations2D}),
 # and for the `ShallowWaterEquations2D` [here](https://trixi-framework.github.io/Trixi.jl/stable/reference-trixi/#Trixi.boundary_condition_slip_wall-Tuple{Any,%20AbstractVector{T}%20where%20T,%20Any,%20Any,%20Any,%20ShallowWaterEquations2D}).
 
 # # More examples
 # Trixi provides more examples with different boundary conditions, e.g. with [`boundary_condition_slip_wall`](@ref)
-# and self-defined boundary conditions using [`BoundaryConditionDirichlet`](@ref).
+# and other self-defined boundary conditions using [`BoundaryConditionDirichlet`](@ref).
 
 # For instance, there is a [Mach 3 flow over forward facing step elixir](https://github.com/trixi-framework/Trixi.jl/blob/main/examples/p4est_2d_dgsem/elixir_euler_forward_step_amr.jl)
 # for the 2D compressible Euler equations discretized with an AMR [`P4estMesh`](@ref).
