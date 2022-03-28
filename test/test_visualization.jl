194bab63393221bec283feb9e4927fc2b201c495
FelipeSantillan <72009492+FelipeSantillan@users.noreply.github.com>
2022-03-28 11:52:28 +0200

    smallest fix

--- test/test_visualization.jl
+++ test/test_visualization.jl
@@ -270,7 +270,7 @@ isdir(outdir) && rm(outdir, recursive=true)
       pd2D = PlotData2D(sol)
       @test_nowarn_debug Plots.plot(pd2D)
       @test_nowarn_debug PlotData2D(sol, slice=:xz, point = (0.5, 0.3, 0.1)) isa Trixi.PlotData2DCartesian
-      @test_nowarn_debug PlotData1D(sol, slice=:xy, point = (0.1, 0.3, 0.3)) isa Trixi.PlotData2DCartesian
+      @test_nowarn_debug PlotData2D(sol, slice=:xy, point = (0.1, 0.3, 0.3)) isa Trixi.PlotData2DCartesian
     end
 
         
