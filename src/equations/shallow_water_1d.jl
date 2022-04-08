54ced5cd17bd61acb1766d16bb57885c0282e21f
maxb <101979498+maxbertrand1996@users.noreply.github.com>
2022-04-08 13:40:48 +0200

    Update src/equations/shallow_water_1d.jl
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/equations/shallow_water_1d.jl
+++ src/equations/shallow_water_1d.jl
@@ -399,7 +399,7 @@ Further details are available in Theorem 1 of the paper:
 
   # Average each factor of products in flux
   v_avg = 0.5 * (v_ll + v_rr)
-  p_avg  = 0.5 * equations.gravity * h_ll * h_rr
+  p_avg = 0.5 * equations.gravity * h_ll * h_rr
 
   # Calculate fluxes depending on orientation
   f1 = 0.5 * (h_v_ll + h_v_rr)
