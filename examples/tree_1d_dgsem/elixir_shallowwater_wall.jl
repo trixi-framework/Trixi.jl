eff48c33bdddb079cfe1177659d89e9ca4cb2269
maxbertrand1996 <maxbertrand1996@googlemail.com>
2022-04-20 17:51:16 +0200

    WIP: SWE Dirichlet EC + Slip Wall

---
+++ examples/tree_1d_dgsem/elixir_shallowwater_wall.jl
@@ -0,0 +1,78 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+###############################################################################
+# Semidiscretization of the shallow water equations
+
+equations = ShallowWaterEquations1D(gravity_constant=9.812, H0=2.0)
+  
+function initial_condition_stone_throw(x, t, equations::ShallowWaterEquations1D)
+    # Set up polar coordinates
+    inicenter = SVector(0.15)
+    x_norm = x[1] - inicenter[1]
+    r = abs(x_norm)
+  
+    # Calculate primitive variables
+    H = equations.H0
+    v = r < 0.6 ? 1.75 : 0.0
+    
+    b = (  1.5 / exp( 0.5 * ((x[1] - 1.0)^2 ) )
+       + 0.75 / exp( 0.5 * ((x[1] + 1.0)^2 ) ) )
+  
+    return prim2cons(SVector(H, v, b), equations)
+end
+  
+initial_condition = initial_condition_stone_throw
+
+boundary_condition = boundary_condition_slip_wall
+
+###############################################################################
+# Get the DG approximation space
+
+volume_flux = (flux_wintermeyer_etal, flux_nonconservative_wintermeyer_etal)
+solver = DGSEM(polydeg=3, surface_flux=(flux_hll, flux_nonconservative_fjordholm_etal),
+               volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
+
+###############################################################################
+# Get the TreeMesh and setup a periodic mesh
+
+coordinates_min = -3.0
+coordinates_max = 3.0
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=2,
+                n_cells_max=10_000,
+                periodicity=false)
+
+# create the semi discretization object
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
+                                    boundary_conditions = boundary_condition)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 3.0)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval, save_analysis=false,
+                                    extra_analysis_integrals=(energy_kinetic,
+                                                              energy_internal))
+
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+
+save_solution = SaveSolutionCallback(interval=100,
+                                     save_initial_solution=true,
+                                     save_final_solution=true)
+
+callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, save_solution)
+
+###############################################################################
+# run the simulation
+
+# use a Runge-Kutta method with automatic (error based) time step size control
+sol = solve(ode, RDPK3SpFSAL49(), abstol=1.0e-8, reltol=1.0e-8,
+            save_everystep=false, callback=callbacks);
+summary_callback() # print the timer summary
