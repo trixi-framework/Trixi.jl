93c718eb00bcc86a1835b39da47bfea313987ae1
Jesse Chan <jesse.chan@rice.edu>
2022-05-20 11:20:05 -0500

    changing name, importing SplitODEFunction

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -26,7 +26,8 @@ using SparseArrays: AbstractSparseMatrix, AbstractSparseMatrixCSC, sparse, dropt
 using Reexport: @reexport
 
 using SciMLBase: CallbackSet, DiscreteCallback,
-                 ODEProblem, ODESolution, ODEFunction
+                 ODEProblem, ODESolution, ODEFunction,
+                 SplitODEProblem
 import SciMLBase: get_du, get_tmp_cache, u_modified!,
                   AbstractODEIntegrator, init, step!, check_error,
                   get_proposed_dt, set_proposed_dt!,
@@ -106,7 +107,7 @@ include("meshes/meshes.jl")
 include("solvers/solvers.jl")
 include("semidiscretization/semidiscretization.jl")
 include("semidiscretization/semidiscretization_hyperbolic.jl")
-include("semidiscretization/semidiscretization_multi_term.jl")
+include("semidiscretization/semidiscretization_hyperbolic_parabolic.jl")
 include("semidiscretization/semidiscretization_euler_acoustics.jl")
 include("callbacks_step/callbacks_step.jl")
 include("callbacks_stage/callbacks_stage.jl")
@@ -184,7 +185,7 @@ export nelements, nnodes, nvariables,
 
 export SemidiscretizationHyperbolic, semidiscretize, compute_coefficients, integrate
 
-export SemidiscretizationMultiTerm
+export SemidiscretizationHyperbolicParabolic
 
 export SemidiscretizationEulerAcoustics
 
