a0a4b8bbfe117b13d8c93823561184825c80138c
Jesse Chan <jesse.chan@rice.edu>
2022-05-22 15:39:45 -0500

    fix multiple includes

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -114,8 +114,6 @@ include("callbacks_stage/callbacks_stage.jl")
 include("semidiscretization/semidiscretization_euler_gravity.jl")
 include("time_integration/time_integration.jl")
 
-include("solvers/dgmulti/dg_parabolic.jl")
-
 # `trixi_include` and special elixirs such as `convergence_test`
 include("auxiliary/special_elixirs.jl")
 
