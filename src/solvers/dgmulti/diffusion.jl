9133d8456e570a4009a5e5f6073d43ad5c824dee
Jesse Chan <jesse.chan@rice.edu>
2022-05-21 23:49:02 -0500

    fix name

--- src/solvers/dgmulti/diffusion.jl
+++ src/solvers/dgmulti/diffusion.jl
@@ -6,7 +6,7 @@ struct ScalarDiffusion2D{T} <: AbstractParabolicEquations{2, 1}
 end
 
 # no orientation here since the flux is vector-valued
-function flux(u, grad_u, equations::ScalarDiffusion)
+function flux(u, grad_u, equations::ScalarDiffusion2D)
   dudx, dudy = grad_u
   return equations.diffusivity * dudx, equations.diffusivity * dudy
 end
