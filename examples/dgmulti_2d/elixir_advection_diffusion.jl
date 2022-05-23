8a11d57b77df86feb791b75a9196cc6325ea4cde
Jesse Chan <jesse.chan@rice.edu>
2022-05-23 14:11:02 -0500

    updated elixir with different types of BCs

--- examples/dgmulti_2d/elixir_advection_diffusion.jl
+++ examples/dgmulti_2d/elixir_advection_diffusion.jl
@@ -5,8 +5,8 @@ dg = DGMulti(polydeg = 3, element_type = Tri(), approximation_type = Polynomial(
              surface_integral = SurfaceIntegralWeakForm(flux_lax_friedrichs),
              volume_integral = VolumeIntegralWeakForm())
 
-equations = LinearScalarAdvectionEquation2D(1.0, 1.0)
-parabolic_equations = ScalarDiffusion2D(1e-3)
+equations = LinearScalarAdvectionEquation2D(1.5, 1.0)
+parabolic_equations = ScalarDiffusion2D(1e-2)
 
 initial_condition_zero(x, t, equations::LinearScalarAdvectionEquation2D) = SVector(0.0)
 initial_condition = initial_condition_zero
@@ -14,22 +14,31 @@ initial_condition = initial_condition_zero
 # tag two separate boundary segments
 inflow(x, tol=50*eps()) = abs(x[1]+1)<tol || abs(x[2]+1)<tol
 rest_of_boundary(x, tol=50*eps()) = !inflow(x, tol)
-is_on_boundary = Dict(:inflow => inflow, :outflow => rest_of_boundary)
+left(x, tol=50*eps()) = abs(x[1]+1)<tol
+right(x, tol=50*eps()) = abs(x[1]-1)<tol
+bottom(x, tol=50*eps()) = abs(x[2]+1)<tol
+is_on_boundary = Dict(:inflow => inflow, :outflow => rest_of_boundary,
+                      :left => left, :right => right, :top => top, :bottom => bottom)
 mesh = DGMultiMesh(dg, cells_per_dimension=(16, 16), is_on_boundary=is_on_boundary)
 
+# BC types
+boundary_condition_one = BoundaryConditionDirichlet((x, t, equations) -> SVector(1.0))
+boundary_condition_zero = BoundaryConditionDirichlet((x, t, equations) -> SVector(0.0))
+boundary_condition_neumann_zero = BoundaryConditionNeumann((x, t, equations) -> SVector(0.0))
+
 # define inviscid boundary conditions
-boundary_condition_inflow = BoundaryConditionDirichlet((x, t, equations) -> SVector(1.0))
-boundary_conditions = (; :inflow => boundary_condition_inflow)
+boundary_conditions = (; :left => boundary_condition_one, :bottom => boundary_condition_zero)
 
 # define viscous boundary conditions
-boundary_condition_wall = BoundaryConditionDirichlet((x, t, equations) -> SVector(0.0))
-parabolic_boundary_conditions = (; :inflow => boundary_condition_inflow,
-                                   :outflow => boundary_condition_wall)
+parabolic_boundary_conditions = (; :left => boundary_condition_one,
+                                   :bottom => boundary_condition_zero,
+                                   :top => boundary_condition_zero,
+                                   :right => boundary_condition_neumann_zero)
 
 semi = SemidiscretizationHyperbolicParabolic(mesh, equations, parabolic_equations, initial_condition, dg;
                                              boundary_conditions, parabolic_boundary_conditions)
 
-tspan = (0.0, .5)
+tspan = (0.0, 1.5)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
