3edb18ce2b9fb9542a6d5416f4b3e51234d3b82b
Jesse Chan <jesse.chan@rice.edu>
2022-05-22 23:57:26 -0500

    cleanup of elixir

--- examples/dgmulti_2d/elixir_advection_diffusion.jl
+++ examples/dgmulti_2d/elixir_advection_diffusion.jl
@@ -11,11 +11,10 @@ parabolic_equations = ScalarDiffusion2D(1e-3)
 initial_condition_zero(x, t, equations::LinearScalarAdvectionEquation2D) = SVector(0.0)
 initial_condition = initial_condition_zero
 
-# example where we tag two separate boundary segments of the mesh
+# tag two separate boundary segments
 inflow(x, tol=50*eps()) = abs(x[1]+1)<tol || abs(x[2]+1)<tol
 rest_of_boundary(x, tol=50*eps()) = !inflow(x, tol)
 is_on_boundary = Dict(:inflow => inflow, :outflow => rest_of_boundary)
-
 mesh = DGMultiMesh(dg, cells_per_dimension=(16, 16), is_on_boundary=is_on_boundary)
 
 # define inviscid boundary conditions
@@ -24,12 +23,13 @@ boundary_conditions = (; :inflow => boundary_condition_inflow)
 
 # define viscous boundary conditions
 boundary_condition_wall = BoundaryConditionDirichlet((x, t, equations) -> SVector(0.0))
-parabolic_boundary_conditions = (; boundary_condition_wall)
+parabolic_boundary_conditions = (; :inflow => boundary_condition_inflow,
+                                   :outflow => boundary_condition_wall)
 
 semi = SemidiscretizationHyperbolicParabolic(mesh, equations, parabolic_equations, initial_condition, dg;
-                                             boundary_conditions)#, parabolic_boundary_conditions)
+                                             boundary_conditions, parabolic_boundary_conditions)
 
-tspan = (0.0, .25)
+tspan = (0.0, .5)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
