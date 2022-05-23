4ef7835c09ab7101c290eacc1f5b355b007362f6
Jesse Chan <jesse.chan@rice.edu>
2022-05-23 14:08:23 -0500

    adding Neumann BCs

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -133,7 +133,7 @@ export AcousticPerturbationEquations2D,
        LatticeBoltzmannEquations2D, LatticeBoltzmannEquations3D,
        ShallowWaterEquations1D, ShallowWaterEquations2D
 
-export ScalarDiffusion2D
+export ScalarDiffusion2D, BoundaryConditionNeumann
 
 export flux, flux_central, flux_lax_friedrichs, flux_hll, flux_hllc, flux_hlle, flux_godunov,
        flux_chandrashekar, flux_ranocha, flux_derigs_etal, flux_hindenlang_gassner,
--- src/equations/scalar_diffusion_2d.jl
+++ src/equations/scalar_diffusion_2d.jl
@@ -2,9 +2,40 @@ struct ScalarDiffusion2D{T} <: AbstractParabolicEquations{2, 1}
   diffusivity::T
 end
 
-# no orientation here since the flux is vector-valued
+# no orientation specified since the flux is vector-valued
 function flux(u, grad_u, equations::ScalarDiffusion2D)
   dudx, dudy = grad_u
   return equations.diffusivity * dudx, equations.diffusivity * dudy
 end
 
+# Dirichlet-type boundary condition for use with a parabolic solver in weak form
+@inline function (boundary_condition::BoundaryConditionDirichlet)(u_inner, normal::AbstractVector, x, t,
+                                                                  operator_type::Gradient,
+                                                                  equations::ScalarDiffusion2D)
+  return boundary_condition.boundary_value_function(x, t, equations)
+end
+
+@inline function (boundary_condition::BoundaryConditionDirichlet)(flux_inner, normal::AbstractVector, x, t,
+                                                                  operator_type::Divergence,
+                                                                  equations::ScalarDiffusion2D)
+  return flux_inner
+end
+
+# `boundary_value_function` should have signature `boundary_value_function(x, t, equations)`
+# For Neumann BCs, this corresponds to kappa * (∇u ⋅ n)
+struct BoundaryConditionNeumann{B}
+  boundary_value_function::B
+end
+
+# Neumann type boundary condition for use with a parabolic solver in weak form
+@inline function (boundary_condition::BoundaryConditionNeumann)(u_inner, normal::AbstractVector, x, t,
+                                                                operator_type::Gradient,
+                                                                equations::ScalarDiffusion2D)
+  return u_inner
+end
+
+@inline function (boundary_condition::BoundaryConditionNeumann)(flux_inner, normal::AbstractVector, x, t,
+                                                                operator_type::Divergence,
+                                                                equations::ScalarDiffusion2D)
+  return boundary_condition.boundary_value_function(x, t, equations)
+end
