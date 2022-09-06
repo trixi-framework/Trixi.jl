5aa73cac82e6e56e3c8906c62b73706041480c3e
Jesse Chan <jesse.chan@rice.edu>
2022-09-06 10:27:43 -0500

    adding docs/warning for gradient variable type

--- src/equations/compressible_navier_stokes_2d.jl
+++ src/equations/compressible_navier_stokes_2d.jl
@@ -100,7 +100,21 @@ struct CompressibleNavierStokesDiffusion2D{GradientVariables, RealT <: Real, E <
   gradient_variables::GradientVariables # GradientVariablesPrimitive or GradientVariablesEntropy
 end
 
-# type parameters for CompressibleNavierStokesDiffusion2D
+"""
+#!!! warning "Experimental code"
+#    This code is experimental and may be changed or removed in any future release.
+
+`GradientVariablesPrimitive` and `GradientVariablesEntropy` are gradient variable type parameters
+for `CompressibleNavierStokesDiffusion2D`. By default, the gradient variables are set to be
+`GradientVariablesPrimitive`. Specifying `GradientVariablesEntropy` instead uses the entropy variable
+formulation from
+- Hughes, Mallet, Franca (1986)
+  A new finite element formulation for computational fluid dynamics: I. Symmetric forms of the
+  compressible Euler and Navier-Stokes equations and the second law of thermodynamics.
+  [https://doi.org/10.1016/0045-7825(86)90127-1](https://doi.org/10.1016/0045-7825(86)90127-1)
+
+Under `GradientVariablesEntropy`, the Navier-Stokes discretization is provably entropy stable.
+"""
 struct GradientVariablesPrimitive end
 struct GradientVariablesEntropy end
 
