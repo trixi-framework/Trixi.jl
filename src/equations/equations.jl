aef9724e87409910e09597e556b4c44938ddff80
ttheine <toskan.theine@googlemail.com>
2022-09-16 12:06:19 +0200

    added a test for linearized euler equations

--- src/equations/equations.jl
+++ src/equations/equations.jl
@@ -329,5 +329,8 @@ include("lattice_boltzmann_3d.jl")
 abstract type AbstractAcousticPerturbationEquations{NDIMS, NVARS} <: AbstractEquations{NDIMS, NVARS} end
 include("acoustic_perturbation_2d.jl")
 
+abstract type AbstractLinearizedEulerEquations{NDIMS, NVARS} <: AbstractEquations{NDIMS, NVARS} end
+include("linearized_euler_2d.jl")
+
 
 end # @muladd
