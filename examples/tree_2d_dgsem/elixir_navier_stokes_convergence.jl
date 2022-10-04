8b7a3bc81f90c04817e52ef0f4e557483dd04802
Hendrik Ranocha <mail@ranocha.de>
2022-10-04 12:20:38 +0200

    activate analysis_callback in Navier-Stokes convergence test

--- examples/tree_2d_dgsem/elixir_navier_stokes_convergence.jl
+++ examples/tree_2d_dgsem/elixir_navier_stokes_convergence.jl
@@ -202,7 +202,7 @@ summary_callback = SummaryCallback()
 alive_callback = AliveCallback(alive_interval=10)
 analysis_interval = 100
 analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
-callbacks = CallbackSet(summary_callback, alive_callback)
+callbacks = CallbackSet(summary_callback, alive_callback, analysis_callback)
 
 ###############################################################################
 # run the simulation
