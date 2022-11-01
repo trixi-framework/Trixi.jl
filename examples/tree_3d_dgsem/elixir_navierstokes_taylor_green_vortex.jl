91bb5a100978a14ae622f9284db29d078761d1e7
Andrew Winters <andrew.ross.winters@liu.se>
2022-11-01 14:55:23 +0100

    add enstrophy callback to TGV elixir

--- examples/tree_3d_dgsem/elixir_navierstokes_taylor_green_vortex.jl
+++ examples/tree_3d_dgsem/elixir_navierstokes_taylor_green_vortex.jl
@@ -34,7 +34,7 @@ end
 initial_condition = initial_condition_taylor_green_vortex
 
 volume_flux = flux_ranocha
-solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs,
+solver = DGSEM(polydeg=3, surface_flux=flux_hllc,
                volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
 
 coordinates_min = (-1.0, -1.0, -1.0) .* pi
@@ -50,24 +50,22 @@ semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabol
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-tspan = (0.0, 10.0)
+tspan = (0.0, 20.0)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
 
-analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+analysis_interval = 50
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval, save_analysis=true,
+                                     extra_analysis_integrals=(energy_kinetic,
+                                                               energy_internal,
+                                                               enstrophy))
 
-alive_callback = AliveCallback(analysis_interval=analysis_interval)
-
-save_solution = SaveSolutionCallback(interval=100,
-                                     save_initial_solution=true,
-                                     save_final_solution=true,
-                                     solution_variables=cons2prim)
+alive_callback = AliveCallback(analysis_interval=analysis_interval,)
 
 callbacks = CallbackSet(summary_callback,
-                        analysis_callback, alive_callback,
-                        save_solution)
+                        analysis_callback,
+                        alive_callback)
 
 ###############################################################################
 # run the simulation
