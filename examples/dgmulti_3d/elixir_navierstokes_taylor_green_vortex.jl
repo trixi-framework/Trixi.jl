8c2e24255c84ebc21c5de80dce096f67793205f9
Jesse Chan <jesse.chan@rice.edu>
2022-11-14 10:05:39 -0600

    adding extra analysis integrals

--- examples/dgmulti_3d/elixir_navierstokes_taylor_green_vortex.jl
+++ examples/dgmulti_3d/elixir_navierstokes_taylor_green_vortex.jl
@@ -57,7 +57,10 @@ ode = semidiscretize(semi, tspan)
 summary_callback = SummaryCallback()
 alive_callback = AliveCallback(alive_interval=10)
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg),
+                                     extra_analysis_integrals=(energy_kinetic,
+                                                               energy_internal,
+                                                               enstrophy))
 callbacks = CallbackSet(summary_callback, alive_callback)
 
 ###############################################################################
