c962ba5b305553faaf35753da58baa6248787211
Andrew Winters <andrew.ross.winters@liu.se>
2022-11-28 09:14:01 +0100

    add tests for Burgers splitting

--- test/test_tree_1d_fdsbp.jl
+++ test/test_tree_1d_fdsbp.jl
@@ -8,6 +8,22 @@ include("test_trixi.jl")
 # pathof(Trixi) returns /path/to/Trixi/src/Trixi.jl, dirname gives the parent directory
 EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "tree_1d_fdsbp")
 
+@testset "Inviscid Burgers" begin
+  @trixi_testset "elixir_burgers_basic.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_burgers_basic.jl"),
+      l2   = [8.316190308678742e-7],
+      linf = [7.1087263324720595e-6],
+      tspan = (0.0, 0.5))
+  end
+
+  @trixi_testset "elixir_burgers_linear_stability.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_burgers_linear_stability.jl"),
+      l2   = [0.9999995642691271],
+      linf = [1.824702804788453],
+      tspan=(0.0, 0.25))
+  end
+end
+
 @testset "Compressible Euler" begin
   @trixi_testset "elixir_euler_convergence.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_convergence.jl"),
