707f7aa267987caf5fae643d55644e92a1b24c83
ttheine <82824733+ttheine@users.noreply.github.com>
2022-12-07 15:41:11 +0100

    Added linearized euler equations (#1218)
    * added linearized euler equations
    
    * added an elixir for linearized euler
    
    * added a test for linearized euler equations
    
    * added a test for linearized euler equations
    
    * implemented suggested changes
    
    * Update src/equations/equations.jl
    
    * added test file to superfile
    
    * add equations to README, index and NEWS
    
    * Make order consistent with README.md
    
    * Fix capitalization
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>
    Co-authored-by: Johannes Markert <10619309+jmark@users.noreply.github.com>

---
+++ test/test_tree_2d_linearizedeuler.jl
@@ -0,0 +1,17 @@
+
+using Test
+using Trixi
+
+include("test_trixi.jl")
+
+# pathof(Trixi) returns /path/to/Trixi/src/Trixi.jl, dirname gives the parent directory
+EXAMPLES_DIR = joinpath(pathof(Trixi) |> dirname |> dirname, "examples", "tree_2d_dgsem")
+
+@testset "Linearized Euler Equations 2D" begin
+  @trixi_testset "elixir_linearizedeuler_convergence.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_linearizedeuler_convergence.jl"),
+      l2 = [0.00020601485381444888, 0.00013380483421751216, 0.0001338048342174503, 0.00020601485381444888],
+      linf = [0.0011006084408365924, 0.0005788678074691855, 0.0005788678074701847, 0.0011006084408365924]
+    )
+  end
+end
