cccade273cc8b5902a10ea6859ef49181964c651
Daniel Doehring <doehringd2@gmail.com>
2022-12-30 11:22:54 +0100

    Update src/solvers/dgsem/basis_lobatto_legendre.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/solvers/dgsem/basis_lobatto_legendre.jl
+++ src/solvers/dgsem/basis_lobatto_legendre.jl
@@ -101,10 +101,10 @@ end
 @inline nnodes(basis::LobattoLegendreBasis{RealT, NNODES}) where {RealT, NNODES} = NNODES
 
 """
-    eachnode(dg::LobattoLegendreBasis)
+    eachnode(basis::LobattoLegendreBasis)
 
 Return an iterator over the indices that specify the location in relevant data structures
-for the nodes in `LobattoLegendreBasis`. 
+for the nodes in `basis`. 
 In particular, not the nodes themselves are returned.
 """
 @inline eachnode(basis::LobattoLegendreBasis) = Base.OneTo(nnodes(basis))
