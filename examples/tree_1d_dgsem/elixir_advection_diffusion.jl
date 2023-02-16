b6d9835dd9f0f717a53d90f7ae99aa82687999f0
Sigrun Ortleb <ortleb@ima-pc24.ingenieur.mathematik.uni-kassel.de>
2023-02-16 03:44:09 +0100

    added DG 1D parabolic and advection-diffusion elixir

---
+++ examples/tree_1d_dgsem/elixir_advection_diffusion.jl
@@ -0,0 +1,86 @@
+
+using OrdinaryDiffEq
+using Trixi
+
+###############################################################################
+# semidiscretization of the linear advection equation
+
+advection_velocity = 1.0e-1
+equations = LinearScalarAdvectionEquation1D(advection_velocity)
+diffusivity() = 1.0e-1
+equations_parabolic = LaplaceDiffusion1D(diffusivity(), equations)
+
+# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
+solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs)
+
+coordinates_min = -pi # minimum coordinate
+coordinates_max =  pi # maximum coordinate
+
+# Create a uniformly refined mesh with periodic boundaries
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=7,
+                n_cells_max=30_000) # set maximum capacity of tree data structure
+
+# Define initial condition
+function initial_condition_diffusive_convergence_test(x, t, equation::LinearScalarAdvectionEquation1D)
+    # Store translated coordinate for easy use of exact solution
+    x_trans = x - equation.advection_velocity * t
+  
+    nu = diffusivity()
+    c = 0.0
+    A = 1.0
+    L = 2
+    f = 1/L
+    omega = 1.0
+    scalar = c + A * sin(omega * sum(x_trans)) * exp(-nu * omega^2 * t)
+    return SVector(scalar)
+  end
+  initial_condition = initial_condition_diffusive_convergence_test
+  
+  # define periodic boundary conditions everywhere
+  boundary_conditions = boundary_condition_periodic
+  boundary_conditions_parabolic = boundary_condition_periodic
+
+# A semidiscretization collects data structures and functions for the spatial discretization
+semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic), initial_condition_diffusive_convergence_test, solver;
+  boundary_conditions=(boundary_conditions, boundary_conditions_parabolic))
+
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+# Create ODE problem with time span from 0.0 to 1.0
+ode = semidiscretize(semi, (0.0, 10.0));
+
+# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
+# and resets the timers
+summary_callback = SummaryCallback()
+
+# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+
+# The AliveCallback prints short status information in regular intervals
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+
+# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
+callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback)
+
+
+###############################################################################
+# run the simulation
+
+# OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
+time_int_tol = 1.0e-8
+time_abs_tol = 1.0e-8
+# sol = solve(ode, KenCarp3(autodiff=false), dt = 5.0e-01, abstol=time_abs_tol, reltol=time_int_tol,
+#            save_everystep=false, callback=callbacks)
+
+#sol = solve(ode, IMEXEuler(autodiff=false), dt = 50*pi/(mesh.tree.length+1),
+#            save_everystep=false, callback=callbacks)
+
+sol = solve(ode, SDIRK2(autodiff=false), abstol=time_int_tol, reltol=time_int_tol,
+           save_everystep=false, callback=callbacks)
+
+# Print the timer summary
+summary_callback()
