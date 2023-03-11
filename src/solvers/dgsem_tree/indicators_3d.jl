cc9aaf3899996fb89629418b400871c78b644a4a
Arpit-Babbar <arpitbabbar@gmail.com>
2023-03-11 20:34:15 +0530

    Extend changes to all 3 dimensions

--- src/solvers/dgsem_tree/indicators_3d.jl
+++ src/solvers/dgsem_tree/indicators_3d.jl
@@ -73,9 +73,18 @@ function (indicator_hg::IndicatorHennemannGassner)(u::AbstractArray{<:Any,5},
       total_energy_clip2 += modal[i, j, k]^2
     end
 
-    # Calculate energy in lower modes
-    energy = max((total_energy - total_energy_clip1) / total_energy,
-                 (total_energy_clip1 - total_energy_clip2) / total_energy_clip1)
+    # Calculate energy in higher modes
+    if iszero(total_energy)
+      energy_frac_1 = (total_energy - total_energy_clip1) / total_energy
+    else
+      energy_frac_1 = zero(total_energy)
+    end
+    if iszero(total_energy_clip1)
+      energy_frac_2 = (total_energy_clip1 - total_energy_clip2) / total_energy_clip1
+    else
+      energy_frac_2 = zero(total_energy_clip1)
+    end
+    energy = max(energy_frac_1, energy_frac_2)
 
     alpha_element = 1 / (1 + exp(-parameter_s / threshold * (energy - threshold)))
 
