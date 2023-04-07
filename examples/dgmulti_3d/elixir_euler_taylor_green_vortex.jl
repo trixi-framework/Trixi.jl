16cceca3c689e93b183c66e7d136a9349c9acfe6
Jesse Chan <jesse.chan@rice.edu>
2023-04-07 09:40:58 -0500

    fixing more elixirs

--- examples/dgmulti_3d/elixir_euler_taylor_green_vortex.jl
+++ examples/dgmulti_3d/elixir_euler_taylor_green_vortex.jl
@@ -33,7 +33,8 @@ solver = DGMulti(polydeg = 3, element_type = Hex(), approximation_type = Polynom
                 surface_integral= SurfaceIntegralWeakForm(surface_flux),
                 volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
-mesh = DGMultiMesh(solver, cells_per_dimension=(8, 8, 8),
+cells_per_dimension = (8, 8, 8)
+mesh = DGMultiMesh(solver, cells_per_dimension,
                    coordinates_min=(-pi, -pi, -pi), coordinates_max=(pi, pi, pi),
                    periodicity=true)
 
