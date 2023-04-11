cf36ce7dda261386e4305d123b7c97863cdcd3ec
patrickersing <patrick.ersing@liu.se>
2023-04-11 08:25:31 +0200

    fix typos in docstring

--- src/equations/shallow_water_two_layer_1d.jl
+++ src/equations/shallow_water_two_layer_1d.jl
@@ -294,7 +294,7 @@ topography is nonzero this should only be used as a surface flux otherwise the s
 well-balanced. For well-balancedness in the volume flux use [`flux_wintermeyer_etal`](@ref).
 
 Details are available in Eq. (4.1) in the paper:
-- Ulrik S. Fjordholm, Siddhartha Mishr and Eitan Tadmor (2011)
+- Ulrik S. Fjordholm, Siddhartha Mishra and Eitan Tadmor (2011)
   Well-balanced and energy stable schemes for the shallow water equations with discontinuous 
   topography [DOI: 10.1016/j.jcp.2011.03.042](https://doi.org/10.1016/j.jcp.2011.03.042)
 and the application to two layers is shown in the paper:
