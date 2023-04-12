9a191cf9d5d131fe81d52c4c5acee5df842b83b4
Knapp <knap_da@sc-030182l.intra.dlr.de>
2023-04-12 14:32:25 +0200

    Update example

--- examples/dgmulti_3d/elixir_euler_tensorWedge.jl
+++ examples/dgmulti_3d/elixir_euler_tensorWedge.jl
@@ -45,7 +45,7 @@ analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=r
 alive_callback = AliveCallback(analysis_interval=analysis_interval)
 
 # The StepsizeCallback handles the re-calculcation of the maximum Δt after each time step
-stepsize_callback = StepsizeCallback(cfl=0.5)
+stepsize_callback = StepsizeCallback(cfl=1.6)
 
 callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, stepsize_callback)
 
