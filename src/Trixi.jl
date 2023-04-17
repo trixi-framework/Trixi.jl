663ea1da555ab3d054209189c8d2b6153cb899aa
David Knapp <david.knapp@dlr.de>
2023-04-17 09:31:53 +0200

    Readd Gauss
    Accidentaly deleted during Merge-Conflict resolvement online

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -76,7 +76,7 @@ import SummationByPartsOperators: integrate, semidiscretize,
   upwind_operators
 
 # DGMulti solvers
-@reexport using StartUpDG: StartUpDG, Polynomial, TensorProductWedge, SBP, Line, Tri, Quad, Hex, Tet, Wedge
+@reexport using StartUpDG: StartUpDG, Polynomial, Gauss, TensorProductWedge, SBP, Line, Tri, Quad, Hex, Tet, Wedge
 using StartUpDG: RefElemData, MeshData, AbstractElemShape
 
 # TODO: include_optimized
