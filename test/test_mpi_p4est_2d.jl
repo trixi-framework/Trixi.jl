3859a779611072d8b5fb9b58b51449741c8a7a68
Michael Schlottke-Lakemper <michael@sloede.com>
2023-04-20 05:47:19 +0200

    Disable more tests

--- test/test_mpi_p4est_2d.jl
+++ test/test_mpi_p4est_2d.jl
@@ -38,10 +38,12 @@ import ..CI_ON_WINDOWS
     end
   end
 
-  @trixi_testset "elixir_advection_nonconforming_flag.jl" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_nonconforming_flag.jl"),
-      l2   = [3.198940059144588e-5],
-      linf = [0.00030636069494005547])
+  if !CI_ON_WINDOWS # see comment on `CI_ON_WINDOWS` in `test/test_mpi.jl`
+    @trixi_testset "elixir_advection_nonconforming_flag.jl" begin
+      @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_nonconforming_flag.jl"),
+        l2   = [3.198940059144588e-5],
+        linf = [0.00030636069494005547])
+    end
   end
 
   if !CI_ON_WINDOWS # see comment on `CI_ON_WINDOWS` in `test/test_mpi.jl`
