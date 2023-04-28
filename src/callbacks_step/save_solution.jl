14edc873c671aaf2118699971b35ea4e6bfd4c1c
Andrew Winters <andrew.ross.winters@liu.se>
2023-04-28 19:17:32 +0200

    Update src/callbacks_step/save_solution.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/callbacks_step/save_solution.jl
+++ src/callbacks_step/save_solution.jl
@@ -15,7 +15,7 @@
 
 Save the current numerical solution in regular intervals. Either pass `interval` to save
 every `interval` time steps or pass `dt` to save in intervals of `dt` in terms
-of integration time by adding additional `tstops` (note that this may change the solution).
+of integration time by adding additional (shortened) time steps where necessary (note that this may change the solution).
 `solution_variables` can be any callable that converts the conservative variables
 at a single point to a set of solution variables. The first parameter passed
 to `solution_variables` will be the set of conservative variables
