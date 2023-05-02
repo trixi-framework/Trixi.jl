ef3115c8758e209929306520ebf64a0b5ed182d9
Jesse Chan <jesse.chan@rice.edu>
2023-05-02 08:53:59 -0500

    unify laplace diffusion BCs

--- src/equations/laplace_diffusion_1d.jl
+++ src/equations/laplace_diffusion_1d.jl
@@ -22,27 +22,28 @@ function flux(u, gradients, orientation::Integer, equations_parabolic::LaplaceDi
 end
 
 
-# Dirichlet-type boundary condition for use with a parabolic solver in weak form
+# Dirichlet and Neumann boundary conditions for use with parabolic solvers in weak form.
+# Note that these are general, so they apply to LaplaceDiffusion in any spatial dimension.
 @inline function (boundary_condition::BoundaryConditionDirichlet)(flux_inner, u_inner, normal::AbstractVector,
                                                                   x, t, operator_type::Gradient,
-                                                                  equations_parabolic::LaplaceDiffusion1D)
+                                                                  equations_parabolic::AbstractLaplaceDiffusion)
   return boundary_condition.boundary_value_function(x, t, equations_parabolic)
 end
 
 @inline function (boundary_condition::BoundaryConditionDirichlet)(flux_inner, u_inner, normal::AbstractVector,
                                                                   x, t, operator_type::Divergence,
-                                                                  equations_parabolic::LaplaceDiffusion1D)
+                                                                  equations_parabolic::AbstractLaplaceDiffusion)
   return flux_inner
 end
 
 @inline function (boundary_condition::BoundaryConditionNeumann)(flux_inner, u_inner, normal::AbstractVector,
                                                                 x, t, operator_type::Divergence,
-                                                                equations_parabolic::LaplaceDiffusion1D)
+                                                                equations_parabolic::AbstractLaplaceDiffusion)
   return boundary_condition.boundary_normal_flux_function(x, t, equations_parabolic)
 end
 
 @inline function (boundary_condition::BoundaryConditionNeumann)(flux_inner, u_inner, normal::AbstractVector,
                                                                 x, t, operator_type::Gradient,
-                                                                equations_parabolic::LaplaceDiffusion1D)
+                                                                equations_parabolic::AbstractLaplaceDiffusion)
   return flux_inner
 end
