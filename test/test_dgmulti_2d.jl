bf5b6351dbad60dffcabaebdf5995312e822b465
Jesse Chan <jesse.chan@rice.edu>
2023-05-16 15:15:31 -0500

    adding a test

--- test/test_dgmulti_2d.jl
+++ test/test_dgmulti_2d.jl
@@ -177,6 +177,14 @@ isdir(outdir) && rm(outdir, recursive=true)
     )
   end
 
+  @trixi_testset "elixir_euler_shockcapturing_curved.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_shockcapturing_curved.jl"),
+      cells_per_dimension = 4, tspan = (0.0, 0.1),
+      l2 = [0.05565849298766252, 0.042322816017256494, 0.042322816017256466, 0.2064212098324083],
+      linf = [0.23633287875008924, 0.16930148707515683, 0.16930148707515688, 0.8587706761131937]
+    )
+  end
+
   @trixi_testset "elixir_euler_weakform.jl (FD SBP)" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_weakform.jl"),
       cells_per_dimension = (2, 2),
