c90117f5604cbc935433471c0ee36941be5d37b9
Janina Bender <jmbender@mathematik.uni-kassel.de>
2023-05-26 09:16:48 +0200

    added comment to polydeg=0 to docstring of LGL Basis

--- src/solvers/dgsem/basis_lobatto_legendre.jl
+++ src/solvers/dgsem/basis_lobatto_legendre.jl
@@ -9,6 +9,9 @@
     LobattoLegendreBasis([RealT=Float64,] polydeg::Integer)
 
 Create a nodal Lobatto-Legendre basis for polynomials of degree `polydeg`.
+
+For the special case `polydeg=0` the DG method reduces to a finite volume method.
+Therefore, this function sets the center point of the cell as single node.
 """
 struct LobattoLegendreBasis{RealT<:Real, NNODES,
                             VectorT<:AbstractVector{RealT},
