edcecbef08eaa59a029ddd90dfcf87f458eb4400
SimonCan <simon.candelaresi@gmail.com>
2023-06-06 08:56:54 +0100

    Increased the frequency of the solution write out for a more usable animation.

--- examples/structured_2d_dgsem/elixir_advection_basic_coupled.jl
+++ examples/structured_2d_dgsem/elixir_advection_basic_coupled.jl
@@ -76,19 +76,19 @@ semi = SemidiscretizationCoupled((semi1, semi2))
 # ODE solvers, callbacks etc.
 
 # Create ODE problem with time span from 0.0 to 1.0
-ode = semidiscretize(semi, (0.0, 1.0));
+ode = semidiscretize(semi, (0.0, 2.0));
 
 # At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
 # and resets the timers
 summary_callback = SummaryCallback()
 
 # The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
-analysis_callback1 = AnalysisCallback(semi1, interval=100)
-analysis_callback2 = AnalysisCallback(semi2, interval=100)
+analysis_callback1 = AnalysisCallback(semi1, interval=2)
+analysis_callback2 = AnalysisCallback(semi2, interval=2)
 analysis_callback = AnalysisCallbackCoupled(semi, analysis_callback1, analysis_callback2)
 
 # The SaveSolutionCallback allows to save the solution to a file in regular intervals
-save_solution = SaveSolutionCallback(interval=100,
+save_solution = SaveSolutionCallback(interval=2,
                                      solution_variables=cons2prim)
 
 # The StepsizeCallback handles the re-calculation of the maximum Δt after each time step
