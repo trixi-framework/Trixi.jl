adfff4dd0cac8ca3e90c5f5636b8c34c28ba02fc
SimonCan <simon.candelaresi@gmail.com>
2023-06-06 18:40:29 +0100

    Added convergence_test for elixir_advection_basic_coupled.

--- test/test_special_elixirs.jl
+++ test/test_special_elixirs.jl
@@ -30,6 +30,11 @@ coverage = occursin("--code-coverage", cmd) && !occursin("--code-coverage=none",
         @test isapprox(mean_convergence[:l2], [4.0], rtol=0.05)
       end
 
+      @timed_testset "structured_2d_dgsem" begin
+        mean_convergence = convergence_test(@__MODULE__, joinpath(EXAMPLES_DIR, "structured_2d_dgsem", "elixir_advection_basic_coupled.jl"), 3)
+        @test isapprox((mean_convergence[1][:l2] + mean_convergence[2][:l2])/2.0, [4.0], rtol=0.05)
+      end
+
       @timed_testset "p4est_2d_dgsem" begin
         # Run convergence test on unrefined mesh
         no_refine = @cfunction((p4est, which_tree, quadrant) -> Cint(0), Cint, (Ptr{Trixi.p4est_t}, Ptr{Trixi.p4est_topidx_t}, Ptr{Trixi.p4est_quadrant_t}))
