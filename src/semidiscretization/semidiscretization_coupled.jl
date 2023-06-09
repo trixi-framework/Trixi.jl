e70fe1438c407d8814f1639fcaef0c391dd5c2e8
Michael Schlottke-Lakemper <michael@sloede.com>
2023-06-09 15:19:01 +0200

    Add xref for Documenter.jl

--- src/semidiscretization/semidiscretization_coupled.jl
+++ src/semidiscretization/semidiscretization_coupled.jl
@@ -4,7 +4,7 @@
 A struct used to bundle multiple semidiscretizations.
 `semidiscretize` will return an `ODEproblem` that synchronizes time steps between the semidiscretizations.
 Each call of `rhs!` will call `rhs!` for each semidiscretization individually.
-The semidiscretizations can be coupled by gluing meshes together using `BoundaryConditionCoupled`.
+The semidiscretizations can be coupled by gluing meshes together using [`BoundaryConditionCoupled`](@ref).
 
 !!! warning "Experimental code"
     This is an experimental feature and can change any time.
