a00909c2dcb52bea5959c10b854e4d92c0ee93f7
Michael Schlottke-Lakemper <michael@sloede.com>
2023-06-09 15:21:10 +0200

    Fixup comment

--- src/semidiscretization/semidiscretization_coupled.jl
+++ src/semidiscretization/semidiscretization_coupled.jl
@@ -2,7 +2,7 @@
     SemidiscretizationCoupled
 
 A struct used to bundle multiple semidiscretizations.
-`semidiscretize` will return an `ODEproblem` that synchronizes time steps between the semidiscretizations.
+[`semidiscretize`](@ref) will return an `ODEProblem` that synchronizes time steps between the semidiscretizations.
 Each call of `rhs!` will call `rhs!` for each semidiscretization individually.
 The semidiscretizations can be coupled by gluing meshes together using [`BoundaryConditionCoupled`](@ref).
 
