f462ed3e40bfb4d593b55e6a30704a8a70ae5cfa
Joshua Lampert <joshua.lampert@tuhh.de>
2023-06-11 22:05:04 +0200

    substituted non-ascii lambda

--- src/callbacks_step/stepsize_dg1d.jl
+++ src/callbacks_step/stepsize_dg1d.jl
@@ -12,14 +12,14 @@ function max_dt(u, t, mesh::TreeMesh{1},
     max_scaled_speed = nextfloat(zero(t))
 
     for element in eachelement(dg, cache)
-        max_λ1 = zero(max_scaled_speed)
+        max_lambda1 = zero(max_scaled_speed)
         for i in eachnode(dg)
             u_node = get_node_vars(u, equations, dg, i, element)
-            λ1, = max_abs_speeds(u_node, equations)
-            max_λ1 = max(max_λ1, λ1)
+            lambda1, = max_abs_speeds(u_node, equations)
+            max_lambda1 = max(max_lambda1, lambda1)
         end
         inv_jacobian = cache.elements.inverse_jacobian[element]
-        max_scaled_speed = max(max_scaled_speed, inv_jacobian * max_λ1)
+        max_scaled_speed = max(max_scaled_speed, inv_jacobian * max_lambda1)
     end
 
     return 2 / (nnodes(dg) * max_scaled_speed)
@@ -32,9 +32,9 @@ function max_dt(u, t, mesh::TreeMesh{1},
     max_scaled_speed = nextfloat(zero(t))
 
     for element in eachelement(dg, cache)
-        max_λ1, = max_abs_speeds(equations)
+        max_lambda1, = max_abs_speeds(equations)
         inv_jacobian = cache.elements.inverse_jacobian[element]
-        max_scaled_speed = max(max_scaled_speed, inv_jacobian * max_λ1)
+        max_scaled_speed = max(max_scaled_speed, inv_jacobian * max_lambda1)
     end
 
     return 2 / (nnodes(dg) * max_scaled_speed)
@@ -47,18 +47,18 @@ function max_dt(u, t, mesh::StructuredMesh{1},
     max_scaled_speed = nextfloat(zero(t))
 
     for element in eachelement(dg, cache)
-        max_λ1 = zero(max_scaled_speed)
+        max_lambda1 = zero(max_scaled_speed)
 
         for i in eachnode(dg)
             u_node = get_node_vars(u, equations, dg, i, element)
-            λ1, = max_abs_speeds(u_node, equations)
+            lambda1, = max_abs_speeds(u_node, equations)
 
             inv_jacobian = cache.elements.inverse_jacobian[i, element]
 
-            max_λ1 = max(max_λ1, inv_jacobian * λ1)
+            max_lambda1 = max(max_lambda1, inv_jacobian * lambda1)
         end
 
-        max_scaled_speed = max(max_scaled_speed, max_λ1)
+        max_scaled_speed = max(max_scaled_speed, max_lambda1)
     end
 
     return 2 / (nnodes(dg) * max_scaled_speed)
@@ -71,11 +71,11 @@ function max_dt(u, t, mesh::StructuredMesh{1},
     max_scaled_speed = nextfloat(zero(t))
 
     for element in eachelement(dg, cache)
-        max_λ1, = max_abs_speeds(equations)
+        max_lambda1, = max_abs_speeds(equations)
 
         for i in eachnode(dg)
             inv_jacobian = cache.elements.inverse_jacobian[i, element]
-            max_scaled_speed = max(max_scaled_speed, inv_jacobian * max_λ1)
+            max_scaled_speed = max(max_scaled_speed, inv_jacobian * max_lambda1)
         end
     end
 
