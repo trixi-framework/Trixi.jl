a8b40f9b0e7d2ea3daae0b4547356dcf223165e7
Joshua Lampert <joshua.lampert@tuhh.de>
2023-06-20 14:29:40 +0200

    fix some typos in docs

--- src/equations/acoustic_perturbation_2d.jl
+++ src/equations/acoustic_perturbation_2d.jl
@@ -145,7 +145,7 @@ function initial_condition_convergence_test(x, t,
 end
 
 """
-  source_terms_convergence_test(u, x, t, equations::AcousticPerturbationEquations2D)
+    source_terms_convergence_test(u, x, t, equations::AcousticPerturbationEquations2D)
 
 Source terms used for convergence tests in combination with
 [`initial_condition_convergence_test`](@ref).
