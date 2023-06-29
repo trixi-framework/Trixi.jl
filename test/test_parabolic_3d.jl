c7107f8105076dec634c39ff0d709a8ab56344ec
Ahmad Peyvan <ahmad_peyvan@brown.edu>
2023-06-29 17:09:50 -0400

    Addin TGV example and test to `P4estMesh{3}`

--- test/test_parabolic_3d.jl
+++ test/test_parabolic_3d.jl
@@ -94,6 +94,13 @@ isdir(outdir) && rm(outdir, recursive=true)
     )
   end
 
+  @trixi_testset "P4estMesh3D: elixir_navierstokes_taylor_green_vortex.jl" begin
+    @test_trixi_include(joinpath(examples_dir(), "p4est_3d_dgsem", "elixir_navierstokes_taylor_green_vortex.jl"),
+      initial_refinement_level = 2, tspan=(0.0, 0.25),
+      l2 = [0.0001547509861140407, 0.015637861347119624, 0.015637861347119687, 0.022024699158522523, 0.009711013505930812], 
+      linf = [0.0006696415247340326, 0.03442565722527785, 0.03442565722577423, 0.06295407168705314, 0.032857472756916195]
+    )
+
 end
 
 # Clean up afterwards: delete Trixi.jl output directory
