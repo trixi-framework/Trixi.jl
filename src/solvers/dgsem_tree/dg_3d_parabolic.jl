42c4016794aeb35522c7efaf05eac49b101d6d6b
Ahmad Peyvan <115842305+apey236@users.noreply.github.com>
2023-06-30 12:43:53 -0400

    Update src/solvers/dgsem_tree/dg_3d_parabolic.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/solvers/dgsem_tree/dg_3d_parabolic.jl
+++ src/solvers/dgsem_tree/dg_3d_parabolic.jl
@@ -807,7 +807,7 @@ end
 # This is because the parabolic fluxes are assumed to be of the form
 #   `du/dt + df/dx = dg/dx + source(x,t)`,
 # where f(u) is the inviscid flux and g(u) is the viscous flux.
-function apply_jacobian_parabolic!(du, mesh::Union{P4estMesh{3}, TreeMesh{3}},
+function apply_jacobian_parabolic!(du, mesh::Union{TreeMesh{3}, P4estMesh{3}},
                                    equations::AbstractEquationsParabolic, dg::DG, cache)
     @threaded for element in eachelement(dg, cache)
         factor = cache.elements.inverse_jacobian[element]
