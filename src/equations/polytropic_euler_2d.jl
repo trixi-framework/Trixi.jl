104d226ae653de8cbb09d25d8b829af77419875a
SimonCan <simon.candelaresi@gmail.com>
2023-07-11 15:09:39 +0100

    Added source term for ECO.

--- src/equations/polytropic_euler_2d.jl
+++ src/equations/polytropic_euler_2d.jl
@@ -202,3 +202,30 @@ function initial_condition_convergence_test(x, t, equations::PolytropicEulerEqua
     return prim2cons(SVector(h, h / 2, 3 * h / 2), equations)
 end
 end # @muladd
+
+
+"""
+source_terms_eoc_test_polytropic(u, x, t, equations::PolytropicEulerEquations2D)
+
+"""
+@inline function source_terms_eoc_test_polytropic(u, x, t,
+                                                  equations::PolytropicEulerEquations2D)
+    # Residual from Winters (2019) [0.1007/s10543-019-00789-w] eq. (5.2).
+    h_t = -2 * pi * cos(2 * pi * x[1]) * sin(2 * pi * x[2]) * sin(2 * pi * t)
+    h_x = -2 * pi * sin(2 * pi * x[1]) * sin(2 * pi * x[2]) * cos(2 * pi * t)
+    h_y = 2 * pi * cos(2 * pi * x[1]) * cos(2 * pi * x[2]) * cos(2 * pi * t)
+
+    c2 = equations.p_avg / equations.rho
+
+    if gamma == 1
+        b = c2
+    else
+        b = equations.kappa * equations.gamma * equations.rho^(equations.gamma-1)
+    end
+
+    r_1 = h_t + h_x / 2 + 3 / 2 * h_y
+    r_2 = h_t / 2 + h_x / 4 + b * equations.rho_x + 3 / 4 * h_y
+    r_3 = h_t / 2 + 3 / 4 * h_x + 9 / 4 * h_y + b * equations.rho_y
+
+    return SVector(r_1, r_2, r_3)
+end
