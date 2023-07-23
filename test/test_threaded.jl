6aa7cd4be0f7a3ac6f2c113ccf66f5beffa15b46
Hendrik Ranocha <mail@ranocha.de>
2023-07-23 13:41:03 +0200

    fixed FDSBP, elixir_euler_convergence.jl

--- test/test_threaded.jl
+++ test/test_threaded.jl
@@ -142,7 +142,7 @@ Trixi.mpi_isroot() && isdir(outdir) && rm(outdir, recursive=true)
         t = sol.t[end]
         u_ode = sol.u[end]
         du_ode = similar(u_ode)
-        @test_broken (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 5000
+        @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 5000
       end
     end
   end
