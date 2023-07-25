253f63ef042ef3f10ca15c5d21327a3b6ce4bcdc
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2023-07-24 22:46:27 -0500

    Fix CI failures related to Makie (#1586)
    * unrelated cleanup
    
    * fix CI issues?

--- ext/TrixiMakieExt.jl
+++ ext/TrixiMakieExt.jl
@@ -335,7 +335,7 @@ end
 # ================== new Makie plot recipes ====================
 
 # This initializes a Makie recipe, which creates a new type definition which Makie uses to create
-# custom `trixiheatmap` plots. See also https://makie.juliaplots.org/stable/recipes.html
+# custom `trixiheatmap` plots. See also https://docs.makie.org/stable/documentation/recipes/
 Makie.@recipe(TrixiHeatmap, plot_data_series) do scene
     Makie.Theme(colormap = default_Makie_colormap())
 end
@@ -346,9 +346,8 @@ function Makie.plot!(myplot::TrixiHeatmap)
     plotting_mesh = global_plotting_triangulation_makie(pds;
                                                         set_z_coordinate_zero = true)
 
-    @unpack variable_id = pds
     pd = pds.plot_data
-    solution_z = vec(StructArrays.component(pd.data, variable_id))
+    solution_z = vec(StructArrays.component(pd.data, pds.variable_id))
     Makie.mesh!(myplot, plotting_mesh, color = solution_z, shading = false,
                 colormap = myplot[:colormap])
     myplot.colorrange = extrema(solution_z)
@@ -411,7 +410,7 @@ function Makie.plot!(fig, pd::PlotData2DTriangulated;
 
         row = row_list[variable_to_plot]
         col = col_list[variable_to_plot]
-        Makie.Colorbar(fig[row, col][1, 2], plt)
+        Makie.Colorbar(fig[row, col][1, 2], colormap = colormap)
 
         ax.aspect = Makie.DataAspect() # equal aspect ratio
         ax.title = variable_name
