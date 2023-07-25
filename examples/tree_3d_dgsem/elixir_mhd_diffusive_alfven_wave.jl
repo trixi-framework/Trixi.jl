d3179a22b92f26fe1db8c7864d109db8b73917fe
SimonCan <simon.candelaresi@gmail.com>
2023-07-25 19:25:25 +0100

    Removed relative errors and reformatted.

--- examples/tree_3d_dgsem/elixir_mhd_diffusive_alfven_wave.jl
+++ examples/tree_3d_dgsem/elixir_mhd_diffusive_alfven_wave.jl
@@ -10,50 +10,51 @@ mu() = 4e-2
 eta = 4e-2
 
 equations = IdealGlmMhdEquations3D(1.4)
-equations_parabolic = CompressibleMhdDiffusion3D(equations, mu=mu(), Prandtl=prandtl_number(),
-						 eta=eta,
-                                                 gradient_variables=GradientVariablesPrimitive())
+equations_parabolic = CompressibleMhdDiffusion3D(equations, mu = mu(),
+                                                 Prandtl = prandtl_number(),
+                                                 eta = eta,
+                                                 gradient_variables = GradientVariablesPrimitive())
 
 volume_flux = (flux_hindenlang_gassner, flux_nonconservative_powell)
-solver = DGSEM(polydeg=3, surface_flux=(flux_lax_friedrichs, flux_nonconservative_powell),
-               volume_integral=VolumeIntegralFluxDifferencing(volume_flux))
+solver = DGSEM(polydeg = 3,
+               surface_flux = (flux_lax_friedrichs, flux_nonconservative_powell),
+               volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
 coordinates_min = (-1.0, -1.0, -1.0) # minimum coordinates (min(x), min(y), min(z))
-coordinates_max = ( 1.0,  1.0,  1.0) # maximum coordinates (max(x), max(y), max(z))
+coordinates_max = (1.0, 1.0, 1.0) # maximum coordinates (max(x), max(y), max(z))
 
 # Create a uniformly refined mesh with periodic boundaries
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=3,
-                n_cells_max=50_000) # set maximum capacity of tree data structure
-
+                initial_refinement_level = 3,
+                n_cells_max = 50_000) # set maximum capacity of tree data structure
 
 function initial_condition_constant_alfven(x, t, equations)
-  p = 1
-  omega = 2*pi / 2 # may be multiplied by frequency
-  # r: length-variable = length of computational domain
-  r = 2
-  e = 0.02
-  sqr = 1
-  Va  = omega
-  phi_alv = omega * (x[1] - t)
+    p = 1
+    omega = 2 * pi / 2 # may be multiplied by frequency
+    # r: length-variable = length of computational domain
+    r = 2
+    e = 0.02
+    sqr = 1
+    Va = omega
+    phi_alv = omega * (x[1] - t)
 
-  rho = 1.
-  rho_v1  = 0
-  rho_v2  = e*cos(phi_alv)
-  rho_v3  =  0
-  rho_e = 10.
-  B1  = 1
-  B2  = -rho_v2*sqr
-  B3  = 0
-  psi = 0
+    rho = 1.0
+    rho_v1 = 0
+    rho_v2 = e * cos(phi_alv)
+    rho_v3 = 0
+    rho_e = 10.0
+    B1 = 1
+    B2 = -rho_v2 * sqr
+    B3 = 0
+    psi = 0
 
-  return SVector(rho, rho_v1, rho_v2, rho_v3, rho_e, B1, B2, B3, psi)
+    return SVector(rho, rho_v1, rho_v2, rho_v3, rho_e, B1, B2, B3, psi)
 end
 
 initial_condition = initial_condition_constant_alfven
 
 semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic),
-					     initial_condition, solver)
+                                             initial_condition, solver)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
@@ -63,18 +64,18 @@ tspan = (0.0, 0.1)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
-alive_callback = AliveCallback(alive_interval=10)
+alive_callback = AliveCallback(alive_interval = 10)
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
-save_solution = SaveSolutionCallback(interval=100,
-                                     save_initial_solution=true,
-                                     save_final_solution=true,
-                                     solution_variables=cons2prim)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
+save_solution = SaveSolutionCallback(interval = 100,
+                                     save_initial_solution = true,
+                                     save_final_solution = true,
+                                     solution_variables = cons2prim)
 
 cfl = 1.5
-stepsize_callback = StepsizeCallback(cfl=cfl)
+stepsize_callback = StepsizeCallback(cfl = cfl)
 
-glm_speed_callback = GlmSpeedCallback(glm_scale=0.5, cfl=cfl)
+glm_speed_callback = GlmSpeedCallback(glm_scale = 0.5, cfl = cfl)
 
 callbacks = CallbackSet(summary_callback,
                         alive_callback,
@@ -83,14 +84,12 @@ callbacks = CallbackSet(summary_callback,
                         stepsize_callback,
                         glm_speed_callback)
 
-
 ###############################################################################
 # run the simulation
 
 time_int_tol = 1e-5
-sol = solve(ode, RDPK3SpFSAL49(), abstol=time_int_tol, reltol=time_int_tol, dt = 1e-5,
-            save_everystep=false, callback=callbacks)
+sol = solve(ode, RDPK3SpFSAL49(), dt = 1e-5,
+            save_everystep = false, callback = callbacks, adaptive = false)
 
 # Print the timer summary.
 summary_callback()
-
