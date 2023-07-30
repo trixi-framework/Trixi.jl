05c1246ef44a5ead7ff5867fdcc7e1c36ae1c6c9
ArseniyKholod <kholod.ap03@gmail.com>
2023-07-30 17:26:52 +0300

    Update test_threaded.jl

--- test/test_threaded.jl
+++ test/test_threaded.jl
@@ -59,6 +59,21 @@ Trixi.mpi_isroot() && isdir(outdir) && rm(outdir, recursive=true)
         linf = [2.3040850347877395e-5]
       )
     end
+
+    @trixi_testset "elixir_euler_density_wave_extended.jl" begin
+      @test_trixi_include(joinpath(joinpath(examples_dir(), "tree_2d_dgsem", "elixir_euler_density_wave_extended.jl"),
+        # Expected errors are exactly the same as in the serial test!
+        l2   = [0.0012049974095327037, 0.00012066222768628522, 0.00024111367644905207, 0.0005962045363712651],
+        linf = [0.004212631542717782, 0.00042010775917775047, 0.0008412890054608387, 0.007078383889172812])
+    end
+
+    @trixi_testset "elixir_euler_density_wave_restart.jl" begin
+      @test_trixi_include(joinpath(joinpath(examples_dir(), "tree_2d_dgsem", "elixir_euler_density_wave_restart.jl"),
+        # Expected errors are exactly the same as in the serial test!
+        # and in the elixir_euler_density_wave_extended.jl
+        l2   = [0.0012049974095327037, 0.00012066222768628522, 0.00024111367644905207, 0.0005962045363712651],
+        linf = [0.004212631542717782, 0.00042010775917775047, 0.0008412890054608387, 0.007078383889172812])
+    end
   end
 
 
