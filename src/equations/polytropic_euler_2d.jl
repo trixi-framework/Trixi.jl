56dbd5e8892c22699dedb342d1bb803ecd87acac
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2023-08-01 18:11:22 +0100

    Update src/equations/polytropic_euler_2d.jl
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/equations/polytropic_euler_2d.jl
+++ src/equations/polytropic_euler_2d.jl
@@ -10,17 +10,17 @@
 
 The polytropic Euler equations
 ```math
-\partial t
+\frac{\partial}{\partial t}
 \begin{pmatrix}
 \rho \\ \rho v_1 \\ \rho v_2
 \end{pmatrix}
 +
-\partial x
+\frac{\partial}{\partial x}
 \begin{pmatrix}
  \rho v_1 \\ \rho v_1^2 + \kappa\rho^\gamma \\ \rho v_1 v_2
 \end{pmatrix}
 +
-\partial y
+\frac{\partial}{\partial y}
 \begin{pmatrix}
 \rho v_2 \\ \rho v_1 v_2 \\ \rho v_2^2 + \kappa\rho^\gamma
 \end{pmatrix}
