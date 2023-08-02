55ba995686e1d44cdb9179205599335c03939ac9
ArseniyKholod <kholod.ap03@gmail.com>
2023-08-02 20:14:25 +0300

    fix shot 4

--- test/configure_packages.jl
+++ test/configure_packages.jl
@@ -1,23 +1,19 @@
-using Pkg
-Pkg.add("Libdl")
-using Libdl
+using Pkg, Libdl
 Pkg.activate(dirname(@__DIR__))
+Pkg.instantiate()
 
 # Configure the test setup based on environment variables set in CI.
 # First, we get the settings and remove all local preference configurations
 # that may still exist.
-const JULIA_HDF5_PATH = get(ENV, "JULIA_HDF5_PATH", "")
 rm(joinpath(dirname(@__DIR__), "LocalPreferences.toml"); force=true)
 
 # Next, we configure MPI.jl appropriately.
-Pkg.add("MPIPreferences")
-Pkg.add("Preferences")
-Pkg.add("UUIDs")
 import MPIPreferences
 MPIPreferences.use_system_binary()
 
 # Finally, we configure HDF5.jl as desired.
 import UUIDs, Preferences
+const JULIA_HDF5_PATH = get(ENV, "JULIA_HDF5_PATH", "")
 Preferences.set_preferences!(
     UUIDs.UUID("f67ccb44-e63f-5c2f-98bd-6dc0ccc4ba2f"), # UUID of HDF5.jl
     "libhdf5" => joinpath(JULIA_HDF5_PATH, "libhdf5." * Libdl.dlext),
