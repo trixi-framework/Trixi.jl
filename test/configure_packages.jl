008ca58427ce54379f2bfc7ca405a84551606ebc
ArseniyKholod <kholod.ap03@gmail.com>
2023-08-02 23:20:26 +0300

    fix shot 11

--- test/configure_packages.jl
+++ test/configure_packages.jl
@@ -8,12 +8,12 @@ const JULIA_HDF5_PATH = get(ENV, "JULIA_HDF5_PATH", "")
 rm(joinpath(dirname(@__DIR__), "LocalPreferences.toml"); force=true)
 
 # Next, we configure MPI.jl appropriately.
-import MPI
-MPI.MPIPreferences.use_system_binary()
+using MPIPreferences
+MPIPreferences.use_system_binary()
 
 # Finally, we configure HDF5.jl as desired.
 import UUIDs
-MPI.MPIPreferences.Preferences.set_preferences!(
+MPIPreferences.Preferences.set_preferences!(
     UUIDs.UUID("f67ccb44-e63f-5c2f-98bd-6dc0ccc4ba2f"), # UUID of HDF5.jl
     "libhdf5" => joinpath(JULIA_HDF5_PATH, "libhdf5." * Libdl.dlext),
     "libhdf5_hl" => joinpath(JULIA_HDF5_PATH, "libhdf5_hl." * Libdl.dlext);
