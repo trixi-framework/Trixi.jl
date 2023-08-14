40cafa1e168d2a4542b33b81c765dab1a09bc72f
SimonCan <simon.candelaresi@gmail.com>
2023-08-14 12:09:43 +0100

    Added convergence test to the diffusive Alfven wave elixir example.

--- examples/tree_3d_dgsem/elixir_mhd_diffusive_alfven_wave.jl
+++ examples/tree_3d_dgsem/elixir_mhd_diffusive_alfven_wave.jl
@@ -25,60 +25,75 @@ coordinates_max = (1.0, 1.0, 1.0) # maximum coordinates (max(x), max(y), max(z))
 
 # Create a uniformly refined mesh with periodic boundaries
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level = 3,
+                initial_refinement_level = 1,
+		periodicity=(true, false, true),
                 n_cells_max = 50_000) # set maximum capacity of tree data structure
 
 function initial_condition_constant_alfven(x, t, equations)
     # Homogeneous background magnetic field in the x-direction that is perturbed
     # by a small change in the y-direction (e.g. Biskamp 2003, section 2.5.1).
+    # This particular set-up is in line with Rembiasz et al. (2018)
+    # DOI: doi.org/10.3847/1538-4365/aa6254, but with p = 1.
     # For a derivation of Alfven waves see e.g.:
     # Alfven H., 150, p. 450, Nature (1942), DOI: 10.1038/150405d0
     # Chandrasekhar, Hydrodynamic and hydromagnetic stability (1961)
     # Biskamp, Magnetohydrodynamic Turbulence (2003)
-    p = 1
-    omega = 2 * pi / 2 # may be multiplied by frequency
-    # r: length-variable = length of computational domain
-    r = 2
-    e = 0.02
-    sqr = 1
-    Va = omega
-    phi_alv = omega * (x[1] - t)
+
+    epsilon = 0.02
+    k = 2*pi*1
+    p = 2e-3
 
     rho = 1.0
     rho_v1 = 0
-    rho_v2 = e * cos(phi_alv)
+    rho_v2 = -epsilon*sin(k*x[1])/sqrt(rho)
     rho_v3 = 0
-    rho_e = 10.0
     B1 = 1
-    B2 = -rho_v2 * sqr
+    B2 = epsilon*sin(k*x[1])
     B3 = 0
+    rho_e = 1
     psi = 0
 
     return SVector(rho, rho_v1, rho_v2, rho_v3, rho_e, B1, B2, B3, psi)
 end
 
+@inline function source_terms_mhd_convergence_test(u, x, t, equations)
+    r_1 = 0
+    r_2 = -0.0008*pi*sin(2*pi*x[1])*cos(2*pi*x[1])
+    r_3 = -0.0032*pi^2*sin(2*pi*x[1]) + 0.04*pi*cos(2*pi*x[1])
+    r_4 = 0
+    r_5 = 0.001664*pi^2*sin(2*pi*x[1])^2 - 0.0016*pi*sin(2*pi*x[1])*cos(2*pi*x[1]) -
+          0.001664*pi^2*cos(2*pi*x[1])^2
+    r_6 = 0
+    r_7 = 0.0032*pi^2*sin(2*pi*x[1]) - 0.04*pi*cos(2*pi*x[1])
+    r_8 = 0
+    r_9 = 0
+
+    return SVector(r_1, r_2, r_3, r_4, r_5, r_6, r_7, r_8, r_9)
+end
+
 initial_condition = initial_condition_constant_alfven
 
 semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic),
-                                             initial_condition, solver)
+                                             initial_condition, solver,
+					     source_terms = source_terms_mhd_convergence_test)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
 
 # Create ODE problem with time span `tspan`
-tspan = (0.0, 1.0)
+tspan = (0.0, 10.0)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
 alive_callback = AliveCallback(alive_interval = 10)
 analysis_interval = 100
 analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
-save_solution = SaveSolutionCallback(interval = 100,
+save_solution = SaveSolutionCallback(interval = 10,
                                      save_initial_solution = true,
                                      save_final_solution = true,
                                      solution_variables = cons2prim)
 
-cfl = 0.75
+cfl = 0.25
 stepsize_callback = StepsizeCallback(cfl = cfl)
 
 glm_speed_callback = GlmSpeedCallback(glm_scale = 0.5, cfl = cfl)
