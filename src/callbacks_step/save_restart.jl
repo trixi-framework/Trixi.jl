24c8c871260980b4c684488c01ae9c6f7502eed5
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2023-08-30 12:45:51 +0300

    Update src/callbacks_step/save_restart.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/callbacks_step/save_restart.jl
+++ src/callbacks_step/save_restart.jl
@@ -144,8 +144,9 @@ end
 """
     load_timestep!(integrator, restart_file::AbstractString)
 
-Load the time step number (`iter` in OrdinaryDiffEq.jl) saved in a `restart_file`. And set
-the number of accepted steps (`stats.naccept` in OrdinaryDiffEq.jl) equal to the time step number.
+Load the time step number saved in a `restart_file` and assign it to both the time step
+number and and the number of accepted steps
+(`iter` and `stats.naccept` in OrdinaryDiffEq.jl, respectively) in `integrator`.
 """
 function load_timestep!(integrator, restart_file::AbstractString)
     integrator.iter = load_timestep(restart_file)
