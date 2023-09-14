de134bfdcb01ec3f3f54fe33c18b7419e4a45b0d
Michael Schlottke-Lakemper <michael@sloede.com>
2023-09-14 12:26:06 +0200

    Skip ambiguities (too many false positives) and account for @jlchan being a 🏴‍☠️

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -7,7 +7,10 @@ using Trixi
 include("test_trixi.jl")
 
 @timed_testset "Aqua.jl" begin
-    Aqua.test_all(Trixi)
+    Aqua.test_all(Trixi,
+                  ambiguities = false,
+                  piracy = (treat_as_own = [Trixi.StartUpDG.RefElemData,
+                                            Trixi.StartUpDG.MeshData],))
 end
 
 end #module
