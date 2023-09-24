177f9e50b7346af28f832a508bbdccef6a7b30a0
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2023-09-24 18:56:45 +0200

    add Visualize the solution

--- docs/literate/src/files/getting_started.jl
+++ docs/literate/src/files/getting_started.jl
@@ -184,15 +184,15 @@ get_examples()
 # - Now you can create your own initial conditions. Add the following code after the
 #   commented line:
 
-    function initial_condition_density_waves(x,t,equations::CompressibleEulerEquations2D)
-      v1 = 0.1 # velocity along x-axis
-      v2 = 0.2 # velocity along y-axis
-      rho = 1.0 + 0.98 * sin(pi * (sum(x) - t * (v1 + v2))) # density wave profile
-      p = 20 # pressure
-      rho_e = p / (equations.gamma - 1) + 1/2 * rho * (v1^2 + v2^2)
-      return SVector(rho, rho*v1, rho*v2, rho_e)
+    function initial_condition_density_waves(x, t, equations::CompressibleEulerEquations2D)
+        v1 = 0.1 # velocity along x-axis
+        v2 = 0.2 # velocity along y-axis
+        rho = 1.0 + 0.98 * sin(pi * (sum(x) - t * (v1 + v2))) # density wave profile
+        p = 20 # pressure
+        rho_e = p / (equations.gamma - 1) + 1/2 * rho * (v1^2 + v2^2)
+        return SVector(rho, rho*v1, rho*v2, rho_e)
     end
-    initial_condition=initial_condition_density_waves
+    initial_condition = initial_condition_density_waves
 
 # - Execute the following code one more time, but instead of `path/to/file` paste the path to the
 #   `elixir_euler_ec.jl` file that you just edited.
@@ -219,106 +219,240 @@ plot(p1, p2, p3, p4) #hide #md
 
 # ### Create first setup
 
-# In this part of the tutorial we will consider a creation of first Trixi.jl setup. To make code
-# easy to read and understand we will discuss only one dimensional case. But all described logic
-# will be valid for more complicated 2D and 3D cases. You can take a look at one of the elixirs
-# to see 2D or 3D use cases, e.g.
+# In this part of the tutorial, we will consider a creation of the first Trixi.jl setup, which is
+# an extension of
 # [`elixir_advection_basic.jl`](https://github.com/trixi-framework/Trixi.jl/blob/main/examples/tree_2d_dgsem/elixir_advection_basic.jl).
+# Trixi.jl has a common basic structure of the setup, so you can create your own by extending
+# the following example.
 
-# The first step is to create and open a file with .jl extension. You can do it with your favorite
-# text editor.
+# Let's consider the linear advection equation in two-dimensional spatial domain
+# [-1.0, 1.0]⨯[-1.0, 1.0] with a source term.
+# ```math
+# \partial_t u(t,x,y) + 0.2 \partial_x u(t,x,y) - 0.7 \partial_y u(t,x,y) = - 2 \exp(-t)
+# \sin\bigl(2 \pi (x - t) \bigr) \sin\bigl(2 \pi (y - t) \bigr)
+# ```
+# With an initial condition
+# ```math
+# u(0,x,y) = \sin\bigl(\pi x \bigr) \sin\bigl(\pi y \bigr).
+# ```
 
-# First you need to connect the packages that you will use in your setup. By default you will always need
-# Trixi.jl itself and [OrdinaryDiffEq.jl](https://github.com/SciML/OrdinaryDiffEq.jl).
+# The first step is to create and open a file with the .jl extension. You can do this with your
+# favorite text editor.
+
+# First you need to connect the packages that you will use in your setup. By default, you will
+# always need Trixi.jl itself and [OrdinaryDiffEq.jl](https://github.com/SciML/OrdinaryDiffEq.jl).
 
 using Trixi
 using OrdinaryDiffEq
 
-# The next thing to do is to choose an equation suitable for your problem. For simplicity we use
-# [`LinearScalarAdvectionEquation1D`](@ref). To see all currently implemented equations, take
-# a look at
-# [src/equations](https://github.com/trixi-framework/Trixi.jl/tree/main/src/equations).
-# If you are interested in adding a new physics model that is not implemented in Trixi.jl yet,
-# take a look at 
-# [Adding a new scalar conservation law](@ref adding_new_scalar_equations) and
-# [Adding a non-conservative equation](@ref adding_nonconservative_equation).
+# The next thing to do is to choose an equation that is suitable for your problem. To see all the
+# currently implemented equations, take a look at
+# [`src/equations`](https://github.com/trixi-framework/Trixi.jl/tree/main/src/equations).
+# If you are interested in adding a new physics model that has not yet been implemented in
+# Trixi.jl, take a look at
+# [adding a new scalar conservation law](@ref adding_new_scalar_equations) and
+# [adding a non-conservative equation](@ref adding_nonconservative_equation).
 
-advection_velocity = 1.0
-equations = LinearScalarAdvectionEquation1D(advection_velocity)
+# The linear scalar advection equation is already implemented in Trixi.jl as
+# [`LinearScalarAdvectionEquation2D`](@ref). For which we need to define a two-dimensional parameter
+# named advection velocity, suitable for our problem is (0.2, -0.7).
 
-# To solve your problem numerically with Trixi.jl, you have to define an instruction for spatial
-# discretization. To do it, you need to setup a mesh. The most widely used in Trixi.jl is the
-# `TreeMesh`. The interval used is [-1.0, 1.0]. We also set a number of elements in
-# the mesh using `initial_refinement_level` that describes the initial height of the tree mesh.
-# The variable `n_cells_max` is used to limit the number of elements in the mesh, which cannot be
-# exceeded due to [adaptive mesh refinement](@ref Adaptive-mesh-refinement).
+advection_velocity = (0.2, -0.7)
+equations = LinearScalarAdvectionEquation2D(advection_velocity)
 
-coordinates_min = -1.0
-coordinates_max =  1.0
+# To solve our problem numerically using Trixi.jl, we have to define an instruction for spatial
+# discretization. To do it, we set up a mesh. The most widely used in Trixi.jl is
+# [`TreeMesh`](@ref). The spatial domain used is [-1.0, 1.0]⨯[-1.0, 1.0]. We also set a number of
+# elements in the mesh using `initial_refinement_level`, which describes the initial height of the
+# tree mesh. The variable `n_cells_max` is used to limit the number of elements in the mesh, which
+# cannot be exceeded due to [adaptive mesh refinement](@ref Adaptive-mesh-refinement).
+
+# All minimum and all maximum coordinates must be combined into `Tuples`.
+
+coordinates_min = (-1.0, -1.0)
+coordinates_max = ( 1.0,  1.0)
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=4,
-                n_cells_max=30_000)
+                initial_refinement_level = 4,
+                n_cells_max = 30_000)
 
-# To approximate the solution of the given model we create a DG solver. The solution in every of
-# the recently defined mesh's elements will be approximated by a polynomial of `polydeg`. See more
-# in the [Introduction to DG methods](@ref scalar_linear_advection_1d).
+# To approximate the solution of the defined model, we create a DG solver. The solution in each of
+# the recently defined mesh elements will be approximated by a polynomial of degree `polydeg`.
+# See more in the [Introduction to DG methods](@ref scalar_linear_advection_1d).
 
 solver = DGSEM(polydeg=3)
 
-# Create an initial conditions, e.g. a Gaussian pulse. All already implemented initial conditions for the
-# [`LinearScalarAdvectionEquation1D`](@ref) can be found in
-# [`src/equations/linear_scalar_advection_1d.jl`](https://github.com/trixi-framework/Trixi.jl/blob/main/src/equations/linear_scalar_advection_1d.jl)
-# or you can implement your initial conditions as shown above for another example.
+# Now we need to define the initial conditions for our problem. All the already implemented
+# initial conditions for [`LinearScalarAdvectionEquation2D`](@ref) can be found in
+# [`src/equations/linear_scalar_advection_2d.jl`](https://github.com/trixi-framework/Trixi.jl/blob/main/src/equations/linear_scalar_advection_2d.jl).
+# If you want to use, for example, a Gaussian pulse, it can be used as follows.
+# ```julia
+# initial_conditions = initial_condition_gauss
+# ```
+# But for our problem, we define our own initial conditions.
+# ```math
+# u(0,x,y) = \sin\bigl(\pi x \bigr) \sin\bigl(\pi y \bigr)
+# ```
+# The initial conditions function must take coordinates, time and the equation itself as arguments
+# and return the initial conditions as a static vector `SVector`. Following the same structure, you
+# can define your own initial conditions.
 
-initial_conditions = initial_condition_gauss
+function initial_condition_sin(x, t, equations::LinearScalarAdvectionEquation2D)
+    scalar = sinpi(x[1]) * sinpi(x[2])
+    return SVector(scalar)
+end
+initial_condition = initial_condition_sin
 
-# Now we collect the information that will be needed to define spatial discretization, and create
-# an ODE problem with a time span fpom 0.0 to 1.0.
+# The next step is to define the function of the source term corresponding to our problem.
+# ```math
+# f(t,u,x,y) = - 2 \exp(-t) \sin\bigl(2 \pi (x - t) \bigr) \sin\bigl(2 \pi (y - t) \bigr)
+# ```
+# This function must take the target variable, coordinates, time and the
+# equation itself as arguments and return the source term as a static vector `SVector`.
 
-semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergence_test, solver)
-tspan=(0.0,1.0)
+function source_term_exp_sin(u, x, t, equations::LinearScalarAdvectionEquation2D)
+    scalar = - 2 * exp(-t) * sinpi(2*(x[1] - t)) * sinpi(2*(x[2] - t))
+    return SVector(scalar)
+end
+
+# Now we are collecting all the information that will be needed to define spatial discretization
+# and to create an ODE problem with a time span from 0.0 s to 1.0 s.
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver;
+                                    source_terms = source_term_exp_sin)
+tspan = (0.0, 1.0)
 ode = semidiscretize(semi, tspan);
 
-# At this point our problem is defined. We will use `solve` function defined in OrdinaryDiffEq.jl.
-# OrdinaryDiffEq.jl gives us possibility to extend the solver using Callbacks without actually
-# modifing it. Trixi.jl already has some implemented [Callbacks](@ref callbacks-id).
+# At this point, our problem is defined. We will use the `solve` function defined in
+# OrdinaryDiffEq.jl to get the solution. OrdinaryDiffEq.jl gives us ability to customize the solver
+# using callbacks without actually modifying it. Trixi.jl already has some implemented
+# [Callbacks](@ref callbacks-id). The most widely used callbacks in Trixi.jl are
+# [step controll callbacks](https://docs.sciml.ai/DiffEqCallbacks/stable/step_control/) that are
+# activated at the end of each time step to perform some actions, e.g. to print a statistics.
+# We will show you how to use some of the common callbacks.
 
-# To print a summary of the simulation setup at the begining of main loop
-# and to reset we use [`SummaryCallback`](@ref).
+# To print a summary of the simulation setup at the beginning of solve-loop
+# and to reset timers we use [`SummaryCallback`](@ref).
 
 summary_callback = SummaryCallback()
 
-# Also we want to analyse the current state of the solution in regular intervals using
-# [`AnalysisCallback`](@ref). 
+# Also we want to analyse the current state of the solution in regular intervals.
+# [`AnalysisCallback`](@ref) outputs some useful statistical information during the solving process
+# every `interval` time steps.
 
-analysis_callback = AnalysisCallback(semi, interval=10)
+analysis_callback = AnalysisCallback(semi, interval = 5)
 
-# It is also possible to controll the time step using [`StepsizeCallback`](@ref).
-# To get more details, look at
-# [CFL based step size control](https://trixi-framework.github.io/Trixi.jl/stable/tutorials/time_stepping/#CFL-based-step-size-control)
+# It is also possible to control the time step size using [`StepsizeCallback`](@ref) if the time
+# integration method isn't adaptive itself. To get more details, look at
+# [CFL based step size control](@ref CFL-based-step-size-control).
 
-stepsize_callback = StepsizeCallback(cfl=1.6)
+stepsize_callback = StepsizeCallback(cfl = 1.6)
 
-# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
+# To save the current numerical solution in regular intervals we use
+# [`SaveSolutionCallback`](@ref). We set the interval equal 5, which means that the solution will
+# be saved every 5 time steps. Also we would like to save the initial and final solutions. Solution
+# will be saved as a HDF5 file located in the `out` folder. Afterwards it is possible to visualize
+# the solution from the saved files using Trixi2Vtk.jl and ParaView, this is described below in the
+# section [Visualize the solution](@ref Visualize-the-solution).
 
-callbacks = CallbackSet(summary_callback, analysis_callback, save_solution, stepsize_callback)
+save_solution = SaveSolutionCallback(interval = 5,
+                                     save_initial_solution = true,
+                                     save_final_solution = true)
 
-# OrdinaryDiffEq.jl defines a wide range of
-# [ODE solvers](https://docs.sciml.ai/DiffEqDocs/latest/solvers/ode_solve/). We will pass the ODE
-# problem, the ODE solver and the callbacks into `solve` function. Also we have to specify
-# explicitly the time step `dt`, which is necessary for the `StepsizeCallback`. And there is no
-# need to save every step of the solution, we are only interested in the final result.
+# Another useful callback is [`SaveRestartCallback`](@ref). It saves information for restarting
+# in regular intervals, which we set to 100 time steps. Also we are interested in saving the
+# restart file for the final solution. To perform a restart, you need to configure the restart
+# setup in a special way, which is described in the section [Restart simulation](@ref restart).
 
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt=1.0, save_everystep=false, callback=callbacks);
+save_restart = SaveRestartCallback(interval = 100, save_final_restart = true)
+
+# Create a `CallbackSet` to collect all callbacks so that they can be passed to the `solve`
+# function.
+
+callbacks = CallbackSet(summary_callback, analysis_callback, stepsize_callback, save_solution,
+                        save_restart)
+
+# The last step is to choose the time integration method, OrdinaryDiffEq.jl defines a wide range of
+# [ODE solvers](https://docs.sciml.ai/DiffEqDocs/latest/solvers/ode_solve/), e.g.
+# `CarpenterKennedy2N54(williamson_condition = false)`. We will pass the ODE
+# problem, the ODE solver and the callbacks to the `solve` function. Also, to use
+# `StepsizeCallback`, we must explicitly specify the time step `dt`, the selected value is not
+# important, because it will be overwritten by `StepsizeCallback`. And there is no need to save
+# every step of the solution, we are only interested in the final result.
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false), dt = 1.0,
+            save_everystep = false, callback = callbacks);
 
 # Finally, we print the timer summary.
+
 summary_callback()
 
-# Now you can plot the solution as shown above, analyse it and improve your setup to make
-# the solution stable, more accurate or more efficient.
+# Now you can plot the solution as shown below, analyse it and improve the stability, accuracy or
+# efficiency of your setup modifying it.
 
+# ### Visualize the solution
 
+# In the previous part of the tutorial, we calculated the final solution of the given problem, now we want
+# to visualize it. A more detailed explanation of visualization methods can be found in the section
+# [Visualization](@ref visualization).
+
+# #### Using Plots.jl
+
+# The first option is to use the [Plots.jl](https://github.com/JuliaPlots/Plots.jl) package
+# directly after the calculation, when the solution is saved in the `sol` variable. We connect the
+# package and use the `plot` function.
+
+using Plots
+plot(sol)
+
+# To show the mesh on the plot, we need to extract the visualization data from the solution as
+# a [`PlotData2D`](@ref) object. Mesh extraction is possible using the [`getmesh`](@ref) function.
+# Plots.jl has the `plot!` function that allows you to modify an already built graph.
+
+pd = PlotData2D(sol)
+plot!(getmesh(pd))
+
+# #### Using Trixi2Vtk.jl
+
+# Another way to visualize a solution is to extract it from a saved HDF5 file. After we used the
+# `solve` function there is a file with the final solution. It is located in the `out` folder and
+# is named as follows: `solution_index.h5`. The `index` is the final time step of the solution
+# that is complemented to 6 digits with zeros from the begining. With [Trixi2Vtk](@ref) you
+# can convert the HDF5 output file generated by Trixi.jl into a VTK file. This can be used in
+# visualization tools such as [ParaView](https://www.paraview.org) or
+# [VisIt](https://visit.llnl.gov) to plot the solution. The important thing is that currently
+# Trixi2Vtk.jl supports conversion only for solutions in 2D and 3D spatial domains.
+
+# If you haven't added a Trixi2Vtk.jl to your project yet, you can add it as follows.
+# ```julia
+# import Pkg
+# Pkg.add(["Trixi2Vtk"])
+# ```
+# Now we are connecting the Trixi2Vtk.jl package and convert the file `out/solution_000018.h5` with
+# the final solution using the [`trixi2vtk`](@ref) function saving the resulted file in the
+# `out` folder.
+
+using Trixi2Vtk
+trixi2vtk(joinpath("out", "solution_000018.h5"), output_directory="out")
+
+# Now two files `solution_000018.vtu` and `solution_000018_celldata.vtu` have been generated in the
+# `out` folder. The first one contains all the information for visualizing the solution, the
+# second one contains all the cell-based or discretization-based information.
+
+# Now let's visualize the solution from the generated files in ParaView. Follow this short
+# instruction to get the visualization.
+# - Download, install and open [ParaView](https://www.paraview.org/download/)
+# - Press `Ctrl+O` and browse the generated files `solution_000018.vtu` and
+#   `solution_000018_celldata.vtu` from the `out` folder.
+# - In the upper-left corner in the Pipeline Browser window, left-click on the eye-icon near
+#   `solution_000018.vtu`.
+# - In the lower-left corner in the Properties window, change the Coloring from Solid Color to
+#   scalar. Now final solution visualization is already generated.
+# - Now let's add the mesh to the visualization. In the upper-left corner in the
+#   Pipeline Browser window, left-click on the eye-icon near `solution_000018_celldata.vtu`.
+# - In the lower-left corner in the Properties window, change the Representation from the Surface
+#   to the Wireframe. Then a white grid should appear on the visualization.
+# Now, if you followed the instructions exactly, you should get an analog image, as shown in the
+# section [Using Plots.jl](@ref Using-Plots.jl).
 
 # ## Next steps: changing Trixi.jl itself
 
