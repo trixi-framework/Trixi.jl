bb4b7781871e4842c528f58a92c3c69c0354c001
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2023-10-05 08:31:43 +0200

    Note possible remedy for paraboic p4est mortars

--- src/solvers/dgsem_p4est/dg_2d_parabolic.jl
+++ src/solvers/dgsem_p4est/dg_2d_parabolic.jl
@@ -569,11 +569,11 @@ function prolong2mortars!(cache, flux_viscous::Vector{Array{uEltype, 4}},
             for i in eachnode(dg)
                 normal_direction = get_normal_direction(direction_index, contravariant_vectors,
                                                         i_small, j_small, element)
-
                 for v in eachvariable(equations)
                     flux_viscous = SVector(flux_viscous_x[v, i_small, j_small, element],
                                            flux_viscous_y[v, i_small, j_small, element])
                     # Coarse on top, fine below
+                    # TODO: Question maybe faces[side] to get these ifs right
                     if direction_index == 3
                         cache.mortars.u[1, v, position, i, mortar] = -dot(flux_viscous, normal_direction)
                     else
