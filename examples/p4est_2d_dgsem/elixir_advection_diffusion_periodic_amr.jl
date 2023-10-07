2e23b36c054ab36e1e91d104a096a6400925ff4c
Jesse Chan <jesse.lee.chan@gmail.com>
2023-10-07 19:16:43 -0400

    more changes to elixir

--- examples/p4est_2d_dgsem/elixir_advection_diffusion_periodic_amr.jl
+++ examples/p4est_2d_dgsem/elixir_advection_diffusion_periodic_amr.jl
@@ -4,7 +4,7 @@ using Trixi
 ###############################################################################
 # semidiscretization of the linear advection-diffusion equation
 
-diffusivity() = 5.0e-2
+diffusivity() = 1.0e-2
 advection_velocity = (1.0, 1.0)
 equations = LinearScalarAdvectionEquation2D(advection_velocity)
 equations_parabolic = LaplaceDiffusion2D(diffusivity(), equations)
@@ -33,22 +33,20 @@ end
 
 # Define initial condition (copied from "examples/tree_1d_dgsem/elixir_advection_diffusion.jl")
 function initial_condition_new_test(x, t, equation::LinearScalarAdvectionEquation2D)
-    # return SVector(x[1] + x[2] > 0)
-    #return SVector(sin(x[2]))
-    return SVector(sin(x[1])*sin(x[2]))
+    return SVector(2 * x[1] + x[2] > 0)
 end
 # initial_condition = initial_condition_diffusive_convergence_test
 initial_condition = initial_condition_new_test
 
 # Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
-solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs)
+solver = DGSEM(polydeg=2, surface_flux=flux_lax_friedrichs)
 
 coordinates_min = (-pi, -pi) # minimum coordinates (min(x), min(y))
 coordinates_max = ( pi,  pi) # maximum coordinates (max(x), max(y))
 
 trees_per_dimension = (4, 4)
 mesh = P4estMesh(trees_per_dimension,
-                 polydeg=3, initial_refinement_level=2,
+                 polydeg=2, initial_refinement_level=2,
                  coordinates_min=coordinates_min, coordinates_max=coordinates_max,
                  periodicity=true)
 
@@ -61,7 +59,7 @@ semi = SemidiscretizationHyperbolicParabolic(mesh,
 # ODE solvers, callbacks etc.
 
 # Create ODE problem with time span `tspan`
-tspan = (0.0, 5e-3)
+tspan = (0.0, 1.0)
 ode = semidiscretize(semi, tspan);
 
 # At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
@@ -75,28 +73,22 @@ analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
 # The AliveCallback prints short status information in regular intervals
 alive_callback = AliveCallback(analysis_interval=analysis_interval)
 
-# amr_controller = ControllerThreeLevel(semi, IndicatorMax(semi, variable=first),
-#                                       base_level=2,
-#                                       med_level=3, med_threshold=0.5,
-#                                       max_level=4, max_threshold=0.75)
-
 amr_controller = ControllerThreeLevel(semi, IndicatorMax(semi, variable=first),
-                                      base_level=1,
-                                      med_level=2, med_threshold=0.5,
-                                      max_level=3, max_threshold=0.75)
+                                      base_level=2,
+                                      med_level=3, med_threshold=0.5,
+                                      max_level=4, max_threshold=0.75)
+
 amr_callback = AMRCallback(semi, amr_controller,
                            interval=5)
 
 # Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
 callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, amr_callback)
-# callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback)
 
 ###############################################################################
 # run the simulation
 
 # OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
 time_int_tol = 1.0e-11
-# function eigen_est()
 sol = solve(ode, RDPK3SpFSAL49(); abstol=time_int_tol, reltol=time_int_tol,
             ode_default_options()..., callback=callbacks)
 # sol = solve(ode, ROCK4(eigen_est=eigen_est); abstol=time_int_tol, reltol=time_int_tol,
