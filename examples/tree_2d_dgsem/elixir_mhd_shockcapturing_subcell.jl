e40f0ea96e6924ffd1ce533c72a4af3e099fc10b
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2023-10-23 10:43:34 +0200

    Added bounds check for GLM-MHD subcell positivity example

--- examples/tree_2d_dgsem/elixir_mhd_shockcapturing_subcell.jl
+++ examples/tree_2d_dgsem/elixir_mhd_shockcapturing_subcell.jl
@@ -100,7 +100,7 @@ callbacks = CallbackSet(summary_callback,
 
 ###############################################################################
 # run the simulation
-stage_callbacks = (SubcellLimiterIDPCorrection(),)
+stage_callbacks = (SubcellLimiterIDPCorrection(), BoundsCheckCallback())
 
 sol = Trixi.solve(ode, Trixi.SimpleSSPRK33(stage_callbacks=stage_callbacks);
                   dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
