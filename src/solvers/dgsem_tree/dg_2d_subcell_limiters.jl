38231a873294eae02fa0c361da8188ba1241248e
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2023-10-23 10:28:44 +0200

    format

--- src/solvers/dgsem_tree/dg_2d_subcell_limiters.jl
+++ src/solvers/dgsem_tree/dg_2d_subcell_limiters.jl
@@ -136,9 +136,9 @@ end
     fhat1_R = fhat1_R_threaded[Threads.threadid()]
     fhat2_L = fhat2_L_threaded[Threads.threadid()]
     fhat2_R = fhat2_R_threaded[Threads.threadid()]
-calcflux_fhat!(fhat1_L, fhat1_R, fhat2_L, fhat2_R, u, mesh,
-               nonconservative_terms, equations, volume_flux_dg, dg, element,
-               cache)
+    calcflux_fhat!(fhat1_L, fhat1_R, fhat2_L, fhat2_R, u, mesh,
+                   nonconservative_terms, equations, volume_flux_dg, dg, element,
+                   cache)
     # low-order FV fluxes
     @unpack fstar1_L_threaded, fstar1_R_threaded, fstar2_L_threaded, fstar2_R_threaded = cache
 
@@ -496,7 +496,7 @@ end
                                     fhat_noncons_temp[v, noncons, i, j + 1]
         end
     end
-    
+
     return nothing
 end
 
