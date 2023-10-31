2bb19d07a6edf2cc655b236fd2f3399ae1c36fbe
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2023-10-31 14:47:17 +0100

    Update src/equations/compressible_euler_2d.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/equations/compressible_euler_2d.jl
+++ src/equations/compressible_euler_2d.jl
@@ -1267,7 +1267,7 @@ of the numerical flux.
   On Godunov-type methods near low densities.
   [DOI: 10.1016/0021-9991(91)90211-3](https://doi.org/10.1016/0021-9991(91)90211-3)
 """
-function min_max_speed_einfeldt(u_ll, u_rr, orientation::Integer,
+@inline function min_max_speed_einfeldt(u_ll, u_rr, orientation::Integer,
                                 equations::CompressibleEulerEquations2D)
     # Calculate primitive variables, enthalpy and speed of sound
     rho_ll, v1_ll, v2_ll, p_ll = cons2prim(u_ll, equations)
