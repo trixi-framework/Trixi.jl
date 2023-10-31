fb3dfc9f043301ffa5a135777c5c5b44ab7652ff
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2023-10-31 15:18:27 +0100

    additional sentence

--- src/equations/numerical_fluxes.jl
+++ src/equations/numerical_fluxes.jl
@@ -308,6 +308,7 @@ const flux_hll = FluxHLL()
     flux_hlle
 
 See [`min_max_speed_einfeldt`](@ref).
+This is a [`FluxHLL`](@ref)-type two-wave solver with special estimates of the wave speeds.
 """
 const flux_hlle = FluxHLL(min_max_speed_einfeldt)
 
