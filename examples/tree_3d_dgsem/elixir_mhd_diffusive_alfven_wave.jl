72bab52681d6d4a1ab7582a9d53d3ba71b6d82d5
SimonCan <simon.candelaresi@gmail.com>
2023-10-31 14:42:37 +0000

    Autoformatter on visco-resistive Walfven waves elixir.

--- examples/tree_3d_dgsem/elixir_mhd_diffusive_alfven_wave.jl
+++ examples/tree_3d_dgsem/elixir_mhd_diffusive_alfven_wave.jl
@@ -10,7 +10,7 @@ prandtl_number() = 0.72
 mu_const = 1e-2
 eta_const = 1e-2
 
-equations = IdealGlmMhdEquations3D(5/3)
+equations = IdealGlmMhdEquations3D(5 / 3)
 equations_parabolic = ViscoResistiveMhd3D(equations, mu = mu_const,
                                           Prandtl = prandtl_number(),
                                           eta = eta_const,
@@ -30,82 +30,94 @@ mesh = TreeMesh(coordinates_min, coordinates_max,
                 n_cells_max = 100_000) # set maximum capacity of tree data structure
 
 function initial_condition_constant_alfven(x, t, equations)
-   # Alfvén wave in three space dimensions
-   # Altmann thesis http://dx.doi.org/10.18419/opus-3895
-   # domain must be set to [-1, 1]^3, γ = 5/3
-   p = 1
-   omega = 2 * pi # may be multiplied by frequency
-   # r: length-variable = length of computational domain
-   r = 2
-   # e: epsilon = 0.02
-   e = 0.02
-   nx = 1 / sqrt(r^2 + 1)
-   ny = r / sqrt(r^2 + 1)
-   sqr = 1
-   Va = omega / (ny * sqr)
-   phi_alv = omega / ny * (nx * (x[1] - 0.5 * r) + ny * (x[2] - 0.5 * r)) - Va * t
+    # Alfvén wave in three space dimensions
+    # Altmann thesis http://dx.doi.org/10.18419/opus-3895
+    # domain must be set to [-1, 1]^3, γ = 5/3
+    p = 1
+    omega = 2 * pi # may be multiplied by frequency
+    # r: length-variable = length of computational domain
+    r = 2
+    # e: epsilon = 0.02
+    e = 0.02
+    nx = 1 / sqrt(r^2 + 1)
+    ny = r / sqrt(r^2 + 1)
+    sqr = 1
+    Va = omega / (ny * sqr)
+    phi_alv = omega / ny * (nx * (x[1] - 0.5 * r) + ny * (x[2] - 0.5 * r)) - Va * t
 
-   rho = 1.0
-   v1 = -e * ny * cos(phi_alv) / rho
-   v2 = e * nx * cos(phi_alv) / rho
-   v3 = e * sin(phi_alv) / rho
-   B1 = nx - rho * v1 * sqr
-   B2 = ny - rho * v2 * sqr
-   B3 = -rho * v3 * sqr
-   psi = 0
+    rho = 1.0
+    v1 = -e * ny * cos(phi_alv) / rho
+    v2 = e * nx * cos(phi_alv) / rho
+    v3 = e * sin(phi_alv) / rho
+    B1 = nx - rho * v1 * sqr
+    B2 = ny - rho * v2 * sqr
+    B3 = -rho * v3 * sqr
+    psi = 0
 
-   return prim2cons(SVector(rho, v1, v2, v3, p, B1, B2, B3, psi), equations)
+    return prim2cons(SVector(rho, v1, v2, v3, p, B1, B2, B3, psi), equations)
 end
 
 @inline function source_terms_mhd_convergence_test(u, x, t, equations)
-   r_1 = 0
-   r_2 = pi*(0.04*sqrt(5)*pi*mu_const*cos(pi*(-sqrt(5)*t + x[1] + 2*x[2]))
-             - 1.35525271560688e-20*sin(pi*(-2*sqrt(5)*t + 2*x[1] + 4*x[2]))
-             - 2.60208521396521e-18*sin(pi*(-sqrt(5)*t + x[1] + 2*x[2])))
-   r_3 = pi*(-0.02*sqrt(5)*pi*mu_const*cos(pi*(-sqrt(5)*t + x[1] + 2*x[2]))
-             - 2.71050543121376e-20*sin(pi*(-2*sqrt(5)*t + 2*x[1] + 4*x[2]))
-             + 3.46944695195361e-18*sin(pi*(-sqrt(5)*t + x[1] + 2*x[2])))
-   r_4 = -0.1*pi^2*mu_const*sin(pi*(-sqrt(5)*t + x[1] + 2*x[2]))
-   r_5 = 2.71050543121376e-19*pi^2*mu_const*sin(pi*(-sqrt(5)*t + x[1] + 2*x[2]))^2
-         - 2.71050543121376e-19*pi^2*mu_const*cos(pi*(sqrt(5)*t - x[1] - 2*x[2] + 3.0))^2
-         + 2.71050543121376e-20*sqrt(5)*pi*sin(pi*(-2*sqrt(5)*t + 2*x[1] + 4*x[2]))
-   r_6 = 0.04*sqrt(5)*pi^2*eta_const*cos(pi*(sqrt(5)*t - x[1] - 2*x[2] + 3.0))
-   r_7 =-0.02*sqrt(5)*pi^2*eta_const*cos(pi*(sqrt(5)*t - x[1] - 2*x[2] + 3.0))
-   r_8 = 0.1*pi^2*eta_const*sin(pi*(-sqrt(5)*t + x[1] + 2*x[2]))
-   r_9 = 0
+    r_1 = 0
+    r_2 = pi * (0.04 * sqrt(5) * pi * mu_const * cos(pi * (-sqrt(5) * t + x[1] + 2 * x[2]))
+           -
+           1.35525271560688e-20 * sin(pi * (-2 * sqrt(5) * t + 2 * x[1] + 4 * x[2]))
+           -
+           2.60208521396521e-18 * sin(pi * (-sqrt(5) * t + x[1] + 2 * x[2])))
+    r_3 = pi *
+          (-0.02 * sqrt(5) * pi * mu_const * cos(pi * (-sqrt(5) * t + x[1] + 2 * x[2]))
+           -
+           2.71050543121376e-20 * sin(pi * (-2 * sqrt(5) * t + 2 * x[1] + 4 * x[2]))
+           +
+           3.46944695195361e-18 * sin(pi * (-sqrt(5) * t + x[1] + 2 * x[2])))
+    r_4 = -0.1 * pi^2 * mu_const * sin(pi * (-sqrt(5) * t + x[1] + 2 * x[2]))
+    r_5 = 2.71050543121376e-19 * pi^2 * mu_const *
+          sin(pi * (-sqrt(5) * t + x[1] + 2 * x[2]))^2
+    -2.71050543121376e-19 * pi^2 * mu_const *
+    cos(pi * (sqrt(5) * t - x[1] - 2 * x[2] + 3.0))^2
+    +2.71050543121376e-20 * sqrt(5) * pi *
+    sin(pi * (-2 * sqrt(5) * t + 2 * x[1] + 4 * x[2]))
+    r_6 = 0.04 * sqrt(5) * pi^2 * eta_const *
+          cos(pi * (sqrt(5) * t - x[1] - 2 * x[2] + 3.0))
+    r_7 = -0.02 * sqrt(5) * pi^2 * eta_const *
+          cos(pi * (sqrt(5) * t - x[1] - 2 * x[2] + 3.0))
+    r_8 = 0.1 * pi^2 * eta_const * sin(pi * (-sqrt(5) * t + x[1] + 2 * x[2]))
+    r_9 = 0
 
-   return SVector(r_1, r_2, r_3, r_4, r_5, r_6, r_7, r_8, r_9)
+    return SVector(r_1, r_2, r_3, r_4, r_5, r_6, r_7, r_8, r_9)
 end
 
 initial_condition = initial_condition_constant_alfven
 
 semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic),
-					     initial_condition, solver,
-					     source_terms = source_terms_mhd_convergence_test)
+                                             initial_condition, solver,
+                                             source_terms = source_terms_mhd_convergence_test)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
 
 # Create ODE problem with time span `tspan`
-tspan = (0.0, 0.1)
+tspan = (0.0, 0.01)
 ode = semidiscretize(semi, tspan)
 
+# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
+# and resets the timers
 summary_callback = SummaryCallback()
-alive_callback = AliveCallback(alive_interval = 10)
-analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
-save_solution = SaveSolutionCallback(interval = 10,
-                                     save_initial_solution = true,
-                                     save_final_solution = true,
+
+# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
+analysis_callback = AnalysisCallback(semi, interval = 100)
+
+# The SaveSolutionCallback allows to save the solution to a file in regular intervals
+save_solution = SaveSolutionCallback(interval = 100,
                                      solution_variables = cons2prim)
 
+# The StepsizeCallback handles the re-calculation of the maximum Δt after each time step
 cfl = 0.25
 stepsize_callback = StepsizeCallback(cfl = cfl)
 
 glm_speed_callback = GlmSpeedCallback(glm_scale = 0.5, cfl = cfl)
 
 callbacks = CallbackSet(summary_callback,
-                        alive_callback,
                         analysis_callback,
                         save_solution,
                         stepsize_callback,
@@ -114,11 +126,9 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-#time_int_tol = 1e-5
-#sol = solve(ode, RDPK3SpFSAL49(), dt = 1e-5,
-#            save_everystep = false, callback = callbacks, adaptive = false)
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
-            dt=1e-5, # solve needs some value here but it will be overwritten by the stepsize_callback
-            save_everystep=false, callback=callbacks);
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+            dt = 1e-5, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false, callback = callbacks);
+
 # Print the timer summary.
 summary_callback()
