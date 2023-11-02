399ea3d4e8bbb14ff0d2818604160e5f45b45e82
Michael Schlottke-Lakemper <michael@sloede.com>
2023-11-02 17:36:38 +0100

    Remove all neural network indicator stuff from `src/`

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -260,9 +260,7 @@ export load_mesh, load_time, load_timestep, load_timestep!, load_dt,
        load_adaptive_time_integrator!
 
 export ControllerThreeLevel, ControllerThreeLevelCombined,
-       IndicatorLöhner, IndicatorLoehner, IndicatorMax,
-       IndicatorNeuralNetwork, NeuralNetworkPerssonPeraire, NeuralNetworkRayHesthaven,
-       NeuralNetworkCNN
+       IndicatorLöhner, IndicatorLoehner, IndicatorMax
 
 # TODO: TrixiShallowWater: move new limiter
 export PositivityPreservingLimiterZhangShu, PositivityPreservingLimiterShallowWater
