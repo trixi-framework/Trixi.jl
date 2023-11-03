c79fa4324726b6894317056c3ece2906ccb9c08d
Hendrik Ranocha <ranocha@users.noreply.github.com>
2023-11-03 13:47:05 +0100

    format examples (#1531)
    * format examples
    
    * check formatting of examples in CI
    
    * update style guide
    
    * fix weird formatting
    
    * fix formatting of binary operators
    
    * format again

--- examples/dgmulti_2d/elixir_shallowwater_source_terms.jl
+++ examples/dgmulti_2d/elixir_shallowwater_source_terms.jl
@@ -4,24 +4,23 @@ using Trixi
 ###############################################################################
 # semidiscretization of the shallow water equations
 
-equations = ShallowWaterEquations2D(gravity_constant=9.81)
+equations = ShallowWaterEquations2D(gravity_constant = 9.81)
 
 initial_condition = initial_condition_convergence_test
 
 volume_flux = (flux_wintermeyer_etal, flux_nonconservative_wintermeyer_etal)
 surface_flux = (flux_lax_friedrichs, flux_nonconservative_fjordholm_etal)
-dg = DGMulti(polydeg=3, element_type = Quad(), approximation_type = SBP(),
+dg = DGMulti(polydeg = 3, element_type = Quad(), approximation_type = SBP(),
              surface_integral = SurfaceIntegralWeakForm(surface_flux),
              volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
 cells_per_dimension = (8, 8)
 mesh = DGMultiMesh(dg, cells_per_dimension,
-                   coordinates_min=(0.0, 0.0), coordinates_max=(sqrt(2), sqrt(2)),
-                   periodicity=true)
-
+                   coordinates_min = (0.0, 0.0), coordinates_max = (sqrt(2), sqrt(2)),
+                   periodicity = true)
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, dg;
-                                    source_terms=source_terms_convergence_test)
+                                    source_terms = source_terms_convergence_test)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
@@ -32,8 +31,8 @@ ode = semidiscretize(semi, tspan)
 summary_callback = SummaryCallback()
 
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval, uEltype=real(dg))
-alive_callback = AliveCallback(analysis_interval=analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval, uEltype = real(dg))
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback,
@@ -42,7 +41,7 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, RDPK3SpFSAL49(); abstol=1.0e-7, reltol=1.0e-7,
-            ode_default_options()..., callback=callbacks);
+sol = solve(ode, RDPK3SpFSAL49(); abstol = 1.0e-7, reltol = 1.0e-7,
+            ode_default_options()..., callback = callbacks);
 
 summary_callback() # print the timer summary
--- examples/structured_2d_dgsem/elixir_shallowwater_conical_island.jl
+++ examples/structured_2d_dgsem/elixir_shallowwater_conical_island.jl
@@ -2,12 +2,12 @@
 using OrdinaryDiffEq
 using Trixi
 
- ###############################################################################
- # Semidiscretization of the shallow water equations
+###############################################################################
+# Semidiscretization of the shallow water equations
 #
 # TODO: TrixiShallowWater: wet/dry example elixir
 
-equations = ShallowWaterEquations2D(gravity_constant=9.81, H0=1.4)
+equations = ShallowWaterEquations2D(gravity_constant = 9.81, H0 = 1.4)
 
 """
     initial_condition_conical_island(x, t, equations::ShallowWaterEquations2D)
@@ -20,29 +20,29 @@ discontinuous water height is smoothed by a logistic function. This simulation u
 boundary conditions.
 """
 function initial_condition_conical_island(x, t, equations::ShallowWaterEquations2D)
-  # Set the background values
+    # Set the background values
 
-  v1 = 0.0
-  v2 = 0.0
+    v1 = 0.0
+    v2 = 0.0
 
-  x1, x2 = x
-  b = max(0.1, 1.0 - 4.0 * sqrt(x1^2 + x2^2))
+    x1, x2 = x
+    b = max(0.1, 1.0 - 4.0 * sqrt(x1^2 + x2^2))
 
-  # use a logistic function to transfer water height value smoothly
-  L  = equations.H0    # maximum of function
-  x0 = 0.3   # center point of function
-  k  = -25.0 # sharpness of transfer
+    # use a logistic function to transfer water height value smoothly
+    L = equations.H0    # maximum of function
+    x0 = 0.3   # center point of function
+    k = -25.0 # sharpness of transfer
 
-  H = max(b, L/(1.0 + exp(-k*(sqrt(x1^2+x2^2) - x0))))
+    H = max(b, L / (1.0 + exp(-k * (sqrt(x1^2 + x2^2) - x0))))
 
-  # It is mandatory to shift the water level at dry areas to make sure the water height h
-  # stays positive. The system would not be stable for h set to a hard 0 due to division by h in
-  # the computation of velocity, e.g., (h v1) / h. Therefore, a small dry state threshold
-  # with a default value of 500*eps() ≈ 1e-13 in double precision, is set in the constructor above
-  # for the ShallowWaterEquations and added to the initial condition if h = 0.
-  # This default value can be changed within the constructor call depending on the simulation setup.
-  H = max(H, b + equations.threshold_limiter)
-  return prim2cons(SVector(H, v1, v2, b), equations)
+    # It is mandatory to shift the water level at dry areas to make sure the water height h
+    # stays positive. The system would not be stable for h set to a hard 0 due to division by h in
+    # the computation of velocity, e.g., (h v1) / h. Therefore, a small dry state threshold
+    # with a default value of 500*eps() ≈ 1e-13 in double precision, is set in the constructor above
+    # for the ShallowWaterEquations and added to the initial condition if h = 0.
+    # This default value can be changed within the constructor call depending on the simulation setup.
+    H = max(H, b + equations.threshold_limiter)
+    return prim2cons(SVector(H, v1, v2, b), equations)
 end
 
 initial_condition = initial_condition_conical_island
@@ -51,19 +51,20 @@ initial_condition = initial_condition_conical_island
 # Get the DG approximation space
 
 volume_flux = (flux_wintermeyer_etal, flux_nonconservative_wintermeyer_etal)
-surface_flux = (FluxHydrostaticReconstruction(flux_hll_chen_noelle, hydrostatic_reconstruction_chen_noelle),
+surface_flux = (FluxHydrostaticReconstruction(flux_hll_chen_noelle,
+                                              hydrostatic_reconstruction_chen_noelle),
                 flux_nonconservative_chen_noelle)
 
 basis = LobattoLegendreBasis(4)
 
 indicator_sc = IndicatorHennemannGassnerShallowWater(equations, basis,
-                                                     alpha_max=0.5,
-                                                     alpha_min=0.001,
-                                                     alpha_smooth=true,
-                                                     variable=waterheight_pressure)
+                                                     alpha_max = 0.5,
+                                                     alpha_min = 0.001,
+                                                     alpha_smooth = true,
+                                                     variable = waterheight_pressure)
 volume_integral = VolumeIntegralShockCapturingHG(indicator_sc;
-                                                 volume_flux_dg=volume_flux,
-                                                 volume_flux_fv=surface_flux)
+                                                 volume_flux_dg = volume_flux,
+                                                 volume_flux_fv = surface_flux)
 
 solver = DGSEM(basis, surface_flux, volume_integral)
 
@@ -71,7 +72,7 @@ solver = DGSEM(basis, surface_flux, volume_integral)
 # Get the StructuredMesh and setup a periodic mesh
 
 coordinates_min = (-1.0, -1.0)
-coordinates_max = (1.0,  1.0)
+coordinates_max = (1.0, 1.0)
 
 cells_per_dimension = (16, 16)
 
@@ -92,22 +93,22 @@ ode = semidiscretize(semi, tspan)
 summary_callback = SummaryCallback()
 
 analysis_interval = 1000
-analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
-alive_callback = AliveCallback(analysis_interval=analysis_interval)
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
 
-save_solution = SaveSolutionCallback(interval=100,
-                                     save_initial_solution=true,
-                                     save_final_solution=true)
+save_solution = SaveSolutionCallback(interval = 100,
+                                     save_initial_solution = true,
+                                     save_final_solution = true)
 
 callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, save_solution)
 
 ###############################################################################
 # run the simulation
 
-stage_limiter! = PositivityPreservingLimiterShallowWater(variables=(Trixi.waterheight,))
+stage_limiter! = PositivityPreservingLimiterShallowWater(variables = (Trixi.waterheight,))
 
 sol = solve(ode, SSPRK43(stage_limiter!);
-            ode_default_options()..., callback=callbacks);
+            ode_default_options()..., callback = callbacks);
 
 summary_callback() # print the timer summary
