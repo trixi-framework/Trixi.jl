716771f1cbd779966b0411f59dce5d7c4dd3b2d5
Jesse Chan <jesse.lee.chan@gmail.com>
2023-11-06 16:44:39 -0600

    remove previously deprecated functions

--- src/solvers/dgmulti/types.jl
+++ src/solvers/dgmulti/types.jl
@@ -438,10 +438,3 @@ end # @muladd
 @deprecate DGMultiMesh(dg::DGMulti{NDIMS}; cells_per_dimension, kwargs...) where {NDIMS} DGMultiMesh(dg,
                                                                                                      cells_per_dimension;
                                                                                                      kwargs...)
-
-# TODO: deprecations introduced in Trixi.jl v0.5
-@deprecate DGMultiMesh(vertex_coordinates, EToV, dg::DGMulti{NDIMS};
-                       kwargs...) where {NDIMS} DGMultiMesh(dg, vertex_coordinates, EToV;
-                                                            kwargs...)
-@deprecate DGMultiMesh(triangulateIO, dg::DGMulti{2, Tri}, boundary_dict::Dict{Symbol, Int};
-                       kwargs...) DGMultiMesh(dg, triangulateIO, boundary_dict; kwargs...)
