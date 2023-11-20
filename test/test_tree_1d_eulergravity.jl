fdba6ddba987d223412a7f5f892eea54e3dc38dc
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2023-11-20 15:24:27 +0100

    fmt

--- test/test_tree_1d_eulergravity.jl
+++ test/test_tree_1d_eulergravity.jl
@@ -13,10 +13,12 @@ EXAMPLES_DIR = pkgdir(Trixi, "examples", "tree_1d_dgsem")
 @trixi_testset "elixir_eulergravity_convergence.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_eulergravity_convergence.jl"),
                         l2=[
-                            0.00021708496949694728, 0.0002913795242132917, 0.0006112500956552259
+                            0.00021708496949694728, 0.0002913795242132917,
+                            0.0006112500956552259,
                         ],
                         linf=[
-                            0.0004977733237385706, 0.0013594226727522418, 0.0020418739554664
+                            0.0004977733237385706, 0.0013594226727522418,
+                            0.0020418739554664,
                         ])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
