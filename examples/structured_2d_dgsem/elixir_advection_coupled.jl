418796a95dfdc5d8a0eea525ecb617674c93a7d7
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2023-12-11 18:00:57 +0000

    Update examples/structured_2d_dgsem/elixir_advection_coupled.jl
    Co-authored-by: Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>

--- examples/structured_2d_dgsem/elixir_advection_coupled.jl
+++ examples/structured_2d_dgsem/elixir_advection_coupled.jl
@@ -43,7 +43,7 @@ equations = LinearScalarAdvectionEquation2D(advection_velocity)
 # Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
 solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
-# These will be the number of elements for each quarter/semidiscretization.
+# This will be the number of elements for each quarter/semidiscretization.
 cells_per_dimension = (8, 8)
 
 ###########
