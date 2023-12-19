2d1e2035465ea257d5b0810eed1ef63c8f0cec5c
Daniel Doehring <doehringd2@gmail.com>
2023-12-19 09:47:22 +0100

    Update src/solvers/dgsem_tree/dg_2d_parabolic.jl

--- src/solvers/dgsem_tree/dg_2d_parabolic.jl
+++ src/solvers/dgsem_tree/dg_2d_parabolic.jl
@@ -291,8 +291,8 @@ function prolong2boundaries!(cache_parabolic, flux_viscous,
     return nothing
 end
 
-function calc_viscous_fluxes!(flux_viscous::Vector{Array{uEltype, 4}},
-                              gradients::Vector{Array{uEltype, 4}}, u_transformed,
+function calc_viscous_fluxes!(flux_viscous,
+                              gradients, u_transformed,
                               mesh::Union{TreeMesh{2}, P4estMesh{2}},
                               equations_parabolic::AbstractEquationsParabolic,
                               dg::DG, cache, cache_parabolic) where {uEltype <: Real}
