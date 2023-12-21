944ec3c570fb6c637ce5e073ed74f89765568bae
bennibolm <benjamin.bolm@gmx.de>
2023-12-21 20:15:42 +0100

    fmt

--- test/test_tree_2d_eulermulti.jl
+++ test/test_tree_2d_eulermulti.jl
@@ -97,22 +97,22 @@ EXAMPLES_DIR = pkgdir(Trixi, "examples", "tree_2d_dgsem")
     @trixi_testset "elixir_eulermulti_shock_bubble_shockcapturing_subcell_minmax.jl" begin
         @test_trixi_include(joinpath(EXAMPLES_DIR,
                                      "elixir_eulermulti_shock_bubble_shockcapturing_subcell_minmax.jl"),
-                        l2=[
-                            76.59096367977872,
-                            1.9879932386864356,
-                            59851.34515039375,
-                            0.18710988181124935,
-                            0.010631432251136084,
-                        ],
-                        linf=[
-                            212.71245739310544,
-                            27.399221359958894,
-                            158389.9681231281,
-                            0.6524718882809865,
-                            0.10630137919864985,
-                        ],
-                        initial_refinement_level=3,
-                        tspan=(0.0, 0.001))
+                            l2=[
+                                76.59096367977872,
+                                1.9879932386864356,
+                                59851.34515039375,
+                                0.18710988181124935,
+                                0.010631432251136084,
+                            ],
+                            linf=[
+                                212.71245739310544,
+                                27.399221359958894,
+                                158389.9681231281,
+                                0.6524718882809865,
+                                0.10630137919864985,
+                            ],
+                            initial_refinement_level=3,
+                            tspan=(0.0, 0.001))
         # Ensure that we do not have excessive memory allocations
         # (e.g., from type instabilities)
         let
