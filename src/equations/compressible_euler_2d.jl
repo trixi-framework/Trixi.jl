1fb202aa8ab792ca3a5d35854b32658652134aa7
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2023-12-30 14:10:01 +0100

    Update compressible_euler_2d.jl

--- src/equations/compressible_euler_2d.jl
+++ src/equations/compressible_euler_2d.jl
@@ -1308,7 +1308,7 @@ function flux_hllc(u_ll, u_rr, normal_direction::AbstractVector,
         f4 = f_rr[4]
     else
         SStar = (rho_ll * v_dot_n_ll * sMu_L - rho_rr * v_dot_n_rr * sMu_R +
-                (p_rr - p_ll) * norm_sq) / (rho_ll * sMu_L - rho_rr * sMu_R)
+                 (p_rr - p_ll) * norm_sq) / (rho_ll * sMu_L - rho_rr * sMu_R)
         if Ssl <= 0.0 <= SStar
             densStar = rho_ll * sMu_L / (Ssl - SStar)
             enerStar = e_ll +
