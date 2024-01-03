db89b06b766e02f0d10923da86fd3b400178f315
Jesse Chan <jesse.lee.chan@gmail.com>
2024-01-03 15:27:33 -0600

    remove incorrect factor of 2

--- src/solvers/dgmulti/flux_differencing.jl
+++ src/solvers/dgmulti/flux_differencing.jl
@@ -60,7 +60,7 @@ end
         for j in col_ids
             u_j = u[j]
             f_ij = volume_flux(u_i, u_j, orientation, equations)
-            du_i = du_i + 2 * A[i, j] * f_ij
+            du_i = du_i + A[i, j] * f_ij
         end
         du[i] = du_i
     end
@@ -181,7 +181,7 @@ end
             # evaluated using both the normal and its average at interfaces.
             u_j = u[j]
             f_ij = volume_flux(u_i, u_j, normal_direction, normal_direction, equations)
-            du_i = du_i + 2 * A_ij * f_ij
+            du_i = du_i + A_ij * f_ij
         end
         du[i] = du_i
     end
