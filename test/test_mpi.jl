06bd3c4cfe3d182567ae3cb0e6e37e7b9e9267fe
Johannes Markert <johannes.markert@dlr.de>
2024-01-11 12:10:08 +0100

    Registered mpi t8code tests.

--- test/test_mpi.jl
+++ test/test_mpi.jl
@@ -21,8 +21,10 @@ CI_ON_WINDOWS = (get(ENV, "GITHUB_ACTIONS", false) == "true") && Sys.iswindows()
 
     # P4estMesh tests
     include("test_mpi_p4est_2d.jl")
+    include("test_mpi_t8code_2d.jl")
     if !CI_ON_WINDOWS # see comment on `CI_ON_WINDOWS` above
         include("test_mpi_p4est_3d.jl")
+        include("test_mpi_t8code_3d.jl")
     end
 end # MPI
 
