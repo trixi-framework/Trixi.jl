3f4068978f3e1badc28c5aed71ba36b05515c5fc
bennibolm <benjamin.bolm@gmx.de>
2024-01-11 12:33:38 +0100

    Fix download links

--- docs/literate/make.jl
+++ docs/literate/make.jl
@@ -10,8 +10,8 @@ function create_files(title, file, repo_src, pages_dir, notebooks_dir; folder=""
     end
 
     binder_logo   = "https://mybinder.org/badge_logo.svg"
-    nbviewer_logo = "https://raw.githubusercontent.com/jupyter/design/master/logos/Badges/nbviewer_badge.svg"
-    download_logo = "https://camo.githubusercontent.com/aea75103f6d9f690a19cb0e17c06f984ab0f472d9e6fe4eadaa0cc438ba88ada/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f646f776e6c6f61642d6e6f7465626f6f6b2d627269676874677265656e"
+    nbviewer_logo = "https://github.com/trixi-framework/Trixi.jl/assets/74359358/fb572b02-9329-48c1-8f19-9140f5dcd4be"
+    download_logo = "https://github.com/trixi-framework/Trixi.jl/assets/74359358/5364d4b9-6db5-4b54-92f9-95319a8b469c"
 
     notebook_path = "tutorials/notebooks/$notebook_filename"
     binder_url   = "https://mybinder.org/v2/gh/trixi-framework/Trixi.jl/tutorial_notebooks?filepath=$notebook_path"
@@ -45,6 +45,7 @@ function create_tutorials(files)
     notebooks_dir   = joinpath(pages_dir, "notebooks")
 
     Sys.rm(pages_dir;       recursive=true, force=true)
+
     Sys.rm("out"; recursive=true, force=true)
 
     # Run tests on all tutorial files
