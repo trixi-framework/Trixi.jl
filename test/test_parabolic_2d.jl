09892e578a532495a7298eff76c8d7c4c3cbdfb0
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-01-19 09:14:32 +0100

    update test val for muladd

--- test/test_parabolic_2d.jl
+++ test/test_parabolic_2d.jl
@@ -561,8 +561,8 @@ end
     @test_trixi_include(joinpath(examples_dir(), "p4est_2d_dgsem",
                                  "elixir_advection_diffusion_nonperiodic_amr.jl"),
                         tspan=(0.0, 0.01),
-                        l2=[0.00793438523666649],
-                        linf=[0.11030633127144573])
+                        l2=[0.007933903205107578],
+                        linf=[0.11029697688907758])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
     let
