b068d1183a5a697effe9db22949f6aefe64d4b95
Benedict Geihe <bgeihe@uni-koeln.de>
2024-01-23 16:14:26 +0100

    less consolidation

--- src/callbacks_step/stepsize_dg2d.jl
+++ src/callbacks_step/stepsize_dg2d.jl
@@ -144,7 +144,7 @@ function max_dt(u, t,
 end
 
 function max_dt(u, t, mesh::ParallelP4estMesh{2},
-                constant_speed::False, equations, dg::DG, cache)
+                constant_speed, equations, dg::DG, cache)
     # call the method accepting a general `mesh::P4estMesh{2}`
     # TODO: MPI, we should improve this; maybe we should dispatch on `u`
     #       and create some MPI array type, overloading broadcasting and mapreduce etc.
@@ -160,39 +160,7 @@ function max_dt(u, t, mesh::ParallelP4estMesh{2},
 end
 
 function max_dt(u, t, mesh::ParallelT8codeMesh{2},
-                constant_speed::False, equations, dg::DG, cache)
-    # call the method accepting a general `mesh::T8codeMesh{2}`
-    # TODO: MPI, we should improve this; maybe we should dispatch on `u`
-    #       and create some MPI array type, overloading broadcasting and mapreduce etc.
-    #       Then, this specific array type should also work well with DiffEq etc.
-    dt = invoke(max_dt,
-                Tuple{typeof(u), typeof(t), T8codeMesh{2},
-                      typeof(constant_speed), typeof(equations), typeof(dg),
-                      typeof(cache)},
-                u, t, mesh, constant_speed, equations, dg, cache)
-    dt = MPI.Allreduce!(Ref(dt), min, mpi_comm())[]
-
-    return dt
-end
-
-function max_dt(u, t, mesh::ParallelP4estMesh{2},
-                constant_speed::True, equations, dg::DG, cache)
-    # call the method accepting a general `mesh::P4estMesh{2}`
-    # TODO: MPI, we should improve this; maybe we should dispatch on `u`
-    #       and create some MPI array type, overloading broadcasting and mapreduce etc.
-    #       Then, this specific array type should also work well with DiffEq etc.
-    dt = invoke(max_dt,
-                Tuple{typeof(u), typeof(t), P4estMesh{2},
-                      typeof(constant_speed), typeof(equations), typeof(dg),
-                      typeof(cache)},
-                u, t, mesh, constant_speed, equations, dg, cache)
-    dt = MPI.Allreduce!(Ref(dt), min, mpi_comm())[]
-
-    return dt
-end
-
-function max_dt(u, t, mesh::ParallelT8codeMesh{2},
-                constant_speed::True, equations, dg::DG, cache)
+                constant_speed, equations, dg::DG, cache)
     # call the method accepting a general `mesh::T8codeMesh{2}`
     # TODO: MPI, we should improve this; maybe we should dispatch on `u`
     #       and create some MPI array type, overloading broadcasting and mapreduce etc.
