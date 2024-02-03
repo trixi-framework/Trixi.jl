42670ee9e3eef440e2d38f357d5a1bba315cbf43
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-02-03 12:59:28 +0200

    Update docs/literate/src/files/changing_trixi.jl
    Co-authored-by: Benedict <135045760+bgeihe@users.noreply.github.com>

--- docs/literate/src/files/changing_trixi.jl
+++ docs/literate/src/files/changing_trixi.jl
@@ -52,7 +52,7 @@
 # mkdir run 
 # cd run
 # julia --project=. -e 'using Pkg; Pkg.develop(PackageSpec(path=".."))' # Install local Trixi.jl clone
-# julia -e 'using Pkg; Pkg.add(["OrdinaryDiffEq", "Plots"])' # Install additional packages'
+# julia --project=. -e 'using Pkg; Pkg.add(["OrdinaryDiffEq", "Plots"])' # Install additional packages'
 # ```
 # Note that if you installed Trixi.jl this way,
 # you always have to start Julia with the `--project` flag set to your `run` directory, e.g.,
