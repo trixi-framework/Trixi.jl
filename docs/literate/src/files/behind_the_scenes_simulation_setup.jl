d2ef3a3e96d064cdf6be86b5c1a4058fd5a8a0e1
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-02-11 18:06:05 +0200

    Update docs/literate/src/files/behind_the_scenes_simulation_setup.jl
    Co-authored-by: Daniel Doehring <doehringd2@gmail.com>

--- docs/literate/src/files/behind_the_scenes_simulation_setup.jl
+++ docs/literate/src/files/behind_the_scenes_simulation_setup.jl
@@ -67,7 +67,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergen
 #   The fundamental elements for approximating the solution are the leaf
 #   cells. The solution is constructed as a polynomial of the degree specified in the `DGSEM`
 #   solver in each spatial direction on each leaf cell. This polynomial approximation is evaluated 
-#   at the Gauss-Lobatto nodes, which were previously illustrated. The `init_elements` function extracts
+#   at the Gauss-Lobatto nodes mentioned earlier. The `init_elements` function extracts
 #   these leaf cells from the `TreeMesh`, assigns them the label "elements", records their
 #   coordinates, and maps the Gauss-Lobatto nodes from the 1D interval ``[-1, 1]`` onto each axis
 #   of every element.
