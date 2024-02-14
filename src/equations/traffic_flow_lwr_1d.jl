25a1b501b6f647d3fc181d923c2683791064b5ac
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-02-14 09:19:11 +0100

    add domain of u

--- src/equations/traffic_flow_lwr_1d.jl
+++ src/equations/traffic_flow_lwr_1d.jl
@@ -9,11 +9,12 @@
     TrafficFlowLWREquations1D
 
 The classic Lighthill-Witham Richards (LWR) model for 1D traffic flow.
-The car density is denoted by $u$ and the maximum possible speed (e.g. due to speed limits) is $v_{\text{max}}$.
+The car density is denoted by $u \in [0, 1]$ and 
+the maximum possible speed (e.g. due to speed limits) is $v_{\text{max}}$.
 ```math
 \partial_t u + v_{\text{max}} \partial_1 [u (1 - u)] = 0
 ```
-See e.g. Section 11.1 of 
+For more details see e.g. Section 11.1 of 
 - Randall LeVeque (2002)
 Finite Volume Methods for Hyperbolic Problems
 [DOI: 10.1017/CBO9780511791253]https://doi.org/10.1017/CBO9780511791253
