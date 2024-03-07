b11bc10d8ac68798ae2e53f9db3d5c4f824af5fa
Benedict <135045760+bgeihe@users.noreply.github.com>
2024-03-07 13:04:19 +0100

    add warning (#1863)

--- src/solvers/dgsem/basis_lobatto_legendre.jl
+++ src/solvers/dgsem/basis_lobatto_legendre.jl
@@ -12,6 +12,7 @@ Create a nodal Lobatto-Legendre basis for polynomials of degree `polydeg`.
 
 For the special case `polydeg=0` the DG method reduces to a finite volume method.
 Therefore, this function sets the center point of the cell as single node.
+This exceptional case is currently only supported for TreeMesh!
 """
 struct LobattoLegendreBasis{RealT <: Real, NNODES,
                             VectorT <: AbstractVector{RealT},
