5f763bbacbef00bc424fc33eca7a51aa8b875d0d
Warisa <compund555@gmail.com>
2024-03-08 10:37:40 +0100

    Make adjustments to a test file and delete example of PERK3

--- test/test_tree_1d_advection.jl
+++ test/test_tree_1d_advection.jl
@@ -81,6 +81,20 @@ end
         @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
     end
 end
+
+@trixi_testset "elixir_advection_PERK2.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_PERK2.jl"),
+                        l2=[0.011662300515980219],
+                        linf=[0.01647256923710194])
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    let
+        t = sol.t[end]
+        u_ode = sol.u[end]
+        du_ode = similar(u_ode)
+        @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
+    end
+end
 end
 
 end # module
