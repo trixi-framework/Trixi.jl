55cf04358762040975d61c6c5ba8fa03dfa87bab
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2024-03-14 11:06:24 +0000

    Update src/semidiscretization/semidiscretization_coupled.jl
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/semidiscretization/semidiscretization_coupled.jl
+++ src/semidiscretization/semidiscretization_coupled.jl
@@ -438,15 +438,15 @@ function (boundary_condition::BoundaryConditionCoupled)(u_inner, orientation, di
     if surface_flux_function isa Tuple
         # In case of conservative and non-conservative fluxes add the non-conservative one with a factor of 1/2.
         if iseven(direction) # u_inner is "left" of boundary, u_boundary is "right" of boundary
-            flux = surface_flux_function[1](u_inner, u_boundary, orientation,
+            flux = (surface_flux_function[1](u_inner, u_boundary, orientation,
                                             equations) +
                    0.5 *
-                   surface_flux_function[2](u_inner, u_boundary, orientation, equations)
+                   surface_flux_function[2](u_inner, u_boundary, orientation, equations))
         else # u_boundary is "left" of boundary, u_inner is "right" of boundary
-            flux = surface_flux_function[1](u_boundary, u_inner, orientation,
+            flux = (surface_flux_function[1](u_boundary, u_inner, orientation,
                                             equations) +
                    0.5 *
-                   surface_flux_function[2](u_boundary, u_inner, orientation, equations)
+                   surface_flux_function[2](u_boundary, u_inner, orientation, equations))
         end
     else
         if iseven(direction) # u_inner is "left" of boundary, u_boundary is "right" of boundary
