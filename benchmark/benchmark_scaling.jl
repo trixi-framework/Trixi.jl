66533b367fadc97e38db749fc83169d72fdfa0c9
Benedict Geihe <bgeihe@uni-koeln.de>
2024-03-27 16:17:47 +0100

    output summary

--- benchmark/benchmark_scaling.jl
+++ benchmark/benchmark_scaling.jl
@@ -3,6 +3,7 @@
 #! format: off
 
 using BenchmarkTools
+using Printf
 using Pkg
 Pkg.activate(@__DIR__)
 
@@ -17,7 +18,7 @@ command(julia, elixir, threads) = `$julia --project=$(@__DIR__)
                                           --threads $threads
                                           benchmark_scaling_threaded.jl $elixir`
 
-for thread_exp in 0:2
+for thread_exp in 0:1
     threads = 2^thread_exp
     run(command(Base.julia_cmd(), elixir, threads))
 
@@ -28,4 +29,16 @@ for thread_exp in 0:2
     # pretty print
     show(stdout, MIME"text/plain"(), SUITE[benchname]["t$threads"][1])
     println()
+end
+
+println()
+@info "Summary"
+
+trial_keys = collect(keys(SUITE[benchname]))
+sort!(trial_keys)
+
+println("#Threads | Median time")
+for key in trial_keys
+    median_time = median(SUITE[benchname][key][1]).time
+    @printf("%8s | %s\n", key, BenchmarkTools.prettytime(median_time))
 end
