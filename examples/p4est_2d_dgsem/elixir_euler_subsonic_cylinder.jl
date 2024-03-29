815a7244ccf96e4d0ed113a22400f0a3d23a0566
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-03-29 11:08:47 +0100

    fmt

--- examples/p4est_2d_dgsem/elixir_euler_subsonic_cylinder.jl
+++ examples/p4est_2d_dgsem/elixir_euler_subsonic_cylinder.jl
@@ -117,7 +117,9 @@ callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, sav
 
 ###############################################################################
 # run the simulation
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false; thread = OrdinaryDiffEq.True()),
+sol = solve(ode,
+            CarpenterKennedy2N54(williamson_condition = false;
+                                 thread = OrdinaryDiffEq.True()),
             dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep = false, callback = callbacks);
 summary_callback() # print the timer summary
