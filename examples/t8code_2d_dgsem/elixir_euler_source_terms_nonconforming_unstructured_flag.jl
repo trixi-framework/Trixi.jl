0fed965b941a3ec5877d438cd7545ae8eeeb14b1
Johannes Markert <johannes.markert@dlr.de>
2024-04-08 13:05:31 +0200

    Updated examples.

--- examples/t8code_2d_dgsem/elixir_euler_source_terms_nonconforming_unstructured_flag.jl
+++ examples/t8code_2d_dgsem/elixir_euler_source_terms_nonconforming_unstructured_flag.jl
@@ -32,12 +32,7 @@ mapping_flag = Trixi.transfinite_mapping(faces)
 mesh_file = Trixi.download("https://gist.githubusercontent.com/efaulhaber/63ff2ea224409e55ee8423b3a33e316a/raw/7db58af7446d1479753ae718930741c47a3b79b7/square_unstructured_2.inp",
                            joinpath(@__DIR__, "square_unstructured_2.inp"))
 
-# INP mesh files are only support by p4est. Hence, we
-# create a p4est connecvity object first from which
-# we can create a t8code mesh.
-conn = Trixi.read_inp_p4est(mesh_file, Val(2))
-
-mesh = T8codeMesh(conn, polydeg = 3,
+mesh = T8codeMesh(mesh_file, 2; polydeg = 3,
                   mapping = mapping_flag,
                   initial_refinement_level = 1)
 
