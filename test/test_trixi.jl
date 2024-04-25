4322976655811767da6d49f3cb0b185d37b0fb24
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-04-25 08:55:44 +0200

    comment

--- test/test_trixi.jl
+++ test/test_trixi.jl
@@ -154,6 +154,7 @@ macro test_nowarn_mod(expr, additional_ignore_content = String[])
                                      r"┌ Warning: `parse\(::Type, ::Coloarant\)` is deprecated.+\n│.+\n│.+\n└ @ Plots.+\n",
                                      # TODO: Silence warning introduced by Flux v0.13.13. Should be properly fixed.
                                      r"┌ Warning: Layer with Float32 parameters got Float64 input.+\n│.+\n│.+\n│.+\n└ @ Flux.+\n",
+                                     # NOTE: These warnings arose from Julia 1.10 onwards
                                      "WARNING: Method definition .* in module .* at .* overwritten in module .* on the same line (check for duplicate calls to `include`).\n",
                                      "WARNING: Method definition .* in module .* at .* overwritten at .* .\n"]
                 append!(ignore_content, $additional_ignore_content)
