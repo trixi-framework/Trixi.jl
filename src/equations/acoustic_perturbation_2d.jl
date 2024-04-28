195da8de2c2c7ad7f9872d7de8eb96f38116c1c1
huiyuxie <huiyuxie.sde@gmail.com>
2024-04-28 16:03:13 -0700

    revise after the first review

--- src/equations/acoustic_perturbation_2d.jl
+++ src/equations/acoustic_perturbation_2d.jl
@@ -112,13 +112,12 @@ A constant initial condition where the state variables are zero and the mean flo
 Uses the global mean values from `equations`.
 """
 function initial_condition_constant(x, t, equations::AcousticPerturbationEquations2D)
-    v1_prime = 0.0
-    v2_prime = 0.0
-    p_prime_scaled = 0.0
+    RealT = eltype(x)
+    v1_prime = zero(RealT)
+    v2_prime = zero(RealT)
+    p_prime_scaled = zero(RealT)
 
-    return SVector(map(v -> convert(eltype(x), v),
-                       (v1_prime, v2_prime, p_prime_scaled,
-                        global_mean_vars(equations)...)))
+    return SVector(v1_prime, v2_prime, p_prime_scaled, global_mean_vars(equations)...)
 end
 
 """
@@ -129,12 +128,13 @@ A smooth initial condition used for convergence tests in combination with
 """
 function initial_condition_convergence_test(x, t,
                                             equations::AcousticPerturbationEquations2D)
-    c = 2.0
-    A = 0.2
-    L = 2.0
-    f = 2.0 / L
-    a = 1.0
-    omega = 2 * pi * f
+    RealT = eltype(x)
+    a = 1
+    c = 2
+    L = 2
+    f = 2 / L
+    A = convert(RealT, 0.2)
+    omega = 2 * convert(RealT, pi) * f
     init = c + A * sin(omega * (x[1] + x[2] - a * t))
 
     v1_prime = init
@@ -143,7 +143,7 @@ function initial_condition_convergence_test(x, t,
 
     prim = SVector(v1_prime, v2_prime, p_prime, global_mean_vars(equations)...)
 
-    return map(v -> convert(eltype(x), v), prim2cons(prim, equations))
+    return prim2cons(prim, equations)
 end
 
 """
@@ -156,12 +156,13 @@ function source_terms_convergence_test(u, x, t,
                                        equations::AcousticPerturbationEquations2D)
     v1_mean, v2_mean, c_mean, rho_mean = cons2mean(u, equations)
 
-    c = 2.0
-    A = 0.2
-    L = 2.0
-    f = 2.0 / L
-    a = 1.0
-    omega = 2 * pi * f
+    RealT = eltype(u)
+    a = 1
+    c = 2
+    L = 2
+    f = 2 / L
+    A = convert(RealT, 0.2)
+    omega = 2 * convert(RealT, pi) * f
 
     si, co = sincos(omega * (x[1] + x[2] - a * t))
     tmp = v1_mean + v2_mean - a
@@ -170,9 +171,9 @@ function source_terms_convergence_test(u, x, t,
     du3 = A * omega * co * (2 * c_mean^2 * rho_mean + 2 * c * tmp + 2 * A * tmp * si) /
           c_mean^2
 
-    du4 = du5 = du6 = du7 = 0.0
+    du4 = du5 = du6 = du7 = zero(RealT)
 
-    return SVector(map(v -> convert(eltype(u), v), (du1, du2, du3, du4, du5, du6, du7)))
+    return SVector(du1, du2, du3, du4, du5, du6, du7)
 end
 
 """
@@ -181,13 +182,14 @@ end
 A Gaussian pulse in a constant mean flow. Uses the global mean values from `equations`.
 """
 function initial_condition_gauss(x, t, equations::AcousticPerturbationEquations2D)
-    v1_prime = 0.0
-    v2_prime = 0.0
+    RealT = eltype(x)
+    v1_prime = zero(RealT)
+    v2_prime = zero(RealT)
     p_prime = exp(-4 * (x[1]^2 + x[2]^2))
 
     prim = SVector(v1_prime, v2_prime, p_prime, global_mean_vars(equations)...)
 
-    return map(v -> convert(eltype(x), v), prim2cons(prim, equations))
+    return prim2cons(prim, equations)
 end
 
 """
@@ -218,7 +220,7 @@ function boundary_condition_wall(u_inner, orientation, direction, x, t,
         flux = surface_flux_function(u_boundary, u_inner, orientation, equations)
     end
 
-    return flux # TODO: Check surface_flux_function
+    return flux
 end
 
 """
@@ -242,14 +244,14 @@ function boundary_condition_slip_wall(u_inner, normal_direction::AbstractVector,
     u_normal = normal[1] * u_inner[1] + normal[2] * u_inner[2]
 
     # create the "external" boundary solution state
-    u_boundary = SVector(u_inner[1] - 2.0 * u_normal * normal[1],
-                         u_inner[2] - 2.0 * u_normal * normal[2],
+    u_boundary = SVector(u_inner[1] - 2 * u_normal * normal[1],
+                         u_inner[2] - 2 * u_normal * normal[2],
                          u_inner[3], cons2mean(u_inner, equations)...)
 
     # calculate the boundary flux
     flux = surface_flux_function(u_inner, u_boundary, normal_direction, equations)
 
-    return map(v -> convert(eltype(u_inner), v), flux)
+    return flux
 end
 
 # Calculate 1D flux for a single point
@@ -259,13 +261,14 @@ end
     v1_mean, v2_mean, c_mean, rho_mean = cons2mean(u, equations)
 
     # Calculate flux for conservative state variables
+    RealT = eltype(u)
     if orientation == 1
         f1 = v1_mean * v1_prime + v2_mean * v2_prime +
              c_mean^2 * p_prime_scaled / rho_mean
-        f2 = zero(eltype(u))
+        f2 = zero(RealT)
         f3 = rho_mean * v1_prime + v1_mean * p_prime_scaled
     else
-        f1 = zero(eltype(u))
+        f1 = zero(RealT)
         f2 = v1_mean * v1_prime + v2_mean * v2_prime +
              c_mean^2 * p_prime_scaled / rho_mean
         f3 = rho_mean * v2_prime + v2_mean * p_prime_scaled
@@ -274,7 +277,7 @@ end
     # The rest of the state variables are actually variable coefficients, hence the flux should be
     # zero. See https://github.com/trixi-framework/Trixi.jl/issues/358#issuecomment-784828762
     # for details.
-    f4 = f5 = f6 = f7 = zero(eltype(u))
+    f4 = f5 = f6 = f7 = zero(RealT)
 
     return SVector(f1, f2, f3, f4, f5, f6, f7)
 end
@@ -315,7 +318,8 @@ end
     # The rest of the state variables are actually variable coefficients, hence the flux should be
     # zero. See https://github.com/trixi-framework/Trixi.jl/issues/358#issuecomment-784828762
     # for details.
-    f4 = f5 = f6 = f7 = zero(eltype(u))
+    RealT = eltype(u)
+    f4 = f5 = f6 = f7 = zero(RealT)
 
     return SVector(f1, f2, f3, f4, f5, f6, f7)
 end
@@ -346,10 +350,12 @@ end
                                                               equations::AcousticPerturbationEquations2D)
     λ = dissipation.max_abs_speed(u_ll, u_rr, orientation_or_normal_direction,
                                   equations)
-    diss = -0.5 * λ * (u_rr - u_ll)
-    z = zero(eltype(u_ll))
-    return SVector(v -> convert(eltype(u_ll), v),
-                   (diss[1], diss[2], diss[3], z, z, z, z))
+    
+    RealT = eltype(u_ll)
+    diss = -convert(RealT, 0.5) * λ * (u_rr - u_ll)
+    z = zero(RealT)
+
+    return SVector(diss[1], diss[2], diss[3], z, z, z, z)
 end
 
 @inline have_constant_speed(::AcousticPerturbationEquations2D) = False()
