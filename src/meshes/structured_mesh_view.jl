282dc9b68a06d88ca0157b984ed25bcdb910347e
SimonCan <simon.candelaresi@gmail.com>
2024-04-29 10:25:27 +0100

    Moved save_mesh_file function for StructuredMeshView.

--- src/meshes/structured_mesh_view.jl
+++ src/meshes/structured_mesh_view.jl
@@ -107,4 +107,27 @@ function calc_node_coordinates!(node_coordinates, element,
                                                       cell_y_offset + dy / 2 * nodes[j])
     end
 end
+
+# Does not save the mesh itself to an HDF5 file. Instead saves important attributes
+# of the mesh, like its size and the type of boundary mapping function.
+# Then, within Trixi2Vtk, the StructuredMesh and its node coordinates are reconstructured from
+# these attributes for plotting purposes.
+function save_mesh_file(mesh::StructuredMeshView, output_directory; system = "",
+                        timestep = 0)
+    # Create output directory (if it does not exist)
+    mkpath(output_directory)
+
+    filename = joinpath(output_directory, @sprintf("mesh_%s_%06d.h5", system, timestep))
+
+    # Open file (clobber existing content)
+    h5open(filename, "w") do file
+        # Add context information as attributes
+        attributes(file)["mesh_type"] = get_name(mesh)
+        attributes(file)["ndims"] = ndims(mesh)
+        attributes(file)["size"] = collect(size(mesh))
+        attributes(file)["mapping"] = mesh.mapping_as_string
+    end
+
+    return filename
+end
 end # @muladd
