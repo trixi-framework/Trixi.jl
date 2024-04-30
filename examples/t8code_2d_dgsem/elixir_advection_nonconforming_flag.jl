48082e6cfa3ddb26dc5da761bebde9f62d8fc77e
Johannes Markert <johannes.markert@dlr.de>
2024-04-30 17:34:57 +0200

    Further increasing code coverage.

--- examples/t8code_2d_dgsem/elixir_advection_nonconforming_flag.jl
+++ examples/t8code_2d_dgsem/elixir_advection_nonconforming_flag.jl
@@ -18,13 +18,12 @@ f3(s) = SVector(s, -1.0 + sin(0.5 * pi * s))
 f4(s) = SVector(s, 1.0 + sin(0.5 * pi * s))
 
 faces = (f1, f2, f3, f4)
-mapping = Trixi.transfinite_mapping(faces)
 
 # Create T8codeMesh with 3 x 2 trees and 6 x 4 elements,
 # approximate the geometry with a smaller polydeg for testing.
 trees_per_dimension = (3, 2)
 mesh = T8codeMesh(trees_per_dimension, polydeg = 3,
-                  mapping = mapping,
+                  faces = faces,
                   initial_refinement_level = 1,
                   periodicity = (true, true))
 
