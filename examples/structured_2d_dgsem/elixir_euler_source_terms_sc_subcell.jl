2ddd78aa0451a00c5929178596ebc711bd19af05
bennibolm <benjamin.bolm@gmx.de>
2024-05-15 13:23:34 +0200

    Use local limiting and nonperiodic domain in source terms elixir

--- examples/structured_2d_dgsem/elixir_euler_source_terms_sc_subcell.jl
+++ examples/structured_2d_dgsem/elixir_euler_source_terms_sc_subcell.jl
@@ -10,15 +10,20 @@ equations = CompressibleEulerEquations2D(1.4)
 initial_condition = initial_condition_convergence_test
 source_terms = source_terms_convergence_test
 
+boundary_condition = BoundaryConditionDirichlet(initial_condition)
+boundary_conditions = (x_neg = boundary_condition,
+                       x_pos = boundary_condition,
+                       y_neg = boundary_condition,
+                       y_pos = boundary_condition)
+
 # Get the DG approximation space
 surface_flux = flux_lax_friedrichs
 volume_flux = flux_ranocha
 polydeg = 3
 basis = LobattoLegendreBasis(polydeg)
 limiter_idp = SubcellLimiterIDP(equations, basis;
-                                local_twosided_variables_cons = ["rho"],
-                                local_onesided_variables_nonlinear = [(Trixi.entropy_guermond_etal,
-                                                                       min)])
+                                positivity_variables_cons = ["rho"],
+                                positivity_variables_nonlinear = [pressure])
 volume_integral = VolumeIntegralSubcellLimiting(limiter_idp;
                                                 volume_flux_dg = volume_flux,
                                                 volume_flux_fv = surface_flux)
@@ -32,9 +37,10 @@ f4(s) = SVector(s, 1.0 + sin(0.5 * pi * s))
 mapping = Trixi.transfinite_mapping((f1, f2, f3, f4))
 
 cells_per_dimension = (16, 16)
-mesh = StructuredMesh(cells_per_dimension, mapping, periodicity = true)
+mesh = StructuredMesh(cells_per_dimension, mapping, periodicity = false)
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
+                                    boundary_conditions = boundary_conditions,
                                     source_terms = source_terms)
 
 ###############################################################################
@@ -55,7 +61,7 @@ save_solution = SaveSolutionCallback(interval = 100,
                                      save_final_solution = true,
                                      solution_variables = cons2prim)
 
-stepsize_callback = StepsizeCallback(cfl = 0.5)
+stepsize_callback = StepsizeCallback(cfl = 0.8)
 
 callbacks = CallbackSet(summary_callback,
                         analysis_callback, alive_callback,
