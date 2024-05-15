f890210222154cb2d14ab6e5af010f07d5196180
Andrew Winters <andrew.ross.winters@liu.se>
2024-05-15 11:39:50 +0200

    solution mean value computation must take metric terms into account on mesh types other than TreeMesh

--- src/callbacks_stage/positivity_zhang_shu_dg2d.jl
+++ src/callbacks_stage/positivity_zhang_shu_dg2d.jl
@@ -6,7 +6,7 @@
 #! format: noindent
 
 function limiter_zhang_shu!(u, threshold::Real, variable,
-                            mesh::AbstractMesh{2}, equations, dg::DGSEM, cache)
+                            mesh::TreeMesh{2}, equations, dg::DGSEM, cache)
     @unpack weights = dg.basis
 
     @threaded for element in eachelement(dg, cache)
@@ -28,6 +28,50 @@ function limiter_zhang_shu!(u, threshold::Real, variable,
         end
         # note that the reference element is [-1,1]^ndims(dg), thus the weights sum to 2
         u_mean = u_mean / 2^ndims(mesh)
+
+        # We compute the value directly with the mean values, as we assume that
+        # Jensen's inequality holds (e.g. pressure for compressible Euler equations).
+        value_mean = variable(u_mean, equations)
+        theta = (value_mean - threshold) / (value_mean - value_min)
+        for j in eachnode(dg), i in eachnode(dg)
+            u_node = get_node_vars(u, equations, dg, i, j, element)
+            set_node_vars!(u, theta * u_node + (1 - theta) * u_mean,
+                           equations, dg, i, j, element)
+        end
+    end
+
+    return nothing
+end
+
+function limiter_zhang_shu!(u, threshold::Real, variable,
+                            mesh::Union{StructuredMesh{2}, StructuredMeshView{2},
+                                        UnstructuredMesh2D, P4estMesh{2},
+                                        T8codeMesh{2}},
+                            equations, dg::DGSEM, cache)
+    @unpack weights = dg.basis
+
+    @threaded for element in eachelement(dg, cache)
+        # determine minimum value
+        value_min = typemax(eltype(u))
+        for j in eachnode(dg), i in eachnode(dg)
+            u_node = get_node_vars(u, equations, dg, i, j, element)
+            value_min = min(value_min, variable(u_node, equations))
+        end
+
+        # detect if limiting is necessary
+        value_min < threshold || continue
+
+        # compute mean value
+        u_mean = zero(get_node_vars(u, equations, dg, 1, 1, element))
+        total_volume = zero(real(mesh))
+        for j in eachnode(dg), i in eachnode(dg)
+            jacobian_node = abs(inv(cache.elements.inverse_jacobian[i, j, element]))
+            u_node = get_node_vars(u, equations, dg, i, j, element)
+            u_mean += u_node * weights[i] * weights[j] * jacobian_node
+            total_volume += weights[i] * weights[j] * jacobian_node
+        end
+        # normalize with the total volume
+        u_mean = u_mean / total_volume
 
         # We compute the value directly with the mean values, as we assume that
         # Jensen's inequality holds (e.g. pressure for compressible Euler equations).
