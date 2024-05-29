594d9b8749dd4c056888fa2d6822565e2ae3dd8a
Benedict Geihe <bgeihe@uni-koeln.de>
2024-05-29 23:00:58 +0200

    3D = p8!

--- src/auxiliary/p4est.jl
+++ src/auxiliary/p4est.jl
@@ -121,7 +121,7 @@ function load_p4est(file, ::Val{3})
     conn_vec = Vector{Ptr{p8est_connectivity_t}}(undef, 1)
     comm = P4est.uses_mpi() ? mpi_comm() : C_NULL # Use Trixi.jl's MPI communicator if p4est supports MPI
     p4est = p8est_load_ext(file, comm, 0, 0, 1, 0, C_NULL, pointer(conn_vec))
-    p4est_reset_data(p4est, 2 * sizeof(Int), C_NULL, C_NULL)
+    p8est_reset_data(p4est, 2 * sizeof(Int), C_NULL, C_NULL)
 end
 
 # Read `p4est` connectivity from Abaqus mesh file (.inp)
