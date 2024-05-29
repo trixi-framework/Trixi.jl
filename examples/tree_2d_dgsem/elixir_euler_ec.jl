fad39b128e65818cf35502cf9b56ce4761603e74
Michael Schlottke-Lakemper <michael@sloede.com>
2024-05-29 16:05:37 +0100

    Fix whitespace

--- examples/tree_2d_dgsem/elixir_euler_ec.jl
+++ examples/tree_2d_dgsem/elixir_euler_ec.jl
@@ -15,15 +15,15 @@ v2 = 1.3 + amplitude * rand()
 p = 7.54 + amplitude * rand()
 return prim2cons(SVector(rho, v1, v2, p), equations)
 end
-initial_condition = initial_condition_weak_blast_wave
-# initial_condition = initial_condition_random_field
+# initial_condition = initial_condition_weak_blast_wave
+initial_condition = initial_condition_random_field
 
 #volume_flux = flux_ranocha
 #solver = DGSEM(polydeg = 3, surface_flux = flux_ranocha,
 #               volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 #
 surface_flux = flux_ranocha
-polydeg = 11
+polydeg = 31
 basis = LobattoLegendreBasis(polydeg; polydeg_projection = 2 * polydeg, polydeg_cutoff = 3)
 # volume_integral = VolumeIntegralWeakFormProjection()
 volume_integral = VolumeIntegralWeakForm()
