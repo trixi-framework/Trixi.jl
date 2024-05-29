d965f247c1c4afe36aec2063aa71d9cafec05b42
Benedict Geihe <bgeihe@uni-koeln.de>
2024-05-29 20:19:26 +0200

    use p4est_reset_data instead

--- src/auxiliary/p4est.jl
+++ src/auxiliary/p4est.jl
@@ -84,14 +84,14 @@ end
 # Save `p4est` data to file
 # 2D
 function save_p4est!(file, p4est::PointerOrWrapper{p4est_t})
-    # Also save user data to be able to reload it for restarts
-    p4est_save(file, p4est, true)
+    # Don't save user data of the quads
+    p4est_save(file, p4est, false)
 end
 
 # 3D
 function save_p4est!(file, p8est::PointerOrWrapper{p8est_t})
-    # Also save user data to be able to reload it for restarts
-    p8est_save(file, p8est, true)
+    # Don't save user data of the quads
+    p8est_save(file, p8est, false)
 end
 
 # Load `p4est` from file
@@ -99,14 +99,29 @@ end
 function load_p4est(file, ::Val{2})
     conn_vec = Vector{Ptr{p4est_connectivity_t}}(undef, 1)
     comm = P4est.uses_mpi() ? mpi_comm() : C_NULL # Use Trixi.jl's MPI communicator if p4est supports MPI
-    p4est_load_ext(file, comm, 2 * sizeof(Int), 1, 1, 0, C_NULL, pointer(conn_vec))
+    p4est = p4est_load_ext(file,
+                           comm,
+                           0,         # Size of user data
+                           0,         # Flag to load user data
+                           1,         # Autopartition: ignore saved partition
+                           0,         # Have only rank 0 read headers and bcast them
+                           C_NULL,    # No pointer to user data
+                           pointer(conn_vec))
+    # p4est_load_ext only allocates memory when also data is read
+    # use p4est_reset_data to allocate uninitialized memory
+    p4est_reset_data(p4est,
+                     2 * sizeof(Int), # Use Int-Vector of size 2 as quadrant user data
+                     C_NULL,          # No init function
+                     C_NULL)          # No pointer to user data
+    return p4est
 end
 
 # 3D
 function load_p4est(file, ::Val{3})
     conn_vec = Vector{Ptr{p8est_connectivity_t}}(undef, 1)
     comm = P4est.uses_mpi() ? mpi_comm() : C_NULL # Use Trixi.jl's MPI communicator if p4est supports MPI
-    p8est_load_ext(file, comm, 2 * sizeof(Int), 1, 1, 0, C_NULL, pointer(conn_vec))
+    p4est = p8est_load_ext(file, comm, 0, 0, 1, 0, C_NULL, pointer(conn_vec))
+    p4est_reset_data(p4est, 2 * sizeof(Int), C_NULL, C_NULL)
 end
 
 # Read `p4est` connectivity from Abaqus mesh file (.inp)
