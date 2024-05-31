bdb8180ee28b1491134a81869ab11fd7c47adb9e
Benedict <135045760+benegee@users.noreply.github.com>
2024-05-31 21:08:37 +0200

    Read in capacity for parallel TreeMesh (#1913)
    * read in capacity for parallel TreeMesh
    
    fixes restarting with AMR and MPI
    
    * add elixir testing restart with AMR
    
    * add elixir with AMR and restart to MPI test suite
    
    * adapt errors in test
    
    * Update test/test_mpi_tree.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- test/test_mpi_tree.jl
+++ test/test_mpi_tree.jl
@@ -70,6 +70,14 @@ CI_ON_WINDOWS = (get(ENV, "GITHUB_ACTIONS", false) == "true") && Sys.iswindows()
                             coverage_override=(maxiters = 6,))
     end
 
+    @trixi_testset "elixir_advection_restart_amr.jl" begin
+        @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                     "elixir_advection_restart_amr.jl"),
+                            l2=[7.870371848717432e-5],
+                            linf=[0.0007374081713964475],
+                            coverage_override=(maxiters = 50,))
+    end
+
     # Linear scalar advection with AMR
     # These example files are only for testing purposes and have no practical use
     @trixi_testset "elixir_advection_amr_refine_twice.jl" begin
