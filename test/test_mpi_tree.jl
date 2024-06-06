344b70730641fefa8398b662fdb4503d064516f6
Benedict Geihe <bgeihe@uni-koeln.de>
2024-06-06 16:13:15 +0200

    missed apapted error

--- test/test_mpi_tree.jl
+++ test/test_mpi_tree.jl
@@ -74,7 +74,7 @@ CI_ON_WINDOWS = (get(ENV, "GITHUB_ACTIONS", false) == "true") && Sys.iswindows()
         @test_trixi_include(joinpath(EXAMPLES_DIR,
                                      "elixir_advection_restart_amr.jl"),
                             l2=[8.018498574373939e-5],
-                            linf=[0.0007374081713964475])
+                            linf=[0.0007307237754662355])
     end
 
     # Linear scalar advection with AMR
