c0f031f9b77fc8338456ae0c8db973ea15f430ee
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2024-06-10 14:33:29 -0500

    Update src/equations/compressible_euler_quasi_1d.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/equations/compressible_euler_quasi_1d.jl
+++ src/equations/compressible_euler_quasi_1d.jl
@@ -1,4 +1,4 @@
-urn # By default, Julia/LLVM does not use fused multiply-add operations (FMAs).
+# By default, Julia/LLVM does not use fused multiply-add operations (FMAs).
 # Since these FMAs can increase the performance of many numerical algorithms,
 # we need to opt-in explicitly.
 # See https://ranocha.de/blog/Optimizing_EC_Trixi for further details.
