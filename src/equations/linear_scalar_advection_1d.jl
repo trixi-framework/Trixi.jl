22ab4dc88a0a20b6f4ebe5416887b2b48e9ce98a
huiyuxie <huiyuxie.sde@gmail.com>
2024-06-11 20:32:44 -0700

    fix zeros

--- src/equations/linear_scalar_advection_1d.jl
+++ src/equations/linear_scalar_advection_1d.jl
@@ -202,14 +202,16 @@ end
 
 @inline function splitting_lax_friedrichs(u, ::Val{:plus}, orientation::Integer,
                                           equations::LinearScalarAdvectionEquation1D)
+    RealT = eltype(u)
     a = equations.advection_velocity[1]
-    return a > 0 ? flux(u, orientation, equations) : zero(u)
+    return a > 0 ? flux(u, orientation, equations) : zero(RealT)
 end
 
 @inline function splitting_lax_friedrichs(u, ::Val{:minus}, orientation::Integer,
                                           equations::LinearScalarAdvectionEquation1D)
+    RealT = eltype(u)
     a = equations.advection_velocity[1]
-    return a < 0 ? flux(u, orientation, equations) : zero(u)
+    return a < 0 ? flux(u, orientation, equations) : zero(RealT)
 end
 
 # Convert conservative variables to primitive
