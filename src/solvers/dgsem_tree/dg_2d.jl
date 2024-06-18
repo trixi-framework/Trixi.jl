8823a9117ea8db232dc1f79d11cc0230fa6ad050
SimonCan <simon.candelaresi@gmail.com>
2024-06-18 16:53:18 +0100

    Added StructuredMeshView{2} to various fluxes.

--- src/solvers/dgsem_tree/dg_2d.jl
+++ src/solvers/dgsem_tree/dg_2d.jl
@@ -36,7 +36,7 @@ end
 
 # The methods below are specialized on the volume integral type
 # and called from the basic `create_cache` method at the top.
-function create_cache(mesh::Union{TreeMesh{2}, StructuredMesh{2}, UnstructuredMesh2D,
+function create_cache(mesh::Union{TreeMesh{2}, StructuredMesh{2}, StructuredMeshView{2}, UnstructuredMesh2D,
                                   P4estMesh{2}, T8codeMesh{2}},
                       equations, volume_integral::VolumeIntegralFluxDifferencing,
                       dg::DG, uEltype)
@@ -233,7 +233,7 @@ end
 # mapping terms, stored in `cache.elements.contravariant_vectors`, is peeled apart
 # from the evaluation of the physical fluxes in each Cartesian direction
 function calc_volume_integral!(du, u,
-                               mesh::Union{TreeMesh{2}, StructuredMesh{2},
+                               mesh::Union{TreeMesh{2}, StructuredMesh{2}, StructuredMeshView{2},
                                            UnstructuredMesh2D, P4estMesh{2},
                                            T8codeMesh{2}},
                                nonconservative_terms, equations,
