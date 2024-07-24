3d591c535c978cbb19d2e22f7e79d5eb5df4ff17
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-07-25 00:11:05 +0200

    typos

--- src/equations/linear_scalar_advection_2d.jl
+++ src/equations/linear_scalar_advection_2d.jl
@@ -278,7 +278,7 @@ end
 # Convert conservative variables to primitive
 @inline cons2prim(u, equation::LinearScalarAdvectionEquation2D) = u
 # Convert primitive variables to conservative
-@inline prim2cons(u, equation::LinearScalarAdvectionEquation1D) = u
+@inline prim2cons(u, equation::LinearScalarAdvectionEquation2D) = u
 
 # Convert conservative variables to entropy variables
 @inline cons2entropy(u, equation::LinearScalarAdvectionEquation2D) = u
--- src/equations/linear_scalar_advection_3d.jl
+++ src/equations/linear_scalar_advection_3d.jl
@@ -197,7 +197,7 @@ end
 # Convert conservative variables to primitive
 @inline cons2prim(u, equation::LinearScalarAdvectionEquation3D) = u
 # Convert primitive variables to conservative
-@inline prim2cons(u, equation::LinearScalarAdvectionEquation1D) = u
+@inline prim2cons(u, equation::LinearScalarAdvectionEquation3D) = u
 
 # Convert conservative variables to entropy variables
 @inline cons2entropy(u, equation::LinearScalarAdvectionEquation3D) = u
