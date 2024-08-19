b0d3a44dc55b6b61148947cc9fc2d8832fb3a33f
Patrick Ersing <114223904+patrickersing@users.noreply.github.com>
2024-08-19 10:47:46 +0200

    Update NC-fluxes for the SWE (#2038)
    * rewrite nonconservative fluxes
    
    * update test values for unstructured2d
    
    * update test values for unstructured 2d
    
    ---------
    
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- test/test_type.jl
+++ test/test_type.jl
@@ -2045,8 +2045,6 @@ isdir(outdir) && rm(outdir, recursive = true)
             @test eltype(@inferred flux_nonconservative_audusse_etal(u_ll, u_rr,
                                                                      orientation,
                                                                      equations)) == RealT
-            @test eltype(@inferred flux_nonconservative_ersing_etal(u_ll, u_rr, orientation,
-                                                                    equations)) == RealT
             @test eltype(@inferred flux_fjordholm_etal(u_ll, u_rr, orientation,
                                                        equations)) == RealT
             @test eltype(@inferred flux_wintermeyer_etal(u_ll, u_rr, orientation,
@@ -2133,10 +2131,6 @@ isdir(outdir) && rm(outdir, recursive = true)
                                                                      normal_direction_ll,
                                                                      normal_direction_average,
                                                                      equations)) == RealT
-            @test eltype(@inferred flux_nonconservative_ersing_etal(u_ll, u_rr,
-                                                                    normal_direction_ll,
-                                                                    normal_direction_average,
-                                                                    equations)) == RealT
             @test eltype(@inferred flux_fjordholm_etal(u_ll, u_rr, normal_direction,
                                                        equations)) == RealT
             @test eltype(@inferred flux_wintermeyer_etal(u_ll, u_rr, normal_direction,
@@ -2180,10 +2174,6 @@ isdir(outdir) && rm(outdir, recursive = true)
                 @test eltype(@inferred flux_nonconservative_audusse_etal(u_ll, u_rr,
                                                                          orientation,
                                                                          equations)) ==
-                      RealT
-                @test eltype(eltype(@inferred flux_nonconservative_ersing_etal(u_ll, u_rr,
-                                                                               orientation,
-                                                                               equations))) ==
                       RealT
                 @test eltype(@inferred flux_fjordholm_etal(u_ll, u_rr, orientation,
                                                            equations)) == RealT
