0994aab1abd88cc35206173e6d29fecdf272e3c9
bennibolm <benjamin.bolm@gmx.de>
2024-08-26 12:42:25 +0200

    Add finalizing of t8codemesh to fv elixirs

--- examples/t8code_2d_fv/elixir_advection_basic.jl
+++ examples/t8code_2d_fv/elixir_advection_basic.jl
@@ -45,3 +45,7 @@ sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
             dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep = false, callback = callbacks);
 summary_callback()
+
+# Finalize `T8codeMesh` to make sure MPI related objects in t8code are
+# released before `MPI` finalizes.
+!isinteractive() && finalize(mesh)
