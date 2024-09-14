03a02ca0768f47fd1aca55741db3190a432bc4c3
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-09-14 18:00:01 +0200

    fmt

--- src/callbacks_stage/entropy_bounded_limiter.jl
+++ src/callbacks_stage/entropy_bounded_limiter.jl
@@ -41,7 +41,7 @@ which balances the trade-off between accuracy and stability.
 """
 function EntropyBoundedLimiter(;
                                exp_entropy_decrease_max::RealT = -1e-13) where {RealT <:
-                                                                              Real}
+                                                                                Real}
     @assert exp_entropy_decrease_max<0 "Supplied `exp_entropy_decrease_max` expected to be negative"
     EntropyBoundedLimiter{RealT}(exp_entropy_decrease_max, Vector{RealT}())
 end
