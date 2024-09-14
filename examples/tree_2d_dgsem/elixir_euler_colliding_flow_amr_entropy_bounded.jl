0af9c7fc257256d77b2145a971856b9c81e5f090
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2024-09-14 18:29:00 +0200

    longer sim

--- examples/tree_2d_dgsem/elixir_euler_colliding_flow_amr_entropy_bounded.jl
+++ examples/tree_2d_dgsem/elixir_euler_colliding_flow_amr_entropy_bounded.jl
@@ -79,7 +79,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-tspan = (0.0, 0.1)
+tspan = (0.0, 25.0)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
