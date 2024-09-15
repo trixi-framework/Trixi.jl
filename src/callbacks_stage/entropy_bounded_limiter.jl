aea5afb0066638b122bca610a51cce12932c5ce6
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2024-09-15 23:14:11 +0200

    Apply suggestions from code review

--- src/callbacks_stage/entropy_bounded_limiter.jl
+++ src/callbacks_stage/entropy_bounded_limiter.jl
@@ -17,7 +17,7 @@ Entropy-bounded limiter by
   Entropy-bounded discontinuous Galerkin scheme for Euler equations
   [doi: 10.1016/j.jcp.2015.04.026](https://doi.org/10.1016/j.jcp.2015.04.026)
 
-This is an ideal-gas specific limiter that bounds the entropy decrease per element 
+This is an ideal-gas specific limiter that bounds the (unphysical) entropy decrease per element 
 from one time step (or Runge-Kutta stage) to the next.
 The parameter `exp_entropy_decrease_max` is the maximum allowed exponentiated
 entropy decrease per element at each element's node.
@@ -32,7 +32,7 @@ This, however, leads in general to very diffusive solutions for timesteps violat
 a CFL condition (Lemma 3 in Lv, Ihme (2015)) which is required for entropy stability in the mean values.
 Since most practical simulations will employ a significantly larger timestep, one can relax the 
 strict entropy increase requirement by setting `exp_entropy_decrease_max` to a negative value.
-The limiter becomes than active if the exponentiated entropy decrease on an element is larger than
+The limiter acts if the exponentiated entropy decrease on an element is larger than
 `exp_entropy_decrease_max`.
 The choice of the tolerated exponentiated entropy decrease is a problem-specific parameter 
 which balances the trade-off between accuracy and stability.
