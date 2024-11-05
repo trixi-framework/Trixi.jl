aa2ed4bc6bb508289cf727b01652890020b02813
Benedict Geihe <bgeihe@uni-koeln.de>
2024-11-05 12:53:43 +0100

    temporarily set negative volume test to broken

--- test/test_t8code_2d.jl
+++ test/test_t8code_2d.jl
@@ -39,7 +39,8 @@ end
 end
 
 @trixi_testset "test check_for_negative_volumes" begin
-    @test_throws "Discovered negative volumes" begin
+    # test is currently broken as t8code applies a correction on the fly
+    @test_throws "Discovered negative volumes" broken=true begin
         # Unstructured mesh with six cells which have left-handed node ordering.
         mesh_file = Trixi.download("https://gist.githubusercontent.com/jmark/bfe0d45f8e369298d6cc637733819013/raw/cecf86edecc736e8b3e06e354c494b2052d41f7a/rectangle_with_negative_volumes.msh",
                                    joinpath(EXAMPLES_DIR,
