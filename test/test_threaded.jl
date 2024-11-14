37404641fec4ed0ee83aae9c70a95c708dc01438
Jesse Chan <jesse.lee.chan@gmail.com>
2024-11-14 10:59:45 -0600

    fix threaded CI test values

--- test/test_threaded.jl
+++ test/test_threaded.jl
@@ -406,18 +406,9 @@ end
         @test_trixi_include(joinpath(examples_dir(), "dgmulti_2d",
                                      "elixir_euler_curved.jl"),
                             alg=RDPK3SpFSAL49(thread = OrdinaryDiffEq.True()),
-                            l2=[
-                                1.7204593127904542e-5,
-                                1.5921547179522804e-5,
-                                1.5921547180107928e-5,
-                                4.894071422525737e-5
-                            ],
-                            linf=[
-                                0.00010525416930584619,
-                                0.00010003778091061122,
-                                0.00010003778085621029,
-                                0.00036426282101720275
-                            ])
+                            l2 = [1.720916434676505e-5, 1.5928649356300228e-5, 1.5928649356913923e-5, 4.896339454587786e-5], 
+                            linf = [0.00010525404319960963, 0.00010003768703459315, 0.00010003768696797977, 0.0003642622844073351]
+                            )
 
         # Ensure that we do not have excessive memory allocations
         # (e.g., from type instabilities)
