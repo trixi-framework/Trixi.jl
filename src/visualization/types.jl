de4d78e30b52b8e88d90eb3fc5f95b124eb61ec4
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2024-11-14 12:22:58 -0600

    Update src/visualization/types.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/visualization/types.jl
+++ src/visualization/types.jl
@@ -615,10 +615,18 @@ function PlotData1D(u, mesh::TreeMesh, equations, solver, cache;
 end
 
 # unwrap u if it is VectorOfArray 
-PlotData1D(u::VectorOfArray, mesh, equations, dg::DGMulti{1}, cache; kwargs...) = 
-    PlotData1D(parent(u), mesh, equations, dg, cache; kwargs...)
-PlotData2D(u::VectorOfArray, mesh, equations, dg::DGMulti{2}, cache; kwargs...) = 
-    PlotData2D(parent(u), mesh, equations, dg, cache; kwargs...)    
+PlotData1D(u::VectorOfArray, mesh, equations, dg::DGMulti{1}, cache; kwargs...) = PlotData1D(parent(u),
+                                                                                             mesh,
+                                                                                             equations,
+                                                                                             dg,
+                                                                                             cache;
+                                                                                             kwargs...)
+PlotData2D(u::VectorOfArray, mesh, equations, dg::DGMulti{2}, cache; kwargs...) = PlotData2D(parent(u),
+                                                                                             mesh,
+                                                                                             equations,
+                                                                                             dg,
+                                                                                             cache;
+                                                                                             kwargs...)
 
 function PlotData1D(u, mesh, equations, solver, cache;
                     solution_variables = nothing, nvisnodes = nothing,
