110ef684e35b04bb9ef05faabfb7504080253fb8
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2024-12-02 18:19:03 +0000

    remove

--- src/solvers/dgsem_tree/dg_3d_parabolic.jl
+++ src/solvers/dgsem_tree/dg_3d_parabolic.jl
@@ -966,7 +966,7 @@ function calc_gradient!(gradients, u_transformed, t,
     # NOTE: This re-uses the implementation for hyperbolic terms in "dg_3d.jl"
     @trixi_timeit timer() "prolong2mortars" begin
         prolong2mortars!(cache, u_transformed, mesh, equations_parabolic,
-                         dg.mortar, dg.surface_integral, dg)
+                         dg.mortar, dg)
     end
 
     # Calculate mortar fluxes
