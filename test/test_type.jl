8185e89bd27da59081616365c1d7d512e905f3a9
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2024-12-05 19:32:46 +0000

    Add some tests

--- test/test_type.jl
+++ test/test_type.jl
@@ -25,6 +25,37 @@ isdir(outdir) && rm(outdir, recursive = true)
         end
     end
 
+    @timed_testset "TreeMesh & SerialTree type consistence" begin
+        for RealT in (Float32, Float64)
+            coordinates_min = -convert(RealT, 1)
+            coordinates_max = convert(RealT, 1)
+
+            mesh = TreeMesh(coordinates_min, coordinates_max,
+                            initial_refinement_level = 4,
+                            n_cells_max = 30_000
+
+            @test mesh.tree == SerialTree{1, RealT}
+
+            coordinates_min = (-convert(RealT, 1), -convert(RealT, 42))
+            coordinates_max = (convert(RealT, 1), convert(RealT, 42))
+
+            mesh = TreeMesh(coordinates_min, coordinates_max,
+                            initial_refinement_level = 4,
+                            n_cells_max = 30_000
+
+            @test mesh.tree == SerialTree{2, RealT}
+
+            coordinates_min = (-convert(RealT, 1), -convert(RealT, 42), -convert(RealT, pi))
+            coordinates_max = (convert(RealT, 1), convert(RealT, 42), convert(RealT, pi))
+
+            mesh = TreeMesh(coordinates_min, coordinates_max,
+                            initial_refinement_level = 3,
+                            n_cells_max = 30_000
+
+            @test mesh.tree == SerialTree{3, RealT}
+        end
+    end
+
     @timed_testset "Acoustic Perturbation 2D" begin
         for RealT in (Float32, Float64)
             v_mean_global = (zero(RealT), zero(RealT))
