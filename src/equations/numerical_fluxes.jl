e44b52e3eb9e9f721d0d5997303b703e2ceb1cf8
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2024-12-11 17:18:37 +0100

    Improved documentation for DissipationEntropyStable

--- src/equations/numerical_fluxes.jl
+++ src/equations/numerical_fluxes.jl
@@ -225,7 +225,7 @@ const flux_lax_friedrichs = FluxLaxFriedrichs()
     DissipationEntropyStable(max_abs_speed=max_abs_speed_naive)
 
 Create a local Lax-Friedrichs-type dissipation operator that is provably entropy stable. This operator
-must be used together with an entropy-conservative two-point flux function (here `flux_ec`) to yield 
+must be used together with an entropy-conservative two-point flux function (e.g., `flux_ec`) to yield 
 an entropy-stable surface flux. The surface flux function can be initialized as:
 ```
 flux_es = FluxPlusDissipation(flux_ec, DissipationEntropyStable())
@@ -233,12 +233,18 @@ flux_es = FluxPlusDissipation(flux_ec, DissipationEntropyStable())
 
 In particular, the numerical flux has the form
 ```math
-f^{ES} = f^{EC} + \frac{1}{2} λ_{max} H (w_R - w_L),
+f^{ES} = f^{EC} + \frac{1}{2} λ_{max} H (w_r - w_l),
 ````
-where ``f^{EC}`` is the entropy-conservative two-point flux function computed with `flux_ec`, ``λ_{max}`` 
-is the maximum wave speed estimated as `max_abs_speed(u_ll, u_rr, orientation_or_normal_direction, equations)`,
+where ``f^{EC}`` is the entropy-conservative two-point flux function (computed with, e.g., `flux_ec`), ``λ_{max}`` 
+is the maximum wave speed estimated as `max_abs_speed(u_l, u_r, orientation_or_normal_direction, equations)`,
 defaulting to [`max_abs_speed_naive`](@ref), ``H`` is a symmetric positive-definite dissipation matrix that
-depends on the states `u_ll` and `u_rr`, and ``(w_R - w_L)`` is the jump in entropy variables.
+depends on the left and right states `u_l` and `u_r`, and ``(w_r - w_l)`` is the jump in entropy variables.
+Ideally, ``H (w_r - w_l) = (u_r - u_l)``, such that the dissipation operator is consistent with the local
+Lax-Friedrichs dissipation.
+
+The entropy-stable dissipation operator is computed with the function
+`function (dissipation::DissipationEntropyStable)(u_l, u_r, orientation_or_normal_direction, equations)`,
+which must be specialized for each equation.
 
 For the derivation of the dissipation matrix for the multi-ion GLM-MHD equations, see:
 - A. Rueda-Ramírez, A. Sikstel, G. Gassner, An Entropy-Stable Discontinuous Galerkin Discretization
