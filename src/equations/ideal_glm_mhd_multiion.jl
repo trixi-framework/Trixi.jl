5dabad14960ba15e9a2ea2ece383436dce2ed350
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2024-12-18 13:22:46 +0100

    Fixed more docstring issues

--- src/equations/ideal_glm_mhd_multiion.jl
+++ src/equations/ideal_glm_mhd_multiion.jl
@@ -43,8 +43,7 @@ end
 Source terms due to the Lorentz' force for plasmas with more than one ion species. These source 
 terms are a fundamental, inseparable part of the multi-ion GLM-MHD equations, and vanish for 
 a single-species plasma. In particular, they have to be used for every
-simulation of [`IdealMhdMultiIonEquations1D`](@ref), [`IdealGlmMhdMultiIonEquations2D`](@ref),
-and [`IdealGlmMhdMultiIonEquations3D`](@ref).
+simulation of [`IdealGlmMhdMultiIonEquations2D`](@ref).
 """
 function source_terms_lorentz(u, x, t, equations::AbstractIdealGlmMhdMultiIonEquations)
     @unpack charge_to_mass = equations
@@ -382,7 +381,7 @@ The collision sources read as
     \\
     s^{ke}_{E_k}  =& 
     3  \left(
-    \bar{\nu}_{ke} \frac{\rho_k M_{1}}{M_k} \underbrace{R_1}_{=1} (T_{e} - T_k)
+    \bar{\nu}_{ke} \frac{\rho_k M_{1}}{M_k} R_1 (T_{e} - T_k)
     \right) 
         +
         \vec{v}_k \cdot \vec{s}_{\rho_k \vec{v}_k},
