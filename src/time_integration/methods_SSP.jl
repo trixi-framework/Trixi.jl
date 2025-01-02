47c6ea65b934110cc26d8d1ff240e5f5ef5ebeeb
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-01-02 13:53:09 +0100

    explain `tdir`

--- src/time_integration/methods_SSP.jl
+++ src/time_integration/methods_SSP.jl
@@ -83,14 +83,14 @@ mutable struct SimpleIntegratorSSP{RealT <: Real, uType, Params, Sol, F, Alg,
     du::uType
     r0::uType
     t::RealT
-    tdir::RealT
+    tdir::RealT # DIRection of time integration, i.e, if one marches forward or backward in time
     dt::RealT # current time step
     dtcache::RealT # manually set time step
     iter::Int # current number of time steps (iteration)
     p::Params # will be the semidiscretization from Trixi
     sol::Sol # faked
     f::F # `rhs` of the semidiscretization
-    alg::Alg
+    alg::Alg # SimpleSSPRK33
     opts::SimpleIntegratorSSPOptions
     finalstep::Bool # added for convenience
     dtchangeable::Bool
--- src/time_integration/paired_explicit_runge_kutta/methods_PERK4.jl
+++ src/time_integration/paired_explicit_runge_kutta/methods_PERK4.jl
@@ -181,7 +181,7 @@ mutable struct PairedExplicitRK4Integrator{RealT <: Real, uType, Params, Sol, F,
     du::uType
     u_tmp::uType
     t::RealT
-    tdir::RealT
+    tdir::RealT # DIRection of time integration, i.e, if one marches forward or backward in time
     dt::RealT # current time step
     dtcache::RealT # manually set time step
     iter::Int # current number of time steps (iteration)
