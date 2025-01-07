d5eea188df81499136185094ea48661e3674ec98
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2025-01-07 14:28:30 +0100

    Update src/time_integration/paired_explicit_runge_kutta/methods_PERK3.jl

--- src/time_integration/paired_explicit_runge_kutta/methods_PERK3.jl
+++ src/time_integration/paired_explicit_runge_kutta/methods_PERK3.jl
@@ -268,7 +268,7 @@ function step!(integrator::PairedExplicitRK3Integrator)
         end
 
         # We need to store `du` of the S-1 stage in `kS1` for the final update:
-        integrator.kS1[i] .= integrator.du[i]
+        integrator.kS1 .= integrator.du
 
         PERK_ki!(integrator, prob.p, alg, alg.num_stages)
 
