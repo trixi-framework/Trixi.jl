f9b19c3742e5acc4a52cd5617de569c273076d1a
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2025-01-08 12:48:28 +0100

    Update examples/dgmulti_2d/elixir_mhd_reflective_wall.jl
    Co-authored-by: Daniel Doehring <doehringd2@gmail.com>

--- examples/dgmulti_2d/elixir_mhd_reflective_wall.jl
+++ examples/dgmulti_2d/elixir_mhd_reflective_wall.jl
@@ -64,8 +64,8 @@ function boundary_condition_velocity_slip_wall(u_inner, normal_direction::Abstra
                                  v2 - 2 * v_normal * normal[2],
                                  v3, p, B1, B2, B3, psi), equations)
     flux = surface_flux_function(u_inner, u_mirror, normal, equations) * norm_
-    noncons = nonconservative_flux_function(u_inner, u_mirror, normal, equations) * norm_
-    return flux, noncons
+    noncons_flux = nonconservative_flux_function(u_inner, u_mirror, normal, equations) * norm_
+    return flux, noncons_flux
 end
 
 boundary_conditions = (; x_neg = boundary_condition_velocity_slip_wall,
