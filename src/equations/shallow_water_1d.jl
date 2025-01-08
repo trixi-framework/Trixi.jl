4d112a443e163ed3576c1ce592eec7d7023626c6
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2025-01-08 12:49:48 +0100

    Update src/equations/shallow_water_1d.jl
    Co-authored-by: Daniel Doehring <doehringd2@gmail.com>

--- src/equations/shallow_water_1d.jl
+++ src/equations/shallow_water_1d.jl
@@ -174,7 +174,7 @@ For details see Section 9.2.5 of the book:
     if iseven(direction) # u_inner is "left" of boundary, u_boundary is "right" of boundary
         flux = surface_flux_function(u_inner, u_boundary, orientation_or_normal,
                                      equations)
-        noncons = nonconservative_flux_function(u_inner, u_boundary,
+        noncons_flux = nonconservative_flux_function(u_inner, u_boundary,
                                                 orientation_or_normal,
                                                 equations)
     else # u_boundary is "left" of boundary, u_inner is "right" of boundary
