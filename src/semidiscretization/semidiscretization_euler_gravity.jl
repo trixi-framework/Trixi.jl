c85580c0a419cf5deeedb301225b9f8393489857
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-01-09 11:15:11 +0100

    typo

--- src/semidiscretization/semidiscretization_euler_gravity.jl
+++ src/semidiscretization/semidiscretization_euler_gravity.jl
@@ -89,7 +89,7 @@ end
 A struct containing everything needed to describe a spatial semidiscretization
 of a the compressible Euler equations with self-gravity, reformulating the
 Poisson equation for the gravitational potential as steady-state problem of
-the hyperblic diffusion equations.
+the hyperbolic diffusion equations.
 - Michael Schlottke-Lakemper, Andrew R. Winters, Hendrik Ranocha, Gregor J. Gassner (2020)
   "A purely hyperbolic discontinuous Galerkin approach for self-gravitating gas dynamics"
   [arXiv: 2008.10593](https://arXiv.org/abs/2008.10593)
