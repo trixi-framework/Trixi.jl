e2cd3f18242dedf64b54e52b570c91da3bfef710
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-01-21 16:28:52 +0100

    mention consistency with stepsize cb

--- src/equations/numerical_fluxes.jl
+++ src/equations/numerical_fluxes.jl
@@ -205,6 +205,10 @@ Simple and fast estimate of the maximal wave speed of the Riemann problem with l
 `u_ll, u_rr`, based only on the local wave speeds associated to `u_ll` and `u_rr`.
 Less diffusive, i.e., overestimating than [`max_abs_speed_naive`](@ref).
 
+In particular, `max_abs_speed(u, u, i, equations)` gives the same result as `max_abs_speeds(u, equations)[i]`,
+i.e., the wave speeds used in `max_dt` which computes the maximum stable time step size through the 
+[`StepsizeCallback`](@ref).
+
 For non-integer arguments `normal_direction` in one dimension, `max_abs_speed_naive` returns
 `abs(normal_direction[1]) * max_abs_speed_naive(u_ll, u_rr, 1, equations)`.
 """
