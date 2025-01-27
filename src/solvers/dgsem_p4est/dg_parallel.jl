0958cba96316dd012ad42493db61880061359474
huiyuxie <huiyuxie.sde@gmail.com>
2025-01-27 09:06:57 -1000

    Fix typo

--- src/solvers/dgsem_p4est/dg_parallel.jl
+++ src/solvers/dgsem_p4est/dg_parallel.jl
@@ -46,7 +46,7 @@ end
 @inline Base.eltype(::P4estMPICache{uEltype}) where {uEltype} = uEltype
 
 ##
-# Note the code in `start_mpi_send`/`finish_mpi_receive!` is sensitivy to inference on (at least) 1.10
+# Note the code in `start_mpi_send`/`finish_mpi_receive!` is sensitive to inference on (at least) 1.10
 # Julia's inference is bi-stable, it can sometimes depend what code has been looked at already, and
 # the presence of a inference result in the cache can have an impact on the inference of code.
 # In this case the `send_buffer[first:last] .= vec(cache.mpi_mortars.u[2, :, :, ..,mortar])`,
