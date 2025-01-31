69afb7bcad2a173d214ad368617280a288fc58dd
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2025-01-31 16:23:25 +0000

    Update examples/p4est_2d_dgsem/elixir_advection_meshview.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- examples/p4est_2d_dgsem/elixir_advection_meshview.jl
+++ examples/p4est_2d_dgsem/elixir_advection_meshview.jl
@@ -65,4 +65,5 @@ sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
 summary_callback()
 
 # Load the mesh file for code coverage.
-loaded_mesh = Trixi.load_mesh_serial(joinpath("out", "mesh.h5"); n_cells_max=0, RealT=typeof(parent_mesh).parameters[3])
+loaded_mesh = Trixi.load_mesh_serial(joinpath("out", "mesh.h5"); n_cells_max = 0,
+                                     RealT = typeof(parent_mesh).parameters[3])
