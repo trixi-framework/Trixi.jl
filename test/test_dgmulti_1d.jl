72f958b4fde9dfccbac0b6912dc8cb67ea08a1e9
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-02-05 11:48:37 +0100

    Reduce number of allowed allocations

--- test/test_dgmulti_1d.jl
+++ test/test_dgmulti_1d.jl
@@ -25,7 +25,7 @@ isdir(outdir) && rm(outdir, recursive = true)
         t = sol.t[end]
         u_ode = sol.u[end]
         du_ode = similar(u_ode)
-        @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 5000
+        @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
     end
 end
 
