4cd807f7da7bb3fb773eab13288225337a9a606c
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-02-06 16:32:50 +0100

    Keep mu as a constant instead of a function

--- examples/dgmulti_3d/elixir_navierstokes_taylor_green_vortex.jl
+++ examples/dgmulti_3d/elixir_navierstokes_taylor_green_vortex.jl
@@ -5,10 +5,10 @@ using Trixi
 # semidiscretization of the compressible Navier-Stokes equations
 
 prandtl_number() = 0.72
-mu() = 6.25e-4 # equivalent to Re = 1600
+mu = 6.25e-4 # equivalent to Re = 1600
 
 equations = CompressibleEulerEquations3D(1.4)
-equations_parabolic = CompressibleNavierStokesDiffusion3D(equations, mu = mu(),
+equations_parabolic = CompressibleNavierStokesDiffusion3D(equations, mu = mu,
                                                           Prandtl = prandtl_number())
 
 """
