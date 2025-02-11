ce06f7d568d8cb45dc1495587803bc7a4cb77b43
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-02-11 16:46:59 +0100

    clarify docstring

--- src/callbacks_step/summary.jl
+++ src/callbacks_step/summary.jl
@@ -12,8 +12,8 @@ summary_callback(integrator) = u_modified!(integrator, false) # the summary call
     SummaryCallback()
 
 Create and return a callback that prints a human-readable summary of the simulation setup at the
-beginning of a simulation and then resets the timer. When the returned callback is executed
-directly, the current timer values are shown.
+beginning of a simulation and then resets the timer. At the end of the simulation the final timer
+values are shown. When the returned callback is executed directly, the current timer values are shown.
 """
 function SummaryCallback(reset_threads = true)
     function initialize(cb, u, t, integrator)
