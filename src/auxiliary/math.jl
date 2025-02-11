d8c1901cd42af20675ea0f27134e3d5f45aa769a
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2025-02-11 09:41:11 +0100

    Update src/auxiliary/math.jl
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- src/auxiliary/math.jl
+++ src/auxiliary/math.jl
@@ -412,8 +412,8 @@ Given ε = 1.0e-4, we use the following algorithm.
         c3 = convert(RealT, -1 / 21) * (2 * gamma * (gamma - 2) - 9) * c2
         return 0.5f0 * (x + y) * @evalpoly(f2, 1, c1, c2, c3)
     else
-        yg = exp(gamma * log(y))
-        xg = exp(gamma * log(x))
+        yg = exp(gamma * log(y)) # equivalent to y^gamma but faster
+        xg = exp(gamma * log(x)) # equivalent to x^gamma but faster
         return (gamma - 1) / gamma * (yg - xg) /
                (yg / y - xg / x)
     end
