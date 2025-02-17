20dde2b031a5ce4ccbfcc3bce5d7c53d1ada55f7
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-02-17 16:25:33 +0100

    Use `ode_default_options()` consistently (#2286)
    * use ode_default_options() consistently in examples
    
    * also replace in test
    
    * same for docs
    
    * same for markdown files in docs
    
    * same for benchmark
    
    * replace commas by semicolons to separate positional args from kwargs
    
    * Update docs/literate/src/files/DGMulti_1.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update examples/dgmulti_1d/elixir_euler_fdsbp_periodic.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update examples/dgmulti_1d/elixir_euler_flux_diff.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update examples/dgmulti_2d/elixir_euler_fdsbp_periodic.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * format
    
    * manually fix some semicolons
    
    * fix some more semicolons manually
    
    * fix semicolon in restart examples
    
    * some more manual fixes
    
    * another manual fix
    
    * manual semicolon fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- examples/p4est_2d_dgsem/elixir_navierstokes_vortex_street.jl
+++ examples/p4est_2d_dgsem/elixir_navierstokes_vortex_street.jl
@@ -49,7 +49,7 @@ mesh = P4estMesh{2}(mesh_file)
 bc_freestream = BoundaryConditionDirichlet(initial_condition)
 
 # Boundary names follow from the mesh file.
-# Since this mesh is been generated using the symmetry feature of 
+# Since this mesh is been generated using the symmetry feature of
 # HOHQMesh.jl (https://trixi-framework.github.io/HOHQMesh.jl/stable/tutorials/symmetric_mesh/)
 # the mirrored boundaries are named with a "_R" suffix.
 boundary_conditions = Dict(:Circle => boundary_condition_slip_wall, # top half of the cylinder
@@ -61,7 +61,7 @@ boundary_conditions = Dict(:Circle => boundary_condition_slip_wall, # top half o
                            :Left => bc_freestream,
                            :Left_R => bc_freestream)
 
-# Parabolic boundary conditions                            
+# Parabolic boundary conditions
 velocity_bc_free = NoSlip((x, t, equations) -> SVector(v_in, 0))
 # Use adiabatic also on the boundaries to "copy" temperature from the domain
 heat_bc_free = Adiabatic((x, t, equations) -> 0)
--- examples/tree_1d_dgsem/elixir_mhdmulti_briowu_shock_tube.jl
+++ examples/tree_1d_dgsem/elixir_mhdmulti_briowu_shock_tube.jl
@@ -117,7 +117,7 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false);
             dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-            save_everystep = false, callback = callbacks);
+            ode_default_options()..., callback = callbacks);
 summary_callback() # print the timer summary
