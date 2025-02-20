41cc9c4e515e32c433653f1962322548817d193c
Andrés Rueda-Ramírez <aruedara@uni-koeln.de>
2025-02-20 15:04:25 +0100

    Changed index of species k'->l to avoid confusion

--- src/equations/ideal_glm_mhd_multiion.jl
+++ src/equations/ideal_glm_mhd_multiion.jl
@@ -495,13 +495,13 @@ end
 Compute the ion-ion collision source terms for the momentum and energy equations of each ion species as
 ```math
 \begin{aligned}
-  \vec{s}_{\rho_k \vec{v}_k} =&  \rho_k\sum_{k'}\bar{\nu}_{kk'}(\vec{v}_{k'} - \vec{v}_k),\\
+  \vec{s}_{\rho_k \vec{v}_k} =&  \rho_k\sum_{l}\bar{\nu}_{kl}(\vec{v}_{l} - \vec{v}_k),\\
   s_{E_k}  =& 
-    3 \sum_{k'} \left(
-    \bar{\nu}_{kk'} \frac{\rho_k M_1}{M_{k'} + M_k} R_1 (T_{k'} - T_k)
+    3 \sum_{l} \left(
+    \bar{\nu}_{kl} \frac{\rho_k M_1}{M_{l} + M_k} R_1 (T_{l} - T_k)
     \right) + 
-    \sum_{k'} \left(
-        \bar{\nu}_{kk'} \rho_k \frac{M_{k'}}{M_{k'} + M_k} \|\vec{v}_{k'} - \vec{v}_k\|^2
+    \sum_{l} \left(
+        \bar{\nu}_{kl} \rho_k \frac{M_{l}}{M_{l} + M_k} \|\vec{v}_{l} - \vec{v}_k\|^2
         \right)
         +
         \vec{v}_k \cdot \vec{s}_{\rho_k \vec{v}_k},
@@ -509,16 +509,16 @@ Compute the ion-ion collision source terms for the momentum and energy equations
 ```
 where ``M_k`` is the molar mass of ion species `k` provided in `equations.molar_masses`, 
 ``R_k`` is the specific gas constant of ion species `k` provided in `equations.gas_constants`, and
- ``\bar{\nu}_{kk'}`` is the effective collision frequency of species `k` with species `k'`, which is computed as
+ ``\bar{\nu}_{kl}`` is the effective collision frequency of species `k` with species `l`, which is computed as
 ```math
 \begin{aligned}
-  \bar{\nu}_{kk'} = \bar{\nu}^1_{kk'} \tilde{B}_{kk'} \frac{\rho_{k'}}{T_{k k'}^{3/2}},
+  \bar{\nu}_{kl} = \bar{\nu}^1_{kl} \tilde{B}_{kl} \frac{\rho_{l}}{T_{k l}^{3/2}},
 \end{aligned}
 ```
-with the so-called reduced temperature ``T_{k k'}`` and the ion-ion collision constants ``\tilde{B}_{kk'}`` provided
+with the so-called reduced temperature ``T_{k l}`` and the ion-ion collision constants ``\tilde{B}_{kl}`` provided
 in `equations.ion_electron_collision_constants` (see [`IdealGlmMhdMultiIonEquations2D`](@ref)).
 
-The additional coefficient ``\bar{\nu}^1_{kk'}`` is a non-dimensional drift correction factor proposed by Rambo and Denavit.
+The additional coefficient ``\bar{\nu}^1_{kl}`` is a non-dimensional drift correction factor proposed by Rambo and Denavit.
 
 References:
 - P. Rambo, J. Denavit, Interpenetration and ion separation in colliding plasmas, Physics of Plasmas 1 (1994) 4050–4060.
@@ -601,7 +601,7 @@ The collision sources read as
 where ``T_e`` is the electron temperature computed with the function `equations.electron_temperature`, 
 ``M_k`` is the molar mass of ion species `k` provided in `equations.molar_masses`, 
 ``R_k`` is the specific gas constant of ion species `k` provided in `equations.gas_constants`, and
-``\bar{\nu}_{kk'}`` is the collision frequency of species `k` with the electrons, which is computed as
+``\bar{\nu}_{ke}`` is the collision frequency of species `k` with the electrons, which is computed as
 ```math
 \begin{aligned}
   \bar{\nu}_{ke} = \tilde{B}_{ke} \frac{e n_e}{T_e^{3/2}},
