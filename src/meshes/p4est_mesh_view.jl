5b3a864e11dc637a82ecb5145a07d02b6e6d5e8e
SimonCan <simon.candelaresi@gmail.com>
2025-03-20 10:33:10 +0000

    Corrected the output files for p4est mesh views.

--- src/meshes/p4est_mesh_view.jl
+++ src/meshes/p4est_mesh_view.jl
@@ -123,18 +123,20 @@ end
 # Then, within Trixi2Vtk, the P4estMeshView and its node coordinates are reconstructured from
 # these attributes for plotting purposes
 # | Warning: This overwrites any existing mesh file, either for a mesh view or parent mesh.
-function save_mesh_file(mesh::P4estMeshView, output_directory, timestep,
-                        mpi_parallel::False)
+function save_mesh_file(mesh::P4estMeshView, output_directory;
+                        timestep = 0, system = "")
     # Create output directory (if it does not exist)
     mkpath(output_directory)
 
+        filename = joinpath(output_directory, @sprintf("mesh_%s_%09d.h5", system, timestep))
+
     # Determine file name based on existence of meaningful time step
     if timestep > 0
-        filename = joinpath(output_directory, @sprintf("mesh_%09d.h5", timestep))
-        p4est_filename = @sprintf("p4est_data_%09d", timestep)
+        filename = joinpath(output_directory, @sprintf("mesh_%s_%09d.h5", system, timestep))
+        p4est_filename = @sprintf("p4est_%s_data_%09d", system, timestep)
     else
-        filename = joinpath(output_directory, "mesh.h5")
-        p4est_filename = "p4est_data"
+        filename = joinpath(output_directory, @sprintf("mesh_%s.h5", system))
+        p4est_filename = @sprintf("p4est_data_%s", system)
     end
 
     p4est_file = joinpath(output_directory, p4est_filename)
