4ce4899daddd61e2cb8c6a30ca0aaec6d3d5ad57
Jesse Chan <jesse.lee.chan@gmail.com>
2025-03-31 11:50:27 -0500

    update docstring

--- src/solvers/solvers_parabolic.jl
+++ src/solvers/solvers_parabolic.jl
@@ -16,13 +16,17 @@ struct ViscousFormulationBassiRebay1 end
 The local DG (LDG) flux from "The Local Discontinuous Galerkin Method for Time-Dependent
 Convection-Diffusion Systems" by Cockburn and Shu (1998).
 
-Note that, since this implementation does not involve the parabolic "upwinding" vector,
+The parabolic "upwinding" vector is currently implemented for `TreeMesh`; for all other mesh types,
 the LDG solver is equivalent to [`ViscousFormulationBassiRebay1`](@ref) with an LDG-type penalization.
 
 - Cockburn and Shu (1998).
   The Local Discontinuous Galerkin Method for Time-Dependent
   Convection-Diffusion Systems
   [DOI: 10.1137/S0036142997316712](https://doi.org/10.1137/S0036142997316712)
+- Cockburn and Dong (2007)  
+  An Analysis of the Minimal Dissipation Local Discontinuous 
+  Galerkin Method for Convection–Diffusion Problems.
+  [DOI: 10.1007/s10915-007-9130-3](https://doi.org/10.1007/s10915-007-9130-3)
 """
 struct ViscousFormulationLocalDG{P}
     penalty_parameter::P
