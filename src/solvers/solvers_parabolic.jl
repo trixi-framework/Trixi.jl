cbbe97cdb8a124417a38d9836844994d015fa26d
Jesse Chan <jesse.lee.chan@gmail.com>
2025-03-31 18:12:23 -0500

    update docstring

--- src/solvers/solvers_parabolic.jl
+++ src/solvers/solvers_parabolic.jl
@@ -28,15 +28,24 @@ the LDG solver is equivalent to [`ViscousFormulationBassiRebay1`](@ref) with an 
   The Local Discontinuous Galerkin Method for Time-Dependent
   Convection-Diffusion Systems
   [DOI: 10.1137/S0036142997316712](https://doi.org/10.1137/S0036142997316712)
-- Cockburn and Dong (2007)  
-  An Analysis of the Minimal Dissipation Local Discontinuous 
-  Galerkin Method for Convection–Diffusion Problems.
-  [DOI: 10.1007/s10915-007-9130-3](https://doi.org/10.1007/s10915-007-9130-3)
 """
 struct ViscousFormulationLocalDG{P}
     penalty_parameter::P
 end
 
+"""
+    ViscousFormulationLocalDG()
+
+The minimum dissipation local DG (LDG) flux from "An Analysis of the Minimal Dissipation Local 
+Discontinuous Galerkin Method for Convection–Diffusion Problems" by Cockburn and Dong (2007). 
+This scheme corresponds to an LDG parabolic "upwinding/downwinding" but no LDG penalty parameter. 
+Cockburn and Dong proved that this scheme is still stable despite the zero penalty parameter. 
+
+- Cockburn and Dong (2007)  
+  An Analysis of the Minimal Dissipation Local Discontinuous 
+  Galerkin Method for Convection–Diffusion Problems.
+  [DOI: 10.1007/s10915-007-9130-3](https://doi.org/10.1007/s10915-007-9130-3)
+"""
 ViscousFormulationLocalDG() = ViscousFormulationLocalDG(nothing)
 
 # Here, the flux is {{f}} + beta * [[f]], where beta is the LDG "switch", 
