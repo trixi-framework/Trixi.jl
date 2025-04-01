9aa8fa49573fdbe5f1b0cfc092459e1a270830dd
Daniel Doehring <doehringd2@gmail.com>
2025-04-01 17:41:55 +0000

    fix CI error

--- src/callbacks_step/euler_acoustics_coupling.jl
+++ src/callbacks_step/euler_acoustics_coupling.jl
@@ -130,8 +130,8 @@ function EulerAcousticsCouplingCallback(ode_euler, mean_values, alg, cfl_acousti
 
     euler_acoustics_coupling = EulerAcousticsCouplingCallback{typeof(cfl_acoustics),
                                                               typeof(mean_values),
-                                                              typeof(integrator_euler)}(StepsizeCallback(cfl_acoustics),
-                                                                                        StepsizeCallback(cfl_euler),
+                                                              typeof(integrator_euler)}(StepsizeCallback(cfl = cfl_acoustics),
+                                                                                        StepsizeCallback(cfl = cfl_euler),
                                                                                         mean_values,
                                                                                         integrator_euler)
     condition = (u, t, integrator) -> true
