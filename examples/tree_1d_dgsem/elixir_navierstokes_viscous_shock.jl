822b8ad5f34cfca4c301d8f8cab20a5fbb07ba01
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-04-01 15:03:10 +0200

    Add comments and enhanced docstring

--- examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
+++ examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock.jl
@@ -145,6 +145,11 @@ boundary_condition_parabolic = BoundaryConditionNavierStokesWall(velocity_bc, he
 boundary_conditions_parabolic = (; x_neg = boundary_condition_parabolic,
                                  x_pos = boundary_condition_parabolic)
 
+# We use by default the Bassi-Rebay 1 scheme.
+# Since this is a diffusion-dominated problem, it might be better to use the LDG scheme
+# by specifying the keyword
+# solver_parabolic = ViscousFormulationLocalDG()
+# in the semidiscretization call below.
 semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic),
                                              initial_condition, solver;
                                              boundary_conditions = (boundary_conditions,
--- src/solvers/solvers_parabolic.jl
+++ src/solvers/solvers_parabolic.jl
@@ -7,6 +7,15 @@ The classical BR1 flux from
   A High-Order Accurate Discontinuous Finite Element Method for
   the Numerical Solution of the Compressible Navier-Stokes Equations
   [DOI: 10.1006/jcph.1996.5572](https://doi.org/10.1006/jcph.1996.5572)
+
+A more detailed study of the BR1 scheme for the DGSEM can be found in
+- G. J. Gassner, A. R. Winters, F. J. Hindenlang, D. Kopriva (2018)
+  The BR1 Scheme is Stable for the Compressible Navier-Stokes Equations
+  [DOI: 10.1007/s10915-018-0702-1](https://doi.org/10.1007/s10915-018-0702-1)
+
+The BR1 scheme works well for convection-dominated problems, but may cause instabilities or 
+reduced convergence for diffusion-dominated problems. 
+In the latter case, the [`ViscousFormulationLocalDG`](@ref) scheme is recommended.
 """
 struct ViscousFormulationBassiRebay1 end
 
