72191b3aa88fcecd365d0f4c51ca0b32f5384eb2
Jesse Chan <jesse.lee.chan@gmail.com>
2025-04-03 06:59:52 -0500

    changing elixir default initial_refinement_level

--- examples/tree_2d_dgsem/elixir_advection_diffusion_amr.jl
+++ examples/tree_2d_dgsem/elixir_advection_diffusion_amr.jl
@@ -15,7 +15,7 @@ solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 coordinates_min = (-1.0, -1.0)
 coordinates_max = (1.0, 1.0)
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level = 4,
+                initial_refinement_level = 2,
                 n_cells_max = 80_000)
 
 # Define initial condition
