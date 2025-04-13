6e03d8b04a8039adc612a8d20f315185e22e6da6
Benedict Geihe <bgeihe@uni-koeln.de>
2025-04-14 00:03:22 +0200

    P4est 2D mpi with noncons

--- src/solvers/dgsem_p4est/dg_2d_parallel.jl
+++ src/solvers/dgsem_p4est/dg_2d_parallel.jl
@@ -133,6 +133,38 @@ end
     end
 end
 
+# Inlined version of the interface flux computation for non-conservative equations
+@inline function calc_mpi_interface_flux!(surface_flux_values,
+                                          mesh::Union{ParallelP4estMesh{2},
+                                                      ParallelT8codeMesh{2}},
+                                          nonconservative_terms::True, equations,
+                                          surface_integral, dg::DG, cache,
+                                          interface_index, normal_direction,
+                                          interface_node_index, local_side,
+                                          surface_node_index, local_direction_index, local_element_index)
+    @unpack u = cache.mpi_interfaces
+    surface_flux, nonconservative_flux = surface_integral.surface_flux
+
+    u_ll, u_rr = get_surface_node_vars(u, equations, dg,
+                                       interface_node_index,
+                                       interface_index)
+
+    # Compute flux and non-conservative term for this side of the interface
+    if local_side == 1
+        flux_ = surface_flux(u_ll, u_rr, normal_direction, equations)
+        noncons_flux_ = nonconservative_flux(u_ll, u_rr, normal_direction, equations)
+    else # local_side == 2
+        flux_ = -surface_flux(u_ll, u_rr, -normal_direction, equations)
+        noncons_flux_ = -nonconservative_flux(u_rr, u_ll, -normal_direction, equations)
+    end
+
+    for v in eachvariable(equations)
+        surface_flux_values[v, surface_node_index,
+        local_direction_index, local_element_index] = flux_[v] +
+                                                      0.5f0 * noncons_flux_[v]
+    end
+end
+
 function prolong2mpimortars!(cache, u,
                              mesh::Union{ParallelP4estMesh{2}, ParallelT8codeMesh{2}},
                              equations,
@@ -273,6 +305,28 @@ end
 
     # Copy flux to buffer
     set_node_vars!(fstar[position_index], flux, equations, dg, node_index)
+end
+
+# Inlined version of the mortar flux computation on small elements for non-conservative equations
+@inline function calc_mpi_mortar_flux!(fstar,
+                                       mesh::Union{ParallelP4estMesh{2},
+                                                   ParallelT8codeMesh{2}},
+                                       nonconservative_terms::True, equations,
+                                       surface_integral, dg::DG, cache,
+                                       mortar_index, position_index, normal_direction,
+                                       node_index)
+    @unpack u = cache.mpi_mortars
+    surface_flux, nonconservative_flux = surface_integral.surface_flux
+
+    u_ll, u_rr = get_surface_node_vars(u, equations, dg, position_index, node_index,
+                                       mortar_index)
+
+    flux = surface_flux(u_ll, u_rr, normal_direction, equations)
+    noncons_flux = nonconservative_flux(u_ll, u_rr, normal_direction, equations)
+
+    for v in eachvariable(equations)
+        fstar[position_index][v, node_index] = flux[v] + 0.5f0 * noncons_flux[v]
+    end
 end
 
 @inline function mpi_mortar_fluxes_to_elements!(surface_flux_values,
