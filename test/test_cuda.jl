4d8a31f0a1f4e08cd72262e90313d862e64f40b1
Valentin Churavy <v.churavy@gmail.com>
2025-04-22 10:25:33 +0200

    add GPU construction test

--- test/test_cuda.jl
+++ test/test_cuda.jl
@@ -12,7 +12,29 @@ include("test_trixi.jl")
 outdir = "out"
 isdir(outdir) && rm(outdir, recursive = true)
 
-# TODO:
+EXAMPLES_DIR = joinpath(examples_dir(), "p4est_2d_dgsem")
+
+@trixi_testset "elixir_advection_basic.jl (Float32)" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
+                        # Expected errors are exactly the same as with TreeMesh!
+                        l2=[8.311947673061856e-6],
+                        linf=[6.627000273229378e-5],
+                        real_type=Float32,
+                        storage_type=CuArray)
+    # # Ensure that we do not have excessive memory allocations
+    # # (e.g., from type instabilities)
+    # let
+    #     t = sol.t[end]
+    #     u_ode = sol.u[end]
+    #     du_ode = similar(u_ode)
+    #     @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
+    # end
+    @test real(ode.p.solver) == Float32
+    @test real(ode.p.solver.basis) == Float32
+    @test real(ode.p.solver.mortar) == Float32
+    # TODO: remake ignores the mesh itself as well
+    @test real(ode.p.mesh) == Float64
+end
 
 # Clean up afterwards: delete Trixi.jl output directory
 @test_nowarn isdir(outdir) && rm(outdir, recursive = true)
