6fd484c82da580cd2dd3bf88a3c0cbc4097983cc
Arpit Babbar <arpitbabbar@gmail.com>
2025-04-25 04:29:00 +0200

    Mention non-conservative equations support

--- src/equations/passive_tracers.jl
+++ src/equations/passive_tracers.jl
@@ -9,7 +9,7 @@
     PassiveTracerEquations(flow_equations; n_tracers)
 
 Adds passive tracers to the `flow_equations`. The tracers are advected by the flow velocity.
-These work for arbitrary dimensions with arbitrary numbers of tracers `n_tracers`. For one dimension, with 
+These work for arbitrary dimensions with arbitrary numbers of tracers `n_tracers`, for conservative and non-conservative equations. For one dimension, with 
 one tracer ``\chi`` and flow with density and velocity ``\rho, v`` respectively, the equation of the
 passive tracer is
 ```math
