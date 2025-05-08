b22e519857881472082f0103284aa8f8adc5bf7d
Daniel Doehring <doehringd2@gmail.com>
2025-05-08 09:43:13 +0200

    Update src/equations/compressible_navier_stokes.jl

--- src/equations/compressible_navier_stokes.jl
+++ src/equations/compressible_navier_stokes.jl
@@ -3,8 +3,8 @@
     struct BoundaryConditionNavierStokesWall
 
 Creates a wall-type boundary conditions for the compressible Navier-Stokes equations, see
-[`CompressibleEulerEquations1D`](@ref), [`CompressibleEulerEquations2D`](@ref), and
-[`CompressibleEulerEquations3D`](@ref).
+[`CompressibleNavierStokesDiffusion1D`](@ref), [`CompressibleNavierStokesDiffusion2D`](@ref), and
+[`CompressibleNavierStokesDiffusion3D`](@ref).
 The fields `boundary_condition_velocity` and `boundary_condition_heat_flux` are intended
 to be boundary condition types such as the [`NoSlip`](@ref) velocity boundary condition and the
 [`Adiabatic`](@ref) or [`Isothermal`](@ref) heat boundary condition.
