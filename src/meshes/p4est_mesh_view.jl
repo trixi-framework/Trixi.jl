ae810f220f2cfcb8325b7d8e12fceafdfc46d563
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2025-06-12 15:53:32 +0100

    Update src/meshes/p4est_mesh_view.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/meshes/p4est_mesh_view.jl
+++ src/meshes/p4est_mesh_view.jl
@@ -240,7 +240,6 @@ end
 function global_element_id_to_local(id::Int, mesh::P4estMeshView)
     # Return -1 if the id is not part of the mesh view.
     local_id = -1
-    
     # Find the index of the cell id in the mesh view
     local_id = findfirst(==(id), mesh.cell_ids)
 
