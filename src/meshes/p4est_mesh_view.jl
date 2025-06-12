3208217f45e52259aadf3983aba7dc797c0cba9f
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2025-06-12 15:55:30 +0100

    Update src/meshes/p4est_mesh_view.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/meshes/p4est_mesh_view.jl
+++ src/meshes/p4est_mesh_view.jl
@@ -185,7 +185,8 @@ function extract_neighbor_ids_global(mesh::P4estMeshView, boundaries_parent,
                    node_indices_to_name(interfaces_parent.node_indices[matching_boundary,
                                                                        interface])
                     if global_id == interfaces_parent.neighbor_ids[1, interface]
-                        neighbor_ids_global[idx] = interfaces_parent.neighbor_ids[2, interface]
+                        neighbor_ids_global[idx] = interfaces_parent.neighbor_ids[2,
+                                                                                  interface]
                     else
                         neighbor_ids_global[idx] = interfaces_parent.neighbor_ids[1, interface]
                     end
