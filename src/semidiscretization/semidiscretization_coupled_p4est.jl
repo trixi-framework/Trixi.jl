ecee86a8189ff7d068ff03afaef834330f0feace
SimonCan <simon.candelaresi@gmail.com>
2025-06-16 17:25:04 +0100

    Removed further redundant definitions.

--- src/semidiscretization/semidiscretization_coupled_p4est.jl
+++ src/semidiscretization/semidiscretization_coupled_p4est.jl
@@ -204,22 +204,6 @@ end
 ### AnalysisCallback
 ################################################################################
 
-"""
-    AnalysisCallbackCoupled(semi, callbacks...)
-
-Combine multiple analysis callbacks for coupled simulations with a
-[`SemidiscretizationCoupled`](@ref). For each coupled system, an indididual
-[`AnalysisCallback`](@ref) **must** be created and passed to the `AnalysisCallbackCoupled` **in
-order**, i.e., in the same sequence as the indidvidual semidiscretizations are stored in the
-`SemidiscretizationCoupled`.
-
-!!! warning "Experimental code"
-    This is an experimental feature and can change any time.
-"""
-struct AnalysisCallbackCoupled{CB}
-    callbacks::CB
-end
-
 function Base.show(io::IO, ::MIME"text/plain",
                    cb_coupled::DiscreteCallback{<:Any, <:AnalysisCallbackCoupled})
     @nospecialize cb_coupled # reduce precompilation time
