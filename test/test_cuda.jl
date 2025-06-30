1fd6fe6614ebe799da375e3cf15569634ca4fb13
Valentin Churavy <v.churavy@gmail.com>
2025-06-30 21:12:08 +0200

    fix first GPU test

--- test/test_cuda.jl
+++ test/test_cuda.jl
@@ -1,12 +1,9 @@
 module TestCUDA
 
-using CUDA
 using Test
 using Trixi
 
 include("test_trixi.jl")
-
-# EXAMPLES_DIR = joinpath(examples_dir(), "dgmulti_1d")
 
 # Start with a clean environment: remove Trixi.jl output directory if it exists
 outdir = "out"
@@ -14,12 +11,17 @@ isdir(outdir) && rm(outdir, recursive = true)
 
 EXAMPLES_DIR = joinpath(examples_dir(), "p4est_2d_dgsem")
 
-@trixi_testset "elixir_advection_basic.jl (Float32)" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
+@trixi_testset "elixir_advection_basic_gpu.jl" begin
+    # Using CUDA inside the testset since otherwise the bindings are hiddend by the anonymous modules
+    using CUDA
+    # TODO(benegee/vchuravy): Port compute_coefficients! to KernelAbstractions.jl
+    CUDA.allowscalar(true)
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic_gpu.jl"),
                         # Expected errors are exactly the same as with TreeMesh!
-                        l2=[8.311947673061856e-6],
-                        linf=[6.627000273229378e-5],
-                        real_type=Float64,
+                        l2=nothing,   # [Float32(8.311947673061856e-6)],
+                        linf=nothing, # [Float32(6.627000273229378e-5)],
+                        RealT=Float32,
+                        real_type=Float32,
                         storage_type=CuArray)
     # # Ensure that we do not have excessive memory allocations
     # # (e.g., from type instabilities)
@@ -36,15 +38,12 @@ EXAMPLES_DIR = joinpath(examples_dir(), "p4est_2d_dgsem")
     @test real(ode.p.mesh) == Float64
 
     @test_broken ode.u0 isa CuArray
-    @test ode.p.basis.boundary_interpolations isa CuArray
-    @test ode.p.basis.derivative_matrix isa CuArray
-
-    @test ode.p.basis.forward_upper isa CuArray
+    @test ode.p.solver.basis.derivative_matrix isa CuArray
 
     @test Trixi.storage_type(ode.p.cache.elements) === CuArray
     @test Trixi.storage_type(ode.p.cache.interfaces) === CuArray
     @test Trixi.storage_type(ode.p.cache.boundaries) === CuArray
-    @test Trixi.storage_type(ode.p.cache.mortrar) === CuArray
+    @test Trixi.storage_type(ode.p.cache.mortars) === CuArray
 end
 
 # Clean up afterwards: delete Trixi.jl output directory
