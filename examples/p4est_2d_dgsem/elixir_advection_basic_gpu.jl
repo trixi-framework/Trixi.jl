68eb9052d11244268e1b1929295dec6bcfe8c070
Benedict Geihe <bgeihe@uni-koeln.de>
2025-07-01 15:16:07 +0200

    fmt

--- examples/p4est_2d_dgsem/elixir_advection_basic_gpu.jl
+++ examples/p4est_2d_dgsem/elixir_advection_basic_gpu.jl
@@ -53,6 +53,6 @@ callbacks = CallbackSet(summary_callback)
 # run the simulation
 
 # # OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
- sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false);
-             dt = 1e-2, # solve needs some value here but it will be overwritten by the stepsize_callback
-             ode_default_options()..., callback = callbacks);
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false);
+            dt = 1e-2, # solve needs some value here but it will be overwritten by the stepsize_callback
+            ode_default_options()..., callback = callbacks);
