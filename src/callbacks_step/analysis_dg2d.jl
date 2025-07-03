1081df072c15ad730202dbd60bf9316f2d0bc8af
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2025-07-03 15:45:43 +0100

    Update src/callbacks_step/analysis_dg2d.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/callbacks_step/analysis_dg2d.jl
+++ src/callbacks_step/analysis_dg2d.jl
@@ -286,24 +286,24 @@ function DGSpaceDerivative_WeakForm!(dg,
     l_minus = dg.basis.boundary_interpolation[:, 1]
     l_plus = dg.basis.boundary_interpolation[:, 2]
 
-#     nvars, Np = size(u)
-#     @autoinfiltrate
-#     F = similar(u)
-#     F_prime = similar(u)
-#
-#     # Multiply Dhat * F for each variable
-#     for k in 1:nvars
-#         for i in 1:Np
-#             F_prime[i, k] = sum(D[i, j] * F[j, k] for j in 1:Np)
-#         end
-#     end
-#
-#     # Store into gradients array
-#     for i in 1:Np
-#         for k in 1:nvars
-#             gradients[k, i] = F_prime[i, k]
-#         end
-#     end
+    #     nvars, Np = size(u)
+    #     @autoinfiltrate
+    #     F = similar(u)
+    #     F_prime = similar(u)
+    #
+    #     # Multiply Dhat * F for each variable
+    #     for k in 1:nvars
+    #         for i in 1:Np
+    #             F_prime[i, k] = sum(D[i, j] * F[j, k] for j in 1:Np)
+    #         end
+    #     end
+    #
+    #     # Store into gradients array
+    #     for i in 1:Np
+    #         for k in 1:nvars
+    #             gradients[k, i] = F_prime[i, k]
+    #         end
+    #     end
 
     n_vars, Np, _, n_elements = size(u)
     gradients = similar(u)
