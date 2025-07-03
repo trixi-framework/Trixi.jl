fe3e52bdf6aef1772587fe59b49b50961129ba2a
Valentin Churavy <v.churavy@gmail.com>
2025-07-03 16:34:01 +0200

    use unsafe_wrap with lock=false for AMDGPU

--- ext/TrixiAMDGPUExt.jl
+++ ext/TrixiAMDGPUExt.jl
@@ -8,4 +8,12 @@ function Trixi.storage_type(::Type{<:ROCArray})
     return ROCArray
 end
 
+function Trixi.unsafe_wrap_or_alloc(to::Type{<:ROCArray}, vector, size)
+    if length(vector) == 0
+        return similar(vector, size)
+    else
+        return unsafe_wrap(to, pointer(vector), size, lock=false)
+    end
+end
+
 end
