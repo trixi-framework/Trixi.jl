8c76db59a46333326e4a3fc81b61cf3eeaf40d38
Valentin Churavy <v.churavy@gmail.com>
2025-07-03 17:02:10 +0200

    Update ext/TrixiAMDGPUExt.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ext/TrixiAMDGPUExt.jl
+++ ext/TrixiAMDGPUExt.jl
@@ -12,7 +12,7 @@ function Trixi.unsafe_wrap_or_alloc(to::Type{<:ROCArray}, vector, size)
     if length(vector) == 0
         return similar(vector, size)
     else
-        return unsafe_wrap(to, pointer(vector), size, lock=false)
+        return unsafe_wrap(to, pointer(vector), size, lock = false)
     end
 end
 
