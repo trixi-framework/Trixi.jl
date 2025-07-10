e0f73ed54304d87fed95e1ba86bc4cb433c2fbd5
Steve <steve.bairos@rwth-aachen.de>
2025-07-10 12:04:30 +0200

    Elixir for implicit solver using sparse jacobian

--- src/semidiscretization/semidiscretization.jl
+++ src/semidiscretization/semidiscretization.jl
@@ -120,6 +120,25 @@ function semidiscretize(semi::AbstractSemidiscretization, tspan;
     return ODEProblem{iip, specialize}(rhs!, u0_ode, tspan, semi)
 end
 
+function semidiscretize(semi::AbstractSemidiscretization, jac_prototype, coloring, tspan;
+                        reset_threads = true)
+    # Optionally reset Polyester.jl threads. See
+    # https://github.com/trixi-framework/Trixi.jl/issues/1583
+    # https://github.com/JuliaSIMD/Polyester.jl/issues/30
+    if reset_threads
+        Polyester.reset_threads!()
+    end
+
+    u0_ode = compute_coefficients(first(tspan), semi)
+    # TODO: MPI, do we want to synchronize loading and print debug statements, e.g. using
+    #       mpi_isparallel() && MPI.Barrier(mpi_comm())
+    #       See https://github.com/trixi-framework/Trixi.jl/issues/328
+    iip = true # is-inplace, i.e., we modify a vector when calling rhs!
+    specialize = SciMLBase.FullSpecialize # specialize on rhs! and parameters (semi)
+    ode = SciMLBase.ODEFunction(rhs!, jac_prototype=jac_prototype, colorvec=coloring)
+    return ODEProblem{iip, specialize}(ode, u0_ode, tspan, semi)
+end
+
 """
     semidiscretize(semi::AbstractSemidiscretization, tspan, 
                    restart_file::AbstractString)
