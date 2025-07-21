889ed359c8780c77d9bd0d2c970a9c5deac2937d
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-07-21 17:51:25 -0400

    clean up indicators

--- src/solvers/dgsem_tree/indicators_1d.jl
+++ src/solvers/dgsem_tree/indicators_1d.jl
@@ -20,7 +20,7 @@ end
 
 # this method is used when the indicator is constructed as for AMR
 function create_cache(typ::Type{IndicatorHennemannGassner}, mesh,
-                      equations::AbstractEquations{1}, dg::DGSEM, cache)
+                      equations::AbstractEquations, dg::DGSEM, cache)
     create_cache(typ, equations, dg.basis)
 end
 
@@ -109,7 +109,7 @@ function apply_smoothing!(mesh::TreeMesh{1}, alpha, alpha_tmp, dg, cache)
 end
 
 # this method is used when the indicator is constructed as for shock-capturing volume integrals
-function create_cache(::Type{IndicatorLöhner}, equations::AbstractEquations{1},
+function create_cache(::Type{IndicatorLöhner}, equations::AbstractEquations,
                       basis::LobattoLegendreBasis)
     alpha = Vector{real(basis)}()
 
@@ -120,7 +120,7 @@ function create_cache(::Type{IndicatorLöhner}, equations::AbstractEquations{1},
 end
 
 # this method is used when the indicator is constructed as for AMR
-function create_cache(typ::Type{IndicatorLöhner}, mesh, equations::AbstractEquations{1},
+function create_cache(typ::Type{IndicatorLöhner}, mesh, equations::AbstractEquations,
                       dg::DGSEM, cache)
     create_cache(typ, equations, dg.basis)
 end
@@ -159,7 +159,7 @@ function (löhner::IndicatorLöhner)(u::AbstractArray{<:Any, 3},
 end
 
 # this method is used when the indicator is constructed as for shock-capturing volume integrals
-function create_cache(::Type{IndicatorMax}, equations::AbstractEquations{1},
+function create_cache(::Type{IndicatorMax}, equations::AbstractEquations,
                       basis::LobattoLegendreBasis)
     alpha = Vector{real(basis)}()
 
@@ -170,7 +170,7 @@ function create_cache(::Type{IndicatorMax}, equations::AbstractEquations{1},
 end
 
 # this method is used when the indicator is constructed as for AMR
-function create_cache(typ::Type{IndicatorMax}, mesh, equations::AbstractEquations{1},
+function create_cache(typ::Type{IndicatorMax}, mesh, equations::AbstractEquations,
                       dg::DGSEM, cache)
     cache = create_cache(typ, equations, dg.basis)
 end
