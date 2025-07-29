9d97bcc0fae335852a146a8bbaeb652aee7ea46c
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-07-29 11:38:50 +0200

    remove redundant comments

--- src/semidiscretization/semidiscretization.jl
+++ src/semidiscretization/semidiscretization.jl
@@ -222,20 +222,8 @@ function semidiscretize(semi::AbstractSemidiscretization, tspan,
     iip = true # is-inplace, i.e., we modify a vector when calling rhs!
     specialize = SciMLBase.FullSpecialize # specialize on rhs! and parameters (semi)
 
-    # See SparseDiffTools.jl docs (https://github.com/JuliaDiff/SparseDiffTools.jl) for documentation of `jac_prototype` and `colorvec`
-
-    # Convert the `jac_prototype` to real type, as seen here:
-    # https://docs.sciml.ai/DiffEqDocs/stable/tutorials/advanced_ode_example/#Declaring-a-Sparse-Jacobian-with-Automatic-Sparsity-Detection
     ode = SciMLBase.ODEFunction(rhs!, jac_prototype = float.(jac_prototype),
                                 colorvec = colorvec)
-
-    # Note: We experimented for linear problems with providing the constant, sparse Jacobian directly via
-    #
-    # const jac_sparse = sparse_jacobian(sparse_adtype, sparse_cache, rhs, du_ode, u0_ode)
-    # const jac_sparse_func!(J, u, p, t) = jac_sparse
-    # SciMLBase.ODEFunction(rhs!, jac_prototype=float.(jac_prototype), colorvec=colorvec, jac = jac_sparse_func!)
-    #
-    # which turned out to be significantly slower than just using the prototype and the coloring vector. 
 
     return ODEProblem{iip, specialize}(ode, u0_ode, tspan, semi)
 end
