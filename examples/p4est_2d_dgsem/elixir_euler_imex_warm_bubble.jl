b4fe4eae68710ba4b4353a27c155824529b32202
Marco Artiano <artianomarco97@gmail.com>
2025-08-11 14:34:29 +0200

    testing some IMEX schemes

--- examples/p4est_2d_dgsem/elixir_euler_imex_warm_bubble.jl
+++ examples/p4est_2d_dgsem/elixir_euler_imex_warm_bubble.jl
@@ -1,136 +1,151 @@
 using Trixi
-using OrdinaryDiffEqSDIRK
+using OrdinaryDiffEqBDF
+using OrdinaryDiffEqIMEXMultistep
 using SparseDiffTools ## This is needed to force 'autodiff = AutoFiniteDiff()' in the ODE solver.
 
 function initial_condition_warm_bubble(x, t, equations::CompressibleEulerEquations2D)
-        g = 9.81
-        c_p = 1004.0
-        c_v = 717.0
+	g = 9.81
+	c_p = 1004.0
+	c_v = 717.0
 
-        # center of perturbation
-        center_x = 10000.0
-        center_z = 2000.0
-        # radius of perturbation
-        radius = 2000.0
-        # distance of current x to center of perturbation
-        r = sqrt((x[1] - center_x)^2 + (x[2] - center_z)^2)
+	# center of perturbation
+	center_x = 10000.0
+	center_z = 2000.0
+	# radius of perturbation
+	radius = 2000.0
+	# distance of current x to center of perturbation
+	r = sqrt((x[1] - center_x)^2 + (x[2] - center_z)^2)
 
-        # perturbation in potential temperature
-        potential_temperature_ref = 300.0
-        potential_temperature_perturbation = 0.0
-        if r <= radius
-            potential_temperature_perturbation = 2 * cospi(0.5 * r / radius)^2
-        end
-        potential_temperature = potential_temperature_ref + potential_temperature_perturbation
+	# perturbation in potential temperature
+	potential_temperature_ref = 300.0
+	potential_temperature_perturbation = 0.0
+	if r <= radius
+		potential_temperature_perturbation = 2 * cospi(0.5 * r / radius)^2
+	end
+	potential_temperature = potential_temperature_ref + potential_temperature_perturbation
 
-        # Exner pressure, solves hydrostatic equation for x[2]
-        exner = 1 - g / (c_p * potential_temperature) * x[2]
+	# Exner pressure, solves hydrostatic equation for x[2]
+	exner = 1 - g / (c_p * potential_temperature) * x[2]
 
-        # pressure
-        p_0 = 100_000.0  # reference pressure
-        R = c_p - c_v    # gas constant (dry air)
-        p = p_0 * exner^(c_p / R)
+	# pressure
+	p_0 = 100_000.0  # reference pressure
+	R = c_p - c_v    # gas constant (dry air)
+	p = p_0 * exner^(c_p / R)
 
-        # temperature
-        T = potential_temperature * exner
+	# temperature
+	T = potential_temperature * exner
 
-        # density
-        rho = p / (R * T)
+	# density
+	rho = p / (R * T)
 
-        v1 = 20.0
-        v2 = 0.0
-        return prim2cons(SVector(rho, v1, v2, p), equations)
+	v1 = 20.0
+	v2 = 0.0
+	return prim2cons(SVector(rho, v1, v2, p), equations)
 end
 
 @inline function boundary_condition_slip_wall_vel(u_inner, normal_direction::AbstractVector,
-        x, t,
-        surface_flux_function,
-        equations::CompressibleEulerEquations2D)
-        # normalize the outward pointing direction
-        normal = normal_direction / Trixi.norm(normal_direction)
+	x, t,
+	surface_flux_function,
+	equations::CompressibleEulerEquations2D)
+	# normalize the outward pointing direction
+	normal = normal_direction / Trixi.norm(normal_direction)
 
-        # compute the normal velocity
-        u_normal = normal[1] * u_inner[2] + normal[2] * u_inner[3]
+	# compute the normal velocity
+	u_normal = normal[1] * u_inner[2] + normal[2] * u_inner[3]
 
-        # create the "external" boundary solution state
-        u_boundary = SVector(u_inner[1],
-            u_inner[2] - 2 * u_normal * normal[1],
-            u_inner[3] - 2 * u_normal * normal[2],
-            u_inner[4])
-        
-        # calculate the boundary flux
-        flux = surface_flux_function(u_inner, u_boundary, normal_direction, equations)
-        
-        return flux
+	# create the "external" boundary solution state
+	u_boundary = SVector(u_inner[1],
+		u_inner[2] - 2 * u_normal * normal[1],
+		u_inner[3] - 2 * u_normal * normal[2],
+		u_inner[4])
+
+	# calculate the boundary flux
+	flux = surface_flux_function(u_inner, u_boundary, normal_direction, equations)
+
+	return flux
 end
-    @inline function flux_zero(u_ll, u_rr, normal_direction,
-        equations::CompressibleEulerEquations2D)
-        return SVector(0.0, 0.0, 0.0, 0.0)
-    end
 
-      @inline function source_terms_gravity(u, x, t, equations::CompressibleEulerEquations2D)
-        g = 9.81
-        rho, _, rho_v2, _ = u
-        return SVector(zero(eltype(u)), zero(eltype(u)), -g * rho, - g* rho_v2)
-    end
+@inline function boundary_condition_zero(u_inner, normal_direction::AbstractVector,
+	x, t,
+	surface_flux_function,
+	equations::CompressibleEulerEquations2D)
 
-    equations = CompressibleEulerEquations2D(1004/717)
+	flux = flux_zero(u_inner, u_inner, normal_direction, equations)
 
-    polydeg = 2
-    basis = LobattoLegendreBasis(polydeg)
+	return flux
+end
 
-    volume_integral_explicit = VolumeIntegralFluxDifferencing(flux_kennedy_gruber)
+@inline function flux_zero(u_ll, u_rr, normal_direction,
+	equations::CompressibleEulerEquations2D)
+	return SVector(0.0, 0.0, 0.0, 0.0)
+end
 
-    solver_explicit = DGSEM(basis, FluxLMARS(340.0), volume_integral_explicit)
-    volume_integral_implicit = VolumeIntegralFluxDifferencing(flux_zero)
+@inline function source_terms_gravity(u, x, t, equations::CompressibleEulerEquations2D)
+	g = 9.81
+	rho, _, rho_v2, _ = u
+	return SVector(zero(eltype(u)), zero(eltype(u)), -g * rho, -g * rho_v2)
+end
 
-    solver_implicit = DGSEM(basis, flux_zero, volume_integral_implicit)
-    coordinates_min = (0.0, 0.0)
-    coordinates_max = (20_000.0, 10_000.0)
+equations = CompressibleEulerEquations2D(1004 / 717)
 
-    trees_per_dimension = (16, 16)
+polydeg = 2
+basis = LobattoLegendreBasis(polydeg)
 
-    mesh = P4estMesh(trees_per_dimension, polydeg = polydeg,
-        coordinates_min = coordinates_min, coordinates_max = coordinates_max,
-        periodicity = (true, false), initial_refinement_level = 0)
+volume_integral_explicit = VolumeIntegralFluxDifferencing(flux_kennedy_gruber)
+solver_explicit = DGSEM(basis, FluxLMARS(340.0), volume_integral_explicit)
 
-    boundary_conditions = Dict(:y_neg => boundary_condition_slip_wall_vel,
-                            :y_pos => boundary_condition_slip_wall_vel)
+volume_integral_implicit = VolumeIntegralFluxDifferencing(flux_zero)
+solver_implicit = DGSEM(basis, flux_zero, volume_integral_implicit)
 
-    initial_condition = initial_condition_warm_bubble
+coordinates_min = (0.0, 0.0)
+coordinates_max = (20_000.0, 10_000.0)
 
-    semi = SemidiscretizationHyperbolicSplit(
-        mesh,
-        (equations, equations),
-        initial_condition,
-        solver_implicit,
-        solver_explicit;
-        boundary_conditions = (boundary_conditions, boundary_conditions),
-        source_terms = (source_terms_gravity, nothing),
-    )
-    T = 1000.0
-    tspan = (0.0, T)
-    ode = semidiscretize(semi, tspan)
+trees_per_dimension = (16, 16)
 
-    summary_callback = SummaryCallback()
+mesh = P4estMesh(trees_per_dimension, polydeg = polydeg,
+	coordinates_min = coordinates_min, coordinates_max = coordinates_max,
+	periodicity = (true, false), initial_refinement_level = 0)
 
-    # The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
-    analysis_callback = AnalysisCallback(semi, interval = 100)
+boundary_conditions_explicit = Dict(:y_neg => boundary_condition_slip_wall_vel,
+	:y_pos => boundary_condition_slip_wall_vel)
+boundary_conditions_implicit = Dict(:y_neg => boundary_condition_zero,
+	:y_pos => boundary_condition_zero)
 
-    # The SaveSolutionCallback allows to save the solution to a file in regular intervals
-    save_solution = SaveSolutionCallback(interval = 100, solution_variables = cons2prim)
+initial_condition = initial_condition_warm_bubble
 
-    # Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
-    callbacks = CallbackSet(summary_callback, analysis_callback, save_solution)
+semi = SemidiscretizationHyperbolicSplit(
+	mesh,
+	(equations, equations),
+	initial_condition,
+	solver_implicit,
+	solver_explicit;
+	boundary_conditions = (boundary_conditions_implicit, boundary_conditions_explicit),
+	source_terms = (source_terms_gravity, nothing),
+)
+T = 10.0
+tspan = (0.0, T)
+ode = semidiscretize(semi, tspan)
 
-    ###############################################################################
-    # run the simulation
-    # OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
-    sol = solve(
-        ode,
-        KenCarp3(autodiff = AutoFiniteDiff());
-        abstol = 1.0e-6, reltol = 1.0e-6,
-        dt = 1e-3, # solve needs some value here but it will be overwritten by the stepsize_callback
-        save_everystep = false,
-        callback = callbacks,
-    );
+summary_callback = SummaryCallback()
+
+# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
+analysis_callback = AnalysisCallback(semi, interval = 100)
+
+# The SaveSolutionCallback allows to save the solution to a file in regular intervals
+save_solution = SaveSolutionCallback(interval = 100, solution_variables = cons2prim)
+
+stepsize_callback = StepsizeCallback(cfl = 0.5)
+
+# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
+callbacks = CallbackSet(summary_callback, analysis_callback, save_solution, stepsize_callback)
+
+###############################################################################
+# run the simulation
+# OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
+sol = solve(
+	ode,
+	SBDF2(autodiff = AutoFiniteDiff());
+	dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+	save_everystep = false,
+	callback = callbacks,
+);
