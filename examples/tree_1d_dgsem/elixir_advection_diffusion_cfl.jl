ce700b4a5092ae5e9f1bb25cfdac484555261192
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-08-22 13:36:49 +0200

    convert

--- examples/tree_1d_dgsem/elixir_advection_diffusion_cfl.jl
+++ examples/tree_1d_dgsem/elixir_advection_diffusion_cfl.jl
@@ -13,8 +13,8 @@ equations_parabolic = LaplaceDiffusion1D(diffusivity(), equations)
 # Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
 solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
-coordinates_min = -pi # minimum coordinate
-coordinates_max = pi # maximum coordinate
+coordinates_min = -convert(Float64, pi) # minimum coordinate
+coordinates_max = convert(Float64, pi) # maximum coordinate
 
 # Create a uniformly refined mesh with periodic boundaries
 mesh = TreeMesh(coordinates_min, coordinates_max,
