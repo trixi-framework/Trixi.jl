f4f13d34c226e542184df588cebb0de5e6392a5e
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-08-25 09:10:05 +0200

    shorten

--- examples/tree_1d_dgsem/elixir_advection_diffusion_cfl.jl
+++ examples/tree_1d_dgsem/elixir_advection_diffusion_cfl.jl
@@ -10,11 +10,10 @@ equations = LinearScalarAdvectionEquation1D(advection_velocity)
 diffusivity() = 0.1
 equations_parabolic = LaplaceDiffusion1D(diffusivity(), equations)
 
-# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
 solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
-coordinates_min = -convert(Float64, pi) # minimum coordinate
-coordinates_max = convert(Float64, pi) # maximum coordinate
+coordinates_min = -convert(Float64, pi)
+coordinates_max = convert(Float64, pi)
 
 # Create a uniformly refined mesh with periodic boundaries
 mesh = TreeMesh(coordinates_min, coordinates_max,
@@ -30,7 +29,6 @@ function x_trans_periodic(x, domain_length = SVector(2 * pi), center = SVector(0
     return center + x_shifted + x_offset
 end
 
-# Define initial condition
 function initial_condition_diffusive_convergence_test(x, t,
                                                       equation::LinearScalarAdvectionEquation1D)
     # Store translated coordinate for easy use of exact solution
@@ -47,32 +45,19 @@ function initial_condition_diffusive_convergence_test(x, t,
 end
 initial_condition = initial_condition_diffusive_convergence_test
 
-# define periodic boundary conditions everywhere
-boundary_conditions = boundary_condition_periodic
-boundary_conditions_parabolic = boundary_condition_periodic
-
-# A semidiscretization collects data structures and functions for the spatial discretization
 semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic),
-                                             initial_condition,
-                                             solver;
-                                             boundary_conditions = (boundary_conditions,
-                                                                    boundary_conditions_parabolic))
+                                             initial_condition, solver)
 
 ###############################################################################
 # ODE solvers, callbacks etc.
 
-# Create ODE problem with time span from 0.0 to 1.0
 tspan = (0.0, 1.0)
 ode = semidiscretize(semi, tspan);
 
-# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
-# and resets the timers
 summary_callback = SummaryCallback()
 
-# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
 analysis_callback = AnalysisCallback(semi, interval = 100)
 
-# The AliveCallback prints short status information in regular intervals
 alive_callback = AliveCallback(analysis_interval = 100)
 
 # Stepsize callback which selects the timestep according to the most restrictive CFL condition.
@@ -81,14 +66,12 @@ alive_callback = AliveCallback(analysis_interval = 100)
 stepsize_callback = StepsizeCallback(cfl = 1.6,
                                      cfl_diffusive = 0.4)
 
-# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
 callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback,
                         stepsize_callback)
 
 ###############################################################################
 # run the simulation
 
-# OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
 sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
             dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep = false, callback = callbacks);
