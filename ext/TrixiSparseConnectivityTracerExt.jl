a9335b2ea43d9a284b6d308a5998b60260b965e7
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-08-27 13:55:00 +0200

    typo

--- ext/TrixiSparseConnectivityTracerExt.jl
+++ ext/TrixiSparseConnectivityTracerExt.jl
@@ -4,7 +4,7 @@ module TrixiSparseConnectivityTracerExt
 import Trixi
 import SparseConnectivityTracer: AbstractTracer
 
-# For the defaul package preference "sqrt_Trixi_NaN" we overload the `Base.sqrt` function
+# For the default package preference "sqrt_Trixi_NaN" we overload the `Base.sqrt` function
 # to first check if the argument is < 0 and then return `NaN` instead of an error.
 # To turn this behaviour off for the datatype `AbstractTracer` used in sparsity detection,
 # we switch back to the Base implementation here which does not contain an if-clause.
