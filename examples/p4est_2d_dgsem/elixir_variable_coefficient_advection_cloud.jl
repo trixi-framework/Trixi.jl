9abed106166d30aebbf726de4c7c6ba9dbfbc713
Benedict Geihe <bgeihe@uni-koeln.de>
2025-08-29 14:06:39 +0200

    add experimental variable coefficient advection equation

---
+++ examples/p4est_2d_dgsem/elixir_variable_coefficient_advection_cloud.jl
@@ -0,0 +1,134 @@
+using OrdinaryDiffEqLowStorageRK
+using Trixi
+using Plots
+
+##############################################################################################
+# initial condition
+
+equations = LinearVariableScalarAdvectionEquation2D()
+
+# initial condition, round density cloud is defined
+@inline function initial_condition_schaer_mountain_cloud(x, t, equations)
+    RealT = eltype(x)
+    x_0, z_0 = -50000.0f0, 9000.0f0
+    rho_0 = 1.0f0
+    A_x, A_z = 25000.0f0, 3000.0f0
+
+    r = sqrt(((x[1] - x_0) / A_x)^2 + ((x[2] - z_0) / A_z)^2)
+
+    if r <= 1
+        rho = convert(RealT, rho_0 * (cos((pi * r) / 2))^2)
+    else
+        rho = 0.0f0
+    end
+    return SVector(rho)
+end
+
+# wind profile in horizontal direktion
+@inline function velocity_schaer_mountain(x, equations)
+    RealT = eltype(x)
+    u_0 = 10.0f0
+    z_1 = 4000.0f0
+    z_2 = 5000.0f0
+
+    if x[2] <= z_1
+        u_1 = 0.0f0
+    elseif z_2 <= x[2]
+        u_1 = 1.0f0
+    else
+        u_1 = convert(RealT, sin((pi / 2) * (x[2] - z_1) / (z_2 - z_1))^2)
+    end
+    return SVector(u_1 * u_0, 0.0f0)
+end
+
+##############################################################################################
+# semidiscretization
+
+polydeg = 3
+
+# P4est HOHQ mesh 
+mesh_file = joinpath(@__DIR__, "schaer_mountain_advection.inp")
+mesh = P4estMesh{2}(mesh_file, polydeg = polydeg)
+
+initial_condition = initial_condition_schaer_mountain_cloud
+
+# flux and solver 
+surface_flux = flux_lax_friedrichs
+
+solver = DGSEM(polydeg = polydeg,
+               surface_flux = surface_flux,
+               volume_integral = VolumeIntegralWeakForm())
+
+# boundary conditions 
+
+boundary_conditions_dirichlet = Dict(:left => BoundaryConditionDirichlet(initial_condition),
+                                     :right => BoundaryConditionDirichlet(initial_condition),
+                                     :top => BoundaryConditionDirichlet(initial_condition),
+                                     :bottom => BoundaryConditionDirichlet(initial_condition),
+                                     :bottom_left => BoundaryConditionDirichlet(initial_condition),
+                                     :bottom_right => BoundaryConditionDirichlet(initial_condition))
+
+#=
+boundary_conditions_dirichlet = Dict(:left => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain),
+                                     :right => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain),
+                                     :bottom => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain),#boundary_condition_slip_wall,
+                                     :top => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain), #boundary_condition_slip_wall,
+                                     :bottom_left => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain), 
+                                     :bottom_right => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain))
+                                     #:bottom_left_connection => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain),
+                                     #:bottom_right_connection => BoundaryConditionDirichletAux(initial_condition_schaer_mountain_cloud, velocity_schaer_mountain))
+=#
+
+# the velocity is passed as auxiliary_field into the cache
+semi = SemidiscretizationHyperbolic(mesh,
+                                    equations,
+                                    initial_condition,
+                                    solver,
+                                    boundary_conditions = boundary_conditions_dirichlet,
+                                    aux_field = velocity_schaer_mountain)
+
+##############################################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 5000.0)
+
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 1000
+solution_variables = cons2prim
+
+analysis_callback = AnalysisCallback(semi,
+                                     interval = analysis_interval,
+                                     extra_analysis_errors = (:entropy_conservation_error,))
+
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
+
+save_solution = SaveSolutionCallback(interval = 1,
+                                     save_initial_solution = true,
+                                     save_final_solution = true,
+                                     output_directory = "out",
+                                     solution_variables = solution_variables)
+
+stepsize_callback = StepsizeCallback(cfl = 0.1)
+
+visualization = VisualizationCallback(interval = 10)
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback,
+                        alive_callback,
+                        save_solution,
+                        #visualization,
+                        stepsize_callback)
+
+###############################################################################
+# run the simulation
+sol = solve(ode,
+            CarpenterKennedy2N54(williamson_condition = false),
+            maxiters = 1.0e7,
+            dt = 0.01, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false,
+            callback = callbacks);
+
+summary_callback()
---
+++ src/equations/linear_variable_scalar_advection_2d.jl
@@ -0,0 +1,43 @@
+@muladd begin
+struct LinearVariableScalarAdvectionEquation2D{} <:
+        AbstractLinearScalarAdvectionEquation{2, 1} end
+
+varnames(::typeof(cons2cons), ::LinearVariableScalarAdvectionEquation2D) = ("scalar",)
+varnames(::typeof(cons2prim), ::LinearVariableScalarAdvectionEquation2D) = ("scalar",)
+varnames(::typeof(cons2aux), ::LinearVariableScalarAdvectionEquation2D) = ("v1", "v2")
+
+have_aux_node_vars(::LinearVariableScalarAdvectionEquation2D) = True()
+n_aux_node_vars(::LinearVariableScalarAdvectionEquation2D) = 2
+
+@inline function flux(u, aux_vars, orientation::Integer,
+                        equations::LinearVariableScalarAdvectionEquation2D)
+    a = aux_vars[orientation]
+    return a * u
+end
+
+@inline function flux(u, aux_vars, normal_direction::AbstractVector,
+                        equation::LinearVariableScalarAdvectionEquation2D)
+    a = dot(aux_vars, normal_direction) # velocity in normal direction
+    return a * u
+end
+
+# Calculate maximum wave speed for local Lax-Friedrichs-type dissipation
+@inline function max_abs_speed_naive(u_ll, u_rr, aux_ll, aux_rr,
+                                        normal_direction::AbstractVector,
+                                        equation::LinearVariableScalarAdvectionEquation2D)
+    # velocity in normal direction
+    v_ll = dot(aux_ll, normal_direction)
+    v_rr = dot(aux_rr, normal_direction)
+    return max(abs(v_ll), abs(v_rr))
+end
+
+# Maximum wave speeds in each direction for CFL calculation
+@inline function Trixi.max_abs_speeds(u, aux_vars,
+                                        equations::LinearVariableScalarAdvectionEquation2D)
+    return abs.(aux_vars)
+end
+
+@inline cons2entropy(u, equations::LinearVariableScalarAdvectionEquation2D) = u
+@inline cons2prim(u, aux, equations::LinearVariableScalarAdvectionEquation2D) = u
+@inline cons2prim(u, equations::LinearVariableScalarAdvectionEquation2D) = u
+end
