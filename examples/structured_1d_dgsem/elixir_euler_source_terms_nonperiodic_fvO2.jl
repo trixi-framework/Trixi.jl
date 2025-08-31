eb18089e93ca899437a9c3e6e7ece71f0791999d
Daniel Doehring <doehringd2@gmail.com>
2025-08-31 22:55:11 +0200

    Update examples/structured_1d_dgsem/elixir_euler_source_terms_nonperiodic_fvO2.jl

--- examples/structured_1d_dgsem/elixir_euler_source_terms_nonperiodic_fvO2.jl
+++ examples/structured_1d_dgsem/elixir_euler_source_terms_nonperiodic_fvO2.jl
@@ -22,7 +22,7 @@ basis = LobattoLegendreBasis(polydeg)
 surface_flux = flux_hll
 volume_integral = VolumeIntegralPureLGLFiniteVolumeO2(basis,
                                                       volume_flux_fv = surface_flux,
-                                                      reconstruction_mode = reconstruction_O2_full,
+                                                      reconstruction_mode = reconstruction_O2_inner,
                                                       slope_limiter = vanLeer)
 solver = DGSEM(polydeg = polydeg, surface_flux = surface_flux,
                volume_integral = volume_integral)
