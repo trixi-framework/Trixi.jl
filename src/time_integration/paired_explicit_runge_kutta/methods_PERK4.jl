bbfa5c52df299ae4effa61a12a242ad8138e65ba
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2025-09-01 17:46:46 +0200

    `uType <: AbstractVector` for code verbosity (#2539)
    * uType <: AbstractVector for code verbosity
    
    * revert for ssp
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/time_integration/paired_explicit_runge_kutta/methods_PERK4.jl
+++ src/time_integration/paired_explicit_runge_kutta/methods_PERK4.jl
@@ -184,7 +184,8 @@ end
 # This implements the interface components described at
 # https://diffeq.sciml.ai/v6.8/basics/integrator/#Handing-Integrators-1
 # which are used in Trixi.jl.
-mutable struct PairedExplicitRK4Integrator{RealT <: Real, uType, Params, Sol, F,
+mutable struct PairedExplicitRK4Integrator{RealT <: Real, uType <: AbstractVector,
+                                           Params, Sol, F,
                                            PairedExplicitRKOptions} <:
                AbstractPairedExplicitRKSingleIntegrator
     u::uType
