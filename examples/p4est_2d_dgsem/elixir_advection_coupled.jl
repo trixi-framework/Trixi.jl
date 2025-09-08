2e18b0309b70c79b86454e4cff96311659778e2e
SimonCan <simon.candelaresi@gmail.com>
2025-09-08 15:41:07 +0100

    Changed boundary coupling functions to array.

--- examples/p4est_2d_dgsem/elixir_advection_coupled.jl
+++ examples/p4est_2d_dgsem/elixir_advection_coupled.jl
@@ -34,12 +34,19 @@ mesh2 = P4estMeshView(parent_mesh, cell_ids2)
 # Define a trivial coupling function.
 coupling_function = (x, u, equations_other, equations_own) -> u
 
+# Define a coupling function for each combination of interfaces.
+coupling_functions = Array{Function}(undef, 2, 2)
+coupling_functions[1, 1] = (x, u, equations_other, equations_own) -> u
+coupling_functions[1, 2] = (x, u, equations_other, equations_own) -> u
+coupling_functions[2, 1] = (x, u, equations_other, equations_own) -> u
+coupling_functions[2, 2] = (x, u, equations_other, equations_own) -> u
+
 # The mesh is coupled across the physical boundaries, which makes this setup
 # effectively double periodic.
-boundary_conditions = Dict(:x_neg => BoundaryConditionCoupledP4est(coupling_function),
-                           :y_neg => BoundaryConditionCoupledP4est(coupling_function),
-                           :y_pos => BoundaryConditionCoupledP4est(coupling_function),
-                           :x_pos => BoundaryConditionCoupledP4est(coupling_function))
+boundary_conditions = Dict(:x_neg => BoundaryConditionCoupledP4est(coupling_functions),
+                           :y_neg => BoundaryConditionCoupledP4est(coupling_functions),
+                           :y_pos => BoundaryConditionCoupledP4est(coupling_functions),
+                           :x_pos => BoundaryConditionCoupledP4est(coupling_functions))
 
 semi1 = SemidiscretizationHyperbolic(mesh1, equations, initial_condition_convergence_test,
                                      solver,
