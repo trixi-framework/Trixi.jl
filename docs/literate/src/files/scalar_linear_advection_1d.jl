a0950906cafa31df945a4371feb3f7969e6cb5d3
Collin Wittenstein <cwittens@students.uni-mainz.de>
2025-09-12 16:32:51 -0400

    small changes

--- docs/literate/src/files/scalar_linear_advection_1d.jl
+++ docs/literate/src/files/scalar_linear_advection_1d.jl
@@ -128,7 +128,7 @@ u0 = initial_condition_sine_wave.(x)
 
 # To have a look at the initial sinus curve, we plot it.
 using Plots
-plot(vec(x), vec(u0), label = "initial condition", legend = :topleft)
+plot(vec(x), vec(u0), label = "initial condition", legend = :topleft, markershape = :circle)
 
 # ### iii. Variational formulation
 # After defining the equation and initial condition, we want to implement an algorithm to
@@ -366,7 +366,7 @@ solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
 # We will now create a mesh with 16 elements for the physical domain `[-1, 1]` with periodic boundaries.
 # We use Trixi.jl's standard mesh [`TreeMesh`](@ref). Since it's limited to hypercube domains, we
-# choose `2^4=16` elements. The mesh type supports AMR, that' why `n_cells_max` has to be set, even
+# choose `2^4=16` elements. The mesh type supports Adaptive Mesh Refinement (AMR), that's why `n_cells_max` has to be set, even
 # if we don't need AMR here.
 coordinates_min = -1.0 # minimum coordinate
 coordinates_max = 1.0  # maximum coordinate
@@ -429,7 +429,7 @@ end
 initial_condition_sine_wave(x) = 1.0 + 0.5 * sin(pi * x)
 u0 = initial_condition_sine_wave.(x)
 
-plot(vec(x), vec(u0), label = "initial condition", legend = :topleft)
+plot(vec(x), vec(u0), label = "initial condition", legend = :topleft, markershape = :circle)
 
 ## flux Lax-Friedrichs
 surface_flux = flux_lax_friedrichs
