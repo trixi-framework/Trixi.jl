7646f31d53bee48227eb068ae0b1529a74da9322
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-09-22 09:56:16 +0200

    use at-test_allocations in new test

--- test/test_p4est_2d.jl
+++ test/test_p4est_2d.jl
@@ -161,12 +161,7 @@ end
                         tspan=(0.0, 0.1))
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
-    let
-        t = sol.t[end]
-        u_ode = sol.u[end]
-        du_ode = similar(u_ode)
-        @test (@allocated Trixi.rhs!(du_ode, u_ode, semi, t)) < 1000
-    end
+    @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
 @trixi_testset "elixir_euler_source_terms_nonconforming_unstructured_flag.jl" begin
