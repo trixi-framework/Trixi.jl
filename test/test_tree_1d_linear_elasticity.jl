797e9b01df2b65f1d2266a28afbf165552aa94f5
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-09-25 15:19:32 +0200

    ci

--- test/test_tree_1d_linear_elasticity.jl
+++ test/test_tree_1d_linear_elasticity.jl
@@ -16,7 +16,10 @@ EXAMPLES_DIR = joinpath(examples_dir(), "tree_1d_dgsem")
                         analysis_callback=AnalysisCallback(semi,
                                                            interval = analysis_interval,
                                                            extra_analysis_errors = (:l2_error_primitive,
-                                                                                    :linf_error_primitive)),
+                                                                                    :linf_error_primitive),
+                                                           extra_analysis_integrals = (energy_kinetic,
+                                                                                       energy_internal,
+                                                                                       entropy)),
                         l2=[0.0007205516785218745, 0.0008036755866155103],
                         linf=[0.0011507266875070855, 0.003249818227066381])
     # Ensure that we do not have excessive memory allocations
