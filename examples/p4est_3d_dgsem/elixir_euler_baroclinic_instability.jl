329d5f115c2e02114f3f5a4d768d909832656e82
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2025-09-25 08:02:12 +0000

    export `P4estMeshCubedSphere` (#2583)
    * export P4estCubedSphere
    
    * remove Trixi. from examples
    
    * Update examples/p4est_3d_dgsem/elixir_euler_source_terms_nonconforming_earth.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update examples/p4est_3d_dgsem/elixir_advection_cubed_sphere.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update examples/p4est_3d_dgsem/elixir_euler_baroclinic_instability.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update examples/p4est_3d_dgsem/elixir_euler_circular_wind_nonconforming.jl
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/Trixi.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- examples/p4est_3d_dgsem/elixir_euler_baroclinic_instability.jl
+++ examples/p4est_3d_dgsem/elixir_euler_baroclinic_instability.jl
@@ -229,8 +229,8 @@ solver = DGSEM(polydeg = 5, surface_flux = surface_flux,
 
 # For optimal results, use (16, 8) here
 trees_per_cube_face = (8, 4)
-mesh = Trixi.P4estMeshCubedSphere(trees_per_cube_face..., 6.371229e6, 30000.0,
-                                  polydeg = 5, initial_refinement_level = 0)
+mesh = P4estMeshCubedSphere(trees_per_cube_face..., 6.371229e6, 30000.0,
+                            polydeg = 5, initial_refinement_level = 0)
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
                                     source_terms = source_terms_baroclinic_instability,
