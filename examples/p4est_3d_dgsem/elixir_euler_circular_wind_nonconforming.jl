182f5bcb9027dd567fb1f27417f80d8e4270c294
Daniel Doehring <doehringd2@gmail.com>
2025-09-25 11:45:09 +0200

    Update examples/p4est_3d_dgsem/elixir_euler_circular_wind_nonconforming.jl

--- examples/p4est_3d_dgsem/elixir_euler_circular_wind_nonconforming.jl
+++ examples/p4est_3d_dgsem/elixir_euler_circular_wind_nonconforming.jl
@@ -114,7 +114,6 @@ trees_per_cube_face = (6, 2)
 inner_radius = 6.371229e6
 thickness = 30000.0 # thickness of the sphere, outer radius is `inner_radius + thickness`
 mesh = P4estMeshCubedSphere(trees_per_cube_face..., inner_radius, thickness,
-                            polydeg = 4, initial_refinement_level = 0)
                             polydeg = 4)
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver,
