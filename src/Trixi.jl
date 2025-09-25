75be46e6f229342dad30d710a9d2166c4a3c6f88
vincmarks <vimarks@students.uni-mainz.de>
2025-09-25 19:29:28 +0200

    included the default_boundary_conditions.jl file and the boundary_condition_default function

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -141,6 +141,7 @@ include("auxiliary/t8code.jl")
 include("equations/equations.jl")
 include("meshes/meshes.jl")
 include("solvers/solvers.jl")
+include("solvers/default_boundary_conditions.jl")  
 include("equations/equations_parabolic.jl") # these depend on parabolic solver types
 include("semidiscretization/semidiscretization.jl")
 include("semidiscretization/semidiscretization_hyperbolic.jl")
@@ -151,7 +152,6 @@ include("time_integration/time_integration.jl")
 include("callbacks_step/callbacks_step.jl")
 include("callbacks_stage/callbacks_stage.jl")
 include("semidiscretization/semidiscretization_euler_gravity.jl")
-
 # Special elixirs such as `convergence_test`
 include("auxiliary/special_elixirs.jl")
 
@@ -228,6 +228,7 @@ export boundary_condition_do_nothing,
        boundary_condition_default_tree_1D,
        boundary_condition_default_tree_2D,
        boundary_condition_default_tree_3D,
+       boundary_condition_default,
        boundary_condition_periodic,
        BoundaryConditionDirichlet,
        BoundaryConditionNeumann,
