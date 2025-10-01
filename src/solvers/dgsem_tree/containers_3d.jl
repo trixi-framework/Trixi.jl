ead059592bd151ff6c0a480aa2d24822a97e51c7
bennibolm <benjamin.bolm@gmx.de>
2025-10-01 16:24:34 +0200

    Reset antidiffusive fluxes after resizing in 3d

--- src/solvers/dgsem_tree/containers_3d.jl
+++ src/solvers/dgsem_tree/containers_3d.jl
@@ -937,6 +937,24 @@ function Base.resize!(fluxes::ContainerAntidiffusiveFlux3D, capacity)
                                                (n_variables, n_nodes, n_nodes,
                                                 n_nodes + 1,
                                                 capacity))
+
+    uEltype = eltype(fluxes.antidiffusive_flux1_L)
+    @threaded for element in axes(fluxes.antidiffusive_flux1_L, 4)
+        fluxes.antidiffusive_flux1_L[:, 1, :, :, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux1_L[:, n_nodes + 1, :, :, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux1_R[:, 1, :, :, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux1_R[:, n_nodes + 1, :, :, element] .= zero(uEltype)
+
+        fluxes.antidiffusive_flux2_L[:, :, 1, :, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux2_L[:, :, n_nodes + 1, :, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux2_R[:, :, 1, :, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux2_R[:, :, n_nodes + 1, :, element] .= zero(uEltype)
+
+        fluxes.antidiffusive_flux3_L[:, :, :, 1, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux3_L[:, :, :, n_nodes + 1, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux3_R[:, :, :, 1, element] .= zero(uEltype)
+        fluxes.antidiffusive_flux3_R[:, :, :, n_nodes + 1, element] .= zero(uEltype)
+    end
     return nothing
 end
 end # @muladd
