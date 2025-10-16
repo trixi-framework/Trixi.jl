5d0054895fac9a6b755eebece317694908a12e91
SimonCan <simon.candelaresi@gmail.com>
2025-10-16 21:01:41 +0100

    Added n::AbstractVector version of max_abs_speed_naive for CompressibleEulerMulticomponentEquations2D.

--- src/equations/compressible_euler_multicomponent_2d.jl
+++ src/equations/compressible_euler_multicomponent_2d.jl
@@ -681,6 +681,41 @@ end
     return max(abs(v_ll), abs(v_rr)) + max(c_ll, c_rr)
 end
 
+# Calculate maximum wave speed for local Lax-Friedrichs-type dissipation
+@inline function max_abs_speed_naive(u_ll, u_rr, n::AbstractVector,
+                                     equations::CompressibleEulerMulticomponentEquations2D)
+    # Unpack conservative variables
+    rho_v1_ll, rho_v2_ll, rho_e_ll = u_ll
+    rho_v1_rr, rho_v2_rr, rho_e_rr = u_rr
+
+    # Get densities and gammas
+    rho_ll = density(u_ll, equations)
+    rho_rr = density(u_rr, equations)
+    gamma_ll = totalgamma(u_ll, equations)
+    gamma_rr = totalgamma(u_rr, equations)
+
+    # Normalize the direction vector
+    n = n / norm(n)
+
+    # Velocity components
+    v_ll_vec = SVector(rho_v1_ll, rho_v2_ll) / rho_ll
+    v_rr_vec = SVector(rho_v1_rr, rho_v2_rr) / rho_rr
+
+    # Project velocities onto the direction n.
+    v_ll = dot(v_ll_vec, n)
+    v_rr = dot(v_rr_vec, n)
+
+    # Compute pressures
+    p_ll = (gamma_ll - 1) * (rho_e_ll - 0.5f0 * dot(v_ll_vec, v_ll_vec) * rho_ll)
+    p_rr = (gamma_rr - 1) * (rho_e_rr - 0.5f0 * dot(v_rr_vec, v_rr_vec) * rho_rr)
+
+    # Sound speeds
+    c_ll = sqrt(gamma_ll * p_ll / rho_ll)
+    c_rr = sqrt(gamma_rr * p_rr / rho_rr)
+
+    return max(abs(v_ll), abs(v_rr)) + max(c_ll, c_rr)
+end
+
 # Less "cautious", i.e., less overestimating `λ_max` compared to `max_abs_speed_naive`
 @inline function max_abs_speed(u_ll, u_rr, orientation::Integer,
                                equations::CompressibleEulerMulticomponentEquations2D)
