92c575d7577f5ff5dc97bdb9f2a2465601948b37
Marco Artiano <artianomarco97@gmail.com>
2025-11-02 14:36:24 +0100

    better splitting

--- examples/p4est_2d_dgsem/elixir_euler_imex_inertia_gravity_waves.jl
+++ examples/p4est_2d_dgsem/elixir_euler_imex_inertia_gravity_waves.jl
@@ -1,45 +1,62 @@
 using Trixi
 using OrdinaryDiffEqBDF
 using SparseDiffTools ## This is needed to force 'autodiff = AutoFiniteDiff()' in the ODE solver.
+using OrdinaryDiffEqLowOrderRK
 
-function initial_condition_warm_bubble(x, t, equations::CompressibleEulerEquations2D)
-    g = 9.81
-    c_p = 1004.0
-    c_v = 717.0
+function initial_condition_inertia_gravity_waves(x, t, equations::CompressibleEulerEquations2D)
+	g = 9.81
+	c_p = 1004.0
+	c_v = 717.0
 
-    # center of perturbation
-    center_x = 10000.0
-    center_z = 2000.0
-    # radius of perturbation
-    radius = 2000.0
-    # distance of current x to center of perturbation
-    r = sqrt((x[1] - center_x)^2 + (x[2] - center_z)^2)
+	A = 5000.0
+	H = 10000
+	b0 = 0.01
+	xc = -150_000 + 100_000
+	binv = (1 + (x[1] - xc)^2 / A^2)
+	potential_temperature_ref = 300 * exp(0.01^2 / g * x[2])
+	potential_temperature_perturbation = b0 * sin(pi * x[2] / H) / binv
 
-    # perturbation in potential temperature
-    potential_temperature_ref = 300.0
-    potential_temperature_perturbation = 0.0
-    if r <= radius
-        potential_temperature_perturbation = 2 * cospi(0.5 * r / radius)^2
-    end
-    potential_temperature = potential_temperature_ref + potential_temperature_perturbation
+	potential_temperature = potential_temperature_ref + potential_temperature_perturbation
 
-    # Exner pressure, solves hydrostatic equation for x[2]
-    exner = 1 - g / (c_p * potential_temperature) * x[2]
+	# Exner pressure, solves hydrostatic equation for x[2]
+	exner = 1 + g^2 / (c_p * 300.0 * 0.01^2) * (exp(-0.01^2 / g * x[2]) - 1)
 
-    # pressure
-    p_0 = 100_000.0  # reference pressure
-    R = c_p - c_v    # gas constant (dry air)
-    p = p_0 * exner^(c_p / R)
+	# pressure
+	p_0 = 100_000.0  # reference pressure
+	R = c_p - c_v    # gas constant (dry air)
+	p = p_0 * exner^(c_p / R)
 
-    # temperature
-    T = potential_temperature * exner
+	# temperature
+	T = potential_temperature * exner
 
-    # density
-    rho = p / (R * T)
+	# density
+	rho = p / (R * T)
 
-    v1 = 20.0
-    v2 = 0.0
-    return prim2cons(SVector(rho, v1, v2, p), equations)
+	v1 = 20.0
+	v2 = 0.0
+	return prim2cons(SVector(rho, v1, v2, p), equations)
+end
+
+@inline function boundary_condition_slip_wall_vel(u_inner, normal_direction::AbstractVector,
+                                                  x, t,
+                                                  surface_flux_function,
+                                                  equations::CompressibleEulerEquations2D)
+    # normalize the outward pointing direction
+    normal = normal_direction / Trixi.norm(normal_direction)
+
+    # compute the normal velocity
+    u_normal = normal[1] * u_inner[2] + normal[2] * u_inner[3]
+
+    # create the "external" boundary solution state
+    u_boundary = SVector(u_inner[1],
+                         u_inner[2] - 2 * u_normal * normal[1],
+                         u_inner[3] - 2 * u_normal * normal[2],
+                         u_inner[4])
+
+    # calculate the boundary flux
+    flux = surface_flux_function(u_inner, u_boundary, normal_direction, equations)
+
+    return flux
 end
 
 @inline function flux_lmars_fast(u_ll, u_rr, normal_direction::AbstractVector,
@@ -165,30 +182,33 @@ solver_explicit = DGSEM(basis, flux_lmars_slow, volume_integral_explicit)
 volume_integral_implicit = VolumeIntegralFluxDifferencing(flux_kennedy_gruber_fast)
 solver_implicit = DGSEM(basis, flux_lmars_fast, volume_integral_implicit)
 
-coordinates_min = (0.0, 0.0)
-coordinates_max = (20_000.0, 10_000.0)
+coordinates_min = (-150_000.0, 0.0)
+coordinates_max = (150_000.0, 10_000.0)
 
-trees_per_dimension = (16, 8)
+trees_per_dimension = (40, 5)
 
 mesh = P4estMesh(trees_per_dimension, polydeg = polydeg,
 	coordinates_min = coordinates_min, coordinates_max = coordinates_max,
 	periodicity = (true, false), initial_refinement_level = 0)
 
-boundary_conditions = Dict(:y_neg => boundary_condition_slip_wall,
-                                    :y_pos => boundary_condition_slip_wall)
+boundary_conditions_explicit = Dict(:y_neg => boundary_condition_slip_wall_vel,
+                                    :y_pos => boundary_condition_slip_wall_vel)
+boundary_conditions_implicit = Dict(:y_neg => boundary_condition_slip_wall_vel,
+                                    :y_pos => boundary_condition_slip_wall_vel)
 
-initial_condition = initial_condition_warm_bubble
+initial_condition = initial_condition_inertia_gravity_waves
 
 semi = SemidiscretizationHyperbolicSplit(mesh,
                                          (equations, equations),
                                          initial_condition,
                                          solver_implicit,
                                          solver_explicit;
-                                         boundary_conditions = (boundary_conditions,
-                                                                boundary_conditions),
+                                         boundary_conditions = (boundary_conditions_implicit,
+                                                                boundary_conditions_explicit),
                                          source_terms = (nothing, source_terms_gravity),)
-dt = 0.5
-tspan = (0.0, 1000.0)
+T = 3000.0
+dt = 1.0
+tspan = (0.0, 3000.0)
 ode = semidiscretize(semi, tspan)
 
 summary_callback = SummaryCallback()
@@ -207,6 +227,7 @@ callbacks = CallbackSet(summary_callback, analysis_callback, save_solution)
 # OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
 sol = solve(ode,
            SBDF2(autodiff = AutoFiniteDiff());
+          # Euler();
             dt = dt, # solve needs some value here but it will be overwritten by the stepsize_callback
             save_everystep = false,
             callback = callbacks,);
