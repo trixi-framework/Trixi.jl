918b9306c5a3c026803616c41b07d67e8946416e
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2025-11-02 15:48:49 +0100

    rm

--- src/time_integration/methods_SSP.jl
+++ src/time_integration/methods_SSP.jl
@@ -177,8 +177,6 @@ function step!(integrator::SimpleIntegratorSSP)
 
     @assert !integrator.finalstep
     if isnan(integrator.dt)
-        println("Current time: ", integrator.t)
-        println("Current iter: ", integrator.iter)
         error("time step size `dt` is NaN")
     end
 
