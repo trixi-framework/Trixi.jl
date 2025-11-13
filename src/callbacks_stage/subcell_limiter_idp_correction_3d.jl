e407366ca2c46bedd7b0e90a4fec466366023c88
bennibolm <benjamin.bolm@gmx.de>
2025-11-13 10:10:13 +0100

    Apply updated 3d version

--- src/callbacks_stage/subcell_limiter_idp_correction_3d.jl
+++ src/callbacks_stage/subcell_limiter_idp_correction_3d.jl
@@ -8,77 +8,98 @@
 function perform_idp_correction!(u, dt,
                                  mesh::P4estMesh{3},
                                  equations, dg, cache)
-    @unpack inverse_weights = dg.basis # Plays role of DG subcell sizes
+    @unpack inverse_weights = dg.basis # Plays role of inverse DG-subcell sizes
     @unpack antidiffusive_flux1_L, antidiffusive_flux1_R, antidiffusive_flux2_L, antidiffusive_flux2_R, antidiffusive_flux3_L, antidiffusive_flux3_R = cache.antidiffusive_fluxes
     @unpack alpha = dg.volume_integral.limiter.cache.subcell_limiter_coefficients
 
+    # The following code implements the IDP correction in flux-differencing form:
+    # u[v, i, j, k, element] += -dt * inverse_jacobian *
+    #    (inverse_weights[i] *
+    #       ((1 - alpha_1_ip1) * antidiffusive_flux1_ip1[v] - (1 - alpha_1) * antidiffusive_flux1[v]) +
+    #     inverse_weights[j] *
+    #       ((1 - alpha_2_jp1) * antidiffusive_flux2_jp1[v] - (1 - alpha_2) * antidiffusive_flux2[v]) +
+    #     inverse_weights[k] *
+    #       ((1 - alpha_3_kp1) * antidiffusive_flux3_kp1[v] - (1 - alpha_3) * antidiffusive_flux3[v]))
+
+    # For LGL nodes, the high-order and low-order fluxes at element interfaces are equal
+    # and therefore, the antidiffusive fluxes are zero there.
+    # To avoid adding zeros and speed up the simulation, we directly loop over the subcell
+    # interfaces.
+
     @threaded for element in eachelement(dg, cache)
-        for k in eachnode(dg), j in eachnode(dg), i in eachnode(dg)
+        # Perform correction in 1st/x-direction
+        for k in eachnode(dg), j in eachnode(dg), i in 2:nnodes(dg)
+            # Apply to right node
+            alpha1 = max(alpha[i - 1, j, k, element], alpha[i, j, k, element])
+
             # Sign switch as in apply_jacobian!
             inverse_jacobian = -get_inverse_jacobian(cache.elements.inverse_jacobian,
                                                      mesh, i, j, k, element)
+            flux1 = get_node_vars(antidiffusive_flux1_R, equations, dg,
+                                  i, j, k, element)
+            dg_factor = -dt * inverse_jacobian * inverse_weights[i] * (1 - alpha1)
+            multiply_add_to_node_vars!(u, dg_factor, flux1,
+                                       equations, dg, i, j, k, element)
 
-            # Note: For LGL nodes, the high-order and low-order fluxes at element interfaces are equal.
-            # Therefore, the antidiffusive fluxes are zero.
-            # To avoid accessing zero entries, we directly use zero vectors instead.
-            if i > 1 # Not at "left" boundary node
-                alpha1 = max(alpha[i - 1, j, k, element], alpha[i, j, k, element])
-                alpha_flux1 = (1 - alpha1) *
-                              get_node_vars(antidiffusive_flux1_R, equations, dg,
-                                            i, j, k, element)
-            else # At "left" boundary node
-                alpha_flux1 = zero(SVector{nvariables(equations), eltype(u)})
-            end
-            if i < nnodes(dg) # Not at "right" boundary node
-                alpha1_ip1 = max(alpha[i, j, k, element], alpha[i + 1, j, k, element])
-                alpha_flux1_ip1 = (1 - alpha1_ip1) *
-                                  get_node_vars(antidiffusive_flux1_L, equations, dg,
-                                                i + 1, j, k, element)
-            else # At "right" boundary node
-                alpha_flux1_ip1 = zero(SVector{nvariables(equations), eltype(u)})
-            end
-            if j > 1 # Not at "bottom" boundary node
-                alpha2 = max(alpha[i, j - 1, k, element], alpha[i, j, k, element])
-                alpha_flux2 = (1 - alpha2) *
-                              get_node_vars(antidiffusive_flux2_R, equations, dg,
-                                            i, j, k, element)
-            else # At "bottom" boundary node
-                alpha_flux2 = zero(SVector{nvariables(equations), eltype(u)})
-            end
-            if j < nnodes(dg) # Not at "top" boundary node
-                alpha2_jp1 = max(alpha[i, j, k, element], alpha[i, j + 1, k, element])
-                alpha_flux2_jp1 = (1 - alpha2_jp1) *
-                                  get_node_vars(antidiffusive_flux2_L, equations, dg,
-                                                i, j + 1, k, element)
-            else # At "top" boundary node
-                alpha_flux2_jp1 = zero(SVector{nvariables(equations), eltype(u)})
-            end
-            if k > 1 # Not at "front" boundary node
-                alpha3 = max(alpha[i, j, k - 1, element], alpha[i, j, k, element])
-                alpha_flux3 = (1 - alpha3) *
-                              get_node_vars(antidiffusive_flux3_R, equations, dg,
-                                            i, j, k, element)
-            else # At "front" boundary node
-                alpha_flux3 = zero(SVector{nvariables(equations), eltype(u)})
-            end
-            if k < nnodes(dg) # Not at "back" boundary node
-                alpha3_kp1 = max(alpha[i, j, k, element], alpha[i, j, k + 1, element])
-                alpha_flux3_kp1 = (1 - alpha3_kp1) *
-                                  get_node_vars(antidiffusive_flux3_L, equations, dg,
-                                                i, j, k + 1, element)
-            else # At "back" boundary node
-                alpha_flux3_kp1 = zero(SVector{nvariables(equations), eltype(u)})
-            end
+            # Apply to left node
+            # Sign switch as in apply_jacobian!
+            inverse_jacobian = -get_inverse_jacobian(cache.elements.inverse_jacobian,
+                                                     mesh, i - 1, j, k, element)
+            flux1_ip1 = get_node_vars(antidiffusive_flux1_L, equations, dg,
+                                      i, j, k, element)
+            dg_factor = dt * inverse_jacobian * inverse_weights[i - 1] * (1 - alpha1)
+            multiply_add_to_node_vars!(u, dg_factor, flux1_ip1,
+                                       equations, dg, i - 1, j, k, element)
+        end
 
-            for v in eachvariable(equations)
-                u[v, i, j, k, element] += dt * inverse_jacobian *
-                                          (inverse_weights[i] *
-                                           (alpha_flux1_ip1[v] - alpha_flux1[v]) +
-                                           inverse_weights[j] *
-                                           (alpha_flux2_jp1[v] - alpha_flux2[v]) +
-                                           inverse_weights[k] *
-                                           (alpha_flux3_kp1[v] - alpha_flux3[v]))
-            end
+        # Perform correction in 2nd/y-direction
+        for k in eachnode(dg), j in 2:nnodes(dg), i in eachnode(dg)
+            # Apply to right node
+            alpha2 = max(alpha[i, j - 1, k, element], alpha[i, j, k, element])
+
+            # Sign switch as in apply_jacobian!
+            inverse_jacobian = -get_inverse_jacobian(cache.elements.inverse_jacobian,
+                                                     mesh, i, j, k, element)
+            flux2 = get_node_vars(antidiffusive_flux2_R, equations, dg,
+                                  i, j, k, element)
+            dg_factor = -dt * inverse_jacobian * inverse_weights[j] * (1 - alpha2)
+            multiply_add_to_node_vars!(u, dg_factor, flux2,
+                                       equations, dg, i, j, k, element)
+
+            # Apply to left node
+            # Sign switch as in apply_jacobian!
+            inverse_jacobian = -get_inverse_jacobian(cache.elements.inverse_jacobian,
+                                                     mesh, i, j - 1, k, element)
+            flux2_jp1 = get_node_vars(antidiffusive_flux2_L, equations, dg,
+                                      i, j, k, element)
+            dg_factor = dt * inverse_jacobian * inverse_weights[j - 1] * (1 - alpha2)
+            multiply_add_to_node_vars!(u, dg_factor, flux2_jp1,
+                                       equations, dg, i, j - 1, k, element)
+        end
+
+        # Perform correction in 3rd/z-direction
+        for k in 2:nnodes(dg), j in eachnode(dg), i in eachnode(dg)
+            # Apply to right node
+            alpha3 = max(alpha[i, j, k - 1, element], alpha[i, j, k, element])
+
+            # Sign switch as in apply_jacobian!
+            inverse_jacobian = -get_inverse_jacobian(cache.elements.inverse_jacobian,
+                                                     mesh, i, j, k, element)
+            flux3 = get_node_vars(antidiffusive_flux3_R, equations, dg,
+                                  i, j, k, element)
+            dg_factor = -dt * inverse_jacobian * inverse_weights[k] * (1 - alpha3)
+            multiply_add_to_node_vars!(u, dg_factor, flux3,
+                                       equations, dg, i, j, k, element)
+
+            # Apply to left node
+            # Sign switch as in apply_jacobian!
+            inverse_jacobian = -get_inverse_jacobian(cache.elements.inverse_jacobian,
+                                                     mesh, i, j, k - 1, element)
+            flux3_kp1 = get_node_vars(antidiffusive_flux3_L, equations, dg,
+                                      i, j, k, element)
+            dg_factor = dt * inverse_jacobian * inverse_weights[k - 1] * (1 - alpha3)
+            multiply_add_to_node_vars!(u, dg_factor, flux3_kp1,
+                                       equations, dg, i, j, k - 1, element)
         end
     end
 
