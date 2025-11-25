7bcf7e355fb72cf5dc5cf7c8b2eb0dc020d862e8
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-11-25 11:23:11 +0100

    fix

--- src/solvers/dgsem_tree/dg_2d_parabolic.jl
+++ src/solvers/dgsem_tree/dg_2d_parabolic.jl
@@ -137,7 +137,7 @@ function transform_variables!(u_transformed, u, mesh::Union{TreeMesh{2}, P4estMe
 end
 
 # This is the version used when calculating the divergence of the viscous fluxes
-function calc_volume_integral!(du, flux_viscous,
+function calc_volume_integral!(du, flux_viscous::Tuple,
                                mesh::TreeMesh{2},
                                equations_parabolic::AbstractEquationsParabolic,
                                dg::DGSEM, cache)
@@ -247,7 +247,7 @@ function calc_interface_flux!(surface_flux_values, mesh::TreeMesh{2},
 end
 
 # This is the version used when calculating the divergence of the viscous fluxes
-function prolong2boundaries!(cache, flux_viscous,
+function prolong2boundaries!(cache, flux_viscous::Tuple,
                              mesh::TreeMesh{2},
                              equations_parabolic::AbstractEquationsParabolic,
                              surface_integral, dg::DG)
