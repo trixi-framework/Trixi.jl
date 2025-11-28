758dcbb09beaed288726f76f37b6a6f5b61f039b
Daniel Doehring <doehringd2@gmail.com>
2025-11-28 08:26:50 +0100

    Update src/solvers/dgsem_tree/dg_1d_parabolic.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/solvers/dgsem_tree/dg_1d_parabolic.jl
+++ src/solvers/dgsem_tree/dg_1d_parabolic.jl
@@ -130,7 +130,7 @@ end
 function calc_volume_integral!(du, flux_viscous,
                                mesh::TreeMesh{1},
                                equations_parabolic::AbstractEquationsParabolic,
-                               dg::DGSEM, cache) where {uEltype <: Real}
+                               dg::DGSEM, cache)
     @unpack derivative_dhat = dg.basis
 
     @threaded for element in eachelement(dg, cache)
