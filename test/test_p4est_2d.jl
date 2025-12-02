579e260566244cb774d9e26361d35ac7f42c593d
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2025-12-02 16:07:32 +0100

    revert

--- test/test_p4est_2d.jl
+++ test/test_p4est_2d.jl
@@ -35,11 +35,8 @@ end
                         # Expected errors are exactly the same as with TreeMesh!
                         l2=[Float32(8.311947673061856e-6)],
                         linf=[Float32(6.627000273229378e-5)],
-                        real_type=Float32,
                         RealT=Float32,
-                        advection_velocity=(Float32(0.2), Float32(-0.7)),
-                        coordinates_min=(-1.0f0, -1.0f0),
-                        coordinates_max=(1.0f0, 1.0f0))
+                        real_type=Float32)
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
     let
