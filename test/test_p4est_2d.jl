75051bc16b5f2ae86c33f9135d3f3b281a8fe0e6
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2025-12-03 08:27:42 +0000

    fix

--- test/test_p4est_2d.jl
+++ test/test_p4est_2d.jl
@@ -34,7 +34,6 @@ end
                         # Expected errors are exactly the same as with TreeMesh!
                         l2=[Float32(8.311947673061856e-6)],
                         linf=[Float32(6.627000273229378e-5)],
-                        RealT=Float32,
                         RealT_for_test_tolerances=Float32,
                         real_type=Float32)
     # Ensure that we do not have excessive memory allocations
