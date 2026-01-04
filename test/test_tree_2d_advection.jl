26f150604853d1fcd3a9a8a229387184c38514c0
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-01-04 12:35:23 +0100

    test

--- test/test_tree_2d_advection.jl
+++ test/test_tree_2d_advection.jl
@@ -80,6 +80,15 @@ end
     @test linf_actual == linf_expected
 end
 
+@trixi_testset "elixir_advection_restart_polydeg_lower.jl" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                 "elixir_advection_restart_polydeg_lower.jl"),
+                        l2=[0.00017439412748604995], linf=[0.0004655222114398061])
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    @test_allocations(Trixi.rhs!, semi_float_type, sol, 1000)
+end
+
 @trixi_testset "elixir_advection_mortar.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_mortar.jl"),
                         # Expected errors are exactly the same as in the parallel test!
