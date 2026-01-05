db54482212fb4c8f2978a23c60a66ea16f95f627
Ed <50782222+edhschen@users.noreply.github.com>
2026-01-05 15:26:52 -0500

    apply suggestion changes

--- src/solvers/dgsem_tree/dg_2d_parabolic.jl
+++ src/solvers/dgsem_tree/dg_2d_parabolic.jl
@@ -1052,17 +1052,18 @@ function calc_sources_parabolic!(du, u, gradients, t, source_terms,
                                  equations_parabolic::AbstractEquations{2}, dg::DG,
                                  cache)
     @unpack node_coordinates = cache.elements
+    equations = equations_parabolic.equations_hyperbolic
 
     @threaded for element in eachelement(dg, cache)
         for j in eachnode(dg), i in eachnode(dg)
-            u_local = get_node_vars(u, equations_parabolic, dg, i, j, element)
-            gradients_x_local = get_node_vars(gradients[1], equations_parabolic, dg, i, j, element)
-            gradients_y_local = get_node_vars(gradients[2], equations_parabolic, dg, i, j, element)
+            u_local = get_node_vars(u, equations, dg, i, j, element)
+            gradients_x_local = get_node_vars(gradients[1], equations, dg, i, j, element)
+            gradients_y_local = get_node_vars(gradients[2], equations, dg, i, j, element)
             gradients_local = (gradients_x_local, gradients_y_local)
-            x_local = get_node_coords(node_coordinates, equations_parabolic, dg,
+            x_local = get_node_coords(node_coordinates, equations, dg,
                                       i, j, element)
             du_local = source_terms(u_local, gradients_local, x_local, t, equations_parabolic)
-            add_to_node_vars!(du, du_local, equations_parabolic, dg, i, j, element)
+            add_to_node_vars!(du, du_local, equations, dg, i, j, element)
         end
     end
 
