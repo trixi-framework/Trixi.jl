d0d11d0647d38d925ba631fd3071c0c761ed7741
Jesse Chan <jesse.lee.chan@gmail.com>
2026-01-06 08:15:03 -0600

    update docstring

--- src/semidiscretization/semidiscretization_hyperbolic_parabolic.jl
+++ src/semidiscretization/semidiscretization_hyperbolic_parabolic.jl
@@ -53,6 +53,7 @@ end
     SemidiscretizationHyperbolicParabolic(mesh, both_equations, initial_condition, solver;
                                           solver_parabolic=default_parabolic_solver(),
                                           source_terms=nothing,
+                                          source_terms_parabolic=nothing,                                          
                                           both_boundary_conditions=(boundary_condition_periodic, boundary_condition_periodic),
                                           RealT=real(solver),
                                           uEltype=RealT)
