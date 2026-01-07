a2836c0518b9c35254c0da5449599055a4a7a505
SimonCan <simon.candelaresi@gmail.com>
2026-01-07 11:03:22 +0000

    Fixed variable name error.

--- src/solvers/dgsem_p4est/dg_2d.jl
+++ src/solvers/dgsem_p4est/dg_2d.jl
@@ -217,7 +217,7 @@ end
 @inline function calc_interface_flux!(surface_flux_values,
                                       mesh::Union{P4estMesh{2}, P4estMeshView{2},
                                                   T8codeMesh{2}},
-                                      nonconservative_terms::True, equations,
+                                      have_nonconservative_terms::True, equations,
                                       surface_integral, dg::DG, cache,
                                       interface_index, normal_direction,
                                       primary_node_index, primary_direction_index,
