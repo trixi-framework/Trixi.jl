dff951fdeb01bfacfafaa8e8c301996f97c4f13e
ArturYeritsyan <artur-yeritsyan@outlook.de>
2026-01-13 06:27:54 +0100

    added second order fv for dg3d tree mesh

--- src/solvers/dgsem/calc_volume_integral.jl
+++ src/solvers/dgsem/calc_volume_integral.jl
@@ -83,6 +83,7 @@ end
 function calc_volume_integral!(du, u,
                                mesh::Union{TreeMesh{1}, StructuredMesh{1},
                                            TreeMesh{2}, StructuredMesh{2}, P4estMesh{2},
+                                           TreeMesh{3},
                                            UnstructuredMesh2D, T8codeMesh{2}},
                                have_nonconservative_terms, equations,
                                volume_integral::VolumeIntegralShockCapturingRRG,
