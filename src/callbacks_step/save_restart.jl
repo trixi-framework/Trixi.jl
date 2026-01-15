6bd0643033248e34e8b436a1086289cad3b90018
termi-official <termi-official@users.noreply.github.com>
2026-01-15 12:36:23 +0100

    Add basic restart support for new controller

--- src/callbacks_step/save_restart.jl
+++ src/callbacks_step/save_restart.jl
@@ -182,6 +182,18 @@ function load_adaptive_time_integrator!(integrator, restart_file::AbstractString
         end
         # Load data that is required both for PIController and PIDController
         integrator.qold = read(attributes(file)["time_integrator_qold"])
+        if hasproperty(controller, :qold)
+            controller.qold = read(attributes(file)["time_integrator_qold"])
+        end
+        if hasproperty(controller, :errold)
+            controller.errold = read(attributes(file)["time_integrator_qold"])
+        end
+        if hasproperty(controller, :dt_factor) # PID controller cache
+            beta1, beta2, beta3 = controller.controller.beta # = integrator.controller_cache.controller
+            alg = integrator.alg
+            k = min(OrdinaryDiffEqCore.alg_order(alg), OrdinaryDiffEqCore.alg_adaptive_order(alg)) + 1
+            controller.dt_factor = err1^(beta1 / k) * err2^(beta2 / k) * err3^(beta3 / k)
+        end
         integrator.dtpropose = read(attributes(file)["time_integrator_dtpropose"])
         # Accept step to use dtpropose already in the first step
         integrator.accept_step = true
