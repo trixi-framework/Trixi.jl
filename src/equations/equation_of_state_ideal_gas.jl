cb338e8f7e05c271430a5e6a160c041a855897d5
Jesse Chan <jesse.lee.chan@gmail.com>
2026-01-15 15:24:37 -0600

    add IdealGas example

---
+++ src/equations/equation_of_state_ideal_gas.jl
@@ -0,0 +1,51 @@
+@doc raw"""
+    This defines the polytropic ideal gas equation of state
+    given by the pressure and internal energy relations
+```math
+p = rho * R * T, e = cv * T
+```
+"""
+struct IdealGas{RealT} <: AbstractEquationOfState
+    gamma::RealT
+    R::RealT
+    cv::RealT
+end
+
+# If not specified, `R` is taken to be the gas constant for air. However, the 
+# precise value does not matter since eliminating temperature yields non-dimensional
+# formulas in terms of only `gamma`. 
+function IdealGas(gamma = 1.4, R = 287)
+    cv = R / (gamma - 1)
+    return IdealGas(promote(gamma, R, cv)...)
+end
+
+function pressure(V, T, eos::IdealGas)
+    (; R) = eos
+    rho = inv(V)
+    p = rho * R * T 
+    return p
+end
+
+function internal_energy(V, T, eos::IdealGas) 
+    (; cv) = eos
+    e = cv * T
+    return e 
+end
+
+function specific_entropy(V, T, eos::IdealGas)
+    (; cv, R) = eos
+    s = cv * log(T) + R * log(V)
+    return s
+end
+
+function speed_of_sound(V, T, eos::IdealGas)
+    (; gamma) = eos
+    p = pressure(V, T, eos)
+    c2 = gamma * p * V
+    return sqrt(c2)
+end
+
+################################################
+#        additional helper functions
+################################################
+
