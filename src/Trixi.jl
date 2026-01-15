5dcb1c53a8a1ca6ec509ee7d387053883999d705
termi-official <9196588+termi-official@users.noreply.github.com>
2026-01-15 18:31:30 +0100

    Reduce Core version

--- src/Trixi.jl
+++ src/Trixi.jl
@@ -44,10 +44,10 @@ using SciMLBase: DiscreteCallback,
                  ODEProblem, ODESolution,
                  SplitODEProblem
 import SciMLBase: get_du, get_tmp_cache, u_modified!,
-                  init, step!, check_error,
+                  init, step!, check_error, alg_order,
                   get_proposed_dt, set_proposed_dt!,
                   terminate!, remake, add_tstop!, has_tstop, first_tstop
-import OrdinaryDiffEqCore: alg_order, alg_adaptive_order
+import OrdinaryDiffEqCore: alg_adaptive_order
 
 using DelimitedFiles: readdlm
 using Downloads: Downloads
