66004f41e862b30419ecf021b85b552dbc12d0fe
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2026-01-18 10:15:26 +0100

    pcom

--- src/auxiliary/precompile.jl
+++ src/auxiliary/precompile.jl
@@ -181,8 +181,8 @@ function _precompile_manual_()
                                     StaticArrays.SVector{nnodes_, RealT},
                                     # InverseVandermondeLegendre
                                     Matrix{RealT},
-                                    # VectorT2, EntriesBoundaryMatrix
-                                    StaticArrays.SVector{2, RealT},
+                                    # boundary_interpolation_factor
+                                    RealT,
                                     # DerivativeMatrix
                                     #StaticArrays.SArray{Tuple{nnodes_,nnodes_},RealT,2,nnodes_^2},
                                     Matrix{RealT}}
