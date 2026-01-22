71c7bd8a631c99a61d0fc7a3bdf67c3f8bc5cb18
Jesse Chan <jesse.lee.chan@gmail.com>
2026-01-22 15:13:28 -0600

    add more tests

--- test/test_unit.jl
+++ test/test_unit.jl
@@ -782,6 +782,8 @@ end
           cons2entropy(u, equations)
     @test flux_lax_friedrichs(u, u, 1, equations) ≈ flux(u, 1, equations)
     @test flux_hll(u, u, 1, equations) ≈ flux(u, 1, equations)
+    @test flux_terashima_etal(u, u, 1, equations) ≈ flux(u, 1, equations)
+    @test flux_central_terashima_etal(u, u, 1, equations) ≈ flux(u, 1, equations)
 
     # check that the fallback temperature and specialized temperature 
     # return the same value 
