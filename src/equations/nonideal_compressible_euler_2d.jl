c1ab7e62a08b0a8e8defcdf3767ea3d9590d92c2
Jesse Chan <jesse.lee.chan@gmail.com>
2026-01-26 13:14:19 -0600

    add StructuredMesh boundary condition

--- src/equations/nonideal_compressible_euler_2d.jl
+++ src/equations/nonideal_compressible_euler_2d.jl
@@ -193,6 +193,31 @@ Should be used together with [`UnstructuredMesh2D`](@ref), [`P4estMesh`](@ref), 
 end
 
 """
+    boundary_condition_slip_wall(u_inner, normal_direction, direction, x, t,
+                                 surface_flux_function, equations::CompressibleEulerEquations2D)
+
+Should be used together with [`StructuredMesh`](@ref).
+"""
+@inline function boundary_condition_slip_wall(u_inner, normal_direction::AbstractVector,
+                                              direction, x, t,
+                                              surface_flux_function,
+                                              equations::NonIdealCompressibleEulerEquations2D)
+    # flip sign of normal to make it outward pointing, then flip the sign of the normal flux back
+    # to be inward pointing on the -x and -y sides due to the orientation convention used by StructuredMesh
+    if isodd(direction)
+        boundary_flux = -boundary_condition_slip_wall(u_inner, -normal_direction,
+                                                      x, t, surface_flux_function,
+                                                      equations)
+    else
+        boundary_flux = boundary_condition_slip_wall(u_inner, normal_direction,
+                                                     x, t, surface_flux_function,
+                                                     equations)
+    end
+
+    return boundary_flux
+end
+
+"""
     flux_terashima_etal(u_ll, u_rr, orientation::Int,
                         equations::NonIdealCompressibleEulerEquations1D)
 
