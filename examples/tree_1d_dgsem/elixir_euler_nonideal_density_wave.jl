cf8892b69b23320d07df9ebb5e75eb39a68bd154
Jesse Chan <jesse.lee.chan@gmail.com>
2026-01-29 23:11:13 -0600

    remove duplicate callbacks

--- examples/tree_1d_dgsem/elixir_euler_nonideal_density_wave.jl
+++ examples/tree_1d_dgsem/elixir_euler_nonideal_density_wave.jl
@@ -83,33 +83,3 @@ callbacks = CallbackSet(summary_callback,
 sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false);
             dt = stepsize_callback(ode), # solve needs some value here but it will be overwritten by the stepsize_callback
             ode_default_options()..., callback = callbacks);
-# ODE solvers, callbacks etc.
-
-tspan = (0.0, 2.0)
-ode = semidiscretize(semi, tspan)
-
-summary_callback = SummaryCallback()
-
-analysis_interval = 2000
-analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
-
-alive_callback = AliveCallback(analysis_interval = analysis_interval)
-
-save_solution = SaveSolutionCallback(interval = 100,
-                                     save_initial_solution = true,
-                                     save_final_solution = true,
-                                     solution_variables = cons2prim)
-
-stepsize_callback = StepsizeCallback(cfl = 0.5)
-
-callbacks = CallbackSet(summary_callback,
-                        analysis_callback, alive_callback,
-                        save_solution,
-                        stepsize_callback)
-
-###############################################################################
-# run the simulation
-
-sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false);
-            dt = stepsize_callback(ode), # solve needs some value here but it will be overwritten by the stepsize_callback
-            ode_default_options()..., callback = callbacks);
