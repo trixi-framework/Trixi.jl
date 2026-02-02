c52f6b65a9d8a11af93f847c0b83740b250d7818
termi-official <termi-official@users.noreply.github.com>
2026-02-02 21:06:57 +0100

    Load correct errold.

--- ext/TrixiOrdinaryDiffEqCore.jl
+++ ext/TrixiOrdinaryDiffEqCore.jl
@@ -29,7 +29,8 @@ end
         if !("time_integrator_qold" in keys(attributes(file)))
             error("Missing data in restart file: check the consistency of adaptive time controller with initial setup!")
         end
-        controller.errold = integrator.qold = read(attributes(file)["time_integrator_qold"])
+        controller.errold = read(attributes(file)["time_integrator_controller_err"])
+        integrator.qold = read(attributes(file)["time_integrator_qold"])
     end
 
     function load_controller!(integrator, controller::PIDControllerCache, file)
@@ -37,7 +38,7 @@ end
              !("time_integrator_controller_err" in keys(attributes(file))))
             error("Missing data in restart file: check the consistency of adaptive time controller with initial setup!")
         end
-        integrator.qold = read(attributes(file)["time_integrator_qold"])
+        controller.dt_factor = integrator.qold = read(attributes(file)["time_integrator_qold"])
         controller.err[:] = read(attributes(file)["time_integrator_controller_err"])
     end
 end
