62e95427a943f026b9f1451cd464cfe887f6891b
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-02-03 15:07:17 +0100

    ds

--- src/equations/nonideal_compressible_euler_1d.jl
+++ src/equations/nonideal_compressible_euler_1d.jl
@@ -353,7 +353,7 @@ end
 end
 
 """
-    entropy_potential(u, orientation::Int, equations::CompressibleEulerEquations1D)
+    entropy_potential(u, orientation::Int, equations::NonIdealCompressibleEulerEquations1D)
 
 Calculate the entropy potential, which for the compressible Euler equations with general
 EOS is ``p v_i / T`` for the choice of entropy ``S(u) = \rho s``. 
