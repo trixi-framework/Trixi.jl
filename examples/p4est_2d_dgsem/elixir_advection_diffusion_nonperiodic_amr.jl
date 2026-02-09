87ea70e2a5f06011f2fae9a4b2552cfc829e64b6
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2026-02-09 16:04:57 +0100

    rev

--- examples/p4est_2d_dgsem/elixir_advection_diffusion_nonperiodic_amr.jl
+++ examples/p4est_2d_dgsem/elixir_advection_diffusion_nonperiodic_amr.jl
@@ -59,7 +59,7 @@ semi = SemidiscretizationHyperbolicParabolic(mesh,
 # ODE solvers, callbacks etc.
 
 # Create ODE problem with time span `tspan`
-tspan = (0.0, 0.1)
+tspan = (0.0, 0.5)
 ode = semidiscretize(semi, tspan)
 
 # At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
