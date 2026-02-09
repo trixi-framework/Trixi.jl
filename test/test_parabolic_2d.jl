18cc16f14b05909e0335d6b97303179097884fea
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2026-02-09 14:30:24 +0100

    reduce

--- test/test_parabolic_2d.jl
+++ test/test_parabolic_2d.jl
@@ -653,24 +653,6 @@ end
     @test_allocations(Trixi.rhs_parabolic!, semi, sol, 1000)
 end
 
-@trixi_testset "P4estMesh2D: elixir_advection_diffusion_nonperiodic_amr.jl (LDG)" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "p4est_2d_dgsem",
-                                 "elixir_advection_diffusion_nonperiodic_amr.jl"),
-                        semi=SemidiscretizationHyperbolicParabolic(mesh,
-                                                                   (equations,
-                                                                    equations_parabolic),
-                                                                   initial_condition,
-                                                                   solver;
-                                                                   solver_parabolic = ViscousFormulationLocalDG(),
-                                                                   boundary_conditions = (boundary_conditions,
-                                                                                          boundary_conditions_parabolic)),
-                        tspan=(0.0, 0.01),
-                        l2=[0.009528054353218938], linf=[0.21211942139742634])
-    # Ensure that we do not have excessive memory allocations
-    # (e.g., from type instabilities)
-    @test_allocations(Trixi.rhs!, semi, sol, 1000)
-end
-
 @trixi_testset "P4estMesh2D: elixir_advection_diffusion_nonperiodic_curved.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "p4est_2d_dgsem",
                                  "elixir_advection_diffusion_nonperiodic_curved.jl"),
