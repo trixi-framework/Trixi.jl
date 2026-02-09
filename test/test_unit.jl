7885f92fb2d1e41cb2ad54fafe9369322c52f109
Jesse Chan <jesse.lee.chan@gmail.com>
2026-02-09 15:05:52 -0600

    formatting

--- test/test_unit.jl
+++ test/test_unit.jl
@@ -867,11 +867,12 @@ end
           flux_central_terashima_etal(u_ll, u_rr, 1, equations) * normal_direction[1] +
           flux_central_terashima_etal(u_ll, u_rr, 2, equations) * normal_direction[2]
 
-    for _flux_function in (flux_lax_friedrichs, min_max_speed_naive, min_max_speed_davis)          
+    for _flux_function in (flux_lax_friedrichs, min_max_speed_naive,
+                           min_max_speed_davis)
         @test all(_flux_function(u_ll, u_rr, 1, equations) .≈
-            _flux_function(u_ll, u_rr, SVector(1, 0), equations))
+                  _flux_function(u_ll, u_rr, SVector(1, 0), equations))
         @test all(_flux_function(u_ll, u_rr, 2, equations) .≈
-            _flux_function(u_ll, u_rr, SVector(0, 1), equations))
+                  _flux_function(u_ll, u_rr, SVector(0, 1), equations))
     end
 
     # check that the fallback temperature and specialized temperature 
