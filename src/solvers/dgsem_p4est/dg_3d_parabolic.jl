9d9ff85ababa00ca4a4e8c7673fe88c6cf5a60f5
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2026-02-10 22:25:58 +0100

    caps

--- src/solvers/dgsem_p4est/dg_3d_parabolic.jl
+++ src/solvers/dgsem_p4est/dg_3d_parabolic.jl
@@ -6,7 +6,7 @@
 #! format: noindent
 
 # This version is called during `calc_gradients!` and must be specialized because the
-# magnitude of the flux does NOT depend on the outward normal.
+# MAGNITUDE of the flux does NOT depend on the outward normal.
 # Thus, you don't need to scale by 2 (e.g., the scaling factor in the normals (and in the
 # contravariant vectors) along large/small elements across a non-conforming
 # interface in 2D) and flip the sign when storing the mortar fluxes back
