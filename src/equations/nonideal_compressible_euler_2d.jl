30c1b45ddbc6dab02b27bb26c2372bf2c967c8d6
Jesse Chan <jesse.lee.chan@gmail.com>
2026-02-13 13:50:19 -0600

    fix negative sign

--- src/equations/nonideal_compressible_euler_2d.jl
+++ src/equations/nonideal_compressible_euler_2d.jl
@@ -573,7 +573,7 @@ end
     entropy_potential(u, orientation_or_normal_direction, equations::NonIdealCompressibleEulerEquations2D)
 
 Calculate the entropy potential, which for the compressible Euler equations with general
-EOS is ``p v_i / T`` for the choice of entropy ``S(u) = \rho s``. 
+EOS is ``p v_i / T`` for the choice of entropy ``S(u) = -\rho s``. 
 """
 @inline function entropy_potential(u, orientation::Int,
                                    equations::NonIdealCompressibleEulerEquations2D)
