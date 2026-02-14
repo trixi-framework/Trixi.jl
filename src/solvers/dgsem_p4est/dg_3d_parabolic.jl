03e5bc8263b721780a2eff4612c103e6dcf6c7a1
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-02-15 00:19:52 +0100

    cosmetics

--- src/solvers/dgsem_p4est/dg_3d_parabolic.jl
+++ src/solvers/dgsem_p4est/dg_3d_parabolic.jl
@@ -603,7 +603,7 @@ function calc_mortar_flux_divergence!(surface_flux_values,
                                       dg::DG, parabolic_scheme, cache)
     @unpack neighbor_ids, node_indices = cache.mortars
     @unpack contravariant_vectors = cache.elements
-    @unpack (fstar_primary_threaded, fstar_tmp_threaded) = cache
+    @unpack fstar_primary_threaded, fstar_tmp_threaded = cache
     index_range = eachnode(dg)
 
     @threaded for mortar in eachmortar(dg, cache)
