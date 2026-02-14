bea259086024126668de7ef9086ab56c2d27844c
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-02-14 17:44:15 +0100

    rem

--- test/test_parabolic_3d.jl
+++ test/test_parabolic_3d.jl
@@ -335,16 +335,6 @@ end
     @test_allocations(Trixi.rhs_parabolic!, semi, sol, 1000)
 end
 
-@trixi_testset "P4estMesh3D: elixir_advection_diffusion_amr_unstructured_curved.jl" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "p4est_3d_dgsem",
-                                 "elixir_advection_diffusion_amr_unstructured_curved.jl"),
-                        l2=[0.00023552239536337634], linf=[0.012186740279098496],
-                        tspan=(0.0, 1.0))
-    # Ensure that we do not have excessive memory allocations
-    # (e.g., from type instabilities)
-    @test_allocations(Trixi.rhs!, semi, sol, 1000)
-end
-
 @trixi_testset "P4estMesh3D: elixir_navierstokes_convergence.jl" begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "p4est_3d_dgsem",
                                  "elixir_navierstokes_convergence.jl"),
