4fad257473dd2b7165680b1740f0576576629a6f
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2026-02-17 15:01:47 +0000

    Update src/meshes/p4est_mesh_view.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/meshes/p4est_mesh_view.jl
+++ src/meshes/p4est_mesh_view.jl
@@ -237,7 +237,7 @@ function extract_neighbor_ids_global(mesh::P4estMeshView,
                     end
                     if boundaries_parent.name[parent_idx] == :y_neg
                         neighbor_ids_global[idx] = parent_ypos_element_ids[findfirst(parent_yneg_element_ids .==
-                                                                                                             boundary)]
+                                                                                     boundary)]
                     end
                     if boundaries_parent.name[parent_idx] == :y_pos
                         neighbor_ids_global[idx] = parent_yneg_element_ids[findfirst(parent_ypos_element_ids .==
