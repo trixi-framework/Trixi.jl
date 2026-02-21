f31fc4e9565a079dc4cc950601b815324ac3ad59
Daniel Doehring <doehringd2@gmail.com>
2026-02-21 10:35:42 +0100

    Update src/solvers/dg.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/solvers/dg.jl
+++ src/solvers/dg.jl
@@ -514,7 +514,8 @@ For the DG-subcells at the boundaries, two options are available:
     2) [`koren_flipped`](@ref) for negative (left-going) velocities
 
 The reconstruction is performed in reconstruction variables, which default to the primitive variables.
-Other choices are possible, e.g. thermodynamic variables, see [`cons2thermo`](@ref) and [`thermo2cons`](@ref).
+Other choices are possible, e.g., thermodynamic variables, see [`cons2thermo`](@ref) and [`thermo2cons`](@ref)
+for [`NonIdealCompressibleEulerEquations1D`](@ref) and [`NonIdealCompressibleEulerEquations2D`](@ref).
 
 !!! note "Conservative Systems only"
     Currently only implemented for systems in conservative form, i.e.,
