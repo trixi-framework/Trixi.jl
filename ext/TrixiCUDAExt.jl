e320bc52afd30e509e39b92d8820b96a3b16ee0d
Valentin Churavy <v.churavy@gmail.com>
2026-02-24 15:08:12 +0100

    define unsafe_wrap_or_alloc fuer CUDA.KernelAdaptor

--- ext/TrixiCUDAExt.jl
+++ ext/TrixiCUDAExt.jl
@@ -8,4 +8,8 @@ function Trixi.storage_type(::Type{<:CuArray})
     return CuArray
 end
 
+function Trixi.unsafe_wrap_or_alloc(::CUDA.KernelAdaptor, vec, size)
+    return Trixi.unsafe_wrap_or_alloc(CuDeviceArray, vec, size)
+end
+
 end
