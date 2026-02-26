627623fe9b488ac1158a77fc86c3651192dc0bc2
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-02-26 10:19:14 +0100

    refd

--- examples/tree_1d_dgsem/elixir_wave_convergence.jl
+++ examples/tree_1d_dgsem/elixir_wave_convergence.jl
@@ -12,7 +12,6 @@ solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
 
 coordinates_min = (-1.0)
 coordinates_max = (1.0)
-
 mesh = TreeMesh(coordinates_min, coordinates_max,
                 initial_refinement_level = 4,
                 n_cells_max = 30_000, periodicity = false)
