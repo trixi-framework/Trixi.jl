872a74b57720260b870edc8d3d2cc97b8b18f481
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-02-26 09:57:56 +0100

    project

--- examples/unstructured_2d_dgsem/elixir_euler_free_stream.jl
+++ examples/unstructured_2d_dgsem/elixir_euler_free_stream.jl
@@ -54,7 +54,7 @@ save_solution = SaveSolutionCallback(interval = 100,
 
 stepsize_callback = StepsizeCallback(cfl = 1.0)
 
-callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, save_solution,
+callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, #save_solution,
                         stepsize_callback)
 
 ###############################################################################
