44ee6dfec9db5462d119dee876604fef5001932e
SimonCan <simon.candelaresi@gmail.com>
2026-02-27 12:18:25 +0000

    Updated doc string for struct BoundaryConditionCoupledP4est.

--- src/semidiscretization/semidiscretization_coupled_p4est.jl
+++ src/semidiscretization/semidiscretization_coupled_p4est.jl
@@ -318,40 +318,13 @@ end
 ################################################################################
 
 """
-    BoundaryConditionCoupled(other_semi_index, indices, uEltype, coupling_converter)
+    BoundaryConditionCoupledP4est(coupling_converter)
 
-Boundary condition to glue two meshes together. Solution values at the boundary
-of another mesh will be used as boundary values. This requires the use
-of [`SemidiscretizationCoupledP4est`](@ref). The other mesh is specified by `other_semi_index`,
-which is the index of the mesh in the tuple of semidiscretizations.
-
-Note that the elements and nodes of the two meshes at the coupled boundary must coincide.
-This is currently only implemented for [`StructuredMesh`](@ref).
+Boundary condition struct where the user can specify the coupling converter function.
 
 # Arguments
-- `other_semi_index`: the index in `SemidiscretizationCoupled` of the semidiscretization
-                      from which the values are copied
-- `indices::Tuple`: node/cell indices at the boundary of the mesh in the other
-                    semidiscretization. See examples below.
-- `uEltype::Type`: element type of solution
 - `coupling_converter::CouplingConverter`: function to call for converting the solution
                                            state of one system to the other system
-
-# Examples
-```julia
-# Connect the left boundary of mesh 2 to our boundary such that our positive
-# boundary direction will match the positive y direction of the other boundary
-BoundaryConditionCoupled(2, (:begin, :i), Float64, fun)
-
-# Connect the same two boundaries oppositely oriented
-BoundaryConditionCoupled(2, (:begin, :i_backwards), Float64, fun)
-
-# Using this as y_neg boundary will connect `our_cells[i, 1, j]` to `other_cells[j, end-i, end]`
-BoundaryConditionCoupled(2, (:j, :i_backwards, :end), Float64, fun)
-```
-
-!!! warning "Experimental code"
-    This is an experimental feature and can change any time.
 """
 mutable struct BoundaryConditionCoupledP4est{CouplingConverter}
     coupling_converter::CouplingConverter
