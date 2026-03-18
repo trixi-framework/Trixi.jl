0a3448f8097de1ba39ff7185950f17b0e1f64867
Benedict Geihe <bgeihe@uni-koeln.de>
2026-03-18 09:30:29 +0100

    add backend argument for coupled semis

--- src/solvers/dgsem_p4est/dg_2d.jl
+++ src/solvers/dgsem_p4est/dg_2d.jl
@@ -994,7 +994,7 @@ end
 # Call this for coupled P4estMeshView simulations.
 # The coupling calculations (especially boundary conditions) require data from the parent mesh, which is why
 # the additional variable u_parent is needed, compared to non-coupled systems.
-function rhs!(du, u, t, u_parent, semis,
+function rhs!(backend, du, u, t, u_parent, semis,
               mesh::P4estMeshView{2},
               equations,
               boundary_conditions, source_terms::Source,
@@ -1004,19 +1004,19 @@ function rhs!(du, u, t, u_parent, semis,
 
     # Calculate volume integral
     @trixi_timeit timer() "volume integral" begin
-        calc_volume_integral!(du, u, mesh,
+        calc_volume_integral!(backend, du, u, mesh,
                               have_nonconservative_terms(equations), equations,
                               dg.volume_integral, dg, cache)
     end
 
     # Prolong solution to interfaces
     @trixi_timeit timer() "prolong2interfaces" begin
-        prolong2interfaces!(cache, u, mesh, equations, dg)
+        prolong2interfaces!(backend, cache, u, mesh, equations, dg)
     end
 
     # Calculate interface fluxes
     @trixi_timeit timer() "interface flux" begin
-        calc_interface_flux!(cache.elements.surface_flux_values, mesh,
+        calc_interface_flux!(backend, cache.elements.surface_flux_values, mesh,
                              have_nonconservative_terms(equations), equations,
                              dg.surface_integral, dg, cache)
     end
@@ -1048,12 +1048,12 @@ function rhs!(du, u, t, u_parent, semis,
 
     # Calculate surface integrals
     @trixi_timeit timer() "surface integral" begin
-        calc_surface_integral!(du, u, mesh, equations,
+        calc_surface_integral!(backend, du, u, mesh, equations,
                                dg.surface_integral, dg, cache)
     end
 
     # Apply Jacobian from mapping to reference element
-    @trixi_timeit timer() "Jacobian" apply_jacobian!(du, mesh, equations, dg, cache)
+    @trixi_timeit timer() "Jacobian" apply_jacobian!(backend, du, mesh, equations, dg, cache)
 
     # Calculate source terms
     @trixi_timeit timer() "source terms" begin
