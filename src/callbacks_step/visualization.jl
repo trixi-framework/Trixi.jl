c1ebfe5b4ed7355475559a0f884fecdccd61cdf4
Shahu Karale <shahukarale283@gmail.com>
2026-03-23 13:47:44 +0530

    Restore recipes_plots.jl include and apply Trixi formatting

--- src/callbacks_step/visualization.jl
+++ src/callbacks_step/visualization.jl
@@ -188,40 +188,7 @@ This function is the default `plot_creator` argument for the [`VisualizationCall
 
 See also: [`VisualizationCallback`](@ref), [`save_plot`](@ref)
 """
-function show_plot(plot_data, variable_names;
-                   show_mesh = true, plot_arguments = Dict{Symbol, Any}(),
-                   time = nothing, timestep = nothing)
-    # Gather subplots
-    plots = []
-    for v in variable_names
-        push!(plots, Plots.plot(plot_data[v]; plot_arguments...))
-    end
-    if show_mesh
-        push!(plots, Plots.plot(getmesh(plot_data); plot_arguments...))
-    end
-
-    # Note, for the visualization callback to work for general equation systems
-    # this layout construction would need to use the if-logic below.
-    # Currently, there is no use case for this so it is left here as a note.
-    #
-    # Determine layout
-    # if length(plots) <= 3
-    #   cols = length(plots)
-    #   rows = 1
-    # else
-    #   cols = ceil(Int, sqrt(length(plots)))
-    #   rows = div(length(plots), cols, RoundUp)
-    # end
-    # layout = (rows, cols)
-
-    # Determine layout
-    cols = ceil(Int, sqrt(length(plots)))
-    rows = div(length(plots), cols, RoundUp)
-    layout = (rows, cols)
-
-    # Show plot
-    return display(Plots.plot(plots..., layout = layout))
-end
+function show_plot end
 
 """
     save_plot(plot_data, variable_names;
@@ -237,28 +204,5 @@ The `timestep` is used in the filename. `time` is currently unused by this funct
 
 See also: [`VisualizationCallback`](@ref), [`show_plot`](@ref)
 """
-function save_plot(plot_data, variable_names;
-                   show_mesh = true, plot_arguments = Dict{Symbol, Any}(),
-                   time = nothing, timestep = nothing)
-    # Gather subplots
-    plots = []
-    for v in variable_names
-        push!(plots, Plots.plot(plot_data[v]; plot_arguments...))
-    end
-    if show_mesh
-        push!(plots, Plots.plot(getmesh(plot_data); plot_arguments...))
-    end
-
-    # Determine layout
-    cols = ceil(Int, sqrt(length(plots)))
-    rows = div(length(plots), cols, RoundUp)
-    layout = (rows, cols)
-
-    # Create plot
-    Plots.plot(plots..., layout = layout)
-
-    # Determine filename and save plot
-    filename = joinpath("out", @sprintf("solution_%09d.png", timestep))
-    return Plots.savefig(filename)
-end
+function save_plot end
 end # @muladd
