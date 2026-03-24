2962ed78ad6db9ea8c96c4f6f99a037a5d498ee1
Valentin Churavy <v.churavy@gmail.com>
2026-03-24 13:50:04 +0100

    add device_override for Trixi.log

--- ext/TrixiCUDAExt.jl
+++ ext/TrixiCUDAExt.jl
@@ -1,7 +1,7 @@
 # Package extension for adding CUDA-based features to Trixi.jl
 module TrixiCUDAExt
 
-import CUDA: CuArray, CuDeviceArray, KernelAdaptor
+import CUDA: CuArray, CuDeviceArray, KernelAdaptor, @device_override
 import Trixi
 
 function Trixi.storage_type(::Type{<:CuArray})
@@ -14,6 +14,14 @@ end
 
 function Trixi.unsafe_wrap_or_alloc(::Type{<:CuDeviceArray}, vec::CuDeviceArray, size)
     return reshape(vec, size)
+end
+
+@static if TRIXI._PREFERENCE_LOG == "log_Trixi_NaN"
+    @device_override Trixi.log(x::Float64) = ccall("extern __nv_log", llvmcall, Cdouble,
+                                                   (Cdouble,), x)
+    @device_override Trixi.log(x::Float32) = ccall("extern __nv_logf", llvmcall, Cfloat,
+                                                   (Cfloat,), x)
+    # TODO: Trixi.log(x::Float16)
 end
 
 end
