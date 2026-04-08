b520cb5da0d69bba1721196192c44c8a9460d7cf
Daniel Doehring <daniel.doehring@rwth-aachen.de>
2026-04-08 06:51:17 +0200

    Remove `gamma`, `inv_gamma_minus_1` from NSF; rename `Fick` => `Fourier` (#2877)
    * rm gamma
    
    * propertynames
    
    * rev
    
    * comment
    
    * Update src/equations/equations_parabolic.jl
    
    Co-authored-by: Jesse Chan <1156048+jlchan@users.noreply.github.com>
    
    * restruct
    
    * comment
    
    * pr
    
    * try
    
    * rev
    
    * promote
    
    * fix
    
    * news
    
    * bb
    
    * rev rev
    
    * rev
    
    * consistency
    
    * simplify
    
    * rev
    
    * shorten
    
    * rev, check allocs
    
    * avoid "."
    
    * propertynames
    
    * Update src/equations/compressible_navier_stokes.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_navier_stokes.jl
    
    * fmt
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Jesse Chan <1156048+jlchan@users.noreply.github.com>
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/equations/compressible_navier_stokes.jl
+++ src/equations/compressible_navier_stokes.jl
@@ -1,4 +1,31 @@
-# TODO: can we generalize this to MHD?
+
+# Compressible Navier-Stokes equations
+abstract type AbstractCompressibleNavierStokesDiffusion{NDIMS, NVARS, GradientVariables} <:
+              AbstractEquationsParabolic{NDIMS, NVARS, GradientVariables} end
+
+# This enables "forwarded" accesses to e.g.`equations_parabolic.gamma` of the "underlying" `equations_hyperbolic`
+# while keeping direct access to parabolic-specific fields like `mu` or `kappa`.
+@inline function Base.getproperty(equations_parabolic::AbstractCompressibleNavierStokesDiffusion,
+                                  field::Symbol)
+    if field === :gamma || field === :inv_gamma_minus_one
+        return getproperty(getfield(equations_parabolic, :equations_hyperbolic), field)
+    else
+        return getfield(equations_parabolic, field)
+    end
+end
+
+# Provide property names for e.g. tab-completion by combining
+# the names from the underlying hyperbolic equations with the fields of this parabolic part.
+@inline function Base.propertynames(equations_parabolic::AbstractCompressibleNavierStokesDiffusion,
+                                    private::Bool = false)
+    names_hyp = (:gamma, :inv_gamma_minus_one)
+    names_para = collect(fieldnames(typeof(equations_parabolic)))
+    names_hyp_para = (names_hyp..., names_para...)
+
+    return names_hyp_para
+end
+
+# TODO: can we generalize this to V(R)-MHD?
 """
     struct BoundaryConditionNavierStokesWall
 
@@ -123,3 +150,7 @@ Also employed in [`linear_structure`](@ref) and [`linear_structure_parabolic`](@
 if the diffusion term is linear in the variables/constant.
 """
 @inline have_constant_diffusivity(::AbstractCompressibleNavierStokesDiffusion) = False()
+
+include("compressible_navier_stokes_1d.jl")
+include("compressible_navier_stokes_2d.jl")
+include("compressible_navier_stokes_3d.jl")
