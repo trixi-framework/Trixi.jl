41761050ab13bbe0ada1e5b014bccc377b3a42a8
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-04-09 14:28:34 +0200

    clean up

--- src/time_integration/methods_SSP.jl
+++ src/time_integration/methods_SSP.jl
@@ -134,16 +134,7 @@ function init(ode::ODEProblem, alg::SimpleAlgorithmSSP;
 
     # Resize container of volume integral for subcell limiting
     _, _, dg, cache = mesh_equations_solver_cache(integrator.p)
-    if dg.volume_integral isa VolumeIntegralSubcellLimiting
-        # `subcell_limiter_coefficients` was created with 0 elements
-        resize!(dg.volume_integral.limiter.cache.subcell_limiter_coefficients,
-                nelements(dg, cache))
-        # TODO: Quick & dirty, need function that dispatches here correctly!
-    elseif dg.volume_integral isa VolumeIntegralAdaptive
-        # `subcell_limiter_coefficients` was created with 0 elements
-        resize!(dg.volume_integral.volume_integral_stabilized.limiter.cache.subcell_limiter_coefficients,
-                nelements(dg, cache))
-    end
+    resize!(ode.p, dg.volume_integral, nelements(dg, cache))
 
     # Standard callbacks
     initialize_callbacks!(callback, integrator)
