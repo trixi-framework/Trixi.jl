9b0ab873985d1eb6b7b5b1b1d9adb4eafcb1ed73
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-04-10 09:27:06 +0200

    start

--- src/solvers/dg.jl
+++ src/solvers/dg.jl
@@ -737,6 +737,17 @@ function Base.show(io::IO, mime::MIME"text/plain",
     end
 end
 
+function resize_volume_integral_cache!(cache, mesh,
+                                       volume_integral::VolumeIntegralSubcellLimiting,
+                                       new_size)
+    # Resize container antidiffusive_fluxes
+    resize!(cache.antidiffusive_fluxes, new_size)
+
+    # Resize container subcell_limiter_coefficients
+    @unpack limiter = volume_integral
+    return resize!(limiter.cache.subcell_limiter_coefficients, new_size)
+end
+
 # TODO: FD. Should this definition live in a different file because it is
 # not strictly a DG method?
 """
