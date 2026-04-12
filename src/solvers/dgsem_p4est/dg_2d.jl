f162652474ca928251d5abfd6cf7180caee33734
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-04-12 22:41:29 +0200

    cont

--- src/solvers/dgsem_p4est/dg_2d.jl
+++ src/solvers/dgsem_p4est/dg_2d.jl
@@ -953,7 +953,45 @@ end
                                      have_nonconservative_terms::True,
                                      combine_conservative_and_nonconservative_fluxes::True,
                                      equations,
-                                     surface_integral, dg::DG, cache,
+                                     surface_integral,
+                                     dg::DGSEM{<:LobattoLegendreBasis}, cache,
+                                     i_index, j_index,
+                                     node_index, direction_index, element_index,
+                                     boundary_index)
+    @unpack elements, boundaries = cache
+    @unpack node_coordinates, contravariant_vectors = elements
+
+    # Extract solution data from boundary container
+    u_inner = get_node_vars(boundaries.u, equations, dg, node_index, boundary_index)
+
+    # Outward-pointing normal direction (not normalized)
+    normal_direction = get_normal_direction(direction_index, contravariant_vectors,
+                                            i_index, j_index, element_index)
+
+    # Coordinates at boundary node
+    x = get_node_coords(node_coordinates, equations, dg,
+                        i_index, j_index, element_index)
+
+    # Call pointwise numerical flux functions for the conservative and nonconservative part
+    # in the normal direction on the boundary
+    flux = boundary_condition(u_inner, normal_direction, x, t,
+                              surface_integral.surface_flux, equations)
+
+    # Copy flux to element storage in the correct orientation
+    for v in eachvariable(equations)
+        surface_flux_values[v, node_index, direction_index, element_index] = flux[v]
+    end
+
+    return nothing
+end
+
+@inline function calc_boundary_flux!(surface_flux_values, t, boundary_condition,
+                                     mesh::Union{P4estMesh{2}, T8codeMesh{2}},
+                                     have_nonconservative_terms::True,
+                                     combine_conservative_and_nonconservative_fluxes::True,
+                                     equations,
+                                     surface_integral, dg::DGSEM{<:GaussLegendreBasis},
+                                     cache,
                                      i_index, j_index,
                                      node_index, direction_index, element_index,
                                      boundary_index)
