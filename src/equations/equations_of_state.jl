7af82922a55888c1201bd22651569177b8b56996
Jesse Chan <jesse.lee.chan@gmail.com>
2026-04-18 15:20:56 -0500

    fix typo unrelated to this PR

--- src/equations/equations_of_state.jl
+++ src/equations/equations_of_state.jl
@@ -89,7 +89,7 @@ function temperature(V, e_internal, eos::AbstractEquationOfState;
         iter += 1
     end
     if iter == maxiter
-        @warn "Solver in `temperature(V, T, eos)` did not converge within $maxiter iterations. " *
+        @warn "Solver in `temperature(V, e_internal, eos)` did not converge within $maxiter iterations. " *
               "Final states: iter = $iter, V, e_internal = $V, $(e_internal) with de = $de"
     end
     return T
