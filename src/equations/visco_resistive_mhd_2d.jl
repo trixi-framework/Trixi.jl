e7369a03e77cbbaa298c8648ae69db52fcf31a26
Simon Candelaresi <10759273+SimonCan@users.noreply.github.com>
2026-04-24 15:32:46 +0100

    Update src/equations/visco_resistive_mhd_2d.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/equations/visco_resistive_mhd_2d.jl
+++ src/equations/visco_resistive_mhd_2d.jl
@@ -207,9 +207,7 @@ end
 # Note, the first component of `gradient_entropy_vars` contains gradient(rho) which is unused.
 # TODO: parabolic; entropy stable viscous terms
 @inline function convert_derivative_to_primitive(u, gradient,
-                                                 ::ViscoResistiveMhd2D{
-                                                                       GradientVariablesPrimitive
-                                                                       })
+                                                 ::ViscoResistiveMhd2D{GradientVariablesPrimitive})
     return gradient
 end
 
