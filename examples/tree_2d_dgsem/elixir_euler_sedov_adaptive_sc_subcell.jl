562b788cc5687ef32f8f67f3d29163e74fa24bbe
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-04-27 09:11:21 +0200

    save

--- examples/tree_2d_dgsem/elixir_euler_sedov_adaptive_sc_subcell.jl
+++ examples/tree_2d_dgsem/elixir_euler_sedov_adaptive_sc_subcell.jl
@@ -107,7 +107,7 @@ callbacks = CallbackSet(summary_callback,
 # run the simulation
 
 stage_callbacks = (SubcellLimiterIDPCorrection(),
-                   BoundsCheckCallback(interval = 100))
+                   BoundsCheckCallback(interval = 100, save_errors = true))
 
 sol = Trixi.solve(ode, Trixi.SimpleSSPRK33(stage_callbacks = stage_callbacks);
                   dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
