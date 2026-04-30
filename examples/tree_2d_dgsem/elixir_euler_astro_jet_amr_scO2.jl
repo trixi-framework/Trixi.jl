e66834d8eeaa579a516c05283a6b016e6abf1dca
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-04-30 18:14:20 +0200

    get qsteady_max from ode algorithm

--- examples/tree_2d_dgsem/elixir_euler_astro_jet_amr_scO2.jl
+++ examples/tree_2d_dgsem/elixir_euler_astro_jet_amr_scO2.jl
@@ -1,5 +1,5 @@
 using OrdinaryDiffEqSSPRK
-using OrdinaryDiffEqCore: PIDController
+using OrdinaryDiffEqCore: OrdinaryDiffEqCore, PIDController
 using Trixi
 
 ###############################################################################
@@ -105,7 +105,9 @@ stage_limiter! = PositivityPreservingLimiterZhangShu(thresholds = (5.0e-6, 5.0e-
 ###############################################################################
 # run the simulation
 
+ode_algorithm = SSPRK43(stage_limiter! = stage_limiter!, thread = Trixi.Threaded())
+qsteady_max = OrdinaryDiffEqCore.qsteady_max_default(ode_algorithm)
 # use adaptive time stepping based on error estimates
-sol = solve(ode, SSPRK43(; stage_limiter! = stage_limiter!, thread = Trixi.Threaded());
-            controller = PIDController(0.55, -0.27, 0.05, qsteady_max = 1.0),
+sol = solve(ode, ode_algorithm;
+            controller = PIDController(0.55, -0.27, 0.05, qsteady_max = qsteady_max),
             ode_default_options()..., callback = callbacks);
