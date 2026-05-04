bdd97fc394ccdbfa17ae786c2c794cc41e76fab1
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-05-04 23:28:42 +0200

    comments

--- test/test_threaded.jl
+++ test/test_threaded.jl
@@ -43,6 +43,7 @@ Trixi.MPI.Barrier(Trixi.mpi_comm())
     # This test covers some of the code paths for the old controller interface in OrdinaryDiffEqCore < v4,
     # which is still supported for backward compatibility.
     @trixi_testset "elixir_advection_restart.jl with adaptive time integration" begin
+        # Perform a standard simulation
         using OrdinaryDiffEqSSPRK: SSPRK43
         println("═"^100)
         println(joinpath(EXAMPLES_DIR, "elixir_advection_timeintegration_adaptive.jl"))
@@ -52,15 +53,17 @@ Trixi.MPI.Barrier(Trixi.mpi_comm())
                       alg = SSPRK43(), tspan = (0.0, 10.0))
         l2_expected, linf_expected = analysis_callback(sol)
 
+         # Perform a simulation restarting from an intermediate state
         println("═"^100)
         println(joinpath(EXAMPLES_DIR, "elixir_advection_restart.jl"))
-        # Errors are exactly the same as in the elixir_advection_extended.jl
         trixi_include(@__MODULE__,
                       joinpath(EXAMPLES_DIR, "elixir_advection_restart.jl"),
                       alg = SSPRK43(),
                       base_elixir = "elixir_advection_timeintegration_adaptive.jl")
         l2_actual, linf_actual = analysis_callback(sol)
 
+        # Check whether the errors are exactly the same as in the uninterrupted run
+        # using the default low-storage RK method with a StepsizeCallback.
         @test l2_actual == l2_expected
         @test linf_actual == linf_expected
     end
