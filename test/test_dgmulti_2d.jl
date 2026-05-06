67ee1caa42f618a9d0d52121191025f46f2b8654
Jesse Chan <jesse.lee.chan@gmail.com>
2026-05-05 22:42:59 -0500

    fix test

--- test/test_dgmulti_2d.jl
+++ test/test_dgmulti_2d.jl
@@ -310,21 +310,27 @@ end
 end
 
 @trixi_testset "elixir_euler_gmsh_square_cylinder.jl" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_gmsh_square_cylinder.jl"),
-                        polydeg=2, adaptive=false, tspan=(0.0, 1e-3),
-                        l2=[
-                            0.00045155999383061246,
-                            0.0007997114439550152,
-                            2.370224947246212e-6,
-                            0.002037206524508959
-                        ],
-                        linf=[
-                            0.07544209921557377,
-                            0.17673745170823008,
-                            0.0009825284037637686,
-                            0.3386662161532019
-                        ])
-    # No `@test_allocations`: HG shock capturing on this Gmsh mesh allocates in `rhs!`.
+    # @test_trixi_include errors due to an @info call by StartUpDG.jl during 
+    # Gmsh file parsing. To avoid this, we directly call trixi_include. 
+    # We pass @__MODULE__ to ensure that variables defined during the test 
+    # are visible inside the @trixi_testset block. 
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR, "elixir_euler_gmsh_square_cylinder.jl"),
+                  polydeg = 2, adaptive = false, tspan = (0.0, 1e-3))
+    l2, linf = analysis_callback(sol)
+    @test isapprox(l2,
+                   [0.00045155999383061246,
+                       0.0007997114439550152,
+                       2.370224947246212e-6,
+                       0.002037206524508959
+                   ])
+    @test isapprox(linf,
+                   [
+                       0.07544209921557377,
+                       0.17673745170823008,
+                       0.0009825284037637686,
+                       0.3386662161532019
+                   ])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
