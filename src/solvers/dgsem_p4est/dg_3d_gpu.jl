74e140da6fcbcf15d7ba46b898ac4c239ac8d37d
Marco Artiano <artianomarco97@gmail.com>
2026-05-18 19:28:59 +0200

    refactor also 3D kernels

--- src/solvers/dgsem_p4est/dg_3d_gpu.jl
+++ src/solvers/dgsem_p4est/dg_3d_gpu.jl
@@ -5,6 +5,82 @@
 @muladd begin
 #! format: noindent
 
+function prolong2interfaces!(backend::Backend, cache, u,
+                             mesh::Union{P4estMesh{3}, T8codeMesh{3}},
+                             equations, dg::DG)
+    @unpack interfaces = cache
+    @unpack neighbor_ids, node_indices = cache.interfaces
+    index_range = eachnode(dg)
+
+    kernel! = prolong2interfaces_KAkernel!(backend)
+    kernel!(interfaces.u, u, typeof(mesh), equations, neighbor_ids, node_indices,
+            index_range,
+            ndrange = ninterfaces(interfaces))
+    return nothing
+end
+
+@kernel function prolong2interfaces_KAkernel!(interface_u, u, MeshT, equations,
+                                              neighbor_ids, node_indices, index_range)
+    interface = @index(Global)
+    prolong2interfaces_per_interface!(interface_u, u, MeshT, equations, neighbor_ids,
+                                      node_indices, index_range, interface)
+end
+
+function calc_surface_integral!(backend::Backend, du, u,
+                                mesh::Union{P4estMesh{3}, T8codeMesh{3}},
+                                equations,
+                                surface_integral::SurfaceIntegralWeakForm,
+                                dg::DGSEM, cache)
+    @unpack inverse_weights = dg.basis
+    @unpack surface_flux_values = cache.elements
+
+    kernel! = calc_surface_integral_KAkernel!(backend)
+    kernel!(du, typeof(mesh), equations, surface_integral, dg, inverse_weights[1],
+            surface_flux_values, ndrange = nelements(cache.elements))
+    return nothing
+end
+
+@kernel function calc_surface_integral_KAkernel!(du, MeshT, equations,
+                                                 surface_integral, dg, factor,
+                                                 surface_flux_values)
+    element = @index(Global)
+    calc_surface_integral_per_element!(du, MeshT,
+                                       equations, surface_integral, dg, factor,
+                                       surface_flux_values, element)
+end
+
+function calc_interface_flux!(backend::Backend, surface_flux_values,
+                              mesh::Union{P4estMesh{3}, T8codeMesh{3}},
+                              have_nonconservative_terms,
+                              equations, surface_integral, dg::DG, cache)
+    @unpack neighbor_ids, node_indices = cache.interfaces
+    @unpack contravariant_vectors = cache.elements
+    index_range = eachnode(dg)
+
+    kernel! = calc_interface_flux_KAkernel!(backend)
+    kernel!(surface_flux_values, typeof(mesh), have_nonconservative_terms, equations,
+            surface_integral, typeof(dg), cache.interfaces.u,
+            neighbor_ids, node_indices, contravariant_vectors, index_range,
+            ndrange = ninterfaces(cache.interfaces))
+    return nothing
+end
+
+@kernel function calc_interface_flux_KAkernel!(surface_flux_values, MeshT,
+                                               have_nonconservative_terms, equations,
+                                               surface_integral, SolverT, u_interface,
+                                               neighbor_ids, node_indices,
+                                               contravariant_vectors, index_range)
+    interface = @index(Global)
+    calc_interface_flux_per_interface!(surface_flux_values,
+                                       MeshT,
+                                       have_nonconservative_terms,
+                                       equations, surface_integral, SolverT,
+                                       u_interface,
+                                       neighbor_ids, node_indices,
+                                       contravariant_vectors,
+                                       index_range, interface)
+end
+
 @kernel function calc_sources_KAkernel!(du, u, t, source_terms,
                                         node_coordinates,
                                         equations::AbstractEquations{3}, dg, cache)
