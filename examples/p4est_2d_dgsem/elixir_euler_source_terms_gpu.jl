d66c664f762188adf3b8a6f5f2483e300b6db03c
Marco Artiano <artianomarco97@gmail.com>
2026-05-19 08:16:26 +0200

    fix elixirs

--- examples/p4est_2d_dgsem/elixir_euler_source_terms_gpu.jl
+++ examples/p4est_2d_dgsem/elixir_euler_source_terms_gpu.jl
@@ -37,7 +37,7 @@ semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver;
 # ODE solvers, callbacks etc.
 
 tspan = (0.0, 2.0)
-ode = semidiscretize(semi, tspan; real_type = real_type, storage_type = storage_type)
+ode = semidiscretize(semi, tspan; real_type = nothing, storage_type = nothing)
 
 summary_callback = SummaryCallback()
 
