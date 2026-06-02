6804698ef6a53bf4d4436f9b35fd1c5572264896
MarcoArtiano <artianomarco97@gmail.com>
2026-06-02 14:28:03 +0200

    fix path

--- test/test_kernelabstractions.jl
+++ test/test_kernelabstractions.jl
@@ -81,6 +81,28 @@ end
     semi = ode.p # `semidiscretize` adapts the semi, so we need to obtain it from the ODE problem.
     @test_allocations(Trixi.rhs!, semi, sol, 600_000)
 end
+
+@trixi_testset "elixir_euler_source_terms.jl Flux Differencing Float32" begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR, "p4est_2d_dgsem",
+                                 "elixir_euler_source_terms.jl"),
+                        l2=Float32[2.7905685982444506e-6,
+                                   2.7719663804722356e-6,
+                                   2.862595247100584e-6,
+                                   6.59779451858695e-6],
+                        linf=Float32[1.904964447030366e-5,
+                                     2.1734684234164803e-5,
+                                     1.988410949715913e-5,
+                                     5.9757232666157734e-5],
+                        RealT_for_test_tolerances=Float32,
+                        real_type=Float32,
+                        solver=DGSEM(polydeg = 3,
+                                     surface_flux = FluxLaxFriedrichs(max_abs_speed_naive),
+                                     volume_integral = VolumeIntegralFluxDifferencing(flux_kennedy_gruber)))
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    semi = ode.p # `semidiscretize` adapts the semi, so we need to obtain it from the ODE problem.
+    @test_allocations(Trixi.rhs!, semi, sol, 600_000)
+end
 end
 
 @testset "KernelAbstractions CPU 3D" begin
@@ -148,27 +170,6 @@ end
                         volume_integral=VolumeIntegralFluxDifferencing(flux_kennedy_gruber),
                         RealT_for_test_tolerances=Float32,
                         real_type=Float32)
-    # Ensure that we do not have excessive memory allocations
-    # (e.g., from type instabilities)
-    semi = ode.p # `semidiscretize` adapts the semi, so we need to obtain it from the ODE problem.
-    @test_allocations(Trixi.rhs!, semi, sol, 600_000)
-end
-
-@trixi_testset "elixir_euler_source_terms.jl Flux Differencing Float32" begin
-    @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_euler_source_terms.jl"),
-                        l2=Float32[2.7905685982444506e-6,
-                                   2.7719663804722356e-6,
-                                   2.862595247100584e-6,
-                                   6.59779451858695e-6],
-                        linf=Float32[1.904964447030366e-5,
-                                     2.1734684234164803e-5,
-                                     1.988410949715913e-5,
-                                     5.9757232666157734e-5],
-                        RealT_for_test_tolerances=Float32,
-                        real_type=Float32,
-                        solver=DGSEM(polydeg = 3,
-                                     surface_flux = FluxLaxFriedrichs(max_abs_speed_naive),
-                                     volume_integral = VolumeIntegralFluxDifferencing(flux_kennedy_gruber)))
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
     semi = ode.p # `semidiscretize` adapts the semi, so we need to obtain it from the ODE problem.
