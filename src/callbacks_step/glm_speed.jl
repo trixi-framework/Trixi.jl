c44b447c9773a96cedb083592772a15788144a97
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2026-06-13 12:35:05 +0200

    Apply suggestions from code review
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/callbacks_step/glm_speed.jl
+++ src/callbacks_step/glm_speed.jl
@@ -87,11 +87,8 @@ function update_cleaning_speed!(semi, glm_speed_callback, dt, t)
 
     mesh, equations, solver, cache = mesh_equations_solver_cache(semi)
 
-    RealT = eltype(cache.elements.node_coordinates)
-
     # compute time step for GLM linear advection equation with c_h=1 (redone due to the possible AMR)
-    c_h_deltat = calc_dt_for_cleaning_speed(RealT(cfl(t)), mesh, equations, solver,
-                                            cache)
+    c_h_deltat = calc_dt_for_cleaning_speed(cfl(t), mesh, equations, solver, cache)
 
     # c_h is proportional to its own time step divided by the complete MHD time step
     # We use @reset here since the equations are immutable (to work on GPUs etc.).
