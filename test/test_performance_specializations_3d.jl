2a8c4366c4ab21de34a17a5e2972743967756ef1
MarcoArtiano <artianomarco97@gmail.com>
2026-06-20 10:01:07 +0200

    remove nonconservative part

--- test/test_performance_specializations_3d.jl
+++ test/test_performance_specializations_3d.jl
@@ -243,45 +243,6 @@ end
     end
 end
 
-@timed_testset "P4estMesh3D, FluxVolumeTurbo(flux_hindenlang_gassner, flux_nonconservative_powell)" begin
-    trixi_include(@__MODULE__,
-                  joinpath(EXAMPLES_DIR, "p4est_3d_dgsem",
-                           "elixir_mhd_alfven_wave_nonperiodic.jl"),
-                  volume_flux = Trixi.FluxVolumeTurbo(flux_hindenlang_gassner,
-                                                      flux_nonconservative_powell))
-    u_ode = copy(sol.u[end])
-    du_ode = zero(u_ode)
-
-    # Preserve original memory since it will be `unsafe_wrap`ped and might
-    # thus otherwise be garbage collected
-    GC.@preserve u_ode du_ode begin
-        u = Trixi.wrap_array(u_ode, semi)
-        du = Trixi.wrap_array(du_ode, semi)
-        have_nonconservative_terms = Trixi.have_nonconservative_terms(semi.equations)
-
-        # Call the optimized version 
-        du .= 0
-        Trixi.flux_differencing_kernel!(du, u, 1, typeof(semi.mesh),
-                                        have_nonconservative_terms, semi.equations,
-                                        semi.solver.volume_integral.volume_flux,
-                                        semi.solver, semi.cache, true)
-        du_specialized = du[:, :, :, :, 1]
-
-        # Call the plain version 
-        du .= 0
-        symmetric_flux = flux_hindenlang_gassner
-        nonconservative_flux = flux_nonconservative_powell
-        Trixi.flux_differencing_kernel!(du, u, 1, typeof(semi.mesh),
-                                        have_nonconservative_terms, semi.equations,
-                                        (; symmetric_flux,
-                                         nonconservative_flux), semi.solver,
-                                        semi.cache, true)
-        du_baseline = du[:, :, :, :, 1]
-
-        @test du_specialized ≈ du_baseline
-    end
-end
-
 @timed_testset "P4estMesh3D, combine_conservative_and_nonconservative_fluxes" begin
     trixi_include(@__MODULE__,
                   joinpath(EXAMPLES_DIR, "p4est_3d_dgsem",
