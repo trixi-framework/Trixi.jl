331f6abfa0dd20d5db2b99d5848989903270d9c0
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-06-22 10:42:44 +0200

    initial TestItems.jl setup

--- test/test_tree_1d_advection.jl
+++ test/test_tree_1d_advection.jl
@@ -1,16 +1,8 @@
-module TestExamples1DAdvection
+@testsnippet TreeMesh1DAdvection begin
+    EXAMPLES_DIR = joinpath(examples_dir(), "tree_1d_dgsem")
+end
 
-using Test
-using Trixi
-
-include("test_trixi.jl")
-
-EXAMPLES_DIR = joinpath(examples_dir(), "tree_1d_dgsem")
-
-@testset "Linear scalar advection" begin
-#! format: noindent
-
-@trixi_testset "elixir_advection_basic.jl" begin
+@testitem "elixir_advection_basic.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
                         l2=[6.0388296447998465e-6],
                         linf=[3.217887726258972e-5])
@@ -19,7 +11,7 @@ EXAMPLES_DIR = joinpath(examples_dir(), "tree_1d_dgsem")
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_gauss_legendre.jl" begin
+@testitem "elixir_advection_gauss_legendre.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_gauss_legendre.jl"),
                         l2=[2.515203865524688e-6], linf=[8.660338936650191e-6])
     # Ensure that we do not have excessive memory allocations
@@ -27,7 +19,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_basic.jl (max_abs_speed)" begin
+@testitem "elixir_advection_basic.jl (max_abs_speed)" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
                         surface_flux=FluxLaxFriedrichs(max_abs_speed),
                         l2=[6.0388296447998465e-6],
@@ -37,7 +29,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_amr.jl" begin
+@testitem "elixir_advection_amr.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_amr.jl"),
                         l2=[0.3540206249507417],
                         linf=[0.9999896603382347])
@@ -46,7 +38,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_amr_nonperiodic.jl" begin
+@testitem "elixir_advection_amr_nonperiodic.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_amr_nonperiodic.jl"),
                         l2=[4.283508859843524e-6],
                         linf=[3.235356127918171e-5])
@@ -55,7 +47,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_basic.jl (No errors)" begin
+@testitem "elixir_advection_basic.jl (No errors)" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_basic.jl"),
                         analysis_callback=AnalysisCallback(semi, interval = 42,
                                                            analysis_errors = Symbol[]))
@@ -64,7 +56,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_convergence_fvO2.jl" begin
+@testitem "elixir_advection_convergence_fvO2.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_convergence_fvO2.jl"),
                         l2=[0.0024544920169555706], linf=[0.007837347144210138])
     # Ensure that we do not have excessive memory allocations
@@ -72,7 +64,7 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_finite_volume.jl" begin
+@testitem "elixir_advection_finite_volume.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_finite_volume.jl"),
                         l2=[0.011662300515980219],
                         linf=[0.01647256923710194])
@@ -81,21 +73,21 @@ end
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
 
-@trixi_testset "elixir_advection_perk2.jl" begin
+@testitem "elixir_advection_perk2.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_perk2.jl"),
                         l2=[0.011288030389423475],
                         linf=[0.01596735472556976])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
-    # Larger values for allowed allocations due to usage of custom 
-    # integrator which are not *recorded* for the methods from 
+    # Larger values for allowed allocations due to usage of custom
+    # integrator which are not *recorded* for the methods from
     # OrdinaryDiffEq.jl
     # Corresponding issue: https://github.com/trixi-framework/Trixi.jl/issues/1877
     @test_allocations(Trixi.rhs!, semi, sol, 8000)
 end
 
 # Testing the second-order paired explicit Runge-Kutta (PERK) method without stepsize callback
-@trixi_testset "elixir_advection_perk2.jl(fixed time step)" begin
+@testitem "elixir_advection_perk2.jl(fixed time step)" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_perk2.jl"),
                         dt=2.0e-3,
                         tspan=(0.0, 20.0),
@@ -106,28 +98,36 @@ end
                         linf=[3.729460413781638e-5])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
-    # Larger values for allowed allocations due to usage of custom 
-    # integrator which are not *recorded* for the methods from 
+    # Larger values for allowed allocations due to usage of custom
+    # integrator which are not *recorded* for the methods from
     # OrdinaryDiffEq.jl
     # Corresponding issue: https://github.com/trixi-framework/Trixi.jl/issues/1877
     @test_allocations(Trixi.rhs!, semi, sol, 8000)
 end
 
 # Testing the second-order paired explicit Runge-Kutta (PERK) method with the optimal CFL number
-@trixi_testset "elixir_advection_perk2_optimal_cfl.jl" begin
+@testitem "elixir_advection_perk2_optimal_cfl.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_perk2_optimal_cfl.jl"),
                         l2=[0.0009700887119146429],
                         linf=[0.00137209242077041])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
-    # Larger values for allowed allocations due to usage of custom 
-    # integrator which are not *recorded* for the methods from 
+    # Larger values for allowed allocations due to usage of custom
+    # integrator which are not *recorded* for the methods from
     # OrdinaryDiffEq.jl
     # Corresponding issue: https://github.com/trixi-framework/Trixi.jl/issues/1877
     @test_allocations(Trixi.rhs!, semi, sol, 8000)
 end
 
-@trixi_testset "elixir_advection_doublefloat.jl" begin
+# TODO (TestItems.jl migration): This test uses typed reference values
+# (`l2 = Double64[...]`). `TrixiTest`'s `@test_trixi_include_base` splices the
+# `l2`/`linf` reference values into its comparison loop *unescaped*, so the bare
+# `Double64` is resolved in the `TrixiTest` module instead of this test item and
+# errors with `UndefVarError: Double64 not defined in TrixiTest`. The legacy
+# `@trixi_testset` masked this because it imported `Double64` into the same
+# temporary module that evaluated the comparison. Re-enable once `TrixiTest`
+# escapes those reference values (or another fix is agreed upon).
+@testitem "elixir_advection_doublefloat.jl" setup=[Setup, TreeMesh1DAdvection] tags=[:tree_part1] begin
     using DoubleFloats: Double64
     @test_trixi_include(joinpath(EXAMPLES_DIR, "elixir_advection_doublefloat.jl"),
                         l2=Double64[6.80895929885700039832943251427357703e-11],
@@ -136,6 +136,3 @@ end
     # (e.g., from type instabilities)
     @test_allocations(Trixi.rhs!, semi, sol, 1000)
 end
-end
-
-end # module
