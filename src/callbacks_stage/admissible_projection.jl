f3254cb67490d7dfcc33806e61f59b9ee3197934
Jesse Chan <jesse.lee.chan@gmail.com>
2026-06-21 23:58:33 -0500

    move project_to_admissible_set

--- src/callbacks_stage/admissible_projection.jl
+++ src/callbacks_stage/admissible_projection.jl
@@ -1,9 +1,17 @@
-# for any scalar equation, the admissible set is assumed to be u > u_lower, and 
-# projection to the admissible set is simply a clipping operation
-@inline function project_to_admissible_set(cell_average, lower_bound, variables,
+"""
+    project_to_admissible_set(cell_average, lower_bound, variables, equations)
+
+For scalar equations, the positivity-preserving limiter enforces `u > u_lower`, and
+projection to the admissible set is a clipping operation. 
+
+To ensure that `variables` is consistent with this assumption, users must set 
+`variables = (first,)`. 
+"""
+@inline function project_to_admissible_set(cell_average, lower_bounds,
+                                           variables::Tuple{typeof(first)},
                                            equations::AbstractEquations{NDIMS, 1}) where {NDIMS}
     # lower_bound and cell_average are SVectors of size 1
-    return SVector(max(lower_bound[1], cell_average[1]))
+    return SVector(max(lower_bounds[1], cell_average[1]))
 end
 
 include("admissible_projection_euler_1d.jl")
