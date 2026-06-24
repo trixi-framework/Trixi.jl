c402149de242e6e49691fd7e32398d636a42d3ab
bennibolm <benjamin.bolm@gmx.de>
2026-06-24 15:12:08 +0200

    Add comment and source to elixirs

--- examples/tree_1d_dgsem/elixir_euler_positivity.jl
+++ examples/tree_1d_dgsem/elixir_euler_positivity.jl
@@ -83,6 +83,11 @@ save_solution = SaveSolutionCallback(interval = 100,
                                      save_final_solution = true,
                                      solution_variables = cons2prim)
 
+# Positivity limiter as described in Remark 3 of the paper:
+# - Arpit Babbar, Praveen Chandrashekar (2025)
+#   Lax-Wendroff flux reconstruction on adaptive curvilinear meshes with
+#   error based time stepping for hyperbolic conservation laws
+#   [doi: 10.1016/j.jcp.2024.113622](https://doi.org/10.1016/j.jcp.2024.113622)
 positivity_limiter = PositivityPreservingLimiterZhangShu(thresholds = (5.0e-6, 5.0e-6),
                                                          variables = (Trixi.density,
                                                                       pressure))
@@ -95,7 +100,7 @@ amr_controller = ControllerThreeLevel(semi, amr_indicator,
                                       med_level = 0, med_threshold = 0.1, # med_level = current level
                                       max_level = 6, max_threshold = 0.3)
 
-# Set `limiter! = positivity_limiter` to apply the positivity-preserving limiter after 
+# Set `limiter! = positivity_limiter` to apply the positivity-preserving limiter after
 # coarsening and refinement steps.
 amr_callback = AMRCallback(semi, amr_controller,
                            interval = 2,
