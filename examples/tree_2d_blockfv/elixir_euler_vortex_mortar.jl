9b68c916fa06c15da3ddd8daf3f505a4a9e1cce1
Hendrik Ranocha <mail@ranocha.de>
2026-06-25 15:21:28 +0200

    fix initial_condition_isentropic_vortex

--- examples/tree_2d_blockfv/elixir_euler_vortex_mortar.jl
+++ examples/tree_2d_blockfv/elixir_euler_vortex_mortar.jl
@@ -15,37 +15,50 @@ The classical isentropic vortex test case of
   Schemes for Hyperbolic Conservation Laws
   [NASA/CR-97-206253](https://ntrs.nasa.gov/citations/19980007543)
 """
+function initial_condition_isentropic_vortex(x, t, equations::CompressibleEulerEquations2D)
+    # This needs appropriate mesh size to ensure that the perturbation is
+    # essentially zero to machine accuracy at the boundary so that we can
+    # use periodic boundary conditions.
+    # In addition, we assume that the domain is of size [-10, -10] x [10, 10]
+    # to handle periodic boundary conditions correctly below.
 
-const initial_condition_isentropic_vortex = (x, t, equations::CompressibleEulerEquations2D) -> begin
-    # needs appropriate mesh size, e.g. [-10,-10]x[10,10]
-    # for error convergence: make sure that the end time is such that the vortex is back at the initial state!!
-    # for the current velocity and domain size: t_end should be a multiple of 20s
     # initial center of the vortex
     RealT = eltype(x)
-    inicenter = SVector(0, 0)
+    center = SVector(0, 0)
+
     # size and strength of the vortex
-    iniamplitude = 5
+    amplitude = 5
+
     # base flow
     rho = 1
     v1 = 1
     v2 = 1
     vel = SVector(v1, v2)
     p = convert(RealT, 25)
-    rt = p / rho                  # ideal gas equation
-    t_loc = 0
-    cent = inicenter + vel * t_loc      # advection of center
-    # ATTENTION: handle periodic BC, but only for v1 = v2 = 1.0 (!!!!)
+
+    rt = p / rho             # R*T by ideal gas equation
+    cent = center + vel * t  # advection of center
     cent = x - cent # distance to center point
+
+    # The domain [-10, 10]^2 is periodic with edge length 20 in each direction.
+    # To obtain a valid initial condition for any time `t` (and any base
+    # velocity `vel`), wrap the distance vector to the nearest periodic image
+    # of the advected vortex center.
+    domain_length = convert(RealT, 20)
+    cent = cent - domain_length * round.(cent / domain_length)
+
     # cent = cross(iniaxis, cent) # distance to axis, tangent vector, length r
     # cross product with iniaxis = [0, 0, 1]
     cent = SVector(-cent[2], cent[1])
+
     r2 = cent[1]^2 + cent[2]^2
-    du = iniamplitude / (2 * convert(RealT, pi)) * exp(0.5f0 * (1 - r2)) # vel. perturbation
+    du = amplitude / (2 * convert(RealT, pi)) * exp(0.5f0 * (1 - r2)) # vel. perturbation
     dtemp = -(equations.gamma - 1) / (2 * equations.gamma * rt) * du^2 # isentropic
     rho = rho * (1 + dtemp)^(1 / (equations.gamma - 1))
     vel = vel + du * cent
     v1, v2 = vel
     p = p * (1 + dtemp)^(equations.gamma / (equations.gamma - 1))
+
     prim = SVector(rho, v1, v2, p)
     return prim2cons(prim, equations)
 end
