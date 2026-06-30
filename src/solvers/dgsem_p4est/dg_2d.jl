0054529eb6cf0d6c5916ff0c5acd95024687dc1c
Shahu Karale <shahukarale283@gmail.com>
2026-07-01 00:46:31 +0530

    multiple dispatch bridge except the rhs!

--- src/solvers/dgsem_p4est/dg_2d.jl
+++ src/solvers/dgsem_p4est/dg_2d.jl
@@ -799,6 +799,15 @@ function calc_boundary_flux!(cache, t, boundary_conditions,
     return nothing
 end
 
+function prolong2mortars!(cache, u,
+                          mesh::Union{P4estMesh{2}, P4estMeshView{2}, T8codeMesh{2}},
+                          equations,
+                          mortar_l2::LobattoLegendreMortarL2,
+                          dg::DGSEM)
+    backend = trixi_backend(u)
+    prolong2mortars!(backend, cache, u, mesh, equations, mortar_l2, dg)
+end
+
 function prolong2mortars!(backend::Nothing, cache, u,
                           mesh::Union{P4estMesh{2}, P4estMeshView{2}, T8codeMesh{2}},
                           equations,
@@ -865,6 +874,16 @@ function prolong2mortars!(backend::Nothing, cache, u,
     end
 
     return nothing
+end
+
+function calc_mortar_flux!(surface_flux_values,
+                           mesh::Union{P4estMesh{2}, P4estMeshView{2}, T8codeMesh{2}},
+                           have_nonconservative_terms, equations,
+                           mortar_l2::LobattoLegendreMortarL2,
+                           surface_integral, dg::DG, cache)
+    backend = trixi_backend(surface_flux_values)
+    calc_mortar_flux!(backend, surface_flux_values, mesh, have_nonconservative_terms, 
+                      equations, mortar_l2, surface_integral, dg, cache)
 end
 
 function calc_mortar_flux!(backend::Nothing, surface_flux_values,
