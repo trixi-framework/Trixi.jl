d851289df32f45429eaa101de7d7bee05fa10373
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2026-07-01 21:35:12 -0500

    Apply suggestion from @jlchan
    format

--- test/test_unit.jl
+++ test/test_unit.jl
@@ -995,7 +995,8 @@ end
     @test (s2 - s1) ≈ cp * log(T_test / T_ref) - R_specific * log(p2 / p1)
     
     # 5. check speed of sound
-    @test Trixi.speed_of_sound(V, T_ref, eos) ≈ sqrt(gamma_target * pressure(V, T_ref, eos) * V)
+    @test Trixi.speed_of_sound(V, T_ref, eos) ≈
+          sqrt(gamma_target * pressure(V, T_ref, eos) * V)
 end
 
 @timed_testset "Test consistency (fluxes, entropy/cons2entropy) for NonIdealCompressibleEulerEquations1D" begin
