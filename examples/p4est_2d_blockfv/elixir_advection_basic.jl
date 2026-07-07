14f2d82bcbc7d7883da8d61b04a26979f6f0cc65
Vincent Marks <vimarks@students.uni-mainz.de>
2026-07-07 09:30:38 +0200

    Update examples/p4est_2d_blockfv/elixir_advection_basic.jl
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- examples/p4est_2d_blockfv/elixir_advection_basic.jl
+++ examples/p4est_2d_blockfv/elixir_advection_basic.jl
@@ -14,14 +14,11 @@ equations = LinearScalarAdvectionEquation2D(advection_velocity)
 # in each cell of the mesh and the (local) Lax-Friedrichs/Rusanov flux as surface flux
 solver = BlockFV(n_nodes = 4, surface_flux = flux_lax_friedrichs)
 
-coordinates_min = (-1.0, -1.0)
-coordinates_max = (1.0, 1.0)
-
-trees_per_dimension = (8, 8)
-
 # Create P4estMesh with 8 x 8 trees and periodic boundaries
-mesh = P4estMesh(trees_per_dimension, polydeg = 3,
-                 coordinates_min = coordinates_min, coordinates_max = coordinates_max,
+trees_per_dimension = (8, 8)
+mesh = P4estMesh(trees_per_dimension; polydeg = 3,
+                 coordinates_min = (-1.0, -1.0),
+                 coordinates_max = (1.0, 1.0),
                  initial_refinement_level = 1,
                  periodicity = true)
 
