60a16aa65618975901db6b1ba3853f7ea30453bd
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2026-07-10 08:52:51 -0500

    Apply suggestion from @DanielDoehring
    Co-authored-by: Daniel Doehring <doehringd2@gmail.com>

--- src/equations/compressible_euler_1d.jl
+++ src/equations/compressible_euler_1d.jl
@@ -254,7 +254,7 @@ Should be used together with [`TreeMesh`](@ref).
     # [DOI: 10.1007/b79761](https://doi.org/10.1007/b79761)
     if v_normal <= 0
         sound_speed = sqrt(equations.gamma * p_local / rho_local) # local sound speed
-        base = (1 + 0.5f0 * (equations.gamma - 1) * v_normal / sound_speed)
+        p_scaling_base = (1 + 0.5f0 * (equations.gamma - 1) * v_normal / sound_speed)
         if base >= 0
             p_star = p_local *
                      base^(2 * equations.gamma * equations.inv_gamma_minus_one)
