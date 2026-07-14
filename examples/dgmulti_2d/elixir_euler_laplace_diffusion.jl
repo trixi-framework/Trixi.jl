97d0d2e4d8dfab4850c14a48191092a6c545d416
Hendrik Ranocha <mail@ranocha.de>
2026-07-14 09:16:34 +0200

    remove the need to pass uEltype = real(dg) to the AnalysisCallback for DGMulti solvers

--- examples/dgmulti_2d/elixir_euler_laplace_diffusion.jl
+++ examples/dgmulti_2d/elixir_euler_laplace_diffusion.jl
@@ -34,7 +34,7 @@ ode = semidiscretize(semi, tspan)
 summary_callback = SummaryCallback()
 alive_callback = AliveCallback(alive_interval = 10)
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi, interval = analysis_interval, uEltype = real(dg))
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 callbacks = CallbackSet(summary_callback,
                         analysis_callback,
                         alive_callback)
