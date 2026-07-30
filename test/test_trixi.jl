a8d0525e14d7add0d09909682572d0eadba580dd
Hendrik Ranocha <ranocha@users.noreply.github.com>
2026-07-30 11:40:15 +0200

    ignore new deprecation warning from OrdinaryDiffEq.jl

--- test/test_trixi.jl
+++ test/test_trixi.jl
@@ -21,6 +21,8 @@ macro test_trixi_include(expr, args...)
         # to just get the definition of the problem and spatial discretization. In this case,
         # OrdinaryDiffEq.jl throws the following warning, which we can safely ignore in our tests:
         r"┌ Warning: Verbosity toggle: dt_epsilon \n│  Initial timestep too small \(near machine epsilon\), using default: dt = 0.0\n└ @ OrdinaryDiffEqCore ~/.julia/packages/OrdinaryDiffEqCore.*\n"
+        # Ignore deprecation warnings from OrdinaryDiffEq
+        r"┌ Warning: Passing `stage_limiter!` to the algorithm constructor is deprecated; pass `stage_limiter` as a keyword argument to `solve`/`init` instead\.\n│   caller = .+\n└ @ Core .+\n"
     ]
     # if `maxiters` is set in tests, it is usually set to a small number to
     # run only a few steps - ignore possible warnings coming from that
