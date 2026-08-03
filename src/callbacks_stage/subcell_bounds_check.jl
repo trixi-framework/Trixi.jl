2b2c6316bddceacdbcf245dc324f26f3d97ab9cb
bennibolm <benjamin.bolm@gmx.de>
2026-08-03 15:41:42 +0200

    Skip computation of deviations if smoothmess indicator is enabled

--- src/callbacks_stage/subcell_bounds_check.jl
+++ src/callbacks_stage/subcell_bounds_check.jl
@@ -35,6 +35,14 @@ end
 
 function (callback::BoundsCheckCallback)(u_ode, integrator, stage)
     mesh, equations, solver, cache = mesh_equations_solver_cache(integrator.p)
+
+    if ndims(equations) == 2 && solver.volume_integral.limiter.indicator !== nothing
+        # When using a smoothness indicator, a convex combination of the limiting factors from
+        # local and positivity limiting are used. However, the deviations are computed solely with
+        # respect to the local bounds. Consequently, the resulting deviation statistics would not
+        # reflect the actual deviations accurately. Skip the computation.
+        return nothing
+    end
     (; t, iter, alg) = integrator
     u = wrap_array(u_ode, mesh, equations, solver, cache)
 
@@ -157,6 +165,13 @@ end
     if semi.solver.volume_integral isa VolumeIntegralAdaptive
         println("Note: The following deviations are only computed in elements where subcell limiting is active.")
         println("In other elements, the solution is not checked for bounds violations.")
+    end
+    if ndims(semi.equations) == 2 && limiter.indicator !== nothing
+        println("Due to the use of a smoothness indicator, a convex combination of the limiting factors of local and")
+        println("positivity limiting was employed. However, the deviations are computed solely with respect to the local")
+        println("bounds. Consequently, the resulting deviation statistics may not be meaningful and can exceed zero.")
+        println("─"^100 * "\n")
+        return nothing
     end
     if local_twosided
         for v in limiter.local_twosided_variables_cons
