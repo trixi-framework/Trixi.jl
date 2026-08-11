7303443b2d5d8dd417b48903a185f6c801ab40d5
bbolm <benjamin.bolm@gmx.de>
2026-08-11 15:45:24 +0200

    Increase tolerance slightly

--- test/test_tree_2d_euler.jl
+++ test/test_tree_2d_euler.jl
@@ -621,7 +621,7 @@ end
     limiter = semi.solver.volume_integral.volume_integral_stabilized.limiter
     deviations = collect(values(limiter.cache.idp_bounds_delta_global))
     @test all(isfinite, deviations)
-    @test maximum(deviations) <= 1.0e-13
+    @test maximum(deviations) <= 2.0e-13
 
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
