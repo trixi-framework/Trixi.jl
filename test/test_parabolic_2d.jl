72bd784b18e287ae6557c262dc5a619d5cfc2dd9
SimonCan <simon.candelaresi@gmail.com>
2026-08-12 09:47:13 +0100

    Change Trixi.rhs! to Trixi.rhs_hyperbolic!.

--- test/test_parabolic_2d.jl
+++ test/test_parabolic_2d.jl
@@ -825,7 +825,7 @@ end
                         ])
     # Ensure that we do not have excessive memory allocations
     # (e.g., from type instabilities)
-    @test_allocations(Trixi.rhs!, semi, sol, 1000)
+    @test_allocations(Trixi.rhs_hyperbolic!, semi, sol, 1000)
     @test_allocations(Trixi.rhs_parabolic!, semi, sol, 1000)
     # Check specialised methods on ViscoResistiveMhd2D for coverage
     equations_parabolic = semi.equations_parabolic
