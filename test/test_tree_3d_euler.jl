9bdfb3b9557de8200a3e4751395faa9323b9096b
bennibolm <benjamin.bolm@gmx.de>
2026-08-19 12:05:28 +0200

    Adapt 3d code; Add test

--- test/test_tree_3d_euler.jl
+++ test/test_tree_3d_euler.jl
@@ -572,6 +572,46 @@ end
     @test_allocations(Trixi.rhs_hyperbolic!, semi, sol, 15_000)
 end
 
+@testitem "TreeMesh3D Euler: elixir_euler_sedov_blast_wave_sc_subcell.jl (FVO2)" setup=[
+    Setup,
+    TreeMesh3DEuler
+] tags=[:tree_part4] begin
+    @test_trixi_include(joinpath(EXAMPLES_DIR,
+                                 "elixir_euler_sedov_blast_wave_sc_subcell.jl"),
+                        volume_integral=VolumeIntegralSubcellLimiting(limiter_idp;
+                                                                      volume_flux_dg = volume_flux,
+                                                                      volume_integral_low_order = VolumeIntegralPureLGLFiniteVolumeO2(basis;
+                                                                                                                                      reconstruction_mode = reconstruction_O2_inner,
+                                                                                                                                      volume_flux_fv = surface_flux)),
+                        l2=[ # TODO
+                            0.24806841083939926,
+                            0.07001337223874464,
+                            0.07001337223806398,
+                            0.0700133722383429,
+                            0.3620366037665587
+                        ],
+                        linf=[
+                            0.9384071822566761,
+                            0.573009568617271,
+                            0.5730095685845291,
+                            0.5730095686063774,
+                            4.861205850307592
+                        ],
+                        tspan=(0.0, 0.5))
+    limiter = semi.solver.volume_integral.limiter
+    deviations = collect(values(limiter.cache.idp_bounds_delta_global))
+    @test all(isfinite, deviations)
+    @test maximum(deviations) <= 3.0e-13
+
+    # Ensure that we do not have excessive memory allocations
+    # (e.g., from type instabilities)
+    # Larger values for allowed allocations due to usage of custom
+    # integrator which are not *recorded* for the methods from
+    # OrdinaryDiffEq.jl
+    # Corresponding issue: https://github.com/trixi-framework/Trixi.jl/issues/1877
+    @test_allocations(Trixi.rhs_hyperbolic!, semi, sol, 15_000)
+end
+
 @testitem "TreeMesh3D Euler: elixir_euler_sedov_blast_wave_sc_subcell.jl (Adaptive Vol Int.)" setup=[
     Setup,
     TreeMesh3DEuler
