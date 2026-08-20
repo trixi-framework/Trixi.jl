a610b70b5a6dc4b25327078a42b4ee8aae89963a
Benjamin Bolm <74359358+bennibolm@users.noreply.github.com>
2026-08-20 09:58:06 +0200

    Update src/solvers/dg.jl
    Co-authored-by: Daniel Doehring <doehringd2@gmail.com>

--- src/solvers/dg.jl
+++ src/solvers/dg.jl
@@ -709,7 +709,7 @@ end
 A subcell limiting volume integral type for DG methods based on subcell blending approaches
 with a low-order FV method. The low-order method can be selected with `volume_integral_low_order`;
 by default, the first-order subcell finite volume scheme [`VolumeIntegralPureLGLFiniteVolume`](@ref)
-is used, while [`VolumeIntegralPureLGLFiniteVolumeO2`](@ref) provides a second-order alternative.
+with [`FluxLaxFriedrichs`](@ref) is used, while [`VolumeIntegralPureLGLFiniteVolumeO2`](@ref) provides a second-order alternative.
 Used with limiter [`SubcellLimiterIDP`](@ref).
 
 !!! note
