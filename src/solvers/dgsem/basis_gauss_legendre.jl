d856e50341cf4cd870d034a8f5f2d0f663b5dbf8
Daniel Doehring <doehringd2@gmail.com>
2026-09-04 11:35:17 +0200

    Update src/solvers/dgsem/basis_gauss_legendre.jl

--- src/solvers/dgsem/basis_gauss_legendre.jl
+++ src/solvers/dgsem/basis_gauss_legendre.jl
@@ -116,7 +116,8 @@ In particular, not the nodes themselves are returned.
 
 # For Gauss-Legendre basis, ALL surface (interface, boundary, mortar) data needs to be interpolated.
 # This is queried e.g. in the containers to decide if for the interfaces, boundaries, mortars etc.
-# interpolation is required (true) or if the data can be taken from the outer/surface element nodes (false).
+# interpolation is required (`surface_interpolation_needed(...) = true`) or if the data can be taken from the
+# outer/surface element nodes (`surface_interpolation_needed(...) = false`).
 @inline surface_interpolation_needed(::GaussLegendreBasis) = true
 
 """
