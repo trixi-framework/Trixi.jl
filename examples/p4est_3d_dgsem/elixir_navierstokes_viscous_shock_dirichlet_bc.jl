f0c66551bb84e78450276d6b9da0b179e65d23cf
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2026-09-05 08:36:18 +0200

    comment on scalings

--- examples/p4est_3d_dgsem/elixir_navierstokes_viscous_shock_dirichlet_bc.jl
+++ examples/p4est_3d_dgsem/elixir_navierstokes_viscous_shock_dirichlet_bc.jl
@@ -28,6 +28,7 @@ using Trixi
 
 ### Fixed parameters ###
 # We want kappa = cp * mu = mu_bar to ensure constant enthalpy
+# See fotonote 1 on page 335 in Margolin et al.
 prandtl_number() = 3 / 4
 
 ### Free choices: ###
@@ -85,7 +86,8 @@ initial_condition = initial_condition_viscous_shock
 equations = CompressibleEulerEquations3D(gamma)
 
 # Trixi implements the stress tensor in deviatoric form, thus we need to
-# convert the "isotropic viscosity" to the "deviatoric viscosity"
+# convert the "isotropic/longitudinal viscosity" to the "deviatoric viscosity"
+# See again fotonote 1 on page 335 in Margolin et al.
 const mu_deviatoric = mu_bar * 3 / 4
 equations_parabolic = CompressibleNavierStokesDiffusion3D(equations, mu = mu_deviatoric,
                                                           Prandtl = prandtl_number(),
--- examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock_imex.jl
+++ examples/tree_1d_dgsem/elixir_navierstokes_viscous_shock_imex.jl
@@ -34,6 +34,7 @@ using ADTypes # To access the types choosing how to evaluate Jacobian-vector pro
 # Corresponds essentially to fixing the Mach number
 alpha = 0.5
 # We want kappa = cp * mu = mu_bar to ensure constant enthalpy
+# See fotonote 1 on page 335 in Margolin et al.
 prandtl_number() = 1
 
 ### Free choices: ###
