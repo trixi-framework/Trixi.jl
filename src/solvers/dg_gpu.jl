cbaa1e43215e71b7dd26f64d756dbdde351f1ca8
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2026-09-10 20:45:11 +0200

    Add 3D flux-differencing GPU turbo kernels (#3229)
    * add full sweep kernel and selection
    
    * fix tests
    
    * cover CPU KA backend
    
    * Apply suggestions from code review
    
    Co-authored-by: Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
    
    * Update NEWS.md
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * rename flux_differencing_kernel
    
    * rename get_node_vars_local
    
    * add local data share for half sweep kernel
    
    * Update NEWS.md
    
    Co-authored-by: Benedict <135045760+benegee@users.noreply.github.com>
    
    * use get node vars
    
    * fix args
    
    * remove Atomix
    
    * add docs
    
    * fix CI
    
    * add turbo kernels
    
    * add tests
    
    * fix float32 turbo
    
    * fix docstring
    
    * Update src/semidiscretization/semidiscretization.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * fix docs and add no lds kernel
    
    * Apply batched suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Apply suggestion from @MarcoArtiano
    
    * fix docs
    
    * re-add kernels + global turbo
    
    * add warning regarding LDS
    
    * remove evalpoly
    
    * improve warning message
    
    * some more improvements in the error message
    
    * improve warning and introduce error for workgroup size
    
    * remove SVector redundancy, improve warning error
    
    * Apply batched suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * fix check function for weak kernels
    
    * rename flux diff ka kernel with turbo
    
    * fix check function
    
    * remove turbo FullSweepGlobal
    
    * restore FluxTurbo FluxSweepGlobal fallback test case
    
    * fix fallback
    
    * Apply batched suggestions from code review
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    Co-authored-by: Benedict <135045760+benegee@users.noreply.github.com>

--- src/solvers/dg_gpu.jl
+++ src/solvers/dg_gpu.jl
@@ -1,3 +1,8 @@
+@inline function get_node_turbo(turbo_local, ::Val{NAUX},
+                                indices...) where {NAUX}
+    return ntuple(v -> (@inbounds turbo_local[v, indices...]), Val(NAUX))
+end
+
 function calc_volume_integral!(backend::Backend, du, u, mesh,
                                have_nonconservative_terms, equations,
                                volume_integral, dg::DGSEM, cache)
@@ -17,3 +22,40 @@ end
     volume_integral_kernel!(du, u, element, MeshT, have_nonconservative_terms,
                             equations, volume_integral, dg, cache)
 end
+
+# The half sweep and full sweep kernels use local share data, which is limited to
+# 48 KiB per workgroup on NVIDIA GPUs and 64 KiB on AMD GPUs (as of 2026).
+function check_flux_differencing_shared_memory(kernel::Union{HalfSweep, FullSweep}, semi)
+    dg = semi.solver
+    equations = semi.equations
+    volume_integral = semi.solver.volume_integral
+
+    nshared = nvariables(equations)
+
+    if volume_integral isa VolumeIntegralFluxDifferencing
+        volume_flux = volume_integral.volume_flux
+        if volume_flux isa FluxTurbo
+            nturbo = typeof(nturbovars(volume_flux.numerical_flux, equations)).parameters[1]
+            nshared = kernel isa HalfSweep ? nvariables(equations) + nturbo : nturbo
+        end
+    end
+
+    shared_memory = nshared * nnodes(dg)^3 * sizeof(real(dg))
+
+    if shared_memory > 48 * 1024
+        @warn "The shared memory required by the selected flux differencing kernel may exceed the limit of the GPU.
+        In case, consider using `flux_differencing_kernel = FullSweepGlobal()`." flux_differencing_kernel=kernel nvariables=nvariables(semi.equations) polydeg=polydeg(dg) shared_memory=Base.format_bytes(shared_memory)
+    end
+
+    workgroup_size = nnodes(dg)^3
+
+    if workgroup_size > 1024
+        @warn "The workgroup size required by the selected flux differencing kernel likely exceeds the device limit.
+        Please, consider reducing the polynomial degree or using `flux_differencing_kernel = FullSweepGlobal()`." flux_differencing_kernel=kernel nvariables=nvariables(semi.equations) polydeg=polydeg(dg) workgroup_size
+    end
+
+    return nothing
+end
+
+# This kernel does not use any shared memory
+check_flux_differencing_shared_memory(::FullSweepGlobal, semi) = nothing
--- test/test_cuda_3d.jl
+++ test/test_cuda_3d.jl
@@ -311,3 +311,92 @@ end
     @test u_half_sweep ≈ u_full_sweep
     @test u_half_sweep ≈ u_full_sweep_global
 end
+
+@testitem "CUDA 3D: elixir_euler_source_terms_nonperiodic.jl FluxTurbo / CUDA" setup=[
+    Setup,
+    CUDA3DExamples
+] tags=[:CUDA] begin
+    # Using CUDA inside the testitem since otherwise the bindings are hidden by the anonymous modules
+    using CUDA
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR, "elixir_euler_source_terms_nonperiodic.jl"),
+                  volume_integral = VolumeIntegralFluxDifferencing(flux_ranocha),
+                  tspan = (0.0, 0.5),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = HalfSweep())
+    @test Trixi.storage_type(ode.p.cache.elements) === CuArray
+    u_plain = Array(sol.u[end])
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR, "elixir_euler_source_terms_nonperiodic.jl"),
+                  volume_integral = VolumeIntegralFluxDifferencing(FluxTurbo(flux_ranocha)),
+                  tspan = (0.0, 0.5),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = HalfSweep())
+    @test ode.p.cache.flux_differencing_kernel === HalfSweep()
+    @test Array(sol.u[end]) ≈ u_plain
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR, "elixir_euler_source_terms_nonperiodic.jl"),
+                  volume_integral = VolumeIntegralFluxDifferencing(FluxTurbo(flux_ranocha)),
+                  tspan = (0.0, 0.5),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = FullSweep())
+    @test ode.p.cache.flux_differencing_kernel === FullSweep()
+    @test Array(sol.u[end]) ≈ u_plain
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR, "elixir_euler_source_terms_nonperiodic.jl"),
+                  volume_integral = VolumeIntegralFluxDifferencing(FluxTurbo(flux_ranocha)),
+                  tspan = (0.0, 0.5),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = FullSweepGlobal())
+    @test ode.p.cache.flux_differencing_kernel === FullSweepGlobal()
+    @test Array(sol.u[end]) ≈ u_plain
+end
+
+@testitem "CUDA 3D: elixir_mhd_alfven_wave_combined_fluxes_nonperiodic.jl FluxTurbo / CUDA" setup=[
+    Setup,
+    CUDA3DExamples
+] tags=[:CUDA] begin
+    # Using CUDA inside the testitem since otherwise the bindings are hidden by the anonymous modules
+    using CUDA
+
+    volume_flux_turbo = FluxTurbo(flux_hindenlang_gassner, flux_nonconservative_powell)
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR,
+                           "elixir_mhd_alfven_wave_combined_fluxes_nonperiodic.jl"),
+                  tspan = (0.0, 0.1),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = HalfSweep())
+    u_plain = Array(sol.u[end])
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR,
+                           "elixir_mhd_alfven_wave_combined_fluxes_nonperiodic.jl"),
+                  volume_integral = VolumeIntegralFluxDifferencing(volume_flux_turbo),
+                  tspan = (0.0, 0.1),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = HalfSweep())
+    @test ode.p.cache.flux_differencing_kernel === HalfSweep()
+    @test Array(sol.u[end]) ≈ u_plain
+
+    trixi_include(@__MODULE__,
+                  joinpath(EXAMPLES_DIR,
+                           "elixir_mhd_alfven_wave_combined_fluxes_nonperiodic.jl"),
+                  volume_integral = VolumeIntegralFluxDifferencing(volume_flux_turbo),
+                  tspan = (0.0, 0.1),
+                  real_type = Float32,
+                  storage_type = CuArray,
+                  flux_differencing_kernel = FullSweep())
+    @test ode.p.cache.flux_differencing_kernel === FullSweep()
+    @test Array(sol.u[end]) ≈ u_plain
+end
