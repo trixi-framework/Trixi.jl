d3cd0906b2f51e69db550e5cdd53e440729d1c3f
Marco Artiano <martiano@uni-mainz.de>
2026-09-10 10:35:15 +0200

    fix check function

--- src/solvers/dg_gpu.jl
+++ src/solvers/dg_gpu.jl
@@ -29,13 +29,14 @@ function check_flux_differencing_shared_memory(kernel::Union{HalfSweep, FullSwee
     dg = semi.solver
     equations = semi.equations
     volume_integral = semi.solver.volume_integral
+
+    nshared = nvariables(equations)
+
     if volume_integral isa VolumeIntegralFluxDifferencing
         volume_flux = volume_integral.volume_flux
         if volume_flux isa FluxTurbo
             nturbo = typeof(nturbovars(volume_flux.numerical_flux, equations)).parameters[1]
             nshared = kernel isa HalfSweep ? nvariables(equations) + nturbo : nturbo
-        else
-            nshared = nvariables(equations)
         end
     end
 
