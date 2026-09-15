5fcbd10787376601f0561f3b7cc7ebb9cd2964e6
Benedict Geihe <bgeihe@uni-koeln.de>
2026-09-15 15:16:56 +0200

    use trixi_timeit_ext

--- src/solvers/dgsem_p4est/dg_2d_parallel_gpu.jl
+++ src/solvers/dgsem_p4est/dg_2d_parallel_gpu.jl
@@ -7,32 +7,32 @@
 
 function rhs_hyperbolic!(backend::Backend,
                          du, u, t,
-                         mesh::Union{TreeMeshParallel{2}, P4estMeshParallel{2},
-                                     T8codeMeshParallel{2}}, equations,
+                         mesh::Union{P4estMeshParallel{2}, T8codeMeshParallel{2}},
+                         equations,
                          boundary_conditions, source_terms::Source,
                          dg::DG, cache) where {Source}
     # Start to receive MPI data
     @trixi_timeit timer() "start MPI receive" start_mpi_receive!(cache.mpi_cache)
 
     # Prolong solution to MPI interfaces
-    @trixi_timeit timer() "prolong2mpiinterfaces" begin
+    @trixi_timeit_ext backend timer() "prolong2mpiinterfaces" begin
         prolong2mpiinterfaces!(backend, cache, u, mesh, equations, dg.surface_integral,
                                dg)
     end
 
     # Prolong solution to MPI mortars
-    @trixi_timeit timer() "prolong2mpimortars" begin
-        prolong2mpimortars!(cache, u, mesh, equations,
+    @trixi_timeit_ext backend timer() "prolong2mpimortars" begin
+        prolong2mpimortars!(backend, cache, u, mesh, equations,
                             dg.mortar, dg)
     end
 
     # Start to send MPI data
-    @trixi_timeit timer() "start MPI send" begin
+    @trixi_timeit_ext backend timer() "start MPI send" begin
         start_mpi_send!(backend, cache.mpi_cache, mesh, equations, dg, cache)
     end
 
     # Calculate volume integral
-    @trixi_timeit timer() "volume integral" begin
+    @trixi_timeit_ext backend timer() "volume integral" begin
         calc_volume_integral!(backend, du, u, mesh,
                               have_nonconservative_terms(equations), equations,
                               dg.volume_integral, dg, cache)
@@ -49,44 +49,44 @@ function rhs_hyperbolic!(backend::Backend,
     end
 
     # Prolong solution to boundaries
-    @trixi_timeit timer() "prolong2boundaries" begin
+    @trixi_timeit_ext backend timer() "prolong2boundaries" begin
         prolong2boundaries!(backend, cache, u, mesh, equations, dg)
     end
 
     # Calculate boundary fluxes
-    @trixi_timeit timer() "boundary flux" begin
+    @trixi_timeit_ext backend timer() "boundary flux" begin
         calc_boundary_flux!(backend, cache, t, boundary_conditions, mesh, equations,
                             dg.surface_integral, dg)
     end
 
     # Prolong solution to mortars
-    @trixi_timeit timer() "prolong2mortars" begin
-        prolong2mortars!(cache, u, mesh, equations,
+    @trixi_timeit_ext backend timer() "prolong2mortars" begin
+        prolong2mortars!(backend, cache, u, mesh, equations,
                          dg.mortar, dg)
     end
 
     # Calculate mortar fluxes
-    @trixi_timeit timer() "mortar flux" begin
-        calc_mortar_flux!(cache.elements.surface_flux_values, mesh,
+    @trixi_timeit_ext backend timer() "mortar flux" begin
+        calc_mortar_flux!(backend, cache.elements.surface_flux_values, mesh,
                           have_nonconservative_terms(equations), equations,
                           dg.mortar, dg.surface_integral, dg, cache)
     end
 
     # Finish to receive MPI data
-    @trixi_timeit timer() "finish MPI receive" begin
+    @trixi_timeit_ext backend timer() "finish MPI receive" begin
         finish_mpi_receive!(backend, cache.mpi_cache, mesh, equations, dg, cache)
     end
 
     # Calculate MPI interface fluxes
-    @trixi_timeit timer() "MPI interface flux" begin
+    @trixi_timeit_ext backend timer() "MPI interface flux" begin
         calc_mpi_interface_flux!(backend, cache.elements.surface_flux_values, mesh,
                                  have_nonconservative_terms(equations), equations,
                                  dg.surface_integral, dg, cache)
     end
 
     # Calculate MPI mortar fluxes
-    @trixi_timeit timer() "MPI mortar flux" begin
-        calc_mpi_mortar_flux!(cache.elements.surface_flux_values, mesh,
+    @trixi_timeit_ext backend timer() "MPI mortar flux" begin
+        calc_mpi_mortar_flux!(backend, cache.elements.surface_flux_values, mesh,
                               have_nonconservative_terms(equations), equations,
                               dg.mortar, dg.surface_integral, dg, cache)
     end
