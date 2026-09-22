cdd8b2a6fe8bc5bb1e4a1f608d0917ae1808c1b0
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2026-09-22 13:22:52 +0200

    Add `thread = Trixi.Threaded()` for `TreeMesh2D` `DGSEM` elixirs (#3255)
    * first commit
    
    * remove thread for failing test
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- examples/tree_2d_dgsem/elixir_euler_astro_jet_amr.jl
+++ examples/tree_2d_dgsem/elixir_euler_astro_jet_amr.jl
@@ -118,5 +118,5 @@ callbacks = CallbackSet(summary_callback,
 ###############################################################################
 # run the simulation
 # use adaptive time stepping based on error estimates, time step roughly dt = 1e-7
-sol = solve(ode, SSPRK43(; stage_limiter! = positivity_limiter);
+sol = solve(ode, SSPRK43(; stage_limiter! = positivity_limiter, thread = Trixi.Threaded());
             ode_default_options()..., callback = callbacks);
