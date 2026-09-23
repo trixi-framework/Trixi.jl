ad5e380d5d0fbf0661e4ee1670bd28d6fc759f10
Benedict Geihe <bgeihe@uni-koeln.de>
2026-09-23 09:35:36 +0200

    back to nexpr

--- src/solvers/dgsem_p4est/dg_2d_gpu.jl
+++ src/solvers/dgsem_p4est/dg_2d_gpu.jl
@@ -652,15 +652,14 @@ end
                                                                     RealType}
     mortar = @index(Global)
 
-    fstar_primary = [MArray{Tuple{NVARS, _nnodes}, RealType}(undef),
-        MArray{Tuple{NVARS, _nnodes}, RealType}(undef)]
-    fstar_secondary = [MArray{Tuple{NVARS, _nnodes}, RealType}(undef),
-        MArray{Tuple{NVARS, _nnodes}, RealType}(undef)]
+    fstar_primary = (MArray{Tuple{NVARS, _nnodes}, RealType}(undef),
+                     MArray{Tuple{NVARS, _nnodes}, RealType}(undef))
+    fstar_secondary = (MArray{Tuple{NVARS, _nnodes}, RealType}(undef),
+                       MArray{Tuple{NVARS, _nnodes}, RealType}(undef))
     u_buffer = MArray{Tuple{NVARS, _nnodes}, RealType}(undef)
 
-    # Warning: creating tuples like (fstar_p1, fstar_p2), as done in the CPU case, results
-    # in non-constant tuple indexing on the GPU later on, which can cause illegal memory
-    # access!
+    # Warning: creating such tuples, as done in the CPU case, may result in non-constant
+    # tuple indexing on the GPU later on, which can cause illegal memory access!
 
     calc_mortar_flux_per_mortar!(backend, mortar, surface_flux_values, MeshT,
                                  have_nonconservative_terms, equations, surface_flux,
